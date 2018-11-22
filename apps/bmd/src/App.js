025c6efa2de96443fe7cac47ab43ab532ebaa1a7
Ben Adida <ben@adida.net>
2018-11-22 01:09:59 +0000

    initial app

---
+++ apps/bmd/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+            <img src="./vw-checkmark.png" className="App-logo" alt="logo" />
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
