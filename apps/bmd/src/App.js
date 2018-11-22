4893477bdaa375d318b6d18a208eac11c3f51458
Ben Adida <ben@adida.net>
2018-11-22 02:08:20 +0000

    first printing

--- apps/bmd/src/App.js
+++ apps/bmd/src/App.js
@@ -1,14 +1,25 @@
 import React, { Component } from 'react';
+import PrintProvider, { Print, NoPrint } from 'react-easy-print';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
+        <div className="App">
+          <header className="App-header">
             <img src="./vw-checkmark.png" className="App-logo" alt="logo" />
-        </header>
-      </div>
+          </header>
+          <PrintProvider>
+	    <Print printonly name="ballot">
+              <table width="100%" style={{fontSize: "1.5em"}}>
+                <tr><th colspan="2" style={{fontSize: "2em"}}>Official Ballot</th></tr>
+                <tr><th>&nbsp;</th></tr>
+                <tr><th align="left" width="30%">Senator</th><td>Dianne Feinstein</td></tr>
+                <tr><th align="left" width="30%">Congressperson</th><td>Anna Eshoo</td></tr>
+              </table>
+	    </Print>
+          </PrintProvider>
+	</div>
     );
   }
 }
