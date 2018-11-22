b3994281e499ec0cee155a9c0b59ba604e095924
Ben Adida <ben@adida.net>
2018-11-22 02:23:29 +0000

    playing with printing

--- apps/bmd/src/App.js
+++ apps/bmd/src/App.js
@@ -1,25 +1,32 @@
-import React, { Component } from 'react';
+import React, { Component, button } from 'react';
 import PrintProvider, { Print, NoPrint } from 'react-easy-print';
 import './App.css';
 
 class App extends Component {
+  print() {
+    window.print();
+  }
+  
   render() {
     return (
-        <div className="App">
-          <header className="App-header">
-            <img src="./vw-checkmark.png" className="App-logo" alt="logo" />
-          </header>
-          <PrintProvider>
-	    <Print printonly name="ballot">
-              <table width="100%" style={{fontSize: "1.5em"}}>
-                <tr><th colspan="2" style={{fontSize: "2em"}}>Official Ballot</th></tr>
-                <tr><th>&nbsp;</th></tr>
-                <tr><th align="left" width="30%">Senator</th><td>Dianne Feinstein</td></tr>
-                <tr><th align="left" width="30%">Congressperson</th><td>Anna Eshoo</td></tr>
-              </table>
-	    </Print>
-          </PrintProvider>
-	</div>
+      <PrintProvider>
+        <NoPrint>
+          <div className="App">
+            <header className="App-header">
+              <img src="./vw-checkmark.png" className="App-logo" alt="logo" />
+              <button onClick={this.print}>print ballot</button>
+            </header>
+          </div>
+        </NoPrint>      
+	<Print name="ballot">
+          <table width="100%" style={{fontSize: "1.5em"}}>
+            <tr><th colspan="2" style={{fontSize: "2em"}}>Official Ballot</th></tr>
+            <tr><th>&nbsp;</th></tr>
+            <tr><th align="left" width="30%">Senator</th><td>Dianne Feinstein</td></tr>
+            <tr><th align="left" width="30%">Congressperson</th><td>Anna Eshoo</td></tr>
+          </table>
+	</Print>
+      </PrintProvider>
     );
   }
 }
