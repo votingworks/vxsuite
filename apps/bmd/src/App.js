10d11eb046c6057c83946aa8c2a3ea77c6a1ec48
Ben Adida <ben@adida.net>
2018-12-01 23:21:29 +0000

    started ballot

--- apps/bmd/src/App.js
+++ apps/bmd/src/App.js
@@ -2,6 +2,15 @@ import React, { Component, button } from 'react';
 import PrintProvider, { Print, NoPrint } from 'react-easy-print';
 import './App.css';
 
+import { withStyles } from '@material-ui/core/styles';
+import Radio from '@material-ui/core/Radio';
+import RadioGroup from '@material-ui/core/RadioGroup';
+import FormHelperText from '@material-ui/core/FormHelperText';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import FormControl from '@material-ui/core/FormControl';
+import FormLabel from '@material-ui/core/FormLabel';
+
+
 var ELECTION = {
   id: "DEMO",
   title: "Demo Election",
@@ -29,7 +38,82 @@ var ELECTION = {
   ]
 };
 
+const styles = theme => ({
+  root: {
+    display: 'flex',
+  },
+  formControl: {
+    margin: theme.spacing.unit * 3,
+  },
+  group: {
+    margin: `${theme.spacing.unit}px 0`,
+  },
+});
+
+
+class BallotContest extends React.Component {
+  handleChange = event => {
+    this.setState({ value: event.target.value });
+  };
+
+  constructor(props) {
+    super(props);
+    this.contest = props.contest;
+    this.state = {value: null};
+  }
+  
+  render() {
+    let contest = this.contest;
+
+    let rows = []
+
+    // Outer loop to create parent
+    for (let option of contest.options) {
+      rows.push(
+        <FormControlLabel
+          value={option}
+          label={option}
+          control={<Radio />}
+          labelPlacement="end"
+        />
+      );
+    }
+
+    return (
+      <FormControl component="fieldset">
+        <FormLabel component="legend">{contest.name}</FormLabel>
+        <RadioGroup
+          aria-label={contest.id}
+          name={contest.id}
+          value={this.state.value}
+          onChange={this.handleChange}
+        >
+        {rows}
+        </RadioGroup>
+      </FormControl>
+    );      
+    
+  }
+}
+
 class Ballot extends Component {
+  constructor(props) {
+    super(props);
+    this.election = props.election
+  }
+
+  render() {
+    let election = this.election;
+    return (
+      <div>
+        <h1>Ballot</h1>
+        <BallotContest contest={election.contests[0]} />
+      </div>
+    );
+  }
+}
+
+class PrintableBallot extends Component {
   constructor(props) {
     super(props);
     this.state = {election: props.election, ballot: props.ballot}
@@ -64,15 +148,14 @@ class App extends Component {
     return (
       <PrintProvider>
 	<Print single printOnly name="ballot">
-          <Ballot election={ELECTION} ballot={{president: "Mickey Mouse", senator: "John Smith"}} />
+          <PrintableBallot election={ELECTION} ballot={{president: "Mickey Mouse", senator: "John Smith"}} />
 	</Print>
         
         <NoPrint force>
           <div className="App">
-            <header className="App-header">
-              <img src="./vw-checkmark.png" className="App-logo" alt="logo" />
-              <button onClick={this.print}>print ballot</button>
-            </header>
+            <img src="./vw-checkmark.png" className="App-logo" alt="logo" />
+            <Ballot election={ELECTION} />
+            <button onClick={this.print}>print ballot</button>
           </div>
         </NoPrint>
       </PrintProvider>
