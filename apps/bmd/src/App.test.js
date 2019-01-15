4da10516e70ef2b6db29105a4f594736499da408
Ben Adida <ben@adida.net>
2019-01-15 02:28:29 +0000

    made tests pass

--- apps/bmd/src/App.test.js
+++ apps/bmd/src/App.test.js
@@ -2,6 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+window.matchMedia = jest.fn().mockImplementation(query => {
+  return {
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: jest.fn(),
+    removeListener: jest.fn(),
+  };
+});
+
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
