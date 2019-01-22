bd50fb28ac572faaa21f0bab521e61a132c1660c
Beau Smith <beau@beausmith.com>
2019-01-22 14:38:01 -0800

    Add basic Typographic prose for readability.

--- apps/bmd/src/pages/StartPage.tsx
+++ apps/bmd/src/pages/StartPage.tsx
@@ -8,8 +8,19 @@ const StartPage = (props: RouteComponentProps) => {
   return (
     <React.Fragment>
       <Article>
-        <h1>Demo Election</h1>
-        <LinkButton to={`/contests`}>Get Started</LinkButton>
+        <div className="prose">
+          <h1>Demo Election</h1>
+          <p>
+            For each contest, measure, or proposition, select your vote and then
+            press the “Next” button.
+          </p>
+          <h2>Heading 2</h2>
+          <p>More text about the election.</p>
+          <p>More text about the election.</p>
+          <p>
+            <LinkButton to={`/contests`}>Get Started</LinkButton>
+          </p>
+        </div>
       </Article>
     </React.Fragment>
   )
