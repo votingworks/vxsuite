4f4201159afc1b51fee90e4378ed6beadb23f851
Beau Smith <beau@beausmith.com>
2019-01-23 15:19:18 -0800

    Reduce unnecessary nesting.

--- apps/bmd/src/pages/StartPage.tsx
+++ apps/bmd/src/pages/StartPage.tsx
@@ -6,25 +6,23 @@ import LinkButton from '../components/LinkButton'
 
 const StartPage = (props: RouteComponentProps) => {
   return (
-    <React.Fragment>
-      <Article>
-        <div className="prose">
-          <h1>Demo Election</h1>
-          <p>
-            For each contest, measure, or proposition, select your vote and then
-            press the “Next” button.
-          </p>
-          <h2>Heading 2</h2>
-          <p>More text about the election.</p>
-          <p>More text about the election.</p>
-          <p>
-            <LinkButton autoFocus to={`/contests`}>
-              Get Started
-            </LinkButton>
-          </p>
-        </div>
-      </Article>
-    </React.Fragment>
+    <Article>
+      <div className="prose">
+        <h1>Demo Election</h1>
+        <p>
+          For each contest, measure, or proposition, select your vote and then
+          press the “Next” button.
+        </p>
+        <h2>Heading 2</h2>
+        <p>More text about the election.</p>
+        <p>More text about the election.</p>
+        <p>
+          <LinkButton autoFocus to={`/contests`}>
+            Get Started
+          </LinkButton>
+        </p>
+      </div>
+    </Article>
   )
 }
 
