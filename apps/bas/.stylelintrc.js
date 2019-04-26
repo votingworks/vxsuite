ee5b8b6ca4db8ef726466e19be254f52cb2f747f
Beau Smith <beau@beausmith.com>
2019-04-25 17:38:17 -0700

    feat: Simple app prototype.

--- apps/bas/.stylelintrc.js
+++ apps/bas/.stylelintrc.js
@@ -1,3 +1,10 @@
 module.exports = {
-  extends: 'stylelint-config-standard',
+  extends: [
+    "stylelint-config-palantir",
+    "stylelint-config-styled-components",
+    "stylelint-config-prettier"
+  ],
+  rules: {
+    "selector-max-id": 1,
+  }
 }
