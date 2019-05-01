2d01ce7128d3b17e5f63f144aab243cc00e313e1
Ben Adida <ben@adida.net>
2019-05-01 22:56:57 +0000

    add card functions and polling in client to read the card when it's there

--- apps/bmd/SERVER.md
+++ apps/bmd/SERVER.md
@@ -9,10 +9,32 @@ The instructions here are for Ubuntu Linux 18.04
 
 ```
 sudo add-apt-repository ppa:deadsnakes/ppa
-sudo apt install python3.7
+sudo make install-python
+sudo make install-smartcard
+make install-dependencies
 pip3 install pipenv
 cd server/
 pipenv install
+```
+
+## Run Tests
+
+Install dependencies you need
+
+```
+make install-dev-dependencies
+```
+
+and then run the tests
+
+```
+make test
+```
+
+With code coverage
+
+```
+make coverage
 ```
 
 ## Start the Development Server
--- apps/bmd/server/bmdserver/core.py
+++ apps/bmd/server/bmdserver/core.py
@@ -1,9 +1,30 @@
 
-from flask import Flask, send_from_directory
+import json
+
+from flask import Flask, send_from_directory, request
+
+from smartcard.System import readers
+from smartcard.util import toHexString, toASCIIBytes, toASCIIString
+
+from .card import CardInterface
 
 STATIC_DIR = "../../build"
 
 app = Flask(__name__, static_folder=STATIC_DIR)
+
+@app.route('/card/read')
+def card_read():
+    card_data = CardInterface.read()
+    if card_data:
+        return json.dumps({"card": card_data})
+    else:
+        return json.dumps({})
+
+@app.route('/card/write', methods=["POST"])
+def card_write():
+    content = request.get_json()
+    rv = CardInterface.write(content['code'])
+    return json.dumps({"success": rv})
 
 @app.route('/')
 def root():
