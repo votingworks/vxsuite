7d03ac8e191bf2a3b3b5a012c465a41050c8a3ba
Ben Adida <ben@adida.net>
2019-05-26 21:08:24 -0700

    moving back to simple pure JS, too gnarly to mix the TS and JS

--- apps/bmd/prodserver/index.js
+++ apps/bmd/prodserver/index.js
@@ -1,5 +1,12 @@
-import express from 'express'
-import * as proxy from '../src/setupProxy'
+// This file runs a simple production web server of the React production build
+// it reuses setupProxy.js for consistent use of backend modules in dev and prod.
+// This is plain JS rather than our TS setup in the React app because of that.
+//
+/* eslint-disable */
+/* istanbul ignore file */
+
+const express = require('express')
+const proxy = require('../src/setupProxy')
 
 const app = express()
 const port = 3000
@@ -7,5 +14,4 @@ const port = 3000
 proxy(app)
 app.use('/', express.static('../build'))
 
-// eslint-disable-next-line no-console
 app.listen(port, () => console.log(`BMD listening on port ${port}!`))
