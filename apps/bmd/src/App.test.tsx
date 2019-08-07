84074330debc17c454de2593298a7f1d0e568b08
Beau Smith <beau@beausmith.com>
2019-08-07 11:06:17 -0700

    Extract Settings tests to file.

--- apps/bmd/src/App.test.tsx
+++ apps/bmd/src/App.test.tsx
@@ -1,16 +1,14 @@
 import React from 'react'
-import { fireEvent, render } from '@testing-library/react'
+import { render } from '@testing-library/react'
 
 import electionSample from './data/electionSample.json'
 
-import App, { electionStorageKey, mergeWithDefaults } from './App'
-import { Election } from './config/types'
+import App, { electionStorageKey } from './App'
 
-const election = electionSample as Election
-
-const electionSampleAsString = JSON.stringify(
-  mergeWithDefaults(electionSample as Election)
-)
+import {
+  election,
+  setElectionInLocalStorage,
+} from './__tests__/helpers/election'
 
 beforeEach(() => {
   window.localStorage.clear()
@@ -22,6 +20,7 @@ describe('loads election', () => {
     const { getByText } = render(<App />)
     getByText('Device Not Configured')
   })
+
   it(`via url hash and does't store in localStorage`, () => {
     window.location.href = '/#sample'
     const { getByText } = render(<App />)
@@ -30,13 +29,13 @@ describe('loads election', () => {
   })
 
   it(`from localStorage`, () => {
-    window.localStorage.setItem(electionStorageKey, electionSampleAsString)
+    setElectionInLocalStorage()
     const { getByText } = render(<App />)
     getByText(election.title)
     expect(window.localStorage.getItem(electionStorageKey)).toBeTruthy()
   })
 
-  // Not sure this test works any longer
+  // TODO: Not sure this test works any longer. Need to relearn how this works.
   xit(`Error in App triggers reset and reloads window location`, () => {
     const mockConsoleError = jest.spyOn(console, 'error')
     mockConsoleError.mockImplementation(() => {
@@ -48,55 +47,5 @@ describe('loads election', () => {
     )
     const { getByText } = render(<App />)
     getByText('Device Not Configured')
-  })
-})
-
-describe(`Can update settings`, () => {
-  it(`Can update font-settings`, () => {
-    window.localStorage.setItem(electionStorageKey, electionSampleAsString)
-    const { getByText, getByTestId, getByLabelText } = render(<App />)
-    fireEvent.change(getByTestId('activation-code'), {
-      target: { value: 'VX.23.12' },
-    })
-    // TODO: replace next line with "Enter" keyDown on activation code input
-    fireEvent.click(getByText('Submit'))
-    fireEvent.click(getByText('Get Started'))
-    fireEvent.click(getByText('Start Voting'))
-    fireEvent.click(getByText('Settings'))
-    expect(
-      (getByLabelText('Font Size') as HTMLInputElement).value === '1'
-    ).toBeTruthy()
-    fireEvent.change(getByLabelText('Font Size'), {
-      target: { value: '0' },
-    })
-    expect(
-      (getByLabelText('Font Size') as HTMLInputElement).value === '0'
-    ).toBeTruthy()
-    fireEvent.change(getByLabelText('Font Size'), {
-      target: { value: '1' },
-    })
-    expect(
-      (getByLabelText('Font Size') as HTMLInputElement).value === '1'
-    ).toBeTruthy()
-    fireEvent.change(getByLabelText('Font Size'), {
-      target: { value: '2' },
-    })
-    expect(
-      (getByLabelText('Font Size') as HTMLInputElement).value === '2'
-    ).toBeTruthy()
-    fireEvent.change(getByLabelText('Font Size'), {
-      target: { value: '3' },
-    })
-    expect(
-      (getByLabelText('Font Size') as HTMLInputElement).value === '3'
-    ).toBeTruthy()
-    fireEvent.click(getByTestId('decrease-font-size-button'))
-    expect(
-      (getByLabelText('Font Size') as HTMLInputElement).value === '2'
-    ).toBeTruthy()
-    fireEvent.click(getByTestId('increase-font-size-button'))
-    expect(
-      (getByLabelText('Font Size') as HTMLInputElement).value === '3'
-    ).toBeTruthy()
   })
 })
---
+++ apps/bmd/src/Settings.test.tsx
@@ -0,0 +1,87 @@
+import React from 'react'
+import { fireEvent, render, wait } from '@testing-library/react'
+import fetchMock from 'fetch-mock'
+
+import App from './App'
+
+import {
+  setElectionInLocalStorage,
+  setStateInLocalStorage,
+} from './__tests__/helpers/election'
+
+import {
+  noCard,
+  voterCard,
+  advanceTimers,
+} from './__tests__/helpers/smartcards'
+
+let currentCard = noCard
+fetchMock.get('/card/read', () => JSON.stringify(currentCard))
+
+beforeEach(() => {
+  window.localStorage.clear()
+  window.location.href = '/'
+})
+
+describe(`Can update settings`, () => {
+  it(`Can update font-settings`, async () => {
+    jest.useFakeTimers()
+    setElectionInLocalStorage()
+    setStateInLocalStorage()
+
+    const { getByText, getByTestId, getByLabelText } = render(<App />)
+
+    currentCard = voterCard
+    advanceTimers()
+
+    await wait(() => getByText(/Precinct: Center Springfield/))
+
+    fireEvent.click(getByText('Get Started'))
+    advanceTimers()
+
+    fireEvent.click(getByText('Start Voting'))
+
+    fireEvent.click(getByText('Settings'))
+    advanceTimers()
+
+    await wait(() =>
+      getByText('Adjust the following settings to meet your needs.')
+    )
+
+    expect(
+      (getByLabelText('Font Size') as HTMLInputElement).value === '1'
+    ).toBeTruthy()
+    fireEvent.change(getByLabelText('Font Size'), {
+      target: { value: '0' },
+    })
+    expect(
+      (getByLabelText('Font Size') as HTMLInputElement).value === '0'
+    ).toBeTruthy()
+    fireEvent.change(getByLabelText('Font Size'), {
+      target: { value: '1' },
+    })
+    expect(
+      (getByLabelText('Font Size') as HTMLInputElement).value === '1'
+    ).toBeTruthy()
+    fireEvent.change(getByLabelText('Font Size'), {
+      target: { value: '2' },
+    })
+    expect(
+      (getByLabelText('Font Size') as HTMLInputElement).value === '2'
+    ).toBeTruthy()
+    fireEvent.change(getByLabelText('Font Size'), {
+      target: { value: '3' },
+    })
+    expect(
+      (getByLabelText('Font Size') as HTMLInputElement).value === '3'
+    ).toBeTruthy()
+    fireEvent.click(getByTestId('decrease-font-size-button'))
+    expect(
+      (getByLabelText('Font Size') as HTMLInputElement).value === '2'
+    ).toBeTruthy()
+    fireEvent.click(getByTestId('increase-font-size-button'))
+    expect(
+      (getByLabelText('Font Size') as HTMLInputElement).value === '3'
+    ).toBeTruthy()
+  })
+})
