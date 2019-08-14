07bcbf2077681db1d52d389b54e2f85efbc254e5
Beau Smith <beau@beausmith.com>
2019-08-13 17:04:28 -0700

    Add missing example of ButtonBar props.

--- apps/bmd/src/components/ButtonBar.test.tsx
+++ apps/bmd/src/components/ButtonBar.test.tsx
@@ -7,3 +7,14 @@ it(`renders ButtonBar`, () => {
   const { container } = render(<ButtonBar>foo</ButtonBar>)
   expect(container.firstChild).toMatchSnapshot()
 })
+
+it(`renders ButtonBar with natural order and primary button separated`, () => {
+  const { container } = render(
+    <ButtonBar separatePrimaryButton naturalOrder>
+      <div>one</div>
+      <div>two</div>
+      <div>three</div>
+    </ButtonBar>
+  )
+  expect(container.firstChild).toMatchSnapshot()
+})
