2f2472f46db83da59a6d72054cfa052ca76f752c
Beau Smith <beau@beausmith.com>
2019-09-09 15:32:12 -0700

    Use JSDoc to get comment in editor intellisense.

--- apps/bmd/src/utils/utcTimestamp.ts
+++ apps/bmd/src/utils/utcTimestamp.ts
@@ -1,3 +1,6 @@
-// returns current UTC unix timestamp (epoch) in seconds
+/**
+ * Returns current UTC unix timestamp (epoch) in seconds
+ */
+
 const utcTimestamp = (): number => Math.round(Date.now() / 1000)
 export default utcTimestamp
