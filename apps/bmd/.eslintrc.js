a2e080de54c71473aea4a96dbeef2c3ce444b576
Brian Donovan <1938+eventualbuddha@users.noreply.github.com>
2019-09-09 16:40:37 -0700

    test(eslint): remove reference to undefined rule
    As discussed at https://github.com/typescript-eslint/typescript-eslint/issues/676, there's no such rule as `@typescript-eslint/no-null-keyword` and no plans to add one.

--- apps/bmd/.eslintrc.js
+++ apps/bmd/.eslintrc.js
@@ -86,6 +86,5 @@ module.exports = {
     strict: 0,
     '@typescript-eslint/explicit-function-return-type': 'off', // Want to use it, but it requires return types for all built-in React lifecycle methods.
     '@typescript-eslint/no-non-null-assertion': 'off',
-    '@typescript-eslint/no-null-keyword': 'on',
   },
 }
