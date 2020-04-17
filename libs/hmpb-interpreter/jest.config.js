a8ea85a989a5b299e90a042fc0e2addca38b6dcc
Brian Donovan <1938+eventualbuddha@users.noreply.github.com>
2020-04-17 15:30:53 -0700

    test: don't run tests in the build directory

--- libs/hmpb-interpreter/jest.config.js
+++ libs/hmpb-interpreter/jest.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  testPathIgnorePatterns: ['<rootDir>/dist/', '<rootDir>/node_modules/'],
   globals: {
     'ts-jest': {
       packageJson: 'package.json',
