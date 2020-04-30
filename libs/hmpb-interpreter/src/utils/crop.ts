3b619a3234feeb4fd23b6e531e732bd4c930a858
Brian Donovan <1938+eventualbuddha@users.noreply.github.com>
2020-04-30 13:15:28 -0700

    refactor: simplify `crop`
    This uses a technique pointed out here: https://github.com/cozmo/jsQR/issues/168#issuecomment-621520019. It should be more efficient, and has the added bonus of working with both RGBA and grayscale.

--- libs/hmpb-interpreter/src/utils/crop.ts
+++ libs/hmpb-interpreter/src/utils/crop.ts
@@ -1,20 +1,15 @@
 import { createImageData } from 'canvas'
 import { Rect } from '../types'
-import { makeImageTransform } from './makeImageTransform'
 
 /**
  * Returns a new image cropped to the specified bounds.
  */
-export default makeImageTransform(gray, rgba)
-
-/**
- * Returns a new grayscale image cropped to the specified bounds.
- */
-export function gray(
-  { data: src, width: srcWidth }: ImageData,
+export default function crop(
+  { data: src, width: srcWidth, height: srcHeight }: ImageData,
   bounds: Rect
 ): ImageData {
-  const dst = new Uint8ClampedArray(bounds.width * bounds.height)
+  const channels = src.length / (srcWidth * srcHeight)
+  const dst = new Uint8ClampedArray(bounds.width * bounds.height * channels)
   const {
     x: srcXOffset,
     y: srcYOffset,
@@ -23,48 +18,12 @@ export function gray(
   } = bounds
 
   for (let y = 0; y < dstHeight; y += 1) {
-    const srcY = srcYOffset + y
-
-    for (let x = 0; x < dstWidth; x += 1) {
-      const srcX = srcXOffset + x
-      const srcOffset = srcX + srcY * srcWidth
-      const dstOffset = x + y * dstWidth
-
-      dst[dstOffset] = src[srcOffset]
-    }
-  }
-
-  return { data: dst, width: dstWidth, height: dstHeight }
-}
-
-/**
- * Returns a new RGBA image cropped to the specified bounds.
- */
-export function rgba(
-  { data: src, width: srcWidth }: ImageData,
-  bounds: Rect
-): ImageData {
-  const dst = new Uint8ClampedArray(bounds.width * bounds.height * 4)
-  const {
-    x: srcXOffset,
-    y: srcYOffset,
-    width: dstWidth,
-    height: dstHeight,
-  } = bounds
-
-  for (let y = 0; y < dstHeight; y += 1) {
-    const srcY = srcYOffset + y
-
-    for (let x = 0; x < dstWidth; x += 1) {
-      const srcX = srcXOffset + x
-      const srcOffset = (srcX + srcY * srcWidth) << 2
-      const dstOffset = (x + y * dstWidth) << 2
-
-      dst[dstOffset] = src[srcOffset]
-      dst[dstOffset + 1] = src[srcOffset + 1]
-      dst[dstOffset + 2] = src[srcOffset + 2]
-      dst[dstOffset + 3] = src[srcOffset + 3]
-    }
+    const srcOffset = (srcYOffset + y) * srcWidth + srcXOffset
+    const dstOffset = y * dstWidth
+    dst.set(
+      src.subarray(srcOffset * channels, (srcOffset + dstWidth) * channels),
+      dstOffset * channels
+    )
   }
 
   return createImageData(dst, dstWidth, dstHeight)
