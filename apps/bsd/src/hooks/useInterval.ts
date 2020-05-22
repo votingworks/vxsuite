8f8248c3b253ca8b36fdcbb6a2aa08180b6e67dd
Brian Donovan <1938+eventualbuddha@users.noreply.github.com>
2020-05-22 08:20:30 -0700

    refactor: remove some unused code
    These files/branches didn't seem to ever be used.

--- apps/bsd/src/hooks/useInterval.ts
+++ apps/bsd/src/hooks/useInterval.ts
@@ -3,10 +3,8 @@ import { useEffect } from 'react'
 
 function useInterval(callback: () => void, delay: number) {
   useEffect(() => {
-    if (delay !== undefined) {
-      const id = setInterval(callback, delay)
-      return () => clearInterval(id)
-    }
+    const id = setInterval(callback, delay)
+    return () => clearInterval(id)
   }, [callback, delay])
 }
 
