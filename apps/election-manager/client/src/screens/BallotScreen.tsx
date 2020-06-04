4e4225a30780a76a864341ece040c4e592b9065e
Beau Smith <beau@beausmith.com>
2020-06-03 22:13:19 -0700

    feat: Add ballot proofing content to Ballot Screen.

--- apps/election-manager/client/src/screens/BallotScreen.tsx
+++ apps/election-manager/client/src/screens/BallotScreen.tsx
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react'
 import { useParams } from 'react-router-dom'
 import { getPrecinctById } from '@votingworks/ballot-encoder'
+import pluralize from 'pluralize'
 
 import { BallotScreenProps } from '../config/types'
 import AppContext from '../contexts/AppContext'
@@ -12,41 +13,73 @@ import HandMarkedPaperBallot from '../components/HandMarkedPaperBallot'
 import { Monospace } from '../components/Text'
 import { getBallotFileName } from '../utils/election'
 import NavigationScreen from '../components/NavigationScreen'
+import HorizontalRule from '../components/HorizontalRule'
 
 const BallotScreen = () => {
   const { precinctId, ballotStyleId } = useParams<BallotScreenProps>()
   const { election: e, electionHash } = useContext(AppContext)
   const election = e!
   const precinctName = getPrecinctById({ election, precinctId })?.name
+  const ballotStyle = election.ballotStyles.find(bs => bs.id === ballotStyleId)!
 
   return (
-    <NavigationScreen>
-      <LinkButton small to={routerPaths.ballotsList}>
-        back to Ballots
-      </LinkButton>
-      <h1>
-        Ballot Style <strong>{ballotStyleId}</strong> for {precinctName}
-      </h1>
-      <p>
-        Filename:{' '}
-        <Monospace>
-          {getBallotFileName({
-            ballotStyleId,
-            election,
-            electionHash,
-            precinctId,
-          })}
-        </Monospace>
-      </p>
-      <p>
-        <Button onPress={window.print}>Print Ballot</Button>
-      </p>
-      <HandMarkedPaperBallot
-        ballotStyleId={ballotStyleId}
-        election={election}
-        precinctId={precinctId}
-      />
-    </NavigationScreen>
+    <React.Fragment>
+      <NavigationScreen>
+        <h1>
+          Ballot Style <strong>{ballotStyleId}</strong> for {precinctName}
+        </h1>
+        <p>
+          <LinkButton small to={routerPaths.ballotsList}>
+            back to Ballots
+          </LinkButton>{' '}
+          <Button small onPress={window.print}>Print Ballot</Button>
+        </p>
+        <p>
+          Filename:{' '}
+          <Monospace>
+            {getBallotFileName({
+              ballotStyleId,
+              election,
+              electionHash,
+              precinctId,
+            })}
+          </Monospace>
+        </p>
+        <HorizontalRule />
+        <p>Ballot style {ballotStyle.id} has {pluralize('contest', election.contests.length, true)}:</p>
+        {election.contests
+          .filter(
+            contest =>
+              ballotStyle.districts.includes(contest.districtId) &&
+              ballotStyle.partyId === contest.partyId
+          )
+          .map(contest => (
+            <React.Fragment key={contest.id}>
+              <h3>{contest.title}</h3>
+              {contest.type === 'candidate' ? (
+                <ul>
+                  {contest.candidates.map(candidate => (
+                    <li key={candidate.id}>{candidate.name}</li>
+                  ))}
+                </ul>
+              ) : (
+                  <ul>
+                    <li>Yes</li>
+                    <li>No</li>
+                  </ul>
+                )}
+            </React.Fragment>
+          ))}
+        <HorizontalRule />
+      </NavigationScreen>
+      <div className="foobar">
+        <HandMarkedPaperBallot
+          ballotStyleId={ballotStyleId}
+          election={election}
+          precinctId={precinctId}
+        />
+      </div>
+    </React.Fragment>
   )
 }
 
