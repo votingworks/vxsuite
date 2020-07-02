2ffb1835cde9dedd47d9bcf52cf67c7556b93a45
Beau Smith <beau@beausmith.com>
2020-07-02 16:32:30 -0700

    feat: Use confirm modal before removing CVR files.

--- apps/election-manager/src/screens/TallyScreen.tsx
+++ apps/election-manager/src/screens/TallyScreen.tsx
@@ -1,5 +1,6 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import fileDownload from 'js-file-download'
+import pluralize from 'pluralize'
 
 import { InputEventFunction } from '../config/types'
 
@@ -16,6 +17,8 @@ import NavigationScreen from '../components/NavigationScreen'
 import { routerPaths } from '../components/ElectionManager'
 import LinkButton from '../components/LinkButton'
 import HorizontalRule from '../components/HorizontalRule'
+import Modal from '../components/Modal'
+import Prose from '../components/Prose'
 
 const TallyScreen = () => {
   const {
@@ -24,6 +27,14 @@ const TallyScreen = () => {
     saveCastVoteRecordFiles,
   } = useContext(AppContext)
   const election = e!
+
+  const [isConfimingRemoveCRVs, setIsConfimingRemoveCRVs] = useState(false)
+  const cancelConfirmingRemoveCRVs = () => {
+    setIsConfimingRemoveCRVs(false)
+  }
+  const confirmRemoveCRVs = () => {
+    setIsConfimingRemoveCRVs(true)
+  }
 
   const votesByPrecinct = getVotesByPrecinct({
     election,
@@ -50,6 +61,7 @@ const TallyScreen = () => {
 
   const resetCastVoteRecordFiles = () => {
     saveCastVoteRecordFiles()
+    setIsConfimingRemoveCRVs(false)
   }
 
   const exportResults = async () => {
@@ -84,179 +96,202 @@ const TallyScreen = () => {
   }
 
   return (
-    <NavigationScreen>
-      <h1>Election Tally</h1>
-      <h2>Cast Vote Record (CVR) files</h2>
-      <Table>
-        <tbody>
-          {hasCastVoteRecordFiles ? (
-            <React.Fragment>
-              <tr>
-                <TD as="th" narrow nowrap>
-                  File Name
-                </TD>
-                <TD as="th" nowrap narrow>
-                  CVR Count
-                </TD>
-                <TD as="th" nowrap>
-                  Precinct(s)
-                </TD>
-              </tr>
-              {castVoteRecordFileList.map(({ name, count, precinctIds }) => (
-                <tr key={name}>
-                  <TD narrow nowrap>
-                    {name}
+    <React.Fragment>
+      <NavigationScreen>
+        <h1>Election Tally</h1>
+        <h2>Cast Vote Record (CVR) files</h2>
+        <Table>
+          <tbody>
+            {hasCastVoteRecordFiles ? (
+              <React.Fragment>
+                <tr>
+                  <TD as="th" narrow nowrap>
+                    File Name
                   </TD>
-                  <TD narrow>{count}</TD>
-                  <TD>{getPrecinctNames(precinctIds)}</TD>
+                  <TD as="th" nowrap narrow>
+                    CVR Count
+                  </TD>
+                  <TD as="th" nowrap>
+                    Precinct(s)
+                  </TD>
                 </tr>
-              ))}
-              <tr>
-                <TD as="th" narrow nowrap>
-                  Total CVRs Count
-                </TD>
-                <TD as="th" narrow>
-                  {castVoteRecordFileList.reduce(
-                    (prev, curr) => prev + curr.count,
-                    0
-                  )}
-                </TD>
-                <TD as="th" />
-              </tr>
-            </React.Fragment>
-          ) : (
-            <tr>
-              <TD colSpan={2}>
-                <em>No CVR files loaded.</em>
-              </TD>
-            </tr>
-          )}
-        </tbody>
-      </Table>
-      {castVoteRecordFiles.duplicateFiles.length > 0 && (
-        <Text warning>
-          {castVoteRecordFiles.duplicateFiles.length === 1 && (
-            <React.Fragment>
-              The file{' '}
-              <strong>{castVoteRecordFiles.duplicateFiles.join(', ')}</strong>{' '}
-              was ignored as a duplicate of a file already loaded.
-            </React.Fragment>
-          )}
-          {castVoteRecordFiles.duplicateFiles.length > 1 && (
-            <React.Fragment>
-              The files{' '}
-              <strong>{castVoteRecordFiles.duplicateFiles.join(', ')}</strong>{' '}
-              were ignored as duplicates of files already loaded.
-            </React.Fragment>
-          )}
-        </Text>
-      )}
-      {castVoteRecordFiles.errorFile && (
-        <Text error>
-          There was an error reading the content of the file{' '}
-          <strong>{castVoteRecordFiles.errorFile}</strong>. Please ensure this
-          file only contains CVR data.
-        </Text>
-      )}
-      <p>
-        <FileInputButton multiple onChange={processCastVoteRecordFiles}>
-          Load CVR Files
-        </FileInputButton>{' '}
-        <Button
-          danger
-          disabled={!hasCastVoteRecordFiles}
-          onPress={resetCastVoteRecordFiles}
-        >
-          Remove All CVR Files
-        </Button>
-      </p>
-      <h2>Ballot Count By Precinct</h2>
-      <Table>
-        <tbody>
-          {hasCastVoteRecordFiles ? (
-            <React.Fragment>
-              <tr>
-                <TD as="th" narrow>
-                  Precinct
-                </TD>
-                <TD as="th">Ballot Count</TD>
-                <TD as="th">View Tally</TD>
-              </tr>
-              {election.precincts
-                .sort((a, b) =>
-                  a.name.localeCompare(b.name, undefined, {
-                    ignorePunctuation: true,
-                  })
-                )
-                .map((precinct) => {
-                  const precinctBallotsCount =
-                    votesByPrecinct && votesByPrecinct[precinct.id]
-                      ? votesByPrecinct[precinct.id]!.length
-                      : 0
-                  return (
-                    <tr key={precinct.id}>
-                      <TD narrow nowrap>
-                        {precinct.name}
-                      </TD>
-                      <TD>{precinctBallotsCount}</TD>
-                      <TD>
-                        {!!precinctBallotsCount && (
-                          <LinkButton
-                            to={routerPaths.tallyReport({
-                              precinctId: precinct.id,
-                            })}
-                          >
-                            View {precinct.name} Tally
-                          </LinkButton>
-                        )}
-                      </TD>
-                    </tr>
-                  )
-                })}
-              <tr>
-                <TD narrow>
-                  <strong>Total Ballot Count</strong>
-                </TD>
-                <TD>
-                  <strong>
-                    {Object.values(votesByPrecinct).reduce(
-                      (prev, curr) => prev + (curr ? curr.length : 0),
+                {castVoteRecordFileList.map(({ name, count, precinctIds }) => (
+                  <tr key={name}>
+                    <TD narrow nowrap>
+                      {name}
+                    </TD>
+                    <TD narrow>{count}</TD>
+                    <TD>{getPrecinctNames(precinctIds)}</TD>
+                  </tr>
+                ))}
+                <tr>
+                  <TD as="th" narrow nowrap>
+                    Total CVRs Count
+                  </TD>
+                  <TD as="th" narrow>
+                    {castVoteRecordFileList.reduce(
+                      (prev, curr) => prev + curr.count,
                       0
                     )}
-                  </strong>
-                </TD>
-                <TD>
-                  <LinkButton
-                    disabled={!hasCastVoteRecordFiles || !fullElectionTally}
-                    to={routerPaths.tallyFullReport}
-                  >
-                    View Full Election Tally
-                  </LinkButton>
+                  </TD>
+                  <TD as="th" />
+                </tr>
+              </React.Fragment>
+            ) : (
+              <tr>
+                <TD colSpan={2}>
+                  <em>No CVR files loaded.</em>
                 </TD>
               </tr>
-            </React.Fragment>
-          ) : (
-            <tr>
-              <TD colSpan={2}>
-                <em>Load CVR files to view ballot count by precinct.</em>
-              </TD>
-            </tr>
-          )}
-        </tbody>
-      </Table>
-      <p>
-        <Button disabled={!hasCastVoteRecordFiles} onPress={exportResults}>
-          Export SEMS Results File
-        </Button>
-      </p>
-      <HorizontalRule />
-      <h2>Test Ballot Deck Tally</h2>
-      <p>
-        <LinkButton to={routerPaths.testDecksTally}>
-          View Test Ballot Deck Tally
-        </LinkButton>
-      </p>
-    </NavigationScreen>
+            )}
+          </tbody>
+        </Table>
+        {castVoteRecordFiles.duplicateFiles.length > 0 && (
+          <Text warning>
+            {castVoteRecordFiles.duplicateFiles.length === 1 && (
+              <React.Fragment>
+                The file{' '}
+                <strong>{castVoteRecordFiles.duplicateFiles.join(', ')}</strong>{' '}
+                was ignored as a duplicate of a file already loaded.
+              </React.Fragment>
+            )}
+            {castVoteRecordFiles.duplicateFiles.length > 1 && (
+              <React.Fragment>
+                The files{' '}
+                <strong>{castVoteRecordFiles.duplicateFiles.join(', ')}</strong>{' '}
+                were ignored as duplicates of files already loaded.
+              </React.Fragment>
+            )}
+          </Text>
+        )}
+        {castVoteRecordFiles.errorFile && (
+          <Text error>
+            There was an error reading the content of the file{' '}
+            <strong>{castVoteRecordFiles.errorFile}</strong>. Please ensure this
+            file only contains CVR data.
+          </Text>
+        )}
+        <p>
+          <FileInputButton multiple onChange={processCastVoteRecordFiles}>
+            Load CVR Files
+          </FileInputButton>{' '}
+          <Button
+            danger
+            disabled={!hasCastVoteRecordFiles}
+            onPress={confirmRemoveCRVs}
+          >
+            Remove CVR Files…
+          </Button>
+        </p>
+        <h2>Ballot Count By Precinct</h2>
+        <Table>
+          <tbody>
+            {hasCastVoteRecordFiles ? (
+              <React.Fragment>
+                <tr>
+                  <TD as="th" narrow>
+                    Precinct
+                  </TD>
+                  <TD as="th">Ballot Count</TD>
+                  <TD as="th">View Tally</TD>
+                </tr>
+                {election.precincts
+                  .sort((a, b) =>
+                    a.name.localeCompare(b.name, undefined, {
+                      ignorePunctuation: true,
+                    })
+                  )
+                  .map((precinct) => {
+                    const precinctBallotsCount =
+                      votesByPrecinct && votesByPrecinct[precinct.id]
+                        ? votesByPrecinct[precinct.id]!.length
+                        : 0
+                    return (
+                      <tr key={precinct.id}>
+                        <TD narrow nowrap>
+                          {precinct.name}
+                        </TD>
+                        <TD>{precinctBallotsCount}</TD>
+                        <TD>
+                          {!!precinctBallotsCount && (
+                            <LinkButton
+                              to={routerPaths.tallyReport({
+                                precinctId: precinct.id,
+                              })}
+                            >
+                              View {precinct.name} Tally
+                            </LinkButton>
+                          )}
+                        </TD>
+                      </tr>
+                    )
+                  })}
+                <tr>
+                  <TD narrow>
+                    <strong>Total Ballot Count</strong>
+                  </TD>
+                  <TD>
+                    <strong>
+                      {Object.values(votesByPrecinct).reduce(
+                        (prev, curr) => prev + (curr ? curr.length : 0),
+                        0
+                      )}
+                    </strong>
+                  </TD>
+                  <TD>
+                    <LinkButton
+                      disabled={!hasCastVoteRecordFiles || !fullElectionTally}
+                      to={routerPaths.tallyFullReport}
+                    >
+                      View Full Election Tally
+                    </LinkButton>
+                  </TD>
+                </tr>
+              </React.Fragment>
+            ) : (
+              <tr>
+                <TD colSpan={2}>
+                  <em>Load CVR files to view ballot count by precinct.</em>
+                </TD>
+              </tr>
+            )}
+          </tbody>
+        </Table>
+        <p>
+          <Button disabled={!hasCastVoteRecordFiles} onPress={exportResults}>
+            Export SEMS Results File
+          </Button>
+        </p>
+        <HorizontalRule />
+        <h2>Test Ballot Deck Tally</h2>
+        <p>
+          <LinkButton to={routerPaths.testDecksTally}>
+            View Test Ballot Deck Tally
+          </LinkButton>
+        </p>
+      </NavigationScreen>
+      <Modal
+        isOpen={isConfimingRemoveCRVs}
+        centerContent
+        content={
+          <Prose textCenter>
+            <p>
+              Do you want to remove the {castVoteRecordFileList.length} uploaded
+              CRV {pluralize('files', castVoteRecordFileList.length)}?
+            </p>
+            <p>All reports will be unavailable without CVR data.</p>
+          </Prose>
+        }
+        actions={
+          <React.Fragment>
+            <Button onPress={cancelConfirmingRemoveCRVs}>Cancel</Button>
+            <Button danger onPress={resetCastVoteRecordFiles}>
+              Remove All CVR Files
+            </Button>
+          </React.Fragment>
+        }
+      />
+    </React.Fragment>
   )
 }
 
