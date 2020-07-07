2f0b70a543172181b302a68dfdc8b4bdca9511a6
Ben Adida <ben@adida.net>
2020-07-06 23:06:21 -0700

    a bit more testing, some coverage

--- apps/election-manager/src/App.test.tsx
+++ apps/election-manager/src/App.test.tsx
@@ -1,25 +1,43 @@
 import React from 'react'
 import { fireEvent, render, screen } from '@testing-library/react'
+import fakeKiosk from '../test/helpers/fakeKiosk'
 
 import App from './App'
 
 beforeEach(() => {
+  // we don't care about network errors logged to the console, they're crowding things
+  jest.spyOn(console, 'error').mockImplementation(() => {}) // eslint-disable-line @typescript-eslint/no-empty-function
+
   window.location.href = '/'
+  window.kiosk = fakeKiosk()
 })
 
 it('basic navigation works', async () => {
-  render(<App />)
+  const { getByText } = render(<App />)
 
-  fireEvent.click(await screen.findByText('Create New Election Definition'))
+  await screen.findByText('Create New Election Definition')
 
-  // navigate a bit
-  fireEvent.click(await screen.findByText('Ballots'))
-  fireEvent.click(await screen.findByText('Tally'))
-  fireEvent.click(await screen.findByText('Definition'))
+  fireEvent.click(getByText('Create New Election Definition'))
+
+  // go print some ballots
+  fireEvent.click(getByText('Ballots'))
+
+  fireEvent.click(getByText('Export Ballot Package'))
+  expect(window.kiosk?.saveAs).toHaveBeenCalledTimes(1)
+
+  // we're not mocking the filestream yet
+  await screen.findByText('Download Failed')
+
+  // first ballot -- FIXME: i18n making this fail right now
+  // fireEvent.click(getAllByText('View Ballot')[0])
+  // fireEvent.click(getByText('English/Spanish'))
+
+  fireEvent.click(getByText('Tally'))
+  fireEvent.click(getByText('Definition'))
 
   // remove the election
-  fireEvent.click(await screen.findByText('Remove'))
-  fireEvent.click(await screen.findByText('Remove Election Definition'))
+  fireEvent.click(getByText('Remove'))
+  fireEvent.click(getByText('Remove Election Definition'))
 
   await screen.findByText('Configure Election Manager')
 })
