1a0ea85f76cfcaef048bd59bf25bd24252608e0a
Ben Adida <ben@adida.net>
2020-08-24 23:57:54 -0700

    disable print button until rendering is done

--- apps/election-manager/client/src/screens/PrintTestDeckScreen.tsx
+++ apps/election-manager/client/src/screens/PrintTestDeckScreen.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react'
+import React, { useState, useContext, useCallback, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 import {
   CandidateContest,
@@ -31,69 +31,121 @@ const allPrecincts: Precinct = {
   name: 'All Precincts',
 }
 
+type OnAllRenderedFunctionType = () => void
+
+interface TestDeckBallotsParams {
+  election: Election
+  precinct: Precinct
+  onAllRendered: OnAllRenderedFunctionType
+}
+
+const TestDeckBallots = ({
+  election,
+  precinct,
+  onAllRendered,
+}: TestDeckBallotsParams) => {
+  console.log('rendering testdeck')
+
+  // generate the possibilities
+  const precinctIds: string[] = precinct.id
+    ? [precinct.id]
+    : election.precincts.map((p) => p.id)
+  const ballots: Dictionary<string | VotesDict>[] = []
+
+  precinctIds.forEach((precinctId) => {
+    const precinct = election.precincts.find((p) => p.id === precinctId)!
+    const precinctBallotStyles = election.ballotStyles.filter((bs) =>
+      bs.precincts.includes(precinct.id)
+    )
+
+    precinctBallotStyles.forEach((ballotStyle) => {
+      const contests = election.contests.filter(
+        (c) =>
+          ballotStyle.districts.includes(c.districtId) &&
+          ballotStyle.partyId === c.partyId
+      )
+
+      const numBallots = Math.max(
+        ...contests.map((c) =>
+          c.type === 'yesno' ? 2 : (c as CandidateContest).candidates.length
+        )
+      )
+
+      for (let ballotNum = 0; ballotNum < numBallots; ballotNum++) {
+        const votes: VotesDict = {}
+        contests.forEach((contest) => {
+          /* istanbul ignore else */
+          if (contest.type === 'yesno') {
+            votes[contest.id] = ballotNum % 2 === 0 ? ['yes'] : ['no']
+          } else if (
+            contest.type === 'candidate' &&
+            contest.candidates.length > 0 // safety check
+          ) {
+            votes[contest.id] = [
+              contest.candidates[ballotNum % contest.candidates.length],
+            ]
+          }
+        })
+        ballots.push({
+          ballotStyleId: ballotStyle.id,
+          precinctId,
+          votes,
+        })
+      }
+    })
+  })
+
+  let numRendered = 0
+  const onRendered = () => {
+    numRendered += 1
+    if (numRendered === ballots.length) {
+      onAllRendered()
+    }
+  }
+
+  return (
+    <div className="print-only">
+      {ballots.map((ballot, i) => (
+        <HandMarkedPaperBallot
+          key={`ballot-${i}`} // eslint-disable-line react/no-array-index-key
+          ballotStyleId={ballot.ballotStyleId as string}
+          election={election}
+          isLiveMode
+          precinctId={ballot.precinctId as string}
+          locales={{ primary: 'en-US' }}
+          votes={ballot.votes as VotesDict}
+          onRendered={() => onRendered()}
+        />
+      ))}
+    </div>
+  )
+}
+
+const TestDeckBallotsMemoized = React.memo(TestDeckBallots)
+
 const PrintTestDeckScreen = () => {
   const { election: e } = useContext(AppContext)
   const election = e!
   const { precinctId: p = '' } = useParams<PrecinctReportScreenProps>()
   const precinctId = p.trim()
 
+  const [printEnabled, setPrintEnabled] = useState(false)
+
+  const pageTitle = 'Print Test Deck'
   const precinct =
     precinctId === 'all'
       ? allPrecincts
       : getPrecinctById({ election, precinctId })
 
-  const pageTitle = 'Print Test Deck'
+  useEffect(() => {
+    setPrintEnabled(false)
+  }, [precinctId])
 
-  if (precinct?.name) {
-    // generate the possibilities
-    const precinctIds: string[] = precinct.id
-      ? [precinctId]
-      : election.precincts.map((p) => p.id)
-    const ballots: Dictionary<string | VotesDict>[] = []
+  const onAllRendered = useCallback(() => {
+    setPrintEnabled(true)
+  }, [])
 
-    precinctIds.forEach((precinctId) => {
-      const precinct = election.precincts.find((p) => p.id === precinctId)!
-      const precinctBallotStyles = election.ballotStyles.filter((bs) =>
-        bs.precincts.includes(precinct.id)
-      )
-
-      precinctBallotStyles.forEach((ballotStyle) => {
-        const contests = election.contests.filter(
-          (c) =>
-            ballotStyle.districts.includes(c.districtId) &&
-            ballotStyle.partyId === c.partyId
-        )
-
-        const numBallots = Math.max(
-          ...contests.map((c) =>
-            c.type === 'yesno' ? 2 : (c as CandidateContest).candidates.length
-          )
-        )
-
-        for (let ballotNum = 0; ballotNum < numBallots; ballotNum++) {
-          const votes: VotesDict = {}
-          contests.forEach((contest) => {
-            /* istanbul ignore else */
-            if (contest.type === 'yesno') {
-              votes[contest.id] = ballotNum % 2 === 0 ? ['yes'] : ['no']
-            } else if (
-              contest.type === 'candidate' &&
-              contest.candidates.length > 0 // safety check
-            ) {
-              votes[contest.id] = [
-                contest.candidates[ballotNum % contest.candidates.length],
-              ]
-            }
-          })
-          ballots.push({
-            ballotStyleId: ballotStyle.id,
-            precinctId,
-            votes,
-          })
-        }
-      })
-    })
-
+  if (precinct) {
     return (
       <React.Fragment>
         <NavigationScreen>
@@ -105,7 +157,13 @@ const PrintTestDeckScreen = () => {
               <strong>Precinct:</strong> {precinct.name}
             </p>
             <p>
-              <PrintButton primary>Print Test Deck</PrintButton>
+              {printEnabled ? (
+                <PrintButton primary>Print Test Deck</PrintButton>
+              ) : (
+                <PrintButton primary disabled>
+                  Generating Test Deck...
+                </PrintButton>
+              )}
             </p>
             <p>
               <LinkButton small to={routerPaths.printTestDecks}>
@@ -114,19 +172,11 @@ const PrintTestDeckScreen = () => {
             </p>
           </Prose>
         </NavigationScreen>
-        <div className="print-only">
-          {ballots.map((ballot, i) => (
-            <HandMarkedPaperBallot
-              key={`ballot-${i}`} // eslint-disable-line react/no-array-index-key
-              ballotStyleId={ballot.ballotStyleId as string}
-              election={election}
-              isLiveMode
-              precinctId={ballot.precinctId as string}
-              locales={{ primary: 'en-US' }}
-              votes={ballot.votes as VotesDict}
-            />
-          ))}
-        </div>
+        <TestDeckBallotsMemoized
+          election={election}
+          precinct={precinct}
+          onAllRendered={onAllRendered}
+        />
       </React.Fragment>
     )
   }
