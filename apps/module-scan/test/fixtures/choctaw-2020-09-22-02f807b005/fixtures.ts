a8668de6d3977f383dfc15bd2ac0f7abc15c36e0
Brian Donovan <1938+eventualbuddha@users.noreply.github.com>
2020-09-04 13:19:02 -0700

    fix: base64-encode the QR code data
    The QR code encoders/decoders we use are sometimes wrong when the data encoded is not ASCII, so let's just give them ASCII.
    
    Refs votingworks/vxmail#244

---
+++ apps/module-scan/test/fixtures/choctaw-2020-09-22-02f807b005/fixtures.ts
@@ -0,0 +1,5 @@
+import { join } from 'path'
+
+export const ballotPdf = join(__dirname, 'ballot.pdf')
+export const blankPage1 = join(__dirname, 'blank-p1.png')
+export const blankPage2 = join(__dirname, 'blank-p2.png')
