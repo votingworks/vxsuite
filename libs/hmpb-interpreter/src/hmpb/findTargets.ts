b5dea26695a090e4615c283552ac432be0e54847
Brian Donovan <1938+eventualbuddha@users.noreply.github.com>
2020-10-13 10:31:51 -0700

    fix: find template targets more intelligently
    Once we've found a single target, we use the dimensions of that target to determine whether future shapes we see are targets too. Since we scan up from the bottom and we're only using this on templates, this should be safe.

--- libs/hmpb-interpreter/src/hmpb/findTargets.ts
+++ libs/hmpb-interpreter/src/hmpb/findTargets.ts
@@ -3,7 +3,7 @@ import { Rect } from '../types'
 import { PIXEL_WHITE } from '../utils/binarize'
 import { rectCenter } from '../utils/geometry'
 import { VisitedPoints } from '../utils/VisitedPoints'
-import { findShape } from './shapes'
+import { findShape, Shape } from './shapes'
 
 const debug = makeDebug('hmpb-interpreter:findTargets')
 
@@ -20,14 +20,16 @@ export default function* findTargets(
     aspectRatio = 1.5,
     aspectRatioTolerance = 0.1,
     expectedWidth = Math.round(0.025 * ballotImage.width),
-    errorMargin = Math.ceil(0.02 * expectedWidth),
+    errorMargin = Math.ceil(0.04 * expectedWidth),
   } = {}
 ): Generator<TargetShape> {
+  debug('finding targets in %o', bounds)
   const visitedPoints = new VisitedPoints(ballotImage.width, ballotImage.height)
   const minAspectRatio = aspectRatio - aspectRatioTolerance
   const maxAspectRatio = aspectRatio + aspectRatioTolerance
 
   const x = bounds.x + Math.round(inset + expectedWidth / 2)
+  let lastShape: Shape | undefined
 
   for (let y = bounds.y + bounds.height - inset; y > bounds.y; y--) {
     const shape = findShape(ballotImage, { x, y }, visitedPoints)
@@ -36,30 +38,58 @@ export default function* findTargets(
       continue
     }
 
-    const actualAspectRatio = shape.bounds.width / shape.bounds.height
-    if (
-      actualAspectRatio < minAspectRatio ||
-      actualAspectRatio > maxAspectRatio
-    ) {
-      debug(
-        'skipping shape because it is the wrong aspect ratio: %d ≉ %d ± %d: %O',
-        actualAspectRatio,
-        aspectRatio,
-        aspectRatioTolerance,
-        shape.bounds
-      )
-    } else if (
-      shape.bounds.width < expectedWidth - errorMargin ||
-      shape.bounds.width > expectedWidth + errorMargin
-    ) {
-      debug(
-        'skipping shape because it is the wrong width: %d ≉ %d ± %d: %O',
-        shape.bounds.width,
-        expectedWidth,
-        errorMargin,
-        shape.bounds
-      )
+    // If we already found one, so let's use it to determine the right size.
+    let found: boolean
+
+    if (lastShape) {
+      if (
+        shape.bounds.width <= lastShape.bounds.width + errorMargin &&
+        shape.bounds.width >= lastShape.bounds.width - errorMargin &&
+        shape.bounds.height <= lastShape.bounds.height + errorMargin &&
+        shape.bounds.height >= lastShape.bounds.height - errorMargin
+      ) {
+        debug('shape matches last target shape!')
+        found = true
+      } else {
+        debug(
+          'skipping shape because it does not match the last known target shape: %O ≉ %O',
+          shape.bounds,
+          lastShape.bounds
+        )
+        found = false
+      }
     } else {
+      const actualAspectRatio = shape.bounds.width / shape.bounds.height
+      if (
+        actualAspectRatio < minAspectRatio ||
+        actualAspectRatio > maxAspectRatio
+      ) {
+        debug(
+          'skipping shape because it is the wrong aspect ratio: %d ≉ %d ± %d: %O',
+          actualAspectRatio,
+          aspectRatio,
+          aspectRatioTolerance,
+          shape.bounds
+        )
+        found = false
+      } else if (
+        shape.bounds.width < expectedWidth - errorMargin ||
+        shape.bounds.width > expectedWidth + errorMargin
+      ) {
+        debug(
+          'skipping shape because it is the wrong width: %d ≉ %d ± %d: %O',
+          shape.bounds.width,
+          expectedWidth,
+          errorMargin,
+          shape.bounds
+        )
+        found = false
+      } else {
+        found = true
+      }
+    }
+
+    if (found) {
       debug('found shape: %O', shape.bounds)
       const innerShape = findShape(
         ballotImage,
@@ -67,6 +97,7 @@ export default function* findTargets(
         undefined,
         { color: PIXEL_WHITE }
       )
+      lastShape = shape
       yield {
         bounds: shape.bounds,
         inner: innerShape.bounds,
