4fcd222e81790134047411be7c5406fbe58e8dd9
Caroline Modic <caroline@voting.works>
2020-11-17 12:53:21 -0800

    add to insert card screen

--- apps/bmd/src/pages/InsertCardScreen.tsx
+++ apps/bmd/src/pages/InsertCardScreen.tsx
@@ -38,6 +38,7 @@ const ActivationScreen = ({
         <ElectionInfo
           electionDefinition={electionDefinition}
           precinctId={appPrecinctId}
+          showElectionHash
         />
       </Sidebar>
       <Main>
