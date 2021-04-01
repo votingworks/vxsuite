eaa7f6fb3aa76082eb9aaca1f5f4b66a9c36c011
Brian Donovan <1938+eventualbuddha@users.noreply.github.com>
2021-04-01 14:19:31 -0700

    refactor(em): extract more general `saveAsPDF`
    The `saveAsPDF` function was actually pretty specific to generating a report PDF. This makes `saveAsPDF` take an arbitrary filename and renames the old one to `saveReportPDF`.

--- apps/election-manager/src/utils/saveAsPDF.ts
+++ apps/election-manager/src/utils/saveAsPDF.ts
@@ -1,16 +1,9 @@
 import { Election } from '@votingworks/types'
 
-export default async function saveAsPDF(
-  fileNamePrefix: string,
-  election: Election,
-  fileSuffix = 'all-precincts'
-): Promise<boolean> {
+export default async function saveAsPDF(fileName: string): Promise<boolean> {
   const data = await window.kiosk!.printToPDF()
   const fileWriter = await window.kiosk!.saveAs({
-    defaultPath: `${`${fileNamePrefix}-${election.county.name}-${election.title}-${fileSuffix}`
-      .replace(/[^a-z0-9]+/gi, '-')
-      .replace(/(^-|-$)+/g, '')
-      .toLocaleLowerCase()}.pdf`,
+    defaultPath: fileName,
   })
   if (!fileWriter) {
     return false
@@ -18,4 +11,17 @@ export default async function saveAsPDF(
   fileWriter.write(data)
   await fileWriter.end()
   return true
+}
+
+export async function saveReportPDF(
+  fileNamePrefix: string,
+  election: Election,
+  fileSuffix = 'all-precincts'
+): Promise<boolean> {
+  return await saveAsPDF(
+    `${`${fileNamePrefix}-${election.county.name}-${election.title}-${fileSuffix}`
+      .replace(/[^a-z0-9]+/gi, '-')
+      .replace(/(^-|-$)+/g, '')
+      .toLocaleLowerCase()}.pdf`
+  )
 }
