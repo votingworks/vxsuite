9a0c526e7263e0309882addf7394572624b088bc
Caroline Modic <caroline@voting.works>
2021-04-09 15:55:06 -0700

    feat(mcs): add endpoint to convert precomputed tallies to sems export

--- apps/module-converter-sems/converter/SEMSoutput.py
+++ apps/module-converter-sems/converter/SEMSoutput.py
@@ -54,6 +54,8 @@ YESNO_CANDIDATES = [
     ## TODO
 ]
 
+INTERNAL_WRITE_IN_ID = '__write-in'
+
 CVR_FIELDS = ["county_id", "precinct_id", "contest_id", "candidate_id"]
 CANDIDATE_FIELDS = ["county_id", "contest_id", "candidate_id"]
 
@@ -67,12 +69,151 @@ def find_contest(contests, contest_id):
         if c['type'] == 'ms-either-neither' and c['eitherNeitherContestId'] == contest_id:
             return {"id": c['eitherNeitherContestId'],
                     "title": c['title'],
-                    "options": c['eitherNeitherOptions']}
+                    "type": "yesno",
+                    "yesOption": c['eitherOption'],
+                    "noOption": c['neitherOption'],
+                    "districtId": c['districtId'],
+                    "options": c['eitherNeitherOptions'] if 'eitherNeitherOptions' in c else None}
         if c['type'] == 'ms-either-neither' and c['pickOneContestId'] == contest_id:
             return {"id": c['pickOneContestId'],
                     "title": c["title"],
-                    "options": c['pickOneOptions']}
+                    "type": "yesno",
+                    "yesOption": c['firstOption'],
+                    "noOption": c['secondOption'],
+                    "districtId": c['districtId'],
+                    "options": c['pickOneOptions'] if 'pickOneOptions' in c else None}
     
+
+def process_tallies_file(election_file_path, vx_results_file_path):
+    election = json.loads(open(election_file_path, "r").read())
+    tallies = json.loads(open(vx_results_file_path, "r").read())
+
+    contests = election["contests"]
+    ballot_styles = election["ballotStyles"]
+    precincts = election["precincts"]
+    parties = election["parties"] + [NOPARTY_PARTY]
+    county_id = election["county"]["id"]
+    tallies_by_precinct = tallies["talliesByPrecinct"]
+
+    contests_by_precinct = {p["id"]: [] for p in precincts}
+    for contest in contests:
+        contest_ballot_styles = [bs for bs in ballot_styles if contest["districtId"] in bs["districts"]]
+        contest_precincts = set()
+        [contest_precincts.update(bs["precincts"]) for bs in contest_ballot_styles]
+        for precinct in contest_precincts:
+            if contest["type"] == "ms-either-neither":
+                contests_by_precinct[precinct].append(contest["eitherNeitherContestId"])
+                contests_by_precinct[precinct].append(contest["pickOneContestId"])
+            else:
+                contests_by_precinct[precinct].append(contest["id"])
+
+    rows_to_write = []
+    for precinct in sorted(precincts, key=lambda precinct: precinct["id"]):
+        precinct_id = precinct["id"]
+        contest_tallies = tallies_by_precinct[precinct_id] if precinct_id in tallies_by_precinct else {}
+        contests_to_check = contests_by_precinct[precinct_id]
+        for contest_id in sorted(contests_to_check):
+            contest_tally = contest_tallies[contest_id] if contest_id in contest_tallies else {}
+            contest = find_contest(contests, contest_id)
+            
+            contest_party_id = contest["partyId"] if "partyId" in contest else None
+            contest_party = [p for p in parties if p["id"] == contest_party_id][0] if contest_party_id is not None else None
+
+            rows_for_contest = []
+            base_row_data = [
+                county_id,
+                precinct_id,
+                contest_id,
+                contest["title"].replace("\n", "\\n"),
+                contest_party_id or "0",
+                contest_party["abbrev"] if contest_party is not None else "NP"
+            ]
+            
+            # Add undervote and overvote row from metadata
+            metadata = contest_tally["metadata"] if "metadata" in contest_tally else {}
+            overvotes_row = base_row_data.copy()
+            overvotes_row.extend([
+                OVERVOTE_CANDIDATE["id"],
+                OVERVOTE_CANDIDATE["name"],
+                "0",
+                "NP",
+                metadata["overvotes"] if "overvotes" in metadata else 0
+            ])
+            rows_for_contest.append(overvotes_row)
+            undervotes_row = base_row_data.copy()
+            undervotes_row.extend([
+                UNDERVOTE_CANDIDATE["id"],
+                UNDERVOTE_CANDIDATE["name"],
+                "0",
+                "NP",
+                metadata["undervotes"] if "undervotes" in metadata else 0
+            ])
+            rows_for_contest.append(undervotes_row)
+
+            option_tallies = contest_tally["tallies"] if "tallies" in contest_tally else {}
+            if contest["type"] == "candidate":
+                options = contest["candidates"].copy()
+                if contest["allowWriteIns"]:
+                    options.append(WRITEIN_CANDIDATE)
+                for option in options:
+                    if option == WRITEIN_CANDIDATE:
+                        count = option_tallies[INTERNAL_WRITE_IN_ID] if INTERNAL_WRITE_IN_ID in option_tallies else 0
+                    else:
+                        count = option_tallies[option["id"]] if option["id"] in option_tallies else 0
+
+                    option_party_id = option["partyId"] if "partyId" in option else None
+                    option_party = [p for p in parties if p["id"] == option_party_id][0] if option_party_id is not None else None
+                    candidate_row = base_row_data.copy()
+                    candidate_row.extend([
+                        option["id"],
+                        option.get("name", option.get("label")),
+                        option_party_id or "0",
+                        option_party["abbrev"] if option_party is not None else "NP",
+                        count
+                    ])
+                    rows_for_contest.append(candidate_row)
+
+            elif contest["type"] == "yesno":
+                # get ids for yes option and no option
+                yes_option = contest["yesOption"]
+                no_option = contest["noOption"]
+                yes_count = option_tallies["yes"] if "yes" in option_tallies else 0
+                no_count = option_tallies["no"] if "no" in option_tallies else 0
+                yes_row = base_row_data.copy()
+                yes_row.extend([
+                    yes_option["id"],
+                    yes_option.get("name", yes_option.get("label")),
+                    "0",
+                    "NP",
+                    yes_count
+                ])
+                rows_for_contest.append(yes_row)
+
+                no_row = base_row_data.copy()
+                no_row.extend([
+                    no_option["id"],
+                    no_option.get("name", no_option.get("label")),
+                    "0",
+                    "NP",
+                    no_count
+                ])
+                rows_for_contest.append(no_row)
+            # Append rows sorted by candidate ID
+            for row in sorted(rows_for_contest, key=lambda row: row[6]):
+                rows_to_write.append(row)
+
+
+    sems_io = io.StringIO()
+    for row in rows_to_write:
+        # a whole rigamarole because SEMS needs a trailing comma
+        row_bytesio = io.StringIO()
+        sems_row_writer = csv.writer(row_bytesio, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
+        sems_row_writer.writerow(row)
+    
+        sems_io.write(row_bytesio.getvalue().strip('\r\n'))
+        sems_io.write(",\r\n")
+
+    return sems_io.getvalue()
 
 def process_results_file(election_file_path, vx_results_file_path):
     election = json.loads(open(election_file_path,"r").read())
@@ -275,7 +416,6 @@ def process_results_file(election_file_path, vx_results_file_path):
         # this is the placeholder row
         if not CVR_candidate_id:
             count = 0
-            
         # a whole rigamarole because SEMS needs a trailing comma
         row_bytesio = io.StringIO()
         sems_row_writer = csv.writer(row_bytesio, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
--- apps/module-converter-sems/tests/test_SEMSoutput.py
+++ apps/module-converter-sems/tests/test_SEMSoutput.py
@@ -2,7 +2,7 @@ from unittest.mock import patch
 
 import pytest, json, io, os
 
-from converter.SEMSoutput import process_results_file
+from converter.SEMSoutput import process_results_file, process_tallies_file
 
 PARENT_DIR = os.path.join(os.path.dirname(os.path.realpath(__file__)), '..')
 SAMPLE_FILES = os.path.join(PARENT_DIR, 'sample_files')
@@ -11,21 +11,25 @@ TESTS = [
     {
         'election': 'general-election.json',
         'cvrs': '10_cvrs.csv',
+        'tallies': '10_tallies.json',
         'sems': '10_expected-sems-results.txt'
     },
     {
         'election': '10_8-26-2020-expected-election.json',
         'cvrs': '10_8-26-2020-cvrs.txt',
+        'tallies': '10_8-26-2020-tallies.json',
         'sems': '10_8-26-2020-expected-sems-output.txt',
     },
     {
         'election': '10_8-26-2020-expected-election.json',
         'cvrs': '10_8-26-2020-b-cvrs.txt',
+        'tallies': '10_8-26-2020-b-tallies.json',
         'sems': '10_8-26-2020-b-expected-sems-output.txt',
     },
     {
         'election': 'electionPrimarySample.json',
         'cvrs': 'election-primary-sample-cvrs.txt',
+        'tallies': 'election-primary-sample-tallies.json',
         'sems': 'election-primary-expected-results.csv',
     }
 ]
@@ -43,4 +47,16 @@ def test_general_results():
         expected_result_file = open(get_sample_file(test['sems']), "rb")
         expected_result = expected_result_file.read()
         
+        assert result.encode('utf-8') == expected_result
+
+def test_general_results_from_tallies():
+    for test in TESTS:
+        print("testing", test)
+        result = process_tallies_file(
+            get_sample_file(test['election']),
+            get_sample_file(test['tallies']))
+
+        expected_result_file = open(get_sample_file(test['sems']), "rb")
+        expected_result = expected_result_file.read()
+
         assert result.encode('utf-8') == expected_result
