17e8937ca566b01da61dc3b3538a511c285d5d6f
Brian Donovan <1938+eventualbuddha@users.noreply.github.com>
2021-05-10 16:58:38 -0700

    style(scan): update formatting
    Need to make this project properly auto-format.

--- libs/plustek-sdk/src/plustekctl.ts
+++ libs/plustek-sdk/src/plustekctl.ts
@@ -1,4 +1,4 @@
-import { ok, Result } from "@votingworks/types";
+import { ok, Result } from '@votingworks/types'
 
 export async function findBinaryPath(): Promise<Result<string, Error>> {
   return ok('plustekctl')
