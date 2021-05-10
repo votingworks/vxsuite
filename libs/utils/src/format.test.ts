d77240bd6b15acfa4ec9737ff848653496694c5a
Caroline Modic <caroline@voting.works>
2021-05-10 07:11:01 -0700

    feat(utils) add IntlDateTimeFormat functions to format

--- libs/utils/src/format.test.ts
+++ libs/utils/src/format.test.ts
@@ -15,3 +15,15 @@ test('formats counts properly', () => {
   expect(format.count(-1000000)).toEqual('-1,000,000')
   expect(format.count(-3141098210928)).toEqual('-3,141,098,210,928')
 })
+
+test('formats locale long date properly', () => {
+  expect(
+    format.localeLongDateAndTime(new Date(2020, 3, 14, 1, 15, 9, 26))
+  ).toEqual('Tuesday, April 14, 2020, 1:15:09 AM UTC')
+})
+
+test('formats locale weekday and date properly', () => {
+  expect(
+    format.localeWeekdayAndDate(new Date(2020, 3, 14, 1, 15, 9, 26))
+  ).toEqual('Tuesday, April 14, 2020')
+})
