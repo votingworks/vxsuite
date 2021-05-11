fc0e214a276faafd8aa012825bed06aa613fac57
Beau Smith <beau@beausmith.com>
2021-05-11 10:27:39 -0700

    feat(ps): Confirm Tabulate

--- apps/precinct-scanner/src/screens/ScanWarningScreen.tsx
+++ apps/precinct-scanner/src/screens/ScanWarningScreen.tsx
@@ -1,13 +1,21 @@
 /* istanbul ignore file */
-import React from 'react'
-import { Button, Text } from '@votingworks/ui'
+import React, { useState } from 'react'
+import { Button, Prose, Text } from '@votingworks/ui'
 
 import { PlaceholderGraphic } from '../components/Graphics'
 import { CenteredLargeProse, CenteredScreen } from '../components/Layout'
+import { Absolute } from '../components/Absolute'
+import { Bar } from '../components/Bar'
+import Modal from '../components/Modal'
 
 const ScanWarningScreen: React.FC = () => {
+  const [confirmTabulate, setConfirmTabulate] = useState(false)
+  const openConfirmTabulateModal = () => setConfirmTabulate(true)
+  const closeConfirmTabulateModal = () => setConfirmTabulate(false)
+
   const onPressPlaceholder = () => {
     // eslint-disable-next-line no-console
+    closeConfirmTabulateModal()
     console.log('dismiss screen')
   }
 
@@ -15,35 +23,37 @@ const ScanWarningScreen: React.FC = () => {
     <CenteredScreen infoBar={false}>
       <PlaceholderGraphic />
       <CenteredLargeProse>
-        <h1>
-          Overvote Warning
-          {/* Undervote Warning */}
-          {/* Overvote and Undervote Warning */}
-          {/* Blank Ballot Warning */}
-        </h1>
-        <p>
-          Contests:{' '}
-          <Text as="span" noWrap>
-            “Mayor”,
-          </Text>{' '}
-          <Text as="span" noWrap>
-            “Dog Catcher”,
-          </Text>{' '}
-          <Text as="span" noWrap>
-            “Board of Supervisors”
-          </Text>
-        </p>
-        <p>
-          <Button primary onPress={onPressPlaceholder}>
-            Tabulate Ballot
-          </Button>{' '}
-          — or —{' '}
-          <Button primary onPress={onPressPlaceholder}>
-            Eject Ballot
-          </Button>
-        </p>
+        <h1>Overvote Warning</h1>
+        <p>Remove the ballot, fix the issue, then scan again.</p>
         <Text italic>Ask a poll worker if you need assistance.</Text>
       </CenteredLargeProse>
+      <Absolute bottom left right>
+        <Bar style={{ justifyContent: 'flex-end' }}>
+          <div>
+            Optionally, this ballot can be tabulated as is:{' '}
+            <Button onPress={openConfirmTabulateModal}>Tabulate Ballot</Button>
+          </div>
+        </Bar>
+      </Absolute>
+      {confirmTabulate && (
+        <Modal
+          content={
+            <Prose textCenter>
+              <h1>Tabulate Ballot with Errors?</h1>
+              <p>Contests with errors will not be counted.</p>
+            </Prose>
+          }
+          actions={
+            <React.Fragment>
+              <Button danger onPress={onPressPlaceholder}>
+                Yes, Tabulate Ballot
+              </Button>
+              <Button onPress={closeConfirmTabulateModal}>Cancel</Button>
+            </React.Fragment>
+          }
+          onOverlayClick={closeConfirmTabulateModal}
+        />
+      )}
     </CenteredScreen>
   )
 }
