3352c368b53c01244abc9d09b2a0ef58bf93a7ea
Beau Smith <beau@beausmith.com>
2021-05-17 15:10:25 -0700

    feat(ps): Open/Close Polls confirm modals

--- apps/precinct-scanner/src/screens/PollWorkerScreen.tsx
+++ apps/precinct-scanner/src/screens/PollWorkerScreen.tsx
@@ -1,18 +1,20 @@
 /* istanbul ignore file */
-import React from 'react'
+import React, { useState } from 'react'
 
 import { Precinct, ElectionDefinition } from '@votingworks/types'
 import { Button, Prose } from '@votingworks/ui'
 import { CenteredScreen } from '../components/Layout'
 import { Absolute } from '../components/Absolute'
 import { Bar } from '../components/Bar'
+import Modal from '../components/Modal'
 
 interface Props {
   appPrecinctId: string
   ballotsScannedCount: number
   electionDefinition: ElectionDefinition
   isPollsOpen: boolean
-  togglePollsOpen: () => void
+  openPolls: () => void
+  closePolls: () => void
 }
 
 const PollWorkerScreen: React.FC<Props> = ({
@@ -20,23 +22,44 @@ const PollWorkerScreen: React.FC<Props> = ({
   ballotsScannedCount,
   electionDefinition,
   isPollsOpen,
-  togglePollsOpen,
+  openPolls,
+  closePolls,
 }) => {
   const { election } = electionDefinition
   const precinct = election.precincts.find(
     (p) => p.id === appPrecinctId
   ) as Precinct
 
+  const [confirmOpenPolls, setConfirmOpenPolls] = useState(false)
+  const openConfirmOpenPollsModal = () => setConfirmOpenPolls(true)
+  const closeConfirmOpenPollsModal = () => setConfirmOpenPolls(false)
+  const openPollsAndSaveZeroReport = () => {
+    openPolls()
+    closeConfirmOpenPollsModal()
+  }
+
+  const [confirmClosePolls, setConfirmClosePolls] = useState(false)
+  const openConfirmClosePollsModal = () => setConfirmClosePolls(true)
+  const closeConfirmClosePollsModal = () => setConfirmClosePolls(false)
+  const closePollsAndSaveTabulationReport = () => {
+    closePolls()
+    closeConfirmClosePollsModal()
+  }
+
   return (
     <CenteredScreen infoBarMode="pollworker">
       <Prose textCenter>
         <h1>Poll Worker Actions</h1>
         <p>
-          <Button large onPress={togglePollsOpen}>
-            {isPollsOpen
-              ? `Close Polls for ${precinct.name}`
-              : `Open Polls for ${precinct.name}`}
-          </Button>
+          {isPollsOpen ? (
+            <Button large onPress={openConfirmClosePollsModal}>
+              Close Polls for {precinct.name}
+            </Button>
+          ) : (
+            <Button large onPress={openConfirmOpenPollsModal}>
+              Open Polls for {precinct.name}
+            </Button>
+          )}
         </p>
       </Prose>
       <Absolute top left>
@@ -46,6 +69,48 @@ const PollWorkerScreen: React.FC<Props> = ({
           </div>
         </Bar>
       </Absolute>
+      {confirmOpenPolls && (
+        <Modal
+          content={
+            <Prose>
+              <h1>Open Polls and Save Zero Report</h1>
+              <p>
+                Zero Report will be saved to the current poll worker card.
+                Insert this card into VxMark to print the report.
+              </p>
+            </Prose>
+          }
+          actions={
+            <React.Fragment>
+              <Button onPress={openPollsAndSaveZeroReport}>
+                Save Zero Report and Open Polls
+              </Button>
+              <Button onPress={closeConfirmOpenPollsModal}>Cancel</Button>
+            </React.Fragment>
+          }
+        />
+      )}
+      {confirmClosePolls && (
+        <Modal
+          content={
+            <Prose>
+              <h1>Close Polls and Save Tabulation Report</h1>
+              <p>
+                Tabulation Report will be saved to the current poll worker card.
+                Insert this card into VxMark to print the report.
+              </p>
+            </Prose>
+          }
+          actions={
+            <React.Fragment>
+              <Button onPress={closePollsAndSaveTabulationReport}>
+                Save Tabulation Report and Close Polls
+              </Button>
+              <Button onPress={closeConfirmClosePollsModal}>Cancel</Button>
+            </React.Fragment>
+          }
+        />
+      )}
     </CenteredScreen>
   )
 }
