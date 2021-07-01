36bf90cba952c87723ff16da33bac0c8d6931665
Beau Smith <beau@beausmith.com>
2021-07-01 14:48:47 -0700

    feat(bmd): Use "test" instead of "it"

--- libs/ui/src/ButonBar.test.tsx
+++ libs/ui/src/ButonBar.test.tsx
@@ -3,7 +3,7 @@ import { render } from '@testing-library/react'
 
 import { ButtonBar } from './ButtonBar'
 
-it('renders ButtonBar', () => {
+test('renders ButtonBar', () => {
   const { container } = render(<ButtonBar>foo</ButtonBar>)
   expect(container.firstChild).toMatchSnapshot()
 })
