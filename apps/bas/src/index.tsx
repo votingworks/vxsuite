399436198c0106b0000b6d99abcfd897b6c5e004
Brian Donovan <1938+eventualbuddha@users.noreply.github.com>
2021-07-15 07:35:08 -0700

    refactor(bas): remove explicit `JSON.parse` calls
    Instead, use `zod` to parse the JSON into validated objects. This also involved fixing the types of various things to allow them to be optional rather than using an empty string.
    
    Additionally, `App` now takes a `card` prop that will make it easier to render for demo purposes.

--- apps/bas/src/index.tsx
+++ apps/bas/src/index.tsx
@@ -1,10 +1,14 @@
+import { WebServiceCard } from '@votingworks/utils'
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 
 import * as serviceWorker from './serviceWorker'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(
+  <App card={new WebServiceCard()} />,
+  document.getElementById('root')
+)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
