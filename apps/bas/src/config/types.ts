efbcd4954b1396a4a6116e61493090783808cd06
Brian Donovan <1938+eventualbuddha@users.noreply.github.com>
2021-07-15 07:35:08 -0700

    refactor(bas): remove explicit `JSON.parse` calls
    Instead, use `zod` to parse the JSON into validated objects. This also involved fixing the types of various things to allow them to be optional rather than using an empty string.
    
    Additionally, `App` now takes a `card` prop that will make it easier to render for demo purposes.

--- apps/bas/src/config/types.ts
+++ apps/bas/src/config/types.ts
@@ -2,14 +2,3 @@
 export type EventTargetFunction = (event: React.FormEvent<EventTarget>) => void
 export type ButtonEvent = React.MouseEvent<HTMLButtonElement>
 export type ButtonEventFunction = (event: ButtonEvent) => void
-
-// Election
-export interface CardAbsentAPI {
-  present: false
-}
-export interface CardPresentAPI {
-  present: true
-  shortValue: string
-  longValueExists?: boolean
-}
-export type CardAPI = CardAbsentAPI | CardPresentAPI
