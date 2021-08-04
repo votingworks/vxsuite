2dcf08f520d472d3da69e0a66d9b0f6426175d6b
Brian Donovan <1938+eventualbuddha@users.noreply.github.com>
2021-08-04 13:32:02 -0700

    test(ps): add `PollWorkerScreen` preview

--- apps/precinct-scanner/src/PreviewApp.tsx
+++ apps/precinct-scanner/src/PreviewApp.tsx
@@ -10,6 +10,7 @@ import PreviewDashboard from './PreviewDashboard'
 import * as AdminScreen from './screens/AdminScreen'
 import * as InsertBallotScreen from './screens/InsertBallotScreen'
 import * as PollsClosedScreen from './screens/PollsClosedScreen'
+import * as PollWorkerScreen from './screens/PollWorkerScreen'
 import * as ScanWarningScreen from './screens/ScanWarningScreen'
 import * as SetupCardReaderPage from './screens/SetupCardReaderPage'
 import * as UnconfiguredElectionScreen from './screens/UnconfiguredElectionScreen'
@@ -26,6 +27,7 @@ const PreviewApp: React.FC = () => {
         AdminScreen,
         InsertBallotScreen,
         PollsClosedScreen,
+        PollWorkerScreen,
         ScanWarningScreen,
         SetupCardReaderPage,
         UnconfiguredElectionScreen,
