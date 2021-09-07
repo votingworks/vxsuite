0e96a23f0a80e83fc6624c5f36afb7e0430091b2
Caroline Modic <caroline@voting.works>
2021-09-07 15:31:38 -0700

    feat(ui) add Table to ui"

---
+++ libs/ui/src/Table.tsx
@@ -0,0 +1,42 @@
+import styled from 'styled-components'
+
+interface Props {
+  borderTop?: boolean
+  condensed?: boolean
+}
+
+const borderColor = 'rgb(194, 200, 203)'
+
+export const Table = styled.table<Props>`
+  border-top: ${({ borderTop = false }) =>
+    borderTop ? '1px solid' : undefined};
+  border-color: ${borderColor};
+  width: 100%;
+  border-collapse: collapse;
+  text-align: left;
+  & th,
+  & td {
+    border-bottom: 1px solid ${borderColor};
+    padding: ${({ condensed }) =>
+      condensed ? '0.15rem 0.25rem' : '0.25rem 0.5rem'};
+  }
+  & th {
+    border-top: 1px solid ${borderColor};
+    font-size: 0.75rem;
+  }
+  @media print {
+    border-top: 1px solid ${borderColor};
+  }
+`
+
+interface TableDataProps {
+  narrow?: boolean
+  nowrap?: boolean
+  textAlign?: 'right' | 'left' | 'center'
+}
+
+export const TD = styled.td<TableDataProps>`
+  width: ${({ narrow = false }) => (narrow ? '1%' : undefined)};
+  text-align: ${({ textAlign }) => textAlign};
+  white-space: ${({ nowrap }) => (nowrap ? 'nowrap' : undefined)};
+`
