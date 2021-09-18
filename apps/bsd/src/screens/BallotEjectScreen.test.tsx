76785330afaabe692f9fd635427df0fe769d3e6f
Ben Adida <ben@adida.net>
2021-09-18 14:50:38 -0700

    regression test for blank-side+write-in, should no longer cause blank or unknown adjudication

--- apps/bsd/src/screens/BallotEjectScreen.test.tsx
+++ apps/bsd/src/screens/BallotEjectScreen.test.tsx
@@ -1,4 +1,5 @@
-import { waitFor, fireEvent } from '@testing-library/react'
+import { waitFor, fireEvent, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import fetchMock from 'fetch-mock'
 import React from 'react'
 import { act } from 'react-dom/test-utils'
@@ -542,4 +543,184 @@ test('shows invalid election screen when appropriate', async () => {
 
   fireEvent.click(getByText('Confirm Ballot Removed and Continue Scanning'))
   expect(continueScanning).toHaveBeenCalledWith({ forceAccept: false })
+})
+
+test('does NOT say ballot is blank if one side is blank and the other requires write-in adjudication', async () => {
+  fetchMock.getOnce(
+    '/scan/hmpb/review/next-sheet',
+    typedAs<GetNextReviewSheetResponse>({
+      interpreted: {
+        id: 'mock-sheet-id',
+        front: {
+          image: { url: '/writeinballot/front/url' },
+          interpretation: {
+            type: 'InterpretedHmpbPage',
+            markInfo: {
+              ballotSize: { width: 1, height: 1 },
+              marks: [],
+            },
+            metadata: {
+              ballotStyleId: '1',
+              precinctId: '1',
+              ballotType: BallotType.Standard,
+              electionHash: '',
+              isTestMode: false,
+              locales: { primary: 'en-US' },
+              pageNumber: 1,
+            },
+            adjudicationInfo: {
+              requiresAdjudication: true,
+              allReasonInfos: [{ type: AdjudicationReason.BlankBallot }],
+              enabledReasons: [
+                AdjudicationReason.BlankBallot,
+                AdjudicationReason.WriteIn,
+              ],
+            },
+            votes: {},
+          },
+        },
+        back: {
+          image: { url: '/writeinballot/back/url' },
+          interpretation: {
+            type: 'InterpretedHmpbPage',
+            markInfo: {
+              ballotSize: { width: 1, height: 1 },
+              marks: [],
+            },
+            metadata: {
+              ballotStyleId: '1',
+              precinctId: '1',
+              ballotType: BallotType.Standard,
+              electionHash: '',
+              isTestMode: false,
+              locales: { primary: 'en-US' },
+              pageNumber: 2,
+            },
+            adjudicationInfo: {
+              requiresAdjudication: true,
+              allReasonInfos: [
+                {
+                  type: AdjudicationReason.WriteIn,
+                  contestId: 'county-commissioners',
+                  optionIndex: 0,
+                  optionId: '__write-in-0',
+                },
+              ],
+              enabledReasons: [
+                AdjudicationReason.BlankBallot,
+                AdjudicationReason.WriteIn,
+              ],
+            },
+            votes: {},
+          },
+        },
+      },
+      layouts: {
+        front: {
+          ballotImage: {
+            metadata: {
+              ballotStyleId: '1',
+              precinctId: '1',
+              ballotType: BallotType.Standard,
+              electionHash: '',
+              isTestMode: false,
+              locales: { primary: 'en-US' },
+              pageNumber: 1,
+            },
+            imageData: {
+              width: 20,
+              height: 20,
+            },
+          },
+          contests: [],
+        },
+        back: {
+          ballotImage: {
+            metadata: {
+              ballotStyleId: '1',
+              precinctId: '1',
+              ballotType: BallotType.Standard,
+              electionHash: '',
+              isTestMode: false,
+              locales: { primary: 'en-US' },
+              pageNumber: 2,
+            },
+            imageData: {
+              width: 20,
+              height: 20,
+            },
+          },
+          contests: [
+            {
+              bounds: { x: 0, y: 0, width: 10, height: 10 },
+              corners: [
+                { x: 0, y: 0 },
+                { x: 1, y: 1 },
+                { x: 2, y: 2 },
+                { x: 3, y: 3 },
+              ],
+              options: [
+                {
+                  bounds: { x: 0, y: 0, width: 10, height: 10 },
+                  target: {
+                    bounds: { x: 0, y: 0, width: 10, height: 10 },
+                    inner: { x: 0, y: 0, width: 10, height: 10 },
+                  },
+                },
+              ],
+            },
+          ],
+        },
+      },
+      definitions: {
+        front: {
+          contestIds: [],
+        },
+        back: {
+          contestIds: ['county-commissioners'],
+        },
+      },
+    })
+  )
+
+  const continueScanning = jest.fn()
+
+  renderInAppContext(
+    <BallotEjectScreen continueScanning={continueScanning} isTestMode />
+  )
+
+  await act(async () => {
+    await waitFor(() => fetchMock.called)
+  })
+
+  expect(screen.queryByText('Blank Ballot')).toBeNull()
+  expect(screen.queryByText('Unknown Reason')).toBeNull()
+  screen.getByText('Write-In')
+
+  userEvent.type(
+    screen.getByTestId(`write-in-input-__write-in-0`),
+    'Lizard People'
+  )
+
+  // doubly nested acts() because there's setIsSaving(true) and then (false).
+  await act(async () => {
+    await act(async () => {
+      userEvent.click(screen.getByText('Save & Continue Scanning'))
+    })
+  })
+
+  expect(continueScanning).toHaveBeenCalledWith({
+    forceAccept: true,
+    frontMarkAdjudications: [],
+    backMarkAdjudications: [
+      {
+        contestId: 'county-commissioners',
+        isMarked: true,
+        name: 'Lizard People',
+        optionId: '__write-in-0',
+        type: 'WriteIn',
+      },
+    ],
+  })
+  continueScanning.mockClear()
 })
