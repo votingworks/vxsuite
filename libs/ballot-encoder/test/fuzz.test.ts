8d0791bdd65ae28c29f06b598c4394147fe8c1a2
Brian Donovan <1938+eventualbuddha@users.noreply.github.com>
2021-10-09 14:42:56 -0700

    test(ballot-encoder): use `fast-check`
    This replaces my homegrown random fuzz tests with ones using `fast-check`. It's much more robust and actually did discover a bug: `writeUint` was doing bounds a check by bit-shifting, but this produced the wrong result when shifting by 31 (`1 << 31` is a negative number in JS because we're working with signed values).

--- libs/ballot-encoder/test/fuzz.test.ts
+++ libs/ballot-encoder/test/fuzz.test.ts
@@ -1,355 +1,66 @@
-/**
- * This test file is a bit different from the others. It does fuzz testing of
- * `BitReader` and `BitWriter` by ensuring that a random sequence of
- * corresponding write and read actions yields the appropriate values.
- */
+import fc from 'fast-check'
+import { WriteInEncoding } from '../src'
+import { BitReader, BitWriter, toUint8 } from '../src/bits'
+import { anyWritable, doReads, doWrites, writeInChar } from './utils'
 
-/* @typescript-eslint/no-explicit-any */
+test('read/write booleans', () => {
+  fc.assert(
+    fc.property(fc.array(fc.boolean()), (values) => {
+      const writer = new BitWriter()
+      for (const value of values) {
+        writer.writeBoolean(value)
+      }
+      const reader = new BitReader(writer.toUint8Array())
+      for (const value of values) {
+        expect(reader.readBoolean()).toEqual(value)
+      }
+    })
+  )
+})
 
-import { Random } from 'random-js'
-import { inspect } from 'util'
-import {
-  BitReader,
-  BitWriter,
-  CustomEncoding,
-  Uint8Index,
-  Uint8Size,
-} from '../src/bits'
-import { SubType } from '../src/types'
+test('read/write bytes', () => {
+  fc.assert(
+    fc.property(fc.uint8Array(), (values) => {
+      const writer = new BitWriter()
+      writer.writeUint8(...[...values].map(toUint8))
+      const reader = new BitReader(writer.toUint8Array())
+      for (const value of values) {
+        expect(reader.readUint8()).toEqual(value)
+      }
+    })
+  )
+})
 
-// eslint-disable-next-line @typescript-eslint/ban-types
-type BitWriterMethods = SubType<BitWriter, Function>
-interface BitWriterAction<
-  M extends keyof BitWriterMethods = keyof BitWriterMethods
-> {
-  method: M
-  args: Parameters<BitWriterMethods[M]>
-  returnValue?: ReturnType<BitWriterMethods[M]>
-}
+test('read/write strings', () => {
+  fc.assert(
+    fc.property(fc.string(), (value) => {
+      const writer = new BitWriter()
+      writer.writeString(value)
+      const reader = new BitReader(writer.toUint8Array())
+      expect(reader.readString()).toEqual(value)
+    })
+  )
+})
 
-// eslint-disable-next-line @typescript-eslint/ban-types
-type BitReaderMethods = SubType<BitReader, Function>
-interface BitReaderAction<
-  M extends keyof BitReaderMethods = keyof BitReaderMethods
-> {
-  method: M
-  args: Parameters<BitReaderMethods[M]>
-  returnValue?: ReturnType<BitReaderMethods[M]>
-}
+test('read/write write-ins', () => {
+  fc.assert(
+    fc.property(fc.stringOf(writeInChar), (writeIn) => {
+      const writer = new BitWriter()
+      writer.writeString(writeIn, { encoding: WriteInEncoding })
+      const reader = new BitReader(writer.toUint8Array())
+      expect(reader.readString({ encoding: WriteInEncoding })).toEqual(writeIn)
+    })
+  )
+})
 
-type Action<
-  C extends BitWriter | BitReader = BitWriter | BitReader
-> = C extends BitWriter ? BitWriterAction : BitReaderAction
+test('read/write various values', () => {
+  fc.assert(
+    fc.property(fc.array(anyWritable), (writables) => {
+      const writer = new BitWriter()
+      doWrites(writer, writables)
 
-interface PerformedAction<
-  C extends BitWriter | BitReader = BitWriter | BitReader
-> {
-  receiver: C
-  action: Action<C>
-}
-
-const random = new Random()
-
-type ActionsPair = [BitWriterAction[], BitReaderAction[]]
-
-/**
- * Test cases are a pair of lists for write and read actions that should match
- * up together. The idea is to generate random values that will better exercise
- * the range of values and order of operations possible than hardcoded tests can.
- */
-const testCaseFactories = {
-  'vararg booleans': (): ActionsPair => {
-    const argCount = random.integer(0, 10)
-    const args = Array.from<boolean>({ length: argCount }).map(() =>
-      random.bool()
-    )
-
-    return [
-      [{ method: 'writeBoolean', args }],
-      args.map((arg) => ({
-        method: 'readBoolean',
-        args: [],
-        returnValue: arg,
-      })),
-    ]
-  },
-
-  'length-prefixed utf-8 strings': (): ActionsPair => {
-    const length = random.integer(0, 50)
-    const maxLength = random.integer(length, length * 2)
-    const string = random.string(length)
-
-    return [
-      [{ method: 'writeString', args: [string, { maxLength }] }],
-      // This `as any` is here because `readString` is overloaded and TS cannot
-      // infer the parameters correctly in this instance.
-      [
-        {
-          method: 'readString',
-          // eslint-disable-next-line @typescript-eslint/no-explicit-any
-          args: [{ maxLength }] as any,
-          returnValue: string,
-        },
-      ],
-    ]
-  },
-
-  'length-prefixed strings with custom encoding': (): ActionsPair => {
-    const chars = Array.from(
-      new Set(random.string(random.integer(1, 20))).values()
-    ).join('')
-    const string = random.string(random.integer(0, 50), chars)
-    const encoding = new CustomEncoding(chars)
-
-    return [
-      [{ method: 'writeString', args: [string, { encoding }] }],
-      [{ method: 'readString', args: [{ encoding }], returnValue: string }],
-    ]
-  },
-
-  'dynamic size uints': (): ActionsPair => {
-    const useMax = random.bool()
-
-    if (useMax) {
-      const max = random.integer(0, 1 << 30)
-      const number = random.integer(0, max)
-
-      return [
-        [
-          {
-            method: 'writeUint',
-            args: ([number, { max }] as unknown) as Parameters<
-              BitWriter['writeUint']
-            >,
-          },
-        ],
-        [
-          {
-            method: 'readUint',
-            args: ([{ max }] as unknown) as Parameters<BitReader['readUint']>,
-            returnValue: number,
-          },
-        ],
-      ]
-    }
-
-    const size = random.integer(0, 30)
-    const number = random.integer(0, 1 << (size - 1))
-
-    return [
-      [
-        {
-          method: 'writeUint',
-          args: [number, { size }],
-        },
-      ],
-      [
-        {
-          method: 'readUint',
-          args: [{ size }],
-          returnValue: number,
-        },
-      ],
-    ]
-  },
-
-  'vararg uint1s': (): ActionsPair => {
-    const argCount = random.integer(0, 10)
-    const args = Array.from<0 | 1>({ length: argCount })
-      .fill(0)
-      .map(() => random.integer(0, 1))
-
-    return [
-      [
-        {
-          method: 'writeUint1',
-          args: args as Parameters<BitWriter['writeUint1']>,
-        },
-      ],
-      args.map((arg) => ({
-        method: 'readUint1',
-        args: [],
-        returnValue: arg,
-      })),
-    ]
-  },
-
-  'vararg uint8s': (): ActionsPair => {
-    const argCount = random.integer(0, 10)
-    const args = Array.from<Uint8Index>({ length: argCount })
-      .fill(0)
-      .map(() => random.integer(0, Uint8Size - 1))
-
-    return [
-      [
-        {
-          method: 'writeUint8',
-          args: args as Parameters<BitWriter['writeUint8']>,
-        },
-      ],
-      args.map((arg) => ({
-        method: 'readUint8',
-        args: [],
-        returnValue: arg,
-      })),
-    ]
-  },
-}
-
-const codeColor = '\x1b[38;5;202m'
-const dimColor = '\x1b[38;5;240m'
-const resetColor = '\x1b[0m'
-
-/**
- * Formats `action` in pseudocode calling syntax.
- *
- * @example
- *
- * formatAction({ method: 'push', args: [1], receiver: array })               // Array#push(1)
- * formatAction({ method: 'pop', args: [], receiver: array, returnValue: 1 }) // Array#pop() → 1
- */
-function formatAction(
-  { action, receiver }: PerformedAction,
-  includeReturnValue = false
-): string {
-  return `${codeColor}${receiver.constructor.name}#${
-    action.method
-  }(${resetColor}${(action.args as string[])
-    .map((arg) => inspect(arg, { colors: true }))
-    .join(', ')}${codeColor})${resetColor}${
-    includeReturnValue && typeof action.returnValue !== 'undefined'
-      ? ` → ${inspect(action.returnValue, { colors: true })}`
-      : ''
-  }`
-}
-
-/**
- * Formats `actions` in a list, one per line.
- *
- * @example
- *
- * // Formats lists.
- * // - Array#push(1)
- * // - Array#pop() → 1
- * formatActions([
- *   { method: 'push', args: [1], receiver: array },
- *   { method: 'pop', args: [], receiver: array, returnValue: 1 },
- * ])
- *
- * // Has a basic "null" state.
- * // - n/a
- * formatActions([])
- */
-function formatActions(actions: PerformedAction[]): string {
-  if (actions.length === 0) {
-    return `- ${dimColor}n/a${resetColor}`
-  }
-
-  return actions.map((action) => `- ${formatAction(action, true)}`).join('\n')
-}
-
-/**
- * Performs `action` with `instance` and logs it in `performedActions`. This
- * amounts to calling a method on `instance` and optionally checking its return
- * value. If either the method call fails or the return value is not the
- * expected value, an error will be thrown with the log of performed actions
- * that led to this failure.
- */
-function performAction<C extends BitWriter | BitReader>(
-  action: Action<C>,
-  receiver: C,
-  log: PerformedAction<C>[]
-): void {
-  let actualValue: typeof action['returnValue']
-
-  try {
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    actualValue = (receiver as any)[action.method](...action.args)
-  } catch (error) {
-    error.message = `After performing these actions:\n${formatActions(
-      log
-    )}\n\nAction ${formatAction({ receiver, action })}${
-      typeof action.returnValue === 'undefined'
-        ? ''
-        : ` (expected return ${inspect(action.returnValue, { colors: true })})`
-    } failed with error message: ${error.message}`
-    throw error
-  }
-
-  if (typeof action.returnValue !== 'undefined') {
-    try {
-      expect(actualValue).toEqual(action.returnValue)
-    } catch (error) {
-      error.message = `After performing these actions:\n${formatActions(
-        log
-      )}\n\nAction ${formatAction({
-        receiver,
-        action,
-      })} did not return expected value.\n\n${error.message}`
-      throw error
-    }
-  }
-
-  log.push({ receiver, action })
-}
-/**
- * Runs a sequence of write operations on `BitWriter`, loads the resulting
- * buffer into a `BitReader`, and then runs a series of read operations checking
- * the return values are as expected.
- *
- * @example
- *
- * doWritesAndReads([
- *   [{ method: 'writeBoolean', args: [true] }],
- *   [{ method: 'readBoolean', args: [], returnValue: true }]
- * ])
- */
-function doWritesAndReads([writes, reads]: ActionsPair): void {
-  const performedActions: PerformedAction[] = []
-  const writer = new BitWriter()
-
-  for (const write of writes) {
-    performAction(write, writer, performedActions)
-  }
-
-  const reader = new BitReader(writer.toUint8Array())
-
-  for (const read of reads) {
-    performAction(read, reader, performedActions)
-  }
-}
-
-const testCaseNames = Object.getOwnPropertyNames(
-  testCaseFactories
-) as (keyof typeof testCaseFactories)[]
-
-for (const testCase of testCaseNames) {
-  test(testCase, () => {
-    for (let i = 0; i < 1000; i += 1) {
-      doWritesAndReads(testCaseFactories[testCase]())
-    }
-  })
-}
-
-/**
- * Bring together a bunch of test cases to be run together on the same
- * `BitWriter` and `BitReader`, to test interactions between them.
- */
-test('all together', () => {
-  for (let i = 0; i < 100; i += 1) {
-    const factories = Array.from({ length: 20 })
-      .fill(undefined)
-      .map(() => testCaseFactories[random.pick(testCaseNames)])
-
-    const [writes, reads]: ActionsPair = [[], []]
-
-    for (const factory of factories) {
-      const [w, r] = factory()
-
-      writes.push(...w)
-      reads.push(...r)
-    }
-
-    doWritesAndReads([writes, reads])
-  }
+      const reader = new BitReader(writer.toUint8Array())
+      doReads(reader, writables)
+    })
+  )
 })
