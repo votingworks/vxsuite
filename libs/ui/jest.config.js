471a063c95ae7e5ae078fc1c3d9354858b3a7bb6
Brian Donovan <1938+eventualbuddha@users.noreply.github.com>
2021-10-13 10:13:19 -0700

    test(ui): collect coverage from already-covered files
    Refs #598

--- libs/ui/jest.config.js
+++ libs/ui/jest.config.js
@@ -6,9 +6,7 @@ module.exports = {
   collectCoverageFrom: [ 
     ...shared.collectCoverageFrom,
     '!src/ContestTally.tsx',
-    '!src/LogoMark.tsx',
     '!src/PrecinctScannerPollsReport.tsx',
     '!src/PrecinctScannerTallyReport.tsx',
-    '!src/TallyReport.tsx',
    ]
 }
