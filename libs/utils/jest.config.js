ce6273e7635524ebf0b843fb6ee0cdf54518ead8
Brian Donovan <1938+eventualbuddha@users.noreply.github.com>
2021-10-13 12:02:08 -0700

    test(utils): enable global coverage collection

--- libs/utils/jest.config.js
+++ libs/utils/jest.config.js
@@ -4,9 +4,4 @@ module.exports = {
   ...shared,
   testEnvironment: 'jsdom',
   setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
-  collectCoverageFrom: [
-    ...shared.collectCoverageFrom,
-    // TODO: remove these by bringing their coverage to 100% (https://github.com/votingworks/vxsuite/issues/995)
-    '!src/cardTallies.ts',
-   ]
 }
