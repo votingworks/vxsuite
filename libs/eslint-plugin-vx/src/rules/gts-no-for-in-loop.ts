f5bd9ef6f14396a03bd7351343a45dc760aa9903
Jonah Kagan <jonah@voting.works>
2021-10-19 08:54:02 -0700

    Remove hasOwnProperty fixing

--- libs/eslint-plugin-vx/src/rules/gts-no-for-in-loop.ts
+++ libs/eslint-plugin-vx/src/rules/gts-no-for-in-loop.ts
@@ -26,99 +26,6 @@ export default createRule({
 
   create(context) {
     const sourceCode = context.getSourceCode()
-
-    function areEqualIdentifiers(node1?: TSESTree.Node, node2?: TSESTree.Node) {
-      return (
-        node1?.type === AST_NODE_TYPES.Identifier &&
-        node2?.type === AST_NODE_TYPES.Identifier &&
-        node1.name === node2.name
-      )
-    }
-
-    function removeHasOwnPropertyGuard(
-      node: TSESTree.ForInStatement,
-      fixer: RuleFixer
-    ) {
-      const { body } = node
-      const guard =
-        body.type === AST_NODE_TYPES.BlockStatement
-          ? body.body[0]
-          : body.type === AST_NODE_TYPES.IfStatement
-          ? body
-          : null
-
-      // for (const k in o) if (o.hasOwnProperty(k)) ...
-      if (
-        guard &&
-        guard.type === AST_NODE_TYPES.IfStatement &&
-        guard.test.type === AST_NODE_TYPES.CallExpression &&
-        guard.test.callee.type === AST_NODE_TYPES.MemberExpression &&
-        guard.test.callee.property.type === AST_NODE_TYPES.Identifier &&
-        guard.test.callee.property.name === 'hasOwnProperty' &&
-        areEqualIdentifiers(guard.test.callee.object, node.right) &&
-        guard.test.arguments.length === 1 &&
-        node.left.type === AST_NODE_TYPES.VariableDeclaration &&
-        node.left.declarations.length === 1 &&
-        areEqualIdentifiers(
-          guard.test.arguments[0],
-          node.left.declarations[0].id
-        ) &&
-        !guard.alternate
-      ) {
-        const consequent =
-          guard.consequent.type === AST_NODE_TYPES.BlockStatement
-            ? guard.consequent.body
-            : [guard.consequent]
-        return [
-          fixer.replaceText(
-            guard,
-            consequent
-              .map((statement) => sourceCode.getText(statement))
-              .join('\n')
-          ),
-        ]
-      }
-
-      // for (const k in o) if (!o.hasOwnProperty(k)) continue
-      if (
-        guard &&
-        guard.type === AST_NODE_TYPES.IfStatement &&
-        guard.test.type === AST_NODE_TYPES.UnaryExpression &&
-        guard.test.operator === '!' &&
-        guard.test.argument.type === AST_NODE_TYPES.CallExpression &&
-        guard.test.argument.callee.type === AST_NODE_TYPES.MemberExpression &&
-        guard.test.argument.callee.property.type ===
-          AST_NODE_TYPES.Identifier &&
-        guard.test.argument.callee.property.name === 'hasOwnProperty' &&
-        areEqualIdentifiers(guard.test.argument.callee.object, node.right) &&
-        guard.test.argument.arguments.length === 1 &&
-        node.left.type === AST_NODE_TYPES.VariableDeclaration &&
-        node.left.declarations.length === 1 &&
-        areEqualIdentifiers(
-          guard.test.argument.arguments[0],
-          node.left.declarations[0].id
-        ) &&
-        (guard.consequent.type === AST_NODE_TYPES.BlockStatement
-          ? guard.consequent.body.length === 1 &&
-            guard.consequent.body[0].type === AST_NODE_TYPES.ContinueStatement
-          : guard.consequent.type === AST_NODE_TYPES.ContinueStatement)
-      ) {
-        const newBody = (guard.alternate ? [guard.alternate] : []).concat(
-          node.body.type === AST_NODE_TYPES.BlockStatement
-            ? node.body.body.slice(1)
-            : node.body
-        )
-        return [
-          fixer.replaceText(
-            node.body,
-            newBody.map((statement) => sourceCode.getText(statement)).join('\n')
-          ),
-        ]
-      }
-
-      return []
-    }
-
     return {
       ForInStatement: (node: TSESTree.ForInStatement) => {
         context.report({
@@ -137,7 +44,6 @@ export default createRule({
                 'of Object.keys('
               ),
               fixer.insertTextAfter(node.right, ')'),
-              ...removeHasOwnPropertyGuard(node, fixer),
             ]
           },
         })
--- libs/eslint-plugin-vx/tests/rules/gts-no-for-in-loop.test.ts
+++ libs/eslint-plugin-vx/tests/rules/gts-no-for-in-loop.test.ts
@@ -13,45 +13,15 @@ const ruleTester = new ESLintUtils.RuleTester({
 
 ruleTester.run('gts-no-for-in-loop', rule, {
   valid: [
-    // { code: 'for (const k of Object.keys({})) {}' },
-    // { code: 'for (const [k, v] of Object.entries({})) {}' },
+    { code: 'for (const k of Object.keys({})) {}' },
+    { code: 'for (const [k, v] of Object.entries({})) {}' },
   ],
   invalid: [
-    // {
-    //   code: 'for (const k in {}) {}',
-    //   output: 'for (const k of Object.keys({})) {}',
-    //   errors: [{ messageId: 'noForInLoop', line: 1 }],
-    // },
-    // {
-    //   code: `
-    //     for (const k in o) {
-    //       if (Object.prototype.hasOwnProperty.call(o, k)) {
-    //         o[k] = 1
-    //       }
-    //     }
-    //   `,
-    //   output: `
-    //     for (const k of Object.keys(o)) {
-    //       o[k] = 1
-    //     }
-    //   `,
-    //   errors: [{ messageId: 'noForInLoop', line: 2 }],
-    // },
-    // {
-    //   code: `
-    //     for (const k in o) {
-    //       if (!Object.prototype.hasOwnProperty.call(o, k))
-    //         continue
-    //       o[k] = 1
-    //     }
-    //   `,
-    //   output: `
-    //     for (const k of Object.keys(o)) {
-    //       o[k] = 1
-    //     }
-    //   `,
-    //   errors: [{ messageId: 'noForInLoop', line: 2 }],
-    // },
+    {
+      code: 'for (const k in {}) {}',
+      output: 'for (const k of Object.keys({})) {}',
+      errors: [{ messageId: 'noForInLoop', line: 1 }],
+    },
     {
       code: `
         for (const k in o) {
@@ -62,49 +32,10 @@ ruleTester.run('gts-no-for-in-loop', rule, {
       `,
       output: `
         for (const k of Object.keys(o)) {
-          o[k] = 1
-        }
-      `,
-      errors: [{ messageId: 'noForInLoop', line: 2 }],
-    },
-    {
-      code: `
-        for (const k in o)
-          if (o.hasOwnProperty(k))
+          if (o.hasOwnProperty(k)) {
             o[k] = 1
-      `,
-      output: `
-        for (const k of Object.keys(o))
-          o[k] = 1
-      `,
-      errors: [{ messageId: 'noForInLoop', line: 2 }],
-    },
-    {
-      code: `
-        for (const k in o) {
-          if (!o.hasOwnProperty(k))
-            continue
-          o[k] = 1
+          }
         }
-      `,
-      output: `
-        for (const k of Object.keys(o)) {
-          o[k] = 1
-        }
-      `,
-      errors: [{ messageId: 'noForInLoop', line: 2 }],
-    },
-    {
-      code: `
-        for (const k in o)
-          if (!o.hasOwnProperty(k))
-            continue
-          else
-            o[k] = 1
-      `,
-      output: `
-        for (const k of Object.keys(o))
-          o[k] = 1
       `,
       errors: [{ messageId: 'noForInLoop', line: 2 }],
     },
