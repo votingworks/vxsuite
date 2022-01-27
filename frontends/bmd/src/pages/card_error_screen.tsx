a5531a88bf336310bf42c1690032f402e3a4e176
Jonah Kagan <jonah@voting.works>
2022-01-27 13:54:37 -0800

    feat(bmd/precinct-scanner): Add rotate card image to card backwards screen

--- frontends/bmd/src/pages/card_error_screen.tsx
+++ frontends/bmd/src/pages/card_error_screen.tsx
@@ -1,10 +1,16 @@
 import React, { useEffect } from 'react';
+import styled from 'styled-components';
 
 import { Main, MainChild } from '@votingworks/ui';
 
 import { Prose } from '../components/prose';
 import { Screen } from '../components/screen';
 import { triggerAudioFocus } from '../utils/trigger_audio_focus';
+
+const RotateCardImage = styled.img`
+  margin: 0 auto 1rem;
+  width: 300px;
+`;
 
 interface Props {
   useEffectToggleLargeDisplay: () => void;
@@ -21,6 +27,11 @@ export function CardErrorScreen({
     <Screen white>
       <Main>
         <MainChild center>
+          <RotateCardImage
+            aria-hidden
+            src="/images/rotate-card.svg"
+            alt="rotate-card"
+          />
           <Prose textCenter id="audiofocus">
             <h1>Card is backwards</h1>
             <p>Remove the card, turn it around, and insert it again.</p>
