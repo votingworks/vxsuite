834a3f17694159907b91a0f5074b43bb23018d69
Jonah Kagan <jonah@voting.works>
2022-02-03 11:31:02 -0800

    feat(bsd): Set current date and time (#1398)

--- frontends/bsd/src/screens/admin_actions_screen.test.tsx
+++ frontends/bsd/src/screens/admin_actions_screen.test.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
-import { render, waitFor } from '@testing-library/react';
+import { render, screen, waitFor, within } from '@testing-library/react';
 import { createMemoryHistory } from 'history';
 import { Router } from 'react-router-dom';
 import { act } from 'react-dom/test-utils';
+import MockDate from 'mockdate';
 import { electionSampleDefinition as testElectionDefinition } from '@votingworks/fixtures';
+import { fakeKiosk } from '@votingworks/test-utils';
+import userEvent from '@testing-library/user-event';
 import { AdminActionsScreen } from './admin_actions_screen';
 
 test('clicking "Export Backup…" shows progress', async () => {
@@ -204,4 +207,52 @@ test('override mark thresholds button shows when there are no overrides', async 
     ).toBe(testCase.expectButtonDisabled);
     unmount();
   }
+});
+
+test('clicking "Update Date and Time…" shows modal to set clock', async () => {
+  MockDate.set('2020-10-31T00:00:00.000Z');
+  window.kiosk = fakeKiosk();
+
+  render(
+    <Router history={createMemoryHistory()}>
+      <AdminActionsScreen
+        hasBatches={false}
+        unconfigureServer={jest.fn()}
+        zeroData={jest.fn()}
+        backup={jest.fn()}
+        isTestMode={false}
+        isTogglingTestMode={false}
+        toggleTestMode={jest.fn()}
+        setMarkThresholdOverrides={jest.fn()}
+        markThresholds={undefined}
+        electionDefinition={testElectionDefinition}
+      />
+    </Router>
+  );
+
+  screen.getByRole('heading', { name: 'Admin Actions' });
+
+  // We just do a simple happy path test here, since the libs/ui/set_clock unit
+  // tests cover full behavior
+  userEvent.click(
+    screen.getByRole('button', { name: 'Update Date and Time…' })
+  );
+
+  // Open modal
+  const modal = screen.getByRole('alertdialog');
+  within(modal).getByText('Sat, Oct 31, 2020, 12:00 AM');
+
+  // Change date
+  const selectYear = screen.getByTestId('selectYear');
+  userEvent.selectOptions(selectYear, '2025');
+
+  // Save date
+  userEvent.click(within(modal).getByRole('button', { name: 'Save' }));
+  await waitFor(() => {
+    expect(window.kiosk?.setClock).toHaveBeenCalledWith({
+      isoDatetime: '2025-10-31T00:00:00.000+00:00',
+      // eslint-disable-next-line vx/gts-identifiers
+      IANAZone: 'UTC',
+    });
+  });
 });
