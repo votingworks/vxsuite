bdd0006af8d2e599c1c6321ee2f1c2f7c22821e3
Jonah Kagan <jonah@voting.works>
2022-03-08 17:04:17 -0800

    follow local conventions

--- frontends/precinct-scanner/src/screens/setup_scanner_screen.tsx
+++ frontends/precinct-scanner/src/screens/setup_scanner_screen.tsx
@@ -1,30 +1,18 @@
-import React, { useEffect } from 'react';
-import { fontSizeTheme, Main, MainChild, Prose, Screen } from '@votingworks/ui';
+import React from 'react';
+import { CenteredScreen, CenteredLargeProse } from '../components/layout';
 
-function doNothing() {
-  // do nothing
-}
-
-interface Props {
-  useEffectToggleLargeDisplay?: () => void;
-}
-
-export function SetupScannerScreen({
-  useEffectToggleLargeDisplay = doNothing,
-}: Props): JSX.Element {
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  useEffect(useEffectToggleLargeDisplay, []);
-
+export function SetupScannerScreen(): JSX.Element {
   return (
-    <Screen white>
-      <Main>
-        <MainChild center maxWidth={false}>
-          <Prose textCenter maxWidth={false} theme={fontSizeTheme.large}>
-            <h1>Scanner Not Detected</h1>
-            <p>Please ask a poll worker to connect scanner.</p>
-          </Prose>
-        </MainChild>
-      </Main>
-    </Screen>
+    <CenteredScreen infoBar={false}>
+      <CenteredLargeProse>
+        <h1>Scanner Not Detected</h1>
+        <p>Please ask a poll worker to connect scanner.</p>
+      </CenteredLargeProse>
+    </CenteredScreen>
   );
+}
+
+/* istanbul ignore next */
+export function DefaultPreview(): JSX.Element {
+  return <SetupScannerScreen />;
 }
