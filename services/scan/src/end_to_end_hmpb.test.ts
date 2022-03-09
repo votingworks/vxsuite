a2d450bb629106ebc1b4326955c20bdabb21cf0c
james <james@voting.works>
2022-03-09 17:20:03 -0500

    fix final tests

--- services/scan/src/end_to_end_hmpb.test.ts
+++ services/scan/src/end_to_end_hmpb.test.ts
@@ -79,9 +79,10 @@ test('going through the whole process works', async () => {
     .set('Accept', 'application/json')
     .expect(200, { status: 'ok' });
 
+  // need to turn off test mode after election is loaded
   await request(app)
     .patch('/config/testMode')
-    .send({ testMode: true })
+    .send({ testMode: false })
     .set('Content-Type', 'application/json')
     .set('Accept', 'application/json')
     .expect(200, { status: 'ok' });
@@ -205,6 +206,14 @@ test('failed scan with QR code can be adjudicated and exported', async () => {
     .patch('/config/election')
     .send(asElectionDefinition(election).electionData)
     .set('Content-Type', 'application/octet-stream')
+    .set('Accept', 'application/json')
+    .expect(200, { status: 'ok' });
+
+  // need to turn off test mode after election is loaded
+  await request(app)
+    .patch('/config/testMode')
+    .send({ testMode: false })
+    .set('Content-Type', 'application/json')
     .set('Accept', 'application/json')
     .expect(200, { status: 'ok' });
 
@@ -357,6 +366,14 @@ test('ms-either-neither end-to-end', async () => {
     .patch('/config/election')
     .send(asElectionDefinition(election).electionData)
     .set('Content-Type', 'application/octet-stream')
+    .set('Accept', 'application/json')
+    .expect(200, { status: 'ok' });
+
+  // need to turn off test mode after election is loaded
+  await request(app)
+    .patch('/config/testMode')
+    .send({ testMode: false })
+    .set('Content-Type', 'application/json')
     .set('Accept', 'application/json')
     .expect(200, { status: 'ok' });
 
