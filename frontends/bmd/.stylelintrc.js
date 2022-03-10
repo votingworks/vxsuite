29c92d20550ed1964f52de63519c3fffe6392b42
Tara Vancil <contact@taravancil.com>
2022-03-10 10:19:17 -0600

    fix: Remove stylelint-processor-styled-components from bmd

--- frontends/bmd/.stylelintrc.js
+++ frontends/bmd/.stylelintrc.js
@@ -6,7 +6,7 @@ const parserPlugins = buildOptions().plugins.filter(plugin => plugin !== 'typesc
 // Config for stylelint parsing CSS in Styled Components
 // Note the `--fix` flag doesn't yet work for CSS-in-JS
 module.exports = {
-  processors: [['stylelint-processor-styled-components', { parserPlugins }]],
+  processors: [],
   extends: [
     'stylelint-config-palantir',
     'stylelint-config-prettier',
