943afcd4d12bcfca5198adf9e2c113d8a0e042b3
Tara Vancil <contact@taravancil.com>
2022-03-10 10:47:41 -0600

    Remove unused variables

--- frontends/election-manager/.stylelintrc.js
+++ frontends/election-manager/.stylelintrc.js
@@ -1,7 +1,3 @@
-// Ensure we use all available babel parser plugins, but not the type annotation
-// ones since stylelint handles that itself.
-const { buildOptions } = require('@codemod/parser');
-
 // Config for stylelint parsing CSS in Styled Components
 // Note the `--fix` flag doesn't yet work for CSS-in-JS
 module.exports = {
