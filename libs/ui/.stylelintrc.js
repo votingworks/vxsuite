87eb642c3796349ce06f8695ea5b2213857eb511
Tara Vancil <contact@taravancil.com>
2022-03-10 10:32:07 -0600

    fix: Remove stylelint-processor-styled-components from lib/ui

--- libs/ui/.stylelintrc.js
+++ libs/ui/.stylelintrc.js
@@ -1,14 +1,11 @@
 // Ensure we use all available babel parser plugins, but not the type annotation
 // ones since stylelint handles that itself.
-const { buildOptions } = require('@codemod/parser')
-const parserPlugins = buildOptions().plugins.filter(
-  (plugin) => plugin !== 'typescript' && plugin !== 'flow'
-)
+const { buildOptions } = require('@codemod/parser');
 
 // Config for stylelint parsing CSS in Styled Components
 // Note the `--fix` flag doesn't yet work for CSS-in-JS
 module.exports = {
-  processors: [['stylelint-processor-styled-components', { parserPlugins }]],
+  processors: [],
   extends: [
     'stylelint-config-palantir',
     'stylelint-config-prettier',
@@ -18,4 +15,4 @@ module.exports = {
     'selector-max-id': 1,
     'selector-max-universal': 1,
   },
-}
+};
