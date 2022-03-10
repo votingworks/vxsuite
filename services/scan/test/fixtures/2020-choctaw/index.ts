4e12bc17bec3c657ebab27963fc3d2cc6dbd29f5
Brian Donovan <1938+eventualbuddha@users.noreply.github.com>
2022-03-10 10:34:40 -0800

    test(scan): update fixture election hashes
    Reads the election definitions in such a way that the election hash is correct. This does not sync the ballot PDFs or images yet, so those are likely still out of sync in some cases.

--- services/scan/test/fixtures/2020-choctaw/index.ts
+++ services/scan/test/fixtures/2020-choctaw/index.ts
@@ -1,8 +1,12 @@
-import { parseElection } from '@votingworks/types';
+import { safeParseElectionDefinition } from '@votingworks/types';
+import { readFileSync } from 'fs';
 import { join } from 'path';
-import electionJson from './election.json';
 
-export const election = parseElection(electionJson);
+const electionJson = readFileSync(join(__dirname, 'election.json'), 'utf8');
+export const electionDefinition = safeParseElectionDefinition(
+  electionJson
+).unsafeUnwrap();
+export const { election } = electionDefinition;
 export const ballotPdf = join(__dirname, 'ballot.pdf');
 export const filledInPage1 = join(__dirname, 'filled-in-p1.png');
 export const filledInPage2 = join(__dirname, 'filled-in-p2.png');
