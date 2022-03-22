9ace35231df6c8396f7c51ba2fcabac8fa2d8794
Brian Donovan <1938+eventualbuddha@users.noreply.github.com>
2022-03-22 14:28:21 -0700

    feat(libs): add `res-to-ts` library
    Allows packaging resource files in a way that TypeScript will include with the build. This will be useful for bundling scanning template images for the browser and fixtures for tests.

---
+++ libs/res-to-ts/src/index.ts
@@ -0,0 +1,3 @@
+/* istanbul ignore file - this just re-exports values for the package */
+export { main, Options } from './cli';
+export { Resource, convert } from './convert';
---
+++ libs/res-to-ts/src/mime.test.ts
@@ -0,0 +1,34 @@
+import { getMimeType, isImageMimeType, isTextMimeType } from './mime';
+
+test('standard types', () => {
+  expect(getMimeType('foo.txt')).toBe('text/plain');
+  expect(getMimeType('foo.js')).toBe('application/javascript');
+  expect(getMimeType('foo.json')).toBe('application/json');
+  expect(getMimeType('foo.png')).toBe('image/png');
+  expect(getMimeType('foo.jpg')).toBe('image/jpeg');
+});
+
+test('fallback types', () => {
+  expect(getMimeType('foo.jsonl')).toBe('application/jsonlines');
+  expect(getMimeType('foo.unknown')).toBe('application/octet-stream');
+});
+
+test('image types', () => {
+  expect(isImageMimeType('image/png')).toBe(true);
+  expect(isImageMimeType('image/jpeg')).toBe(true);
+  expect(isImageMimeType('image/gif')).toBe(true);
+  expect(isImageMimeType('image/svg+xml')).toBe(true);
+  expect(isImageMimeType('image/tiff')).toBe(true);
+  expect(isImageMimeType('image/bmp')).toBe(true);
+  expect(isImageMimeType('text/plain')).toBe(false);
+  expect(isImageMimeType('application/octet-stream')).toBe(false);
+});
+
+test('text types', () => {
+  expect(isTextMimeType('text/plain')).toBe(true);
+  expect(isTextMimeType('text/csv')).toBe(true);
+  expect(isTextMimeType('application/json')).toBe(true);
+  expect(isTextMimeType('application/jsonlines')).toBe(true);
+  expect(isTextMimeType('image/png')).toBe(false);
+  expect(isTextMimeType('application/octet-stream')).toBe(false);
+});
