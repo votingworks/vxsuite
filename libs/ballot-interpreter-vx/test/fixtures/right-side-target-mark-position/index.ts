d935a3b33f8485a403feee683a70910a9ed97558
Brian Donovan <1938+eventualbuddha@users.noreply.github.com>
2022-03-31 16:57:45 -0700

    feat(scan): enable scanning right-side targets
    Closes https://github.com/votingworks/vxsuite/issues/1505
    
    Changes `findTargets` to accept a `targetMarkPosition` which determines whether it starts looking offset from the left or the right side of the contest box.
    
    Updates the `layout` CLI command to find the targets and annotate them in the output. It also now takes an option to specify whether to look on the right or left for the targets.
    
    Adds a basic test for scanning a filled-in ballot with the right-side targets.

---
+++ libs/ballot-interpreter-vx/test/fixtures/right-side-target-mark-position/index.ts
@@ -0,0 +1,10 @@
+import { electionMinimalExhaustiveSampleRightSideTargetsDefinition } from '@votingworks/fixtures';
+import { join } from 'path';
+import { Fixture } from '../../fixtures';
+
+export const electionDefinition = electionMinimalExhaustiveSampleRightSideTargetsDefinition;
+export const { election } = electionDefinition;
+export const blankPage1 = new Fixture(join(__dirname, 'ballot-p1.jpeg'));
+export const blankPage2 = new Fixture(join(__dirname, 'ballot-p2.jpeg'));
+export const filledInPage1 = new Fixture(join(__dirname, 'filled-in-p1.jpeg'));
+export const filledInPage2 = new Fixture(join(__dirname, 'filled-in-p2.jpeg'));
