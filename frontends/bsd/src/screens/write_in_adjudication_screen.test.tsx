6fc958a7df6d716b2108aa430d462fa4b65e22ff
Brian Donovan <1938+eventualbuddha@users.noreply.github.com>
2022-04-04 11:16:36 -0700

    test(scan): add regression test for write-in adjudication
    Test covering the bugfix from 5953fbe4f213e6f00fe343cad43dc4d1f915d09f.

--- frontends/bsd/src/screens/write_in_adjudication_screen.test.tsx
+++ frontends/bsd/src/screens/write_in_adjudication_screen.test.tsx
@@ -14,7 +14,12 @@ import {
   WriteInIdSchema,
 } from '@votingworks/types';
 
-import { allContestOptions, assert, typedAs } from '@votingworks/utils';
+import {
+  allContestOptions,
+  assert,
+  groupBy,
+  typedAs,
+} from '@votingworks/utils';
 import React from 'react';
 import { makeAppContext } from '../../test/render_in_app_context';
 import { AppContext } from '../contexts/app_context';
@@ -404,4 +409,102 @@ test('can adjudicate front & back in succession', async () => {
   await waitFor(() => {
     expect(onAdjudicationComplete).toHaveBeenCalledTimes(2);
   });
+});
+
+test('uses the layout embedded definition to crop the ballot image correctly if available', async () => {
+  const [contest] = contestsWithWriteIns.filter(({ seats }) => seats > 1);
+  const options = Array.from(allContestOptions(contest));
+  const optionsByIsWriteIn = groupBy(
+    options,
+    (option) => option.type === 'candidate' && option.isWriteIn
+  );
+  const maximumWriteInOptionIndex = Math.max(
+    ...Array.from(optionsByIsWriteIn.get(true)!).map(
+      ({ optionIndex }) => optionIndex
+    )
+  );
+  const optionsWithReversedWriteIns = [...options].sort((a, b) => {
+    if (a.type !== 'candidate' || b.type !== 'candidate') {
+      return -1;
+    }
+
+    if (a.isWriteIn !== b.isWriteIn) {
+      return -1;
+    }
+
+    return b.optionIndex - a.optionIndex;
+  });
+
+  // Use the first write-in space, which is the last one in the definition list.
+  const writeInOption = optionsWithReversedWriteIns.find(
+    (option) => option.type === 'candidate' && option.isWriteIn
+  )!;
+  const onAdjudicationComplete = jest
+    .fn<
+      ReturnType<onAdjudicationCompleteType>,
+      Parameters<onAdjudicationCompleteType>
+    >()
+    .mockResolvedValue();
+
+  const writeIns: Array<
+    WriteInAdjudicationReasonInfo | UnmarkedWriteInAdjudicationReasonInfo
+  > = [
+    {
+      type: AdjudicationReason.WriteIn,
+      contestId: writeInOption.contestId,
+      optionId: writeInOption.id,
+      // You'd expect this to be `writeInOption.optionIndex`, but because
+      // `allContestOptions` doesn't know the write-ins are reversed, it will
+      // return the write-in options in the wrong order. So this is set to the
+      // last write-in option index because that's where `allContestOptions`
+      // puts the write-in we're looking for.
+      optionIndex: maximumWriteInOptionIndex,
+    },
+  ];
+
+  const layout: BallotPageLayout = {
+    pageSize: { width: 1, height: 1 },
+    metadata: buildMetadata({ pageNumber: 1 }),
+    contests: [
+      {
+        bounds: { x: 0, y: 0, width: 100, height: 100 },
+        corners: [
+          { x: 0, y: 0 },
+          { x: 100, y: 0 },
+          { x: 100, y: 100 },
+          { x: 0, y: 100 },
+        ],
+        options: optionsWithReversedWriteIns.map((option, i) => ({
+          definition: option,
+          bounds: { x: 0, y: 200 + 50 * i, width: 100, height: 50 },
+          target: {
+            bounds: { x: 20, y: 200 + 50 * i + 10, width: 20, height: 10 },
+            inner: { x: 22, y: 200 + 50 * i + 12, width: 16, height: 6 },
+          },
+        })),
+      },
+    ],
+  };
+
+  render(
+    <AppContext.Provider value={makeAppContext()}>
+      <WriteInAdjudicationScreen
+        sheetId="test-sheet"
+        side="front"
+        imageUrl="/test-sheet/front.jpg"
+        writeIns={writeIns}
+        layout={layout}
+        allContestIds={[contest.id]}
+        onAdjudicationComplete={onAdjudicationComplete}
+      />
+    </AppContext.Provider>
+  );
+
+  screen.getByText('Write-In Adjudication');
+  screen.getByText(contest.title);
+
+  const writeInImage = screen.getByAltText('write-in area');
+  expect(writeInImage.dataset.crop).toMatchInlineSnapshot(
+    `"x=0, y=185, width=100, height=80"`
+  );
 });
