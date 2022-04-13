da6b9c91007305e744eb14db58d7e8d1e05eafa3
Tara Vancil <contact@taravancil.com>
2022-04-13 10:05:32 -0400

    Fix failing integration tests

--- integration-testing/bsd/cypress/integration/configuration.ts
+++ integration-testing/bsd/cypress/integration/configuration.ts
@@ -40,8 +40,10 @@ describe('BSD and services/Scan', () => {
     cy.contains('Scan New Batch').click();
     cy.contains('A total of 1 ballot has been scanned in 1 batch.');
     cy.contains('Export').click();
+    cy.get('[data-testid="manual-export"]').click();
+    cy.contains('Cancel').click();
     cy.contains('Admin').click();
-    cy.contains('Export Backup').click();
+    cy.contains('Export Backup…').click();
     cy.contains('Delete Ballot Data').click();
     cy.contains('Yes, Delete Ballot Data').click();
     cy.contains('No ballots have been scanned', { timeout: 20000 });
