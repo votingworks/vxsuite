729ee6ab46ed2ea628d5ea11d53e3f5908844326
Tara Vancil <contact@taravancil.com>
2022-05-03 15:57:20 -0400

    fix coverage

--- libs/ui/src/modal.test.tsx
+++ libs/ui/src/modal.test.tsx
@@ -81,6 +81,28 @@ describe('Modal', () => {
     `);
   });
 
+  it('can configure fullscreen', () => {
+    render(<Modal fullscreen content="Do you want to do the thing?" />);
+
+    const modal = screen.getByRole('alertdialog');
+    expect(modal).toMatchInlineSnapshot(`
+      <div
+        aria-label="Alert Modal"
+        aria-modal="true"
+        class="sc-gsDJrp hXVzBN ReactModal__Content _"
+        data-testid="modal"
+        role="alertdialog"
+        tabindex="-1"
+      >
+        <div
+          class="sc-hKwCoD kFWWdL"
+        >
+          Do you want to do the thing?
+        </div>
+      </div>
+    `);
+  });
+
   it('handles overlay click', () => {
     const onOverlayClick = jest.fn();
     const { baseElement } = render(
