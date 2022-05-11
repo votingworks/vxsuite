0018e631cd7c30cfe0cefe78d8eb0c362b2b98a9
Tara Vancil <contact@taravancil.com>
2022-05-11 10:38:35 -0400

    Add tests to write-in adjudication screens
    This adds more tests and does some minor refactoring to improve test
    coverage for the write-in adjudication screens.

--- frontends/election-manager/src/screens/write_ins_transcription_screen.test.tsx
+++ frontends/election-manager/src/screens/write_ins_transcription_screen.test.tsx
@@ -1,18 +1,44 @@
 import { screen } from '@testing-library/react';
 import React from 'react';
-import { electionSample as election } from '@votingworks/fixtures';
+import { electionMinimalExhaustiveSampleDefinition as electionDefinition } from '@votingworks/fixtures';
 import { CandidateContest } from '@votingworks/types';
 import { renderInAppContext } from '../../test/render_in_app_context';
 import { WriteInsTranscriptionScreen } from './write_ins_transcription_screen';
 
 test('write-ins screen', () => {
-  const contest = election.contests[0] as CandidateContest;
+  const contest = electionDefinition.election.contests[0] as CandidateContest;
+  const onClickNext = jest.fn();
+  const onClickPrevious = jest.fn();
+  const onClose = jest.fn();
+  const onListAll = jest.fn();
+  const saveTranscribedValue = jest.fn();
+
   renderInAppContext(
     <WriteInsTranscriptionScreen
-      election={election}
+      election={electionDefinition.election}
       contest={contest}
-      onClose={jest.fn()}
-    />
+      onClickNext={onClickNext}
+      onClickPrevious={onClickPrevious}
+      onClose={onClose}
+      onListAll={onListAll}
+      saveTranscribedValue={saveTranscribedValue}
+    />,
+    { electionDefinition }
   );
   screen.getByText('BALLOT IMAGES GO HERE');
+
+  screen.getByText('Mickey Mouse').click();
+  expect(saveTranscribedValue).toHaveBeenCalledWith('Mickey Mouse');
+
+  screen.getByText('List All').click();
+  expect(onListAll).toHaveBeenCalledTimes(1);
+
+  screen.getByText('Previous').click();
+  expect(onClickPrevious).toHaveBeenCalledTimes(1);
+
+  screen.getByText('Next').click();
+  expect(onClickNext).toHaveBeenCalledTimes(1);
+
+  screen.getByText('Exit').click();
+  expect(onClose).toHaveBeenCalledTimes(1);
 });
