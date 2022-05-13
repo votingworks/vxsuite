819d7298936a9297d9e5bb2ecf7f728b4a14e23e
Andrew Hayes <adghayes@gmail.com>
2022-05-13 13:46:32 -0700

    increase test delay for larger test deck

--- frontends/election-manager/src/screens/print_test_deck_screen.test.tsx
+++ frontends/election-manager/src/screens/print_test_deck_screen.test.tsx
@@ -36,51 +36,51 @@ test('Printing the full test deck sorts precincts', async () => {
 
   // Check that the printing modals appear in alphabetical order
   await waitFor(() => getByLabelText('Printing Test Deck, (1 of 13),: ,Bywy.'));
-  jest.advanceTimersByTime(15000);
+  jest.advanceTimersByTime(30000);
   await waitFor(() =>
     getByLabelText('Printing Test Deck, (2 of 13),: ,Chester.')
   );
-  jest.advanceTimersByTime(15000);
+  jest.advanceTimersByTime(30000);
   await waitFor(() =>
     getByLabelText('Printing Test Deck, (3 of 13),: ,District 5.')
   );
-  jest.advanceTimersByTime(15000);
+  jest.advanceTimersByTime(30000);
   await waitFor(() =>
     getByLabelText('Printing Test Deck, (4 of 13),: ,East Weir.')
   );
-  jest.advanceTimersByTime(15000);
+  jest.advanceTimersByTime(30000);
   await waitFor(() =>
     getByLabelText('Printing Test Deck, (5 of 13),: ,Fentress.')
   );
-  jest.advanceTimersByTime(15000);
+  jest.advanceTimersByTime(30000);
   await waitFor(() =>
     getByLabelText('Printing Test Deck, (6 of 13),: ,French Camp.')
   );
-  jest.advanceTimersByTime(15000);
+  jest.advanceTimersByTime(30000);
   await waitFor(() =>
     getByLabelText('Printing Test Deck, (7 of 13),: ,Hebron.')
   );
-  jest.advanceTimersByTime(15000);
+  jest.advanceTimersByTime(30000);
   await waitFor(() =>
     getByLabelText('Printing Test Deck, (8 of 13),: ,Kenego.')
   );
-  jest.advanceTimersByTime(15000);
+  jest.advanceTimersByTime(30000);
   await waitFor(() =>
     getByLabelText('Printing Test Deck, (9 of 13),: ,Panhandle.')
   );
-  jest.advanceTimersByTime(15000);
+  jest.advanceTimersByTime(30000);
   await waitFor(() =>
     getByLabelText('Printing Test Deck, (10 of 13),: ,Reform.')
   );
-  jest.advanceTimersByTime(15000);
+  jest.advanceTimersByTime(30000);
   await waitFor(() =>
     getByLabelText('Printing Test Deck, (11 of 13),: ,Sherwood.')
   );
-  jest.advanceTimersByTime(15000);
+  jest.advanceTimersByTime(30000);
   await waitFor(() =>
     getByLabelText('Printing Test Deck, (12 of 13),: ,Southwest Ackerman.')
   );
-  jest.advanceTimersByTime(15000);
+  jest.advanceTimersByTime(30000);
   await waitFor(() =>
     getByLabelText('Printing Test Deck, (13 of 13),: ,West Weir.')
   );
