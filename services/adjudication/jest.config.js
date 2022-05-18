9b5e374ad36cbffbfe6963516ee93e4d93915a92
Tara Vancil <contact@taravancil.com>
2022-05-18 08:25:58 -0400

    chore(adjudication): rename write-ins service to adjudication

---
+++ services/adjudication/jest.config.js
@@ -0,0 +1,16 @@
+const shared = require('../../jest.config.shared');
+
+module.exports = {
+  ...shared,
+  // This is here because jest finds `build/__mocks__`,
+  // which we should probably make not be there by using smarter
+  // tsconfig.json values.
+  roots: ['<rootDir>/src'],
+  collectCoverageFrom: [
+    '**/*.{ts,tsx}',
+    '!**/node_modules/**',
+    '!src/index.ts',
+    '!src/types.ts',
+    '!test/**/*',
+  ],
+};
---
+++ services/adjudication/src/server.ts
@@ -0,0 +1,51 @@
+import { LogEventId, Logger, LogSource } from '@votingworks/logging';
+import express, { Application } from 'express';
+import { PORT } from './globals';
+
+type NoParams = never;
+
+/**
+ * Builds an express application.
+ */
+export function buildApp(): Application {
+  const app: Application = express();
+
+  app.use(express.raw());
+  app.use(express.json({ limit: '5mb', type: 'application/json' }));
+  app.use(express.urlencoded({ extended: false }));
+
+  app.get<NoParams>('/', (_, response) => {
+    response.send('Hello world');
+  });
+
+  return app;
+}
+
+/**
+ * Options for starting the admin service.
+ */
+export interface StartOptions {
+  app: Application;
+  logger: Logger;
+  port: number | string;
+}
+
+/**
+ * Starts the server with all the default options.
+ */
+// eslint-disable-next-line @typescript-eslint/require-await
+export async function start({
+  app,
+  logger = new Logger(LogSource.VxScanService),
+  port = PORT,
+}: Partial<StartOptions>): Promise<void> {
+  /* istanbul ignore next */
+  const resolvedApp = app ?? buildApp();
+
+  resolvedApp.listen(port, async () => {
+    await logger.log(LogEventId.ApplicationStartup, 'system', {
+      message: `Admin Service running at http://localhost:${port}/`,
+      disposition: 'success',
+    });
+  });
+}
