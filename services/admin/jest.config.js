772af81853a4e0b3b8f8f4d2ce3271a0322dec39
Tara Vancil <contact@taravancil.com>
2022-05-19 12:51:01 -0400

    chore(admin): Set coverage thresholds to 100%

--- services/admin/jest.config.js
+++ services/admin/jest.config.js
@@ -15,10 +15,10 @@ module.exports = {
   ],
   coverageThreshold: {
     global: {
-      statements: 90,
-      branches: 90,
-      functions: 90,
-      lines: 90,
+      statements: 100,
+      branches: 100,
+      functions: 100,
+      lines: 100,
     },
   },
 };
