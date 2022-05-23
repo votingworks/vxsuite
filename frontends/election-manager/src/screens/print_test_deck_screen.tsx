54ea445459c91094b5c80bd11128ff611b961001
Arsalan Sufi <arsalan@voting.works>
2022-05-23 10:37:47 -0400

    Miscellaneous PrintTestDeckScreen cleanup, including:
    - Accounting for new Ballot type def
    - Typing PrintIndex.component as a union of string literals
    - Storing print times in constants for clarity
    - Fixing incorrect sleep time
    - Using null as more explicit empty value than undefined
    - Updating prop interface names to end with Props

--- frontends/election-manager/src/screens/print_test_deck_screen.tsx
+++ frontends/election-manager/src/screens/print_test_deck_screen.tsx
@@ -2,34 +2,32 @@ import React, { useState, useContext, useCallback, useEffect } from 'react';
 import {
   Election,
   getPrecinctById,
+  Precinct,
   PrecinctId,
-  VotesDict,
   Tally,
   VotingMethod,
 } from '@votingworks/types';
 import { assert, sleep, tallyVotesByContest } from '@votingworks/utils';
 import { LogEventId } from '@votingworks/logging';
-
 import { useCancelablePromise, Modal, Prose } from '@votingworks/ui';
 
 import { AppContext } from '../contexts/app_context';
-
 import { Button } from '../components/button';
 import { ButtonList } from '../components/button_list';
 import { Loading } from '../components/loading';
-
 import { NavigationScreen } from '../components/navigation_screen';
-
 import { HandMarkedPaperBallot } from '../components/hand_marked_paper_ballot';
 import { TestDeckTallyReport } from '../components/test_deck_tally_report';
-
 import {
   generateTestDeckBallots,
   generateBlankBallots,
   generateOvervoteBallot,
 } from '../utils/election';
 
-interface PrecinctTallyReportParams {
+const ONE_SIDED_PAGE_PRINT_TIME_MS = 3000;
+const TWO_SIDED_PAGE_PRINT_TIME_MS = 5000;
+
+interface PrecinctTallyReportProps {
   election: Election;
   precinctId: PrecinctId;
   onRendered: (numPages: number) => void;
@@ -39,16 +37,15 @@ function PrecinctTallyReport({
   election,
   precinctId,
   onRendered,
-}: PrecinctTallyReportParams) {
+}: PrecinctTallyReportProps): JSX.Element {
   const ballots = generateTestDeckBallots({ election, precinctId });
 
-  const votes: VotesDict[] = ballots.map((b) => b.votes as VotesDict);
   const testDeckTally: Tally = {
     numberOfBallotsCounted: ballots.length,
     castVoteRecords: new Set(),
     contestTallies: tallyVotesByContest({
       election,
-      votes,
+      votes: ballots.map((b) => b.votes),
     }),
     ballotCountsByVotingMethod: { [VotingMethod.Unknown]: ballots.length },
   };
@@ -67,7 +64,7 @@ function PrecinctTallyReport({
   );
 }
 
-interface HandMarkedPaperBallotsParams {
+interface HandMarkedPaperBallotsProps {
   election: Election;
   electionHash: string;
   precinctId: PrecinctId;
@@ -79,12 +76,15 @@ function HandMarkedPaperBallots({
   electionHash,
   precinctId,
   onAllRendered,
-}: HandMarkedPaperBallotsParams) {
-  const ballots = generateTestDeckBallots({ election, precinctId });
-  ballots.push(...generateBlankBallots({ election, precinctId, numBlanks: 2 }));
-
+}: HandMarkedPaperBallotsProps): JSX.Element {
+  const ballots = [
+    ...generateTestDeckBallots({ election, precinctId }),
+    ...generateBlankBallots({ election, precinctId, numBlanks: 2 }),
+  ];
   const overvoteBallot = generateOvervoteBallot({ election, precinctId });
-  if (overvoteBallot) ballots.push(overvoteBallot);
+  if (overvoteBallot) {
+    ballots.push(overvoteBallot);
+  }
 
   let numRendered = 0;
   function onRendered() {
@@ -99,14 +99,14 @@ function HandMarkedPaperBallots({
       {ballots.map((ballot, i) => (
         <HandMarkedPaperBallot
           key={`ballot-${i}`} // eslint-disable-line react/no-array-index-key
-          ballotStyleId={ballot.ballotStyleId as string}
+          ballotStyleId={ballot.ballotStyleId}
           election={election}
           electionHash={electionHash}
           isLiveMode={false}
           isAbsentee={false}
-          precinctId={ballot.precinctId as string}
+          precinctId={ballot.precinctId}
           locales={{ primary: 'en-US' }}
-          votes={ballot.votes as VotesDict}
+          votes={ballot.votes}
           onRendered={() => onRendered()}
         />
       ))}
@@ -129,7 +129,7 @@ const HandMarkedPaperBallotsMemoized = React.memo(HandMarkedPaperBallots);
 
 interface PrintIndex {
   precinctIndex: number;
-  component: string;
+  component: 'TallyReport' | 'HandMarkedPaperBallots';
 }
 
 export function PrintTestDeckScreen(): JSX.Element {
@@ -141,7 +141,7 @@ export function PrintTestDeckScreen(): JSX.Element {
   const currentUserType = currentUserSession.type;
   const { election, electionHash } = electionDefinition;
   const [precinctIds, setPrecinctIds] = useState<string[]>([]);
-  const [printIndex, setPrintIndex] = useState<PrintIndex | undefined>();
+  const [printIndex, setPrintIndex] = useState<PrintIndex | null>();
 
   const pageTitle = 'L&A Packages';
 
@@ -163,7 +163,7 @@ export function PrintTestDeckScreen(): JSX.Element {
       const printers = await window.kiosk.getPrinterInfo();
       if (printers.some((p) => p.connected)) {
         setPrecinctIds(generatePrecinctIds(precinctId));
-        setPrintIndex({ precinctIndex: 0, component: 'tally' });
+        setPrintIndex({ precinctIndex: 0, component: 'TallyReport' });
       } else {
         // eslint-disable-next-line no-alert
         window.alert('please connect the printer.');
@@ -176,7 +176,7 @@ export function PrintTestDeckScreen(): JSX.Element {
       }
     } else {
       setPrecinctIds(generatePrecinctIds(precinctId));
-      setPrintIndex({ precinctIndex: 0, component: 'tally' });
+      setPrintIndex({ precinctIndex: 0, component: 'TallyReport' });
     }
   }
 
@@ -186,19 +186,18 @@ export function PrintTestDeckScreen(): JSX.Element {
         return;
       }
 
+      const precinctId = precinctIds[printIndex.precinctIndex];
       await printer.print({ sides: 'one-sided' });
       await logger.log(LogEventId.TestDeckTallyReportPrinted, currentUserType, {
         disposition: 'success',
-        message: `Test deck tally report printed as part of L&A package for precinct id: ${
-          precinctIds[printIndex.precinctIndex]
-        }`,
-        precinctId: precinctIds[printIndex.precinctIndex],
+        message: `Test deck tally report printed as part of L&A package for precinct ID: ${precinctId}`,
+        precinctId,
       });
 
-      await makeCancelable(sleep(numPages * 3000));
+      await makeCancelable(sleep(numPages * ONE_SIDED_PAGE_PRINT_TIME_MS));
       setPrintIndex({
         precinctIndex: printIndex.precinctIndex,
-        component: 'hmpb',
+        component: 'HandMarkedPaperBallots',
       });
     },
     [printIndex, printer, logger, currentUserType, precinctIds, makeCancelable]
@@ -210,43 +209,43 @@ export function PrintTestDeckScreen(): JSX.Element {
         return;
       }
 
+      const precinctId = precinctIds[printIndex.precinctIndex];
       await printer.print({ sides: 'two-sided-long-edge' });
       await logger.log(LogEventId.TestDeckPrinted, currentUserType, {
         disposition: 'success',
-        message: `Hand-marked paper ballot test deck printed as part of L&A package for precinct id: ${
-          precinctIds[printIndex.precinctIndex]
-        }`,
-        precinctId: precinctIds[printIndex.precinctIndex],
+        message: `Hand-marked paper ballot test deck printed as part of L&A package for precinct ID: ${precinctId}`,
+        precinctId,
       });
 
       if (printIndex.precinctIndex < precinctIds.length - 1) {
-        // wait 5s per ballot printed
-        // that's how long printing takes in duplex, no reason to get ahead of it.
-        await makeCancelable(sleep(numBallots * 5000));
+        await makeCancelable(sleep(numBallots * TWO_SIDED_PAGE_PRINT_TIME_MS));
         setPrintIndex({
           precinctIndex: printIndex.precinctIndex + 1,
-          component: 'tally',
+          component: 'TallyReport',
         });
       } else {
-        await makeCancelable(sleep(3000));
-        setPrintIndex(undefined);
+        await makeCancelable(sleep(numBallots * TWO_SIDED_PAGE_PRINT_TIME_MS));
+        setPrintIndex(null);
         setPrecinctIds([]);
       }
     },
     [printIndex, printer, logger, currentUserType, precinctIds, makeCancelable]
   );
 
-  const currentPrecinct =
-    printIndex === undefined
-      ? undefined
-      : getPrecinctById({
-          election,
-          precinctId: precinctIds[printIndex.precinctIndex],
-        });
+  const currentPrecinctId = printIndex
+    ? precinctIds[printIndex.precinctIndex]
+    : '';
+  const currentPrecinct: Precinct | null =
+    (printIndex &&
+      getPrecinctById({
+        election,
+        precinctId: currentPrecinctId,
+      })) ||
+    null;
 
   return (
     <React.Fragment>
-      {printIndex !== undefined && currentPrecinct && (
+      {printIndex && currentPrecinct && (
         <Modal
           centerContent
           content={
@@ -286,21 +285,21 @@ export function PrintTestDeckScreen(): JSX.Element {
             ))}
         </ButtonList>
       </NavigationScreen>
-      {printIndex !== undefined &&
-        (printIndex.component === 'tally' ? (
-          <PrecinctTallyReport
-            election={election}
-            precinctId={precinctIds[printIndex.precinctIndex]}
-            onRendered={onPrecinctTallyReportRendered}
-          />
-        ) : (
-          <HandMarkedPaperBallotsMemoized
-            election={election}
-            electionHash={electionHash}
-            precinctId={precinctIds[printIndex.precinctIndex]}
-            onAllRendered={onAllHandMarkedPaperBallotsRendered}
-          />
-        ))}
+      {printIndex?.component === 'TallyReport' && (
+        <PrecinctTallyReport
+          election={election}
+          precinctId={currentPrecinctId}
+          onRendered={onPrecinctTallyReportRendered}
+        />
+      )}
+      {printIndex?.component === 'HandMarkedPaperBallots' && (
+        <HandMarkedPaperBallotsMemoized
+          election={election}
+          electionHash={electionHash}
+          precinctId={currentPrecinctId}
+          onAllRendered={onAllHandMarkedPaperBallotsRendered}
+        />
+      )}
     </React.Fragment>
   );
 }
