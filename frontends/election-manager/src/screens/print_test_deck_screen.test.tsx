e9a091d78a14835e7e6cf2ddc0f4c300bea17979
Arsalan Sufi <arsalan@voting.works>
2022-05-23 10:51:28 -0400

    Update tests to account for addition of VxMark test decks to VxAdmin

--- frontends/election-manager/src/screens/print_test_deck_screen.test.tsx
+++ frontends/election-manager/src/screens/print_test_deck_screen.test.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import { fireEvent, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import { fakeKiosk, fakePrinterInfo } from '@votingworks/test-utils';
+import { screen } from '@testing-library/react';
 
 import { PrintTestDeckScreen } from './print_test_deck_screen';
 import { renderInAppContext } from '../../test/render_in_app_context';
@@ -22,110 +23,36 @@ test('Printing all L&A packages sorts precincts', async () => {
     fakePrinterInfo({ name: 'VxPrinter', connected: true }),
   ]);
 
-  const { getByText, getByLabelText } = renderInAppContext(
-    <PrintTestDeckScreen />
-  );
+  renderInAppContext(<PrintTestDeckScreen />);
 
-  fireEvent.click(getByText('All Precincts'));
+  userEvent.click(screen.getByText('All Precincts'));
 
   // Check that the printing modals appear in alphabetical order
-  await waitFor(() => getByLabelText('Printing L&A Package (1 of 13): Bywy.'));
-  jest.advanceTimersByTime(3000);
-  await waitFor(() => getByLabelText('Printing L&A Package (1 of 13): Bywy.'));
-  jest.advanceTimersByTime(30000);
-  await waitFor(() =>
-    getByLabelText('Printing L&A Package (2 of 13): Chester.')
-  );
-  jest.advanceTimersByTime(3000);
-  await waitFor(() =>
-    getByLabelText('Printing L&A Package (2 of 13): Chester.')
-  );
-  jest.advanceTimersByTime(30000);
-  await waitFor(() =>
-    getByLabelText('Printing L&A Package (3 of 13): District 5.')
-  );
-  jest.advanceTimersByTime(3000);
-  await waitFor(() =>
-    getByLabelText('Printing L&A Package (3 of 13): District 5.')
-  );
-  jest.advanceTimersByTime(30000);
-  await waitFor(() =>
-    getByLabelText('Printing L&A Package (4 of 13): East Weir.')
-  );
-  jest.advanceTimersByTime(3000);
-  await waitFor(() =>
-    getByLabelText('Printing L&A Package (4 of 13): East Weir.')
-  );
-  jest.advanceTimersByTime(30000);
-  await waitFor(() =>
-    getByLabelText('Printing L&A Package (5 of 13): Fentress.')
-  );
-  jest.advanceTimersByTime(3000);
-  await waitFor(() =>
-    getByLabelText('Printing L&A Package (5 of 13): Fentress.')
-  );
-  jest.advanceTimersByTime(30000);
-  await waitFor(() =>
-    getByLabelText('Printing L&A Package (6 of 13): French Camp.')
-  );
-  jest.advanceTimersByTime(3000);
-  await waitFor(() =>
-    getByLabelText('Printing L&A Package (6 of 13): French Camp.')
-  );
-  jest.advanceTimersByTime(30000);
-  await waitFor(() =>
-    getByLabelText('Printing L&A Package (7 of 13): Hebron.')
-  );
-  jest.advanceTimersByTime(3000);
-  await waitFor(() =>
-    getByLabelText('Printing L&A Package (7 of 13): Hebron.')
-  );
-  jest.advanceTimersByTime(30000);
-  await waitFor(() =>
-    getByLabelText('Printing L&A Package (8 of 13): Kenego.')
-  );
-  jest.advanceTimersByTime(3000);
-  await waitFor(() =>
-    getByLabelText('Printing L&A Package (8 of 13): Kenego.')
-  );
-  jest.advanceTimersByTime(30000);
-  await waitFor(() =>
-    getByLabelText('Printing L&A Package (9 of 13): Panhandle.')
-  );
-  jest.advanceTimersByTime(3000);
-  await waitFor(() =>
-    getByLabelText('Printing L&A Package (9 of 13): Panhandle.')
-  );
-  jest.advanceTimersByTime(30000);
-  await waitFor(() =>
-    getByLabelText('Printing L&A Package (10 of 13): Reform.')
-  );
-  jest.advanceTimersByTime(3000);
-  await waitFor(() =>
-    getByLabelText('Printing L&A Package (10 of 13): Reform.')
-  );
-  jest.advanceTimersByTime(30000);
-  await waitFor(() =>
-    getByLabelText('Printing L&A Package (11 of 13): Sherwood.')
-  );
-  jest.advanceTimersByTime(3000);
-  await waitFor(() =>
-    getByLabelText('Printing L&A Package (11 of 13): Sherwood.')
-  );
-  jest.advanceTimersByTime(30000);
-  await waitFor(() =>
-    getByLabelText('Printing L&A Package (12 of 13): Southwest Ackerman.')
-  );
-  jest.advanceTimersByTime(3000);
-  await waitFor(() =>
-    getByLabelText('Printing L&A Package (12 of 13): Southwest Ackerman.')
-  );
-  jest.advanceTimersByTime(30000);
-  await waitFor(() =>
-    getByLabelText('Printing L&A Package (13 of 13): West Weir.')
-  );
-  jest.advanceTimersByTime(3000);
-  await waitFor(() =>
-    getByLabelText('Printing L&A Package (13 of 13): West Weir.')
-  );
+  const precinctsInAlphabeticalOrder = [
+    'Bywy',
+    'Chester',
+    'District 5',
+    'East Weir',
+    'Fentress',
+    'French Camp',
+    'Hebron',
+    'Kenego',
+    'Panhandle',
+    'Reform',
+    'Sherwood',
+    'Southwest Ackerman',
+    'West Weir',
+  ];
+  for (let i = 0; i < precinctsInAlphabeticalOrder.length; i += 1) {
+    const precinct = precinctsInAlphabeticalOrder[i];
+    const printText = `Printing L&A Package (${i + 1} of 13): ${precinct}.`;
+    await screen.findByLabelText(printText);
+    jest.advanceTimersByTime(5000);
+    await screen.findByLabelText(printText);
+    jest.advanceTimersByTime(30000);
+    await screen.findByLabelText(printText);
+    jest.advanceTimersByTime(30000);
+  }
+  await screen.findByText('All Precincts');
+  expect(screen.queryByText('Printing')).not.toBeInTheDocument();
 });
