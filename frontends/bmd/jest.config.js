e8498b5e459d42cb7462ef95bee449185e2e140d
Brian Donovan <1938+eventualbuddha@users.noreply.github.com>
2022-05-30 14:20:16 -0700

    test(bmd): simplify `jest.config.js`

--- frontends/bmd/jest.config.js
+++ frontends/bmd/jest.config.js
@@ -1,7 +1,15 @@
+const shared = require('../../jest.config.shared');
+
 /**
  * @type {import('@jest/types').Config.InitialOptions}
  */
 module.exports = {
+  ...shared,
+
+  // TODO: Remove this and fix the coverage differences between babel-jest and
+  // ts-jest.
+  preset: undefined,
+
   collectCoverageFrom: [
     'src/**/*.{js,jsx,ts,tsx}',
     '!src/config/*',
@@ -9,53 +17,17 @@ module.exports = {
     '!src/index.tsx',
     '!src/contexts/ballot_context.ts',
   ],
-  coverageThreshold: {
-    global: {
-      statements: 100,
-      branches: 100,
-      lines: 100,
-      functions: 100,
-    },
-  },
-  moduleFileExtensions: [
-    'web.js',
-    'js',
-    'web.ts',
-    'ts',
-    'web.tsx',
-    'tsx',
-    'json',
-    'web.jsx',
-    'jsx',
-    'node',
-  ],
-  moduleNameMapper: {
-    '^react-native$': 'react-native-web',
-    '^.+\\.module\\.(css|sass|scss)$': 'identity-obj-proxy',
-  },
-  modulePaths: [],
   resetMocks: true,
   roots: ['<rootDir>/src'],
   setupFiles: ['react-app-polyfill/jsdom'],
   setupFilesAfterEnv: ['<rootDir>/src/setupTests.tsx'],
   testEnvironment: 'jsdom',
-  testMatch: [
-    '<rootDir>/src/**/__tests__/**/*.{js,jsx,ts,tsx}',
-    '<rootDir>/src/**/*.{spec,test}.{js,jsx,ts,tsx}',
-  ],
-  testRunner: require.resolve('jest-circus/runner'),
   transform: {
     '^.+\\.(js|jsx|mjs|cjs|ts|tsx)$': '<rootDir>/node_modules/babel-jest',
     '^.+\\.css$': '<rootDir>/config/jest/cssTransform.js',
-    '^(?!.*\\.(js|jsx|mjs|cjs|ts|tsx|css|json)$)':
-      '<rootDir>/config/jest/fileTransform.js',
   },
   transformIgnorePatterns: [
     '[/\\\\]node_modules[/\\\\].+\\.(js|jsx|mjs|cjs|ts|tsx)$',
     '^.+\\.module\\.(css|sass|scss)$',
-  ],
-  watchPlugins: [
-    'jest-watch-typeahead/filename',
-    'jest-watch-typeahead/testname',
   ],
 };
