ab549e8e04788d646a384ba994703b6a825244d8
Brian Donovan <1938+eventualbuddha@users.noreply.github.com>
2022-06-23 20:25:37 -0700

    test(bsd): switch from CRA to vite
    Refs #1314

---
+++ frontends/bsd/jest.config.js
@@ -0,0 +1,32 @@
+const shared = require('../../jest.config.shared');
+
+/**
+ * @type {import('@jest/types').Config.InitialOptions}
+ */
+module.exports = {
+  ...shared,
+  collectCoverageFrom: [
+    'src/**/*.{js,jsx,ts,tsx}',
+    '!src/config/*',
+    '!src/**/*.d.ts',
+    '!src/index.tsx',
+  ],
+  coverageThreshold: {
+    global: {
+      statements: 67,
+      branches: 59,
+      functions: 64,
+      lines: 67,
+    },
+  },
+  resetMocks: true,
+  setupFiles: ['react-app-polyfill/jsdom'],
+  setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
+  testEnvironment: 'jsdom',
+  transform: {
+    '^.+\\.css$': '<rootDir>/config/jest/cssTransform.js',
+  },
+  transformIgnorePatterns: [
+    '[/\\\\]node_modules[/\\\\].+\\.(js|jsx|mjs|cjs|ts|tsx)$',
+  ],
+};
