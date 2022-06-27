f45799e4f82977896760e8739de77d0184118434
Arsalan Sufi <arsalan@voting.works>
2022-06-27 17:44:32 -0400

    Tweak super admin Smartcards screens

--- frontends/election-manager/src/screens/election_smartcards_screen.tsx
+++ frontends/election-manager/src/screens/election_smartcards_screen.tsx
@@ -1,22 +1,31 @@
 import React from 'react';
+import styled from 'styled-components';
 import { LinkButton, Prose } from '@votingworks/ui';
 
-import { CardProgrammingPrompt } from '../components/card_programming_prompt';
 import { NavigationScreen } from '../components/navigation_screen';
 import { routerPaths } from '../router_paths';
 
+const Body = styled(Prose)`
+  flex-grow: 1;
+`;
+
+const SuperAdminCardsLinkButton = styled(LinkButton)`
+  align-self: start;
+`;
+
 export function ElectionSmartcardsScreen(): JSX.Element {
   return (
-    <NavigationScreen>
-      <Prose maxWidth={false}>
+    <NavigationScreen flexRow>
+      <Body maxWidth={false}>
         <h1>Smartcards</h1>
-        <CardProgrammingPrompt cardType="election" />
         <p>
-          <LinkButton small to={routerPaths.superAdminSmartcards}>
-            Create Super Admin Cards Instead
-          </LinkButton>
+          Insert a card to view card details or to create an{' '}
+          <strong>Admin or Poll Worker</strong> card for this election.
         </p>
-      </Prose>
+      </Body>
+      <SuperAdminCardsLinkButton small to={routerPaths.superAdminSmartcards}>
+        Create Super Admin Cards
+      </SuperAdminCardsLinkButton>
     </NavigationScreen>
   );
 }
--- frontends/election-manager/src/screens/super_admin_smartcards_screen.tsx
+++ frontends/election-manager/src/screens/super_admin_smartcards_screen.tsx
@@ -1,22 +1,31 @@
 import React from 'react';
+import styled from 'styled-components';
 import { LinkButton, Prose } from '@votingworks/ui';
 
-import { CardProgrammingPrompt } from '../components/card_programming_prompt';
 import { NavigationScreen } from '../components/navigation_screen';
 import { routerPaths } from '../router_paths';
 
+const Body = styled(Prose)`
+  flex-grow: 1;
+`;
+
+const ElectionCardsLinkButton = styled(LinkButton)`
+  align-self: start;
+`;
+
 export function SuperAdminSmartcardsScreen(): JSX.Element {
   return (
-    <NavigationScreen>
-      <Prose maxWidth={false}>
+    <NavigationScreen flexRow>
+      <Body maxWidth={false}>
         <h1>Smartcards</h1>
-        <CardProgrammingPrompt cardType="superAdmin" />
         <p>
-          <LinkButton small to={routerPaths.electionSmartcards}>
-            Create Election Cards Instead
-          </LinkButton>
+          Insert a card to view card details or to create a{' '}
+          <strong>Super Admin</strong> card.
         </p>
-      </Prose>
+      </Body>
+      <ElectionCardsLinkButton small to={routerPaths.electionSmartcards}>
+        Create Election Cards
+      </ElectionCardsLinkButton>
     </NavigationScreen>
   );
 }
