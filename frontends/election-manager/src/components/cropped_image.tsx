5fc6075a938ffb3c189c1c196587ad5e163ea6e0
Tara Vancil <contact@taravancil.com>
2022-06-30 16:16:46 -0400

    First pass at using real ballot images and bounds in transcription screen

---
+++ frontends/election-manager/src/components/cropped_image.tsx
@@ -0,0 +1,38 @@
+import { Rect } from '@votingworks/types';
+import React, { useEffect, useState } from 'react';
+
+export interface Props {
+  src: string;
+  alt: string;
+  crop: Rect;
+  style?: React.CSSProperties;
+}
+
+export function CroppedImage({ src, alt, crop, style }: Props): JSX.Element {
+  const [dataUrl, setDataUrl] = useState<string>();
+
+  useEffect(() => {
+    const image = new Image();
+    image.src = src;
+    image.onload = () => {
+      const canvas = document.createElement('canvas');
+      canvas.width = crop.width;
+      canvas.height = crop.height;
+      const context = canvas.getContext('2d');
+
+      if (context) {
+        context.drawImage(image, -crop.x, -crop.y);
+        setDataUrl(canvas.toDataURL());
+      }
+    };
+  }, [crop.height, crop.width, crop.x, crop.y, src]);
+
+  return (
+    <img
+      src={dataUrl}
+      alt={alt}
+      style={style}
+      data-crop={`x=${crop.x}, y=${crop.y}, width=${crop.width}, height=${crop.height}`}
+    />
+  );
+}
