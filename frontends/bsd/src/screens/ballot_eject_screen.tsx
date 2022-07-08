c5a5653b8f473ca88358b602b793c8a92f984734
Ben Adida <ben@adida.net>
2022-07-08 08:53:29 -0400

    fix(bsd): significantly reduce text on adjudication screen, it was far too much.

--- frontends/bsd/src/screens/ballot_eject_screen.tsx
+++ frontends/bsd/src/screens/ballot_eject_screen.tsx
@@ -397,7 +397,7 @@ export function BallotEjectScreen({
             primary
             onPress={() => continueScanning({ forceAccept: false })}
           >
-            Confirm Ballot Removed and Continue Scanning
+            I&apos;ve Removed the Ballot, Continue Scanning
           </Button>
         ) : ballotState === 'acceptBallot' ? (
           <Button
@@ -410,7 +410,7 @@ export function BallotEjectScreen({
               })
             }
           >
-            Tabulate Ballot and Continue Scanning
+            Count It and Keep Scanning
           </Button>
         ) : (
           <Button disabled onPress={doNothing}>
@@ -441,49 +441,26 @@ export function BallotEjectScreen({
                 : 'Unknown Reason'}
             </EjectReason>
             <p>
-              This last scanned sheet <strong>was not tabulated</strong>.
+              This ballot <strong>has not yet been tabulated</strong>. What
+              would you like to do?
             </p>
             {allowBallotDuplication ? (
               <React.Fragment>
-                <h4>Original Ballot Scan</h4>
                 <p>
-                  Remove ballot and create a duplicate ballot for the Resolution
-                  Board to review.
-                  <br />
                   <Button
                     primary={ballotState === 'removeBallot'}
                     onPress={() => setBallotState('removeBallot')}
                   >
-                    Original Ballot Removed
+                    Don&apos;t Count It
                   </Button>
                 </p>
-                <h4>Duplicate Ballot Scan</h4>
                 <p>
-                  {isOvervotedSheet ? (
-                    <React.Fragment>
-                      Confirm ballot sheet was reviewed by the Resolution Board
-                      and tabulate as ballot sheet with an{' '}
-                      <strong>overvote</strong>.
-                    </React.Fragment>
-                  ) : isBlankSheet ? (
-                    <React.Fragment>
-                      Confirm ballot sheet was reviewed by the Resolution Board
-                      and tabulate as a <strong>blank</strong> ballot sheet and
-                      has no votes.
-                    </React.Fragment>
-                  ) : (
-                    <React.Fragment>
-                      Confirm ballot sheet was reviewed by the Resolution Board
-                      and tabulate as ballot with issue which could not be
-                      determined.
-                    </React.Fragment>
-                  )}
                   <br />
                   <Button
                     primary={ballotState === 'acceptBallot'}
                     onPress={() => setBallotState('acceptBallot')}
                   >
-                    Tabulate Duplicate Ballot
+                    Count It As Is
                   </Button>
                 </p>
               </React.Fragment>
