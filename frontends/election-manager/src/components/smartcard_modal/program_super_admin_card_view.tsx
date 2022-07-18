8d7f5d74fe00e537e4b27de3733c2f7c2cf5a731
Arsalan Sufi <arsalan@voting.works>
2022-07-18 19:20:21 -0400

    Implement super admin smartcard programming view

--- frontends/election-manager/src/components/smartcard_modal/program_super_admin_card_view.tsx
+++ frontends/election-manager/src/components/smartcard_modal/program_super_admin_card_view.tsx
@@ -1,18 +1,51 @@
 import React from 'react';
-import { Button } from '@votingworks/ui';
+import { Button, Prose } from '@votingworks/ui';
 import { CardProgramming } from '@votingworks/types';
 
+import { SmartcardActionStatus, StatusMessage } from './status_message';
+
 interface Props {
+  actionStatus?: SmartcardActionStatus;
   card: CardProgramming;
+  setActionStatus: (status?: SmartcardActionStatus) => void;
 }
 
-export function ProgramSuperAdminCardView({ card }: Props): JSX.Element {
+export function ProgramSuperAdminCardView({
+  actionStatus,
+  card,
+  setActionStatus,
+}: Props): JSX.Element {
+  async function programSuperAdminCard() {
+    setActionStatus({
+      action: 'Program',
+      role: 'superadmin',
+      status: 'InProgress',
+    });
+    const result = await card.programUser({ role: 'superadmin' });
+    setActionStatus({
+      action: 'Program',
+      role: 'superadmin',
+      status: result.isOk() ? 'Success' : 'Error',
+    });
+  }
+
   return (
-    <React.Fragment>
+    <Prose textCenter>
       <h2>Program Super Admin Card</h2>
-      <Button onPress={() => card.programUser({ role: 'superadmin' })}>
-        Program
-      </Button>
-    </React.Fragment>
+      {/* An empty div to maintain space between the header and subsequent p. TODO: Consider adding
+        a `maintainSpaceBelowHeaders` prop to `Prose` */}
+      <div />
+      {actionStatus && <StatusMessage actionStatus={actionStatus} />}
+      <p>
+        This card performs all system actions. Strictly limit the number created
+        and keep all Super Admin cards secure.
+      </p>
+      <p>Remove card to leave card unprogrammed.</p>
+      <p>
+        <Button onPress={programSuperAdminCard}>
+          Program Super Admin Card
+        </Button>
+      </p>
+    </Prose>
   );
 }
