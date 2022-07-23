56add6ab69bb9b35a645531b880bcc995d67982d
Ben Adida <ben@adida.net>
2022-07-23 14:19:10 -0400

    feat(types): add new tally data types, no migration of any current feature yet.

--- libs/types/src/tallies.ts
+++ libs/types/src/tallies.ts
@@ -1,6 +1,6 @@
 import { z } from 'zod';
 import { CastVoteRecord } from './cast_vote_record';
-import { Contest, Candidate } from './election';
+import { Contest, Candidate, Election } from './election';
 import { Dictionary, Optional } from './generic';
 
 export type YesNoVoteId = 'yes' | 'no';
@@ -172,3 +172,76 @@ export const CompressedTallySchema: z.ZodSchema<CompressedTally> = z.array(
     MsEitherNeitherContestCompressedTallySchema,
   ])
 );
+
+//
+// new tally data structures, serializable.
+//
+
+export type ContestTallies = ContestTally[]; // array matching a set of contests in order
+
+export interface SingleTally {
+  readonly byVotingMethod: Dictionary<ContestTallies>;
+}
+
+export interface CompleteTally {
+  readonly precinctTallies: Dictionary<SingleTally>; // indexed by precinctId, only reporting precincts
+  readonly generatedAt: Date;
+}
+
+export function generateZeroSingleTallyForVotingMethod(
+  election: Election,
+  votingMethod: VotingMethod
+): SingleTally {
+  const contestTallies: ContestTallies = election.contests.map((contest) => {
+    switch (contest.type) {
+      case 'candidate': {
+        const initialTallies: Dictionary<ContestOptionTally> = {};
+        return {
+          contest,
+          tallies: contest.candidates.reduce((result, candidate) => {
+            const newResult: Dictionary<ContestOptionTally> = { ...result };
+            newResult[candidate.id] = {
+              option: candidate,
+              tally: 0,
+            };
+            return newResult;
+          }, initialTallies),
+          metadata: {
+            ballots: 0,
+            overvotes: 0,
+            undervotes: 0,
+          },
+        };
+      }
+      case 'yesno':
+        return {
+          contest,
+          tallies: {
+            yes: {
+              option: ['yes'],
+              tally: 0,
+            },
+            no: {
+              option: ['no'],
+              tally: 0,
+            },
+          },
+          metadata: {
+            ballots: 0,
+            overvotes: 0,
+            undervotes: 0,
+          },
+        };
+      default:
+        throw new Error(`Unexpected contest type: ${contest.type}`);
+    }
+  });
+
+  const returnValue: SingleTally = {
+    byVotingMethod: {},
+  };
+
+  returnValue.byVotingMethod[votingMethod] = contestTallies;
+
+  return returnValue;
+}
