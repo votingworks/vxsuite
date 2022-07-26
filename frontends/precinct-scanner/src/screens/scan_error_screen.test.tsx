6cf69eee4d42c74afd73e421241523d579a6334e
Jonah Kagan <jonah@voting.works>
2022-07-26 12:20:59 -0700

    Update scan error screen

--- frontends/precinct-scanner/src/screens/scan_error_screen.test.tsx
+++ frontends/precinct-scanner/src/screens/scan_error_screen.test.tsx
@@ -1,79 +1,39 @@
 import React from 'react';
-import { fireEvent, render, screen } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import { ScanErrorScreen } from './scan_error_screen';
-import { RejectedScanningReason } from '../config/types';
 
 test('render correct test ballot error screen when we are in test mode', async () => {
-  const dismiss = jest.fn();
-  render(
-    <ScanErrorScreen
-      dismissError={dismiss}
-      rejectionReason={RejectedScanningReason.InvalidTestMode}
-      isTestMode
-    />
-  );
+  render(<ScanErrorScreen error="invalid_test_mode" isTestMode />);
   await screen.findByText('Scanning Error');
   await screen.findByText('Live Ballot detected.');
-  fireEvent.click(await screen.findByText('Dismiss Error'));
-  expect(dismiss).toHaveBeenCalled();
 });
 
 test('render correct test ballot error screen when we are in live mode', async () => {
-  render(
-    <ScanErrorScreen
-      rejectionReason={RejectedScanningReason.InvalidTestMode}
-      isTestMode={false}
-    />
-  );
+  render(<ScanErrorScreen error="invalid_test_mode" isTestMode={false} />);
   await screen.findByText('Scanning Error');
   await screen.findByText('Test ballot detected.');
-  expect(screen.queryByText('Dismiss Error')).toBeNull();
 });
 
 test('render correct invalid precinct screen', async () => {
-  render(
-    <ScanErrorScreen
-      rejectionReason={RejectedScanningReason.InvalidPrecinct}
-      isTestMode
-    />
-  );
+  render(<ScanErrorScreen error="invalid_precinct" isTestMode />);
   await screen.findByText('Scanning Error');
   await screen.findByText(
     'Scanned ballot does not match the precinct this scanner is configured for.'
   );
-  expect(screen.queryByText('Dismiss Error')).toBeNull();
 });
 
 test('render correct invalid election hash screen', async () => {
-  const dismiss = jest.fn();
-  render(
-    <ScanErrorScreen
-      dismissError={dismiss}
-      rejectionReason={RejectedScanningReason.InvalidElectionHash}
-      isTestMode={false}
-    />
-  );
+  render(<ScanErrorScreen error="invalid_election_hash" isTestMode={false} />);
   await screen.findByText('Scanning Error');
   await screen.findByText(
     'Scanned ballot does not match the election this scanner is configured for.'
   );
-  fireEvent.click(await screen.findByText('Dismiss Error'));
-  expect(dismiss).toHaveBeenCalled();
 });
 
 test('render correct unreadable ballot screen', async () => {
-  const dismiss = jest.fn();
-  render(
-    <ScanErrorScreen
-      dismissError={dismiss}
-      rejectionReason={RejectedScanningReason.Unreadable}
-      isTestMode
-    />
-  );
+  render(<ScanErrorScreen error="unreadable" isTestMode />);
   await screen.findByText('Scanning Error');
   await screen.findByText(
     'There was a problem reading this ballot. Please try again.'
   );
-  fireEvent.click(await screen.findByText('Dismiss Error'));
-  expect(dismiss).toHaveBeenCalled();
 });
