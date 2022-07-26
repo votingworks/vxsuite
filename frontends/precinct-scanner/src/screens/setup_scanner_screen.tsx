b6ad06aa7d44cf334b98e99e33cafe1a54e976ad
Jonah Kagan <jonah@voting.works>
2022-07-26 12:19:04 -0700

    Update setup scanner screen
    - Combine both messages into one screen (that way we can easily show the right message both when kiosk-browser can't detect the scanner and when plustekctl isn't connected)
    - Simplify the copy

--- frontends/precinct-scanner/src/screens/setup_scanner_screen.tsx
+++ frontends/precinct-scanner/src/screens/setup_scanner_screen.tsx
@@ -4,21 +4,39 @@ import {
   CenteredLargeProse,
 } from '../components/layout';
 
-export function SetupScannerScreen(): JSX.Element {
+interface Props {
+  batteryIsCharging: boolean;
+}
+
+export function SetupScannerScreen({ batteryIsCharging }: Props): JSX.Element {
+  // If the power cord is plugged in, but we can't detect a scanner, it's an
+  // internal wiring issue. Otherwise if we can't detect the scanner, the power
+  // cord is likely not plugged in.
   return (
     <ScreenMainCenterChild infoBar={false}>
-      <CenteredLargeProse>
-        <h1>Scanner Not Detected</h1>
-        <p>
-          Please ask a poll worker to check that the power cable is connected to
-          an outlet.
-        </p>
-      </CenteredLargeProse>
+      {batteryIsCharging ? (
+        <CenteredLargeProse>
+          <h1>Internal Connection Problem</h1>
+          <p>Please tell the election clerk.</p>
+        </CenteredLargeProse>
+      ) : (
+        <CenteredLargeProse>
+          <h1>Power Cord Unplugged</h1>
+          <p>
+            Please ask a poll worker to check that the power cord is plugged in.
+          </p>
+        </CenteredLargeProse>
+      )}
     </ScreenMainCenterChild>
   );
 }
 
 /* istanbul ignore next */
-export function DefaultPreview(): JSX.Element {
-  return <SetupScannerScreen />;
+export function PowerDisconnectedPreview(): JSX.Element {
+  return <SetupScannerScreen batteryIsCharging={false} />;
+}
+
+/* istanbul ignore next */
+export function ScannerDisconnectedPreview(): JSX.Element {
+  return <SetupScannerScreen batteryIsCharging />;
 }
