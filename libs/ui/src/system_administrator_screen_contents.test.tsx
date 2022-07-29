e7778855c095f8b6dd98f748a531e5253555e815
Arsalan Sufi <arsalan@voting.works>
2022-07-29 15:05:35 -0400

    Add tests for sys admin screen shared component(s)

---
+++ libs/ui/src/system_administrator_screen_contents.test.tsx
@@ -0,0 +1,143 @@
+import React from 'react';
+import userEvent from '@testing-library/user-event';
+import { fakeKiosk, mockOf } from '@votingworks/test-utils';
+import { fakeLogger } from '@votingworks/logging';
+import { render, screen } from '@testing-library/react';
+import { usbstick } from '@votingworks/utils';
+
+import { isVxDev } from './config/features';
+import { SystemAdministratorScreenContents } from './system_administrator_screen_contents';
+
+jest.mock('./config/features', (): typeof import('./config/features') => {
+  return {
+    ...jest.requireActual('./config/features'),
+    isVxDev: jest.fn(),
+  };
+});
+
+beforeEach(() => {
+  mockOf(isVxDev).mockImplementation(() => false);
+  window.kiosk = undefined;
+});
+
+const renderTestCases: Array<{
+  description: string;
+  displayRemoveCardToLeavePromptPropValue?: boolean;
+  simulateVxDev?: boolean;
+  shouldRemoveCardToLeavePromptBeDisplayed: boolean;
+  shouldQuitButtonBeDisplayed: boolean;
+}> = [
+  {
+    description: 'base case',
+    shouldRemoveCardToLeavePromptBeDisplayed: false,
+    shouldQuitButtonBeDisplayed: false,
+  },
+  {
+    description: 'when displayRemoveCardToLeavePrompt is specified',
+    displayRemoveCardToLeavePromptPropValue: true,
+    shouldRemoveCardToLeavePromptBeDisplayed: true,
+    shouldQuitButtonBeDisplayed: false,
+  },
+  {
+    description: 'when on VxDev',
+    simulateVxDev: true,
+    shouldRemoveCardToLeavePromptBeDisplayed: false,
+    shouldQuitButtonBeDisplayed: true,
+  },
+  {
+    description:
+      'when displayRemoveCardToLeavePrompt is specified and on VxDev',
+    displayRemoveCardToLeavePromptPropValue: true,
+    simulateVxDev: true,
+    shouldRemoveCardToLeavePromptBeDisplayed: true,
+    shouldQuitButtonBeDisplayed: true,
+  },
+];
+
+test.each(renderTestCases)(
+  'SystemAdministratorScreenContents renders expected contents ($description)',
+  ({
+    displayRemoveCardToLeavePromptPropValue,
+    simulateVxDev,
+    shouldRemoveCardToLeavePromptBeDisplayed,
+    shouldQuitButtonBeDisplayed,
+  }) => {
+    if (simulateVxDev) {
+      mockOf(isVxDev).mockImplementation(() => true);
+    }
+    const logger = fakeLogger();
+    const unconfigureMachine = jest.fn();
+    render(
+      <SystemAdministratorScreenContents
+        displayRemoveCardToLeavePrompt={displayRemoveCardToLeavePromptPropValue}
+        logger={logger}
+        primaryText="To adjust settings for the current election, please insert an Election Manager card."
+        unconfigureMachine={unconfigureMachine}
+        usbDriveStatus={usbstick.UsbDriveStatus.mounted}
+      />
+    );
+
+    screen.getByText(
+      'To adjust settings for the current election, please insert an Election Manager card.'
+    );
+    if (shouldRemoveCardToLeavePromptBeDisplayed) {
+      screen.getByText(
+        'Remove the System Administrator card to leave this screen.'
+      );
+    } else {
+      expect(
+        screen.queryByText(
+          'Remove the System Administrator card to leave this screen.'
+        )
+      ).not.toBeInTheDocument();
+    }
+
+    // These buttons are all tested further in their respective test files
+    screen.getByRole('button', { name: 'Reboot from USB' });
+    screen.getByRole('button', { name: 'Reboot to BIOS' });
+    screen.getByRole('button', { name: 'Unconfigure Machine' });
+
+    if (shouldQuitButtonBeDisplayed) {
+      screen.getByRole('button', { name: 'Quit' });
+    } else {
+      expect(
+        screen.queryByRole('button', { name: 'Quit' })
+      ).not.toBeInTheDocument();
+    }
+  }
+);
+
+test('Quit button makes expected call', () => {
+  mockOf(isVxDev).mockImplementation(() => true);
+  window.kiosk = fakeKiosk();
+  const logger = fakeLogger();
+  const unconfigureMachine = jest.fn();
+  render(
+    <SystemAdministratorScreenContents
+      logger={logger}
+      primaryText="To adjust settings for the current election, please insert an Election Manager card."
+      unconfigureMachine={unconfigureMachine}
+      usbDriveStatus={usbstick.UsbDriveStatus.mounted}
+    />
+  );
+
+  userEvent.click(screen.getByRole('button', { name: 'Quit' }));
+  expect(window.kiosk.quit).toBeCalledTimes(1);
+});
+
+test('Quit button does nothing when kiosk is undefined', () => {
+  mockOf(isVxDev).mockImplementation(() => true);
+  window.kiosk = undefined;
+  const logger = fakeLogger();
+  const unconfigureMachine = jest.fn();
+  render(
+    <SystemAdministratorScreenContents
+      logger={logger}
+      primaryText="To adjust settings for the current election, please insert an Election Manager card."
+      unconfigureMachine={unconfigureMachine}
+      usbDriveStatus={usbstick.UsbDriveStatus.mounted}
+    />
+  );
+
+  userEvent.click(screen.getByRole('button', { name: 'Quit' }));
+});
---
+++ libs/ui/src/unconfigure_machine_button.test.tsx
@@ -0,0 +1,75 @@
+import React from 'react';
+import userEvent from '@testing-library/user-event';
+import { render, screen, waitFor, within } from '@testing-library/react';
+import { sleep } from '@votingworks/utils';
+
+import {
+  MIN_TIME_TO_UNCONFIGURE_MACHINE_MS,
+  UnconfigureMachineButton,
+} from './unconfigure_machine_button';
+
+jest.mock('@votingworks/utils', (): typeof import('@votingworks/utils') => {
+  return {
+    ...jest.requireActual('@votingworks/utils'),
+    sleep: jest.fn(),
+  };
+});
+
+test('UnconfigureMachineButton interactions', async () => {
+  const unconfigureMachine = jest.fn();
+  render(<UnconfigureMachineButton unconfigureMachine={unconfigureMachine} />);
+
+  // Cancel the first time
+  userEvent.click(screen.getByRole('button', { name: 'Unconfigure Machine' }));
+  let modal = await screen.findByRole('alertdialog');
+  within(modal).getByRole('heading', {
+    name: 'Delete all election data?',
+  });
+  userEvent.click(within(modal).getByRole('button', { name: 'Cancel' }));
+  await waitFor(() => expect(modal).not.toBeInTheDocument());
+
+  // Proceed the second time
+  userEvent.click(screen.getByRole('button', { name: 'Unconfigure Machine' }));
+  modal = await screen.findByRole('alertdialog');
+  within(modal).getByRole('heading', {
+    name: 'Delete all election data?',
+  });
+  userEvent.click(
+    within(modal).getByRole('button', {
+      name: 'Yes, Delete Election Data',
+    })
+  );
+  await within(modal).findByText(/Deleting election data/);
+  await waitFor(() =>
+    expect(screen.queryByRole('alertdialog')).not.toBeInTheDocument()
+  );
+
+  expect(unconfigureMachine).toHaveBeenCalledTimes(1);
+  expect(sleep).toHaveBeenCalledTimes(1);
+  const sleepTime = (sleep as jest.Mock).mock.calls[0][0];
+  expect(sleepTime).toBeGreaterThan(0);
+  expect(sleepTime).toBeLessThan(MIN_TIME_TO_UNCONFIGURE_MACHINE_MS);
+});
+
+test('UnconfigureMachineButton does not sleep when not necessary', async () => {
+  const unconfigureMachine = jest.fn(async () => {
+    await new Promise<void>((resolve) => {
+      setTimeout(resolve, MIN_TIME_TO_UNCONFIGURE_MACHINE_MS);
+    });
+  });
+  render(<UnconfigureMachineButton unconfigureMachine={unconfigureMachine} />);
+
+  userEvent.click(screen.getByRole('button', { name: 'Unconfigure Machine' }));
+  const modal = await screen.findByRole('alertdialog');
+  userEvent.click(
+    within(modal).getByRole('button', {
+      name: 'Yes, Delete Election Data',
+    })
+  );
+  await waitFor(() =>
+    expect(screen.queryByRole('alertdialog')).not.toBeInTheDocument()
+  );
+
+  expect(unconfigureMachine).toHaveBeenCalledTimes(1);
+  expect(sleep).toHaveBeenCalledTimes(0);
+});
