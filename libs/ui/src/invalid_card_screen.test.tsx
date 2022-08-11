e6c566be6c21978f0a1e261f089470cb75238400
Arsalan Sufi <arsalan@voting.works>
2022-08-11 10:08:10 -0400

    Update InvalidCardScreen tests

--- libs/ui/src/invalid_card_screen.test.tsx
+++ libs/ui/src/invalid_card_screen.test.tsx
@@ -7,11 +7,28 @@ const testCases: Array<{
   description: string;
   reason: Props['reason'];
   recommendedAction?: string;
+  expectedHeading: string;
   expectedText: string;
 }> = [
   {
+    description: 'card is backwards',
+    reason: 'card_error',
+    expectedHeading: 'Card is Backwards',
+    expectedText: 'Remove the card, turn it around, and insert it again.',
+  },
+  {
+    description:
+      'election manager card election hash does not match machine election hash',
+    reason: 'election_manager_wrong_election',
+    expectedHeading: 'Invalid Card',
+    expectedText:
+      'The inserted Election Manager card is programmed for another election and cannot be used to unlock this machine. ' +
+      'Please insert a valid Election Manager or System Administrator card.',
+  },
+  {
     description: 'machine not configured',
     reason: 'machine_not_configured',
+    expectedHeading: 'Invalid Card',
     expectedText:
       'This machine is unconfigured and cannot be unlocked with this card. ' +
       'Please insert a valid Election Manager or System Administrator card.',
@@ -20,21 +37,15 @@ const testCases: Array<{
     description: 'machine not configured, custom recommended action',
     reason: 'machine_not_configured',
     recommendedAction: 'Please insert a System Administrator card.',
+    expectedHeading: 'Invalid Card',
     expectedText:
       'This machine is unconfigured and cannot be unlocked with this card. ' +
       'Please insert a System Administrator card.',
   },
   {
-    description:
-      'election manager card election hash does not match machine election hash',
-    reason: 'election_manager_wrong_election',
-    expectedText:
-      'The inserted Election Manager card is programmed for another election and cannot be used to unlock this machine. ' +
-      'Please insert a valid Election Manager or System Administrator card.',
-  },
-  {
     description: 'other error',
     reason: 'invalid_user_on_card',
+    expectedHeading: 'Invalid Card',
     expectedText:
       'Please insert a valid Election Manager or System Administrator card.',
   },
@@ -42,7 +53,7 @@ const testCases: Array<{
 
 test.each(testCases)(
   'InvalidCardScreen renders expected text ($description)',
-  ({ reason, recommendedAction, expectedText }) => {
+  ({ reason, recommendedAction, expectedHeading, expectedText }) => {
     render(
       <InvalidCardScreen
         reason={reason}
@@ -50,6 +61,7 @@ test.each(testCases)(
       />
     );
 
+    screen.getByRole('heading', { name: expectedHeading });
     screen.getByText(expectedText);
   }
 );
