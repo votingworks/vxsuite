5cc9a9c6ee49fd3c4e206bf647c6ba1789026f73
Jonah Kagan <jonah@voting.works>
2022-08-17 10:07:38 -0700

    Move template routes to api/config.ts

--- frontends/precinct-scanner/src/api/config.test.ts
+++ frontends/precinct-scanner/src/api/config.test.ts
@@ -1,6 +1,7 @@
 import { electionSampleDefinition as testElectionDefinition } from '@votingworks/fixtures';
 import { Scan } from '@votingworks/api';
 import fetchMock from 'fetch-mock';
+import { Buffer } from 'buffer';
 import * as config from './config';
 
 test('GET /config/election', async () => {
@@ -152,4 +153,104 @@ test('setMarkThresholds deletes', async () => {
       method: 'DELETE',
     })
   ).toHaveLength(1);
+});
+
+test('addTemplates configures the server with the contained election', async () => {
+  fetchMock.patchOnce('/precinct-scanner/config/election', {
+    body: { status: 'ok' },
+  });
+
+  await new Promise<void>((resolve, reject) => {
+    config
+      .addTemplates({ testElectionDefinition, ballots: [] })
+      .on('error', (error) => {
+        reject(error);
+      })
+      .on('completed', () => {
+        resolve();
+      });
+  });
+});
+
+test('addTemplates emits an event each time a ballot begins uploading', async () => {
+  fetchMock.patchOnce('/precinct-scanner/config/election', {
+    body: { status: 'ok' },
+  });
+  fetchMock.post('/precinct-scanner/config/addTemplates', {
+    body: { status: 'ok' },
+  });
+
+  const uploading = jest.fn();
+
+  await new Promise<void>((resolve, reject) => {
+    config
+      .addTemplates({
+        testElectionDefinition,
+        ballots: [
+          {
+            ballotConfig: {
+              ballotStyleId: '5',
+              precinctId: '21',
+              isLiveMode: true,
+              isAbsentee: false,
+              contestIds: ['a', 'b', 'c'],
+              filename: 'ballot-1.pdf',
+              locales: { primary: 'en-US' },
+              layoutFilename: 'layout-1.json',
+            },
+            pdf: Buffer.of(),
+            layout: [],
+          },
+          {
+            ballotConfig: {
+              ballotStyleId: '5',
+              precinctId: '21',
+              isLiveMode: false,
+              isAbsentee: false,
+              contestIds: ['a', 'b', 'c'],
+              filename: 'ballot-1-test.pdf',
+              locales: { primary: 'en-US' },
+              layoutFilename: 'layout-1.json',
+            },
+            pdf: Buffer.of(),
+            layout: [],
+          },
+        ],
+      })
+      .on('error', (error) => {
+        reject(error);
+      })
+      .on('uploading', uploading)
+      .on('completed', () => {
+        resolve();
+      });
+  });
+
+  expect(
+    fetchMock.calls('/precinct-scanner/config/addTemplates').length
+  ).toEqual(2);
+});
+
+test('addTemplates emits error on API failure', async () => {
+  const body: Scan.PatchElectionConfigResponse = {
+    status: 'error',
+    errors: [{ type: 'invalid-value', message: 'bad election!' }],
+  };
+  fetchMock.patchOnce('/precinct-scanner/config/election', {
+    status: 400,
+    body,
+  });
+
+  await expect(
+    new Promise<void>((resolve, reject) => {
+      config
+        .addTemplates({ testElectionDefinition, ballots: [] })
+        .on('error', (error) => {
+          reject(error);
+        })
+        .on('completed', () => {
+          resolve();
+        });
+    })
+  ).rejects.toThrowError();
 });
--- frontends/precinct-scanner/src/api/config.ts
+++ frontends/precinct-scanner/src/api/config.ts
@@ -6,6 +6,8 @@ import {
   safeParseJson,
 } from '@votingworks/types';
 import { ErrorsResponse, OkResponse, Scan } from '@votingworks/api';
+import { BallotPackage, BallotPackageEntry, assert } from '@votingworks/utils';
+import EventEmitter from 'events';
 
 async function patch<Body extends string | ArrayBuffer | unknown>(
   url: string,
@@ -165,4 +167,102 @@ export async function setCurrentPrecinctId(
       }
     );
   }
+}
+
+export interface AddTemplatesEvents extends EventEmitter {
+  on(
+    event: 'configuring',
+    callback: (
+      pkg: BallotPackage,
+      electionDefinition: ElectionDefinition
+    ) => void
+  ): this;
+  on(
+    event: 'uploading',
+    callback: (pkg: BallotPackage, entry: BallotPackageEntry) => void
+  ): this;
+  on(event: 'completed', callback: (pkg: BallotPackage) => void): this;
+  on(event: 'error', callback: (error: Error) => void): this;
+  off(
+    event: 'configuring',
+    callback: (
+      pkg: BallotPackage,
+      electionDefinition: ElectionDefinition
+    ) => void
+  ): this;
+  off(
+    event: 'uploading',
+    callback: (pkg: BallotPackage, entry: BallotPackageEntry) => void
+  ): this;
+  off(event: 'completed', callback: (pkg: BallotPackage) => void): this;
+  off(event: 'error', callback: (error: Error) => void): this;
+  emit(
+    event: 'configuring',
+    pkg: BallotPackage,
+    electionDefinition: ElectionDefinition
+  ): boolean;
+  emit(
+    event: 'uploading',
+    pkg: BallotPackage,
+    entry: BallotPackageEntry
+  ): boolean;
+  emit(event: 'completed', pkg: BallotPackage): boolean;
+  emit(event: 'error', error: Error): boolean;
+}
+
+export function addTemplates(pkg: BallotPackage): AddTemplatesEvents {
+  const result: AddTemplatesEvents = new EventEmitter();
+
+  setImmediate(async () => {
+    try {
+      result.emit('configuring', pkg, pkg.testElectionDefinition);
+      await setElection(pkg.testElectionDefinition.electionData);
+
+      for (const ballot of pkg.ballots) {
+        result.emit('uploading', pkg, ballot);
+
+        const body = new FormData();
+
+        body.append(
+          'ballots',
+          new Blob([ballot.pdf], { type: 'application/pdf' }),
+          ballot.ballotConfig.filename
+        );
+
+        body.append(
+          'metadatas',
+          new Blob([JSON.stringify(ballot.ballotConfig)], {
+            type: 'application/json',
+          }),
+          'ballot-config.json'
+        );
+
+        if (ballot.layout) {
+          body.append(
+            'layouts',
+            new Blob([JSON.stringify(ballot.layout)], {
+              type: 'application/json',
+            }),
+            ballot.ballotConfig.layoutFilename
+          );
+        }
+
+        await fetch('/precinct-scanner/config/addTemplates', {
+          method: 'POST',
+          body,
+        });
+      }
+
+      result.emit('completed', pkg);
+    } catch (error) {
+      assert(error instanceof Error);
+      result.emit('error', error);
+    }
+  });
+
+  return result;
+}
+
+export async function doneTemplates(): Promise<void> {
+  await fetch('/precinct-scanner/config/doneTemplates', { method: 'POST' });
 }
