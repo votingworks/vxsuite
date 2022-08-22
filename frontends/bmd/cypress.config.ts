0e5713576fd7bac9f2634285f7ca656997fe9bf3
Jonah Kagan <jonah@voting.works>
2022-08-22 14:19:26 -0700

    chore(all): Increase default Cypress timeout from 4s to 8s

--- frontends/bmd/cypress.config.ts
+++ frontends/bmd/cypress.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'cypress'
 export default defineConfig({
   viewportHeight: 1080,
   viewportWidth: 1920,
+  defaultCommandTimeout: 8_000,
   e2e: {
     baseUrl: 'http://localhost:3000',
     specPattern: 'cypress/tests/**/*.cy.{js,jsx,ts,tsx}',
--- integration-testing/bsd/cypress.config.ts
+++ integration-testing/bsd/cypress.config.ts
@@ -6,6 +6,7 @@ import { defineConfig } from 'cypress';
 export default defineConfig({
   viewportHeight: 1080,
   viewportWidth: 1920,
+  defaultCommandTimeout: 8_000,
   e2e: {
     baseUrl: 'http://localhost:3000',
   },
