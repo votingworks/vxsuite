3dec8cae80b6da54874a1976a769614271f4f173
Jonah Kagan <jonah@voting.works>
2022-08-22 16:31:48 -0400

    fix(scan): Reconnect to plustek after any unexpected scanning error (#2452)
    Previously, if we got an unexpected scanning error and the Plustek was still responsive, we would either automatically retry scanning or reject and let the voter retry, depending on the paper status. Now, if we get an unexpected scanning error, we handle it like any other unexpected error, reconnecting to Plustek. This will hopefully make us more resilient to scanning errors where we don't know the root cause.

--- services/scan/src/precinct_scanner_app.test.ts
+++ services/scan/src/precinct_scanner_app.test.ts
@@ -1042,7 +1042,9 @@ test('insert second ballot while first ballot is returning', async () => {
 });
 
 test('jam on scan', async () => {
-  const { app, mockPlustek } = await createApp();
+  const { app, mockPlustek } = await createApp({
+    DELAY_RECONNECT_ON_UNEXPECTED_ERROR: 500,
+  });
   await configureApp(app);
 
   await mockPlustek.simulateLoadSheet(ballotImages.completeBmd);
@@ -1050,9 +1052,10 @@ test('jam on scan', async () => {
 
   mockPlustek.simulateJamOnNextOperation();
   await post(app, '/precinct-scanner/scanner/scan');
-  await waitForStatus(app, { state: 'jammed' });
-
-  await mockPlustek.simulateRemoveSheet();
+  await waitForStatus(app, {
+    state: 'recovering_from_error',
+    error: 'plustek_error',
+  });
   await waitForStatus(app, { state: 'no_paper' });
 });
 
@@ -1214,8 +1217,10 @@ test('scan fails repeatedly and eventually gives up', async () => {
   await waitForStatus(app, { state: 'rejected', error: 'scanning_failed' });
 });
 
-test('scan fails with the paper in the back afterwards', async () => {
-  const { app, mockPlustek } = await createApp();
+test('scan fails due to plustek error', async () => {
+  const { app, mockPlustek } = await createApp({
+    DELAY_RECONNECT_ON_UNEXPECTED_ERROR: 500,
+  });
   await configureApp(app);
 
   await mockPlustek.simulateLoadSheet(ballotImages.completeBmd);
@@ -1224,8 +1229,11 @@ test('scan fails with the paper in the back afterwards', async () => {
   await post(app, '/precinct-scanner/scanner/scan');
   await expectStatus(app, { state: 'scanning' });
   mockPlustek.simulateScanError('bad_scan_result');
-  await waitForStatus(app, { state: 'rejecting', error: 'plustek_error' });
-  await waitForStatus(app, { state: 'rejected', error: 'plustek_error' });
+  await waitForStatus(app, {
+    state: 'recovering_from_error',
+    error: 'plustek_error',
+  });
+  await waitForStatus(app, { state: 'no_paper' });
 });
 
 test('scanning time out', async () => {
