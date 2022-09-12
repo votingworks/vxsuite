51ca657543ceaba30e9d5b4bbed25dbed71c7c88
Beau Smith <beau@beausmith.com>
2022-09-12 10:45:43 -0700

    feat(ui): Select supports primary prop

--- libs/ui/src/select.tsx
+++ libs/ui/src/select.tsx
@@ -2,9 +2,10 @@
 import styled from 'styled-components';
 
 interface Props {
-  fullWidth?: boolean;
-  large?: boolean;
-  small?: boolean;
+  readonly fullWidth?: boolean;
+  readonly large?: boolean;
+  readonly small?: boolean;
+  readonly primary?: boolean;
 }
 
 export const Select = styled.select<Props>`
@@ -13,8 +14,14 @@ export const Select = styled.select<Props>`
   border: none;
   border-radius: 0.25rem;
   box-sizing: border-box;
-  background-color: rgb(211, 211, 211);
-  background-image: url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 287.87"><path style="fill:darkslategrey" d="M502.54,9.46A30.83,30.83,0,0,0,479.78,0H32.22A30.84,30.84,0,0,0,9.63,9.46,30.81,30.81,0,0,0,0,31.87,30.8,30.8,0,0,0,9.46,54.46l224.13,224q22.41,18.91,44.82,0L502.54,54.29q19-22.49-.17-44.83Z" /></svg>');
+  background-color: ${({ primary }) =>
+    (primary && 'rgb(71, 167, 75)') || 'rgb(211, 211, 211)'};
+  /* stylelint-disable-next-line string-no-newline */
+  background-image: url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 287.87"><path style="fill:${({
+    primary,
+  }) =>
+    (primary && 'white') ||
+    'darkslategrey'}" d="M502.54,9.46A30.83,30.83,0,0,0,479.78,0H32.22A30.84,30.84,0,0,0,9.63,9.46,30.81,30.81,0,0,0,0,31.87,30.8,30.8,0,0,0,9.46,54.46l224.13,224q22.41,18.91,44.82,0L502.54,54.29q19-22.49-.17-44.83Z" /></svg>');
   background-position: right
     ${({ large, small }) =>
       (small && '0.5em') || (large && '1.125em') || '0.75em'}
@@ -30,6 +37,7 @@ export const Select = styled.select<Props>`
     (large && '1em 2.5em 1em 1.75em') ||
     '0.75rem 1.75rem 0.75rem 1rem'};
   line-height: 1.25;
+  color: ${({ primary }) => (primary && '#FFFFFF') || 'black'};
   font-size: ${({ large }) => large && '1.25em'};
   appearance: none;
   &:disabled {
