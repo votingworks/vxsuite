288b9508c8e4fd89a56370dea1d4eca4c5a7a2ea
Brian Donovan <1938+eventualbuddha@users.noreply.github.com>
2022-09-19 11:02:06 -0700

    fix(admin): make adjudicating official candidates work
    Closes #2576
    
    We weren't properly passing the `adjudicatedOptionId` value when initially creating an adjudication for an official candidate. We were doing so when changing the adjudication, so that flow actually did work correctly and was the only way to associate a transcription with an official candidate.

--- frontends/election-manager/src/screens/write_ins_screen.test.tsx
+++ frontends/election-manager/src/screens/write_ins_screen.test.tsx
@@ -1,6 +1,8 @@
 import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
+import { Admin } from '@votingworks/api';
 import { electionMinimalExhaustiveSampleFixtures } from '@votingworks/fixtures';
+import { typedAs } from '@votingworks/utils';
 import React from 'react';
 import { renderInAppContext } from '../../test/render_in_app_context';
 import { ElectionManagerStoreMemoryBackend } from '../lib/backends';
@@ -67,4 +69,67 @@ test('ballot pagination', async () => {
       break;
     }
   }
+});
+
+test('adjudication', async () => {
+  const backend = new ElectionManagerStoreMemoryBackend({
+    electionDefinition,
+  });
+  await backend.addCastVoteRecordFile(
+    new File([abbreviatedCvrData], 'cvrs.jsonl')
+  );
+
+  renderInAppContext(<WriteInsScreen />, {
+    electionDefinition,
+    backend,
+  });
+
+  // transcribe
+  userEvent.click(await screen.findByText('Transcribe (8 new)'));
+  userEvent.click(await screen.findByText('Add new +'));
+  userEvent.type(
+    await screen.findByLabelText('Transcribed Value'),
+    'Dark Helmet'
+  );
+  userEvent.click(await screen.findByText('Save'));
+  expect(await screen.findByText('Dark Helmet')).toBeInTheDocument();
+
+  userEvent.click(await screen.findByText('Exit'));
+
+  // adjudicate
+  userEvent.click(await screen.findByText('Adjudicate (1 new)'));
+  expect(await screen.findAllByText('Dark Helmet')).toHaveLength(2); // 1 in the table, 1 in the adjudication list
+
+  userEvent.selectOptions(
+    await screen.findByRole('combobox'),
+    'Zebra (official candidate)'
+  );
+
+  expect(await screen.findByText('Change')).toBeInTheDocument();
+  expect(await screen.findByText('Dark Helmet')).toBeInTheDocument();
+  expect(
+    await screen.findByText('Zebra (official candidate)')
+  ).toBeInTheDocument();
+
+  const summary = await backend.getWriteInSummary({
+    contestId: 'zoo-council-mammal',
+  });
+  const adjudicatedSummaryEntry = summary.find(
+    (entry) => entry.transcribedValue === 'Dark Helmet'
+  );
+
+  expect(adjudicatedSummaryEntry).toEqual(
+    typedAs<Admin.WriteInSummaryEntry>({
+      contestId: 'zoo-council-mammal',
+      transcribedValue: 'Dark Helmet',
+      writeInAdjudication: {
+        adjudicatedOptionId: 'zebra',
+        adjudicatedValue: 'Zebra (official candidate)',
+        contestId: 'zoo-council-mammal',
+        id: expect.any(String),
+        transcribedValue: 'Dark Helmet',
+      },
+      writeInCount: 1,
+    })
+  );
 });
