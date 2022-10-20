4c5a768fe42db71b246bab5857ccae7e04ac5935
Kofi Ohene-Adu <kofi@voting.works>
2022-10-20 17:56:35 -0500

    [generatePin] don't return weak PINs

--- libs/utils/src/pins.test.ts
+++ libs/utils/src/pins.test.ts
@@ -1,8 +1,10 @@
 import { mockOf } from '@votingworks/test-utils';
 import fc from 'fast-check';
+import randomBytes from 'randombytes';
+import { Buffer } from 'buffer';
 
 import { isFeatureFlagEnabled } from './features';
-import { generatePin, hyphenatePin } from './pins';
+import { generatePin, hyphenatePin, MIN_PIN_LENGTH } from './pins';
 
 jest.mock('./features', (): typeof import('./features') => {
   return {
@@ -11,26 +13,33 @@ jest.mock('./features', (): typeof import('./features') => {
   };
 });
 
+jest.mock('randombytes', (): typeof import('randombytes') => jest.fn());
+
+function setMockRandomBytesResultOnce(pin: string) {
+  const pinAsByteArray = Buffer.from(
+    pin.split('').map((char) => Number.parseInt(char, 10))
+  );
+  mockOf(randomBytes).mockImplementationOnce(() => pinAsByteArray);
+}
+
 beforeEach(() => {
   mockOf(isFeatureFlagEnabled).mockImplementation(() => false);
+  mockOf(randomBytes).mockReset();
 });
 
-test('generatePin generates PINs', () => {
-  const digitRegex = /^[0-9]+$/;
+test('generatePin defaults to MIN_PIN_LENGTH', () => {
+  setMockRandomBytesResultOnce('1029384756');
+  expect(generatePin()).toHaveLength(MIN_PIN_LENGTH);
+});
 
-  // check default length
-  expect(generatePin()).toHaveLength(6);
+test('generatePin generates PINs of specified length', () => {
+  setMockRandomBytesResultOnce('1020304050');
+  expect(generatePin(7)).toBe('1020304');
+});
 
-  fc.assert(
-    fc.property(fc.integer(1, 100), (length) => {
-      const pin = generatePin(length);
-      expect(pin).toMatch(digitRegex);
-      expect(pin).toHaveLength(length);
-    })
-  );
-
-  expect(() => generatePin(0)).toThrow('PIN length must be greater than 0');
-  expect(() => generatePin(-1)).toThrow('PIN length must be greater than 0');
+test('generatePin throws on invalid PIN length', () => {
+  expect(() => generatePin(0)).toThrow(/PIN length must be greater than \d/);
+  expect(() => generatePin(-1)).toThrow(/PIN length must be greater than \d/);
 });
 
 test('generatePIN generates PINs with all zeros when all-zero smartcard PIN generation feature flag is enabled', () => {
@@ -43,6 +52,48 @@ test('generatePIN generates PINs with all zeros when all-zero smartcard PIN gene
       expect(pin).toHaveLength(length);
     })
   );
+});
+
+test('generatePin returns first non-weak random PINs', () => {
+  const nonWeakPin = '551028';
+
+  setMockRandomBytesResultOnce(nonWeakPin);
+  setMockRandomBytesResultOnce('555555');
+  setMockRandomBytesResultOnce('123456');
+
+  expect(generatePin()).toBe(nonWeakPin);
+  expect(mockOf(randomBytes)).toBeCalledTimes(1);
+});
+
+test('generatePin skips PINs with repeating digits', () => {
+  const nonWeakPin = '223344';
+
+  setMockRandomBytesResultOnce('555555');
+  setMockRandomBytesResultOnce('122227');
+  setMockRandomBytesResultOnce(nonWeakPin);
+
+  expect(generatePin()).toBe(nonWeakPin);
+});
+
+test('generatePin skips PINs with long strings of sequential digits', () => {
+  const nonWeakPin = '123789';
+
+  setMockRandomBytesResultOnce('123456');
+  setMockRandomBytesResultOnce('654321');
+  setMockRandomBytesResultOnce(nonWeakPin);
+
+  expect(generatePin()).toBe(nonWeakPin);
+});
+
+test('generatePin skips PINs with not enough digit variety', () => {
+  const nonWeakPin = '204060';
+
+  setMockRandomBytesResultOnce('121212');
+  setMockRandomBytesResultOnce('363636');
+  setMockRandomBytesResultOnce('188188');
+  setMockRandomBytesResultOnce(nonWeakPin);
+
+  expect(generatePin()).toBe(nonWeakPin);
 });
 
 test('hyphenatePin hyphenates PINs', () => {
