58439f38b3a60a51611879f8136af76754fcd26a
Kofi Ohene-Adu <kofi@voting.works>
2022-10-25 11:14:23 -0500

    [generatePin] Avoid generating weak PINs (#2737)
    - Creates denylist of common weak pin patterns (repeated digits, sequential digits) and checks against them when generating random PINs.
    -  Retries random PIN generation until we find a non-weak PIN.
    - Also raises minimum PIN length from 1 to 6

--- libs/utils/src/pins.test.ts
+++ libs/utils/src/pins.test.ts
@@ -1,8 +1,15 @@
 import { mockOf } from '@votingworks/test-utils';
 import fc from 'fast-check';
+import randomBytes from 'randombytes';
+import { Buffer } from 'buffer';
 
 import { isFeatureFlagEnabled } from './features';
-import { generatePin, hyphenatePin } from './pins';
+import {
+  generatePin,
+  hyphenatePin,
+  MAX_PIN_LENGTH,
+  MIN_PIN_LENGTH,
+} from './pins';
 
 jest.mock('./features', (): typeof import('./features') => {
   return {
@@ -11,38 +18,103 @@ jest.mock('./features', (): typeof import('./features') => {
   };
 });
 
+jest.mock('randombytes', (): typeof import('randombytes') => jest.fn());
+
+const WEAK_PIN_EXAMPLES: string[] = [
+  '000000',
+  '111111',
+  '222222',
+  '333333',
+  '444444',
+  '555555',
+  '666666',
+  '777777',
+  '888888',
+  '999999',
+  '122227',
+  '588881',
+  '129999',
+  '123456',
+  '654321',
+  '012345',
+  '543210',
+  '456789',
+  '987654',
+  '121212',
+  '363636',
+  '555000',
+  '883333',
+  '188188',
+  '200200',
+];
+
+function setMockRandomBytesResultOnce(pin: string) {
+  const pinAsByteArray = Buffer.from(
+    pin.split('').map((char) => Number.parseInt(char, 10))
+  );
+  mockOf(randomBytes).mockImplementationOnce(() => pinAsByteArray);
+}
+
 beforeEach(() => {
   mockOf(isFeatureFlagEnabled).mockImplementation(() => false);
+  mockOf(randomBytes).mockReset();
 });
 
-test('generatePin generates PINs', () => {
-  const digitRegex = /^[0-9]+$/;
+test('generatePin defaults to MIN_PIN_LENGTH', () => {
+  setMockRandomBytesResultOnce('1029384756');
+  expect(generatePin()).toHaveLength(MIN_PIN_LENGTH);
+});
 
-  // check default length
-  expect(generatePin()).toHaveLength(6);
+test('generatePin generates PINs of specified length', () => {
+  setMockRandomBytesResultOnce('1020304050');
+  expect(generatePin(7)).toBe('1020304');
+});
 
-  fc.assert(
-    fc.property(fc.integer(1, 100), (length) => {
-      const pin = generatePin(length);
-      expect(pin).toMatch(digitRegex);
-      expect(pin).toHaveLength(length);
-    })
+test('generatePin throws on invalid PIN length', () => {
+  expect(() => generatePin(0)).toThrow(
+    /PIN length must be greater than or equal to \d/
   );
-
-  expect(() => generatePin(0)).toThrow('PIN length must be greater than 0');
-  expect(() => generatePin(-1)).toThrow('PIN length must be greater than 0');
+  expect(() => generatePin(-1)).toThrow(
+    /PIN length must be greater than or equal to \d/
+  );
+  expect(() => generatePin(50)).toThrow(
+    /PIN length must be less than or equal to \d/
+  );
 });
 
 test('generatePIN generates PINs with all zeros when all-zero smartcard PIN generation feature flag is enabled', () => {
   mockOf(isFeatureFlagEnabled).mockImplementation(() => true);
 
   fc.assert(
-    fc.property(fc.integer(1, 100), (length) => {
+    fc.property(fc.integer(MIN_PIN_LENGTH, MAX_PIN_LENGTH), (length) => {
       const pin = generatePin(length);
       expect(pin).toMatch(/^[0]+$/);
       expect(pin).toHaveLength(length);
     })
   );
+});
+
+test('generatePin returns first non-weak random PIN', () => {
+  const nonWeakPin = '551028';
+
+  setMockRandomBytesResultOnce(nonWeakPin);
+  for (const weakPin of WEAK_PIN_EXAMPLES) {
+    setMockRandomBytesResultOnce(weakPin);
+  }
+
+  expect(generatePin()).toBe(nonWeakPin);
+  expect(mockOf(randomBytes)).toBeCalledTimes(1);
+});
+
+test('generatePin skips weak PINs', () => {
+  const nonWeakPin = '223344';
+
+  for (const weakPin of WEAK_PIN_EXAMPLES) {
+    setMockRandomBytesResultOnce(weakPin);
+  }
+  setMockRandomBytesResultOnce(nonWeakPin);
+
+  expect(generatePin()).toBe(nonWeakPin);
 });
 
 test('hyphenatePin hyphenates PINs', () => {
