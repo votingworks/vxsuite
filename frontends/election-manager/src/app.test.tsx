ddd1f79238db35a78e6f5e7419f3b2d9030a7b88
Ben Adida <ben@adida.net>
2022-11-10 17:03:31 -0500

    fix(sems): SEMS wants a .txt extension now.

--- frontends/election-manager/src/app.test.tsx
+++ frontends/election-manager/src/app.test.tsx
@@ -658,7 +658,7 @@ test('tabulating CVRs', async () => {
   fireEvent.click(getByText('Save SEMS Results'));
   jest.advanceTimersByTime(2000);
   getByText(
-    'votingworks-sems-live-results_choctaw-county_mock-general-election-choctaw-2020_2020-11-03_22-22-00.csv'
+    'votingworks-sems-live-results_choctaw-county_mock-general-election-choctaw-2020_2020-11-03_22-22-00.txt'
   );
 
   fireEvent.click(getByText('Save'));
@@ -669,7 +669,7 @@ test('tabulating CVRs', async () => {
     expect(mockKiosk.writeFile).toHaveBeenCalledTimes(2);
     expect(mockKiosk.writeFile).toHaveBeenNthCalledWith(
       2,
-      'fake mount point/votingworks-sems-live-results_choctaw-county_mock-general-election-choctaw-2020_2020-11-03_22-22-00.csv',
+      'fake mount point/votingworks-sems-live-results_choctaw-county_mock-general-election-choctaw-2020_2020-11-03_22-22-00.txt',
       'test-content'
     );
   });
@@ -796,7 +796,7 @@ test('tabulating CVRs with SEMS file', async () => {
   fireEvent.click(getByText('Save SEMS Results'));
   jest.advanceTimersByTime(2000);
   getByText(
-    'votingworks-sems-live-results_choctaw-county_mock-general-election-choctaw-2020_2020-11-03_22-22-00.csv'
+    'votingworks-sems-live-results_choctaw-county_mock-general-election-choctaw-2020_2020-11-03_22-22-00.txt'
   );
 
   fireEvent.click(getByText('Save'));
@@ -807,7 +807,7 @@ test('tabulating CVRs with SEMS file', async () => {
     expect(mockKiosk.writeFile).toHaveBeenCalledTimes(1);
     expect(mockKiosk.writeFile).toHaveBeenNthCalledWith(
       1,
-      'fake mount point/votingworks-sems-live-results_choctaw-county_mock-general-election-choctaw-2020_2020-11-03_22-22-00.csv',
+      'fake mount point/votingworks-sems-live-results_choctaw-county_mock-general-election-choctaw-2020_2020-11-03_22-22-00.txt',
       'test-content'
     );
   });
