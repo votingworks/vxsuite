584e4c7033468c9df3b8e956c3df91dd923168ec
Jonah Kagan <jonah@voting.works>
2022-11-28 11:58:19 -0800

    Prototype listening to server events

--- apps/vx-scan/backend/src/server.ts
+++ apps/vx-scan/backend/src/server.ts
@@ -13,6 +13,25 @@ let store: { config: Config | undefined } = {
   },
 };
 
+export type ScannerStatus = 'no_paper' | 'scanning' | 'accepted';
+
+const scannerStateMachine = {
+  state: 'no_paper' as ScannerStatus,
+  transition() {
+    switch (this.state) {
+      case 'no_paper':
+        this.state = 'scanning';
+        break;
+      case 'scanning':
+        this.state = 'accepted';
+        break;
+      case 'accepted':
+        this.state = 'no_paper';
+        break;
+    }
+  },
+};
+
 const api = grout.createApi({
   getConfig: grout.query(async (): Promise<Config | undefined> => {
     return store.config;
@@ -21,6 +40,15 @@ const api = grout.createApi({
   updateConfig: grout.mutation(async (newConfig: Config): Promise<void> => {
     store.config = newConfig;
   }),
+
+  getScannerStatus: grout.query(
+    async (): Promise<{ status: ScannerStatus }> => {
+      if (Math.random() < 1 / 3) {
+        scannerStateMachine.transition();
+      }
+      return { status: scannerStateMachine.state };
+    }
+  ),
 });
 
 export type VxScanApi = typeof api;
--- apps/vx-scan/frontend/src/App.tsx
+++ apps/vx-scan/frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import type { VxScanApi, Config } from '@votingworks/vx-scan-backend';
 import grout from '@votingworks/grout';
 import {
@@ -7,25 +7,28 @@ import {
   useMutation,
   useQuery,
   useQueryClient,
+  UseQueryOptions,
+  UseQueryResult,
 } from '@tanstack/react-query';
+import { ScannerStatus } from '@votingworks/vx-scan-backend/build/server';
 
 const apiClient = grout.createClient<VxScanApi>();
 
-const useConfigQuery = () =>
-  useQuery(['config'], () => apiClient.queries.getConfig());
+function useConfigQuery() {
+  return useQuery(['config'], () => apiClient.queries.getConfig());
+}
 
-const useUpdateConfigMutation = () => {
+function useUpdateConfigMutation() {
   const queryClient = useQueryClient();
   return useMutation(apiClient.mutations.updateConfig, {
     onSuccess: () => {
       queryClient.invalidateQueries(['config']);
     },
   });
-};
+}
 
 function ConfigForm({ config }: { config: Config | undefined }) {
   const updateConfigMutation = useUpdateConfigMutation();
-  console.log({ config });
   const [formState, setFormState] = useState<Config>(
     config ?? { electionName: '', precinctName: '' }
   );
@@ -84,12 +87,81 @@ function ConfigPanel() {
   );
 }
 
+function useScannerStatusQuery(
+  options: UseQueryOptions<{ status: ScannerStatus }> = {}
+) {
+  return useQuery<{ status: ScannerStatus }>(
+    ['scannerStatus'],
+    () => apiClient.queries.getScannerStatus(),
+    options
+  );
+}
+
+function deepEqual(a: any, b: any) {
+  return JSON.stringify(a) === JSON.stringify(b);
+}
+
+/**
+ * Registers a handler that will be called whenever the result of a useQuery
+ * hook changes (based on a deep equality check).
+ *
+ * Think of this like an event listener for events that are triggered via the
+ * server (e.g. similar to how we can put an onChange handler on, say, a button,
+ * for a user event that is triggered in the browser).
+ *
+ * It can be combined with the refetchInterval option of useQuery to make the
+ * query poll for new data regularly.
+ *
+ * Example use cases:
+ * - Playing a sound when the scanner accepts a ballot
+ * - Redirecting to a new URL when a background task is complete
+ */
+function useQueryChangeListener<TData>(
+  query: UseQueryResult<TData>,
+  changeHandler: (newData: TData, previousData: TData | undefined) => void
+) {
+  const previousData = useRef<TData>();
+
+  useEffect(() => {
+    if (query.isSuccess && !deepEqual(previousData.current, query.data)) {
+      changeHandler(query.data, previousData.current);
+      previousData.current = query.data;
+    }
+  }, [query.data]);
+}
+
+function playSound() {
+  console.log('Ding! Ballot accepted');
+}
+
+function ScannerStatusPanel() {
+  const scannerStatusQuery = useScannerStatusQuery({
+    refetchInterval: 1000,
+  });
+
+  useQueryChangeListener(scannerStatusQuery, (newStatus, previousStatus) => {
+    if (
+      previousStatus?.status === 'scanning' &&
+      newStatus.status === 'accepted'
+    ) {
+      playSound();
+    }
+  });
+
+  return (
+    <div>
+      <p>Scanner status: {scannerStatusQuery.data?.status}</p>
+    </div>
+  );
+}
+
 export function App(): JSX.Element | null {
   return (
     <QueryClientProvider client={new QueryClient()}>
       <div>
         <h1>VxScan</h1>
         <ConfigPanel />
+        <ScannerStatusPanel />
       </div>
     </QueryClientProvider>
   );
