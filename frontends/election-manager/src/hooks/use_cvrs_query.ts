24621b7f27743928b13dc9df9acfa89965abadab
Kofi Ohene-Adu <kofi@voting.works>
2022-11-29 14:48:54 -0600

    [VxAdmin] Use CVRs from the server when computing tallies (#2807)

--- frontends/election-manager/src/hooks/use_cvrs_query.ts
+++ frontends/election-manager/src/hooks/use_cvrs_query.ts
@@ -1,24 +1,23 @@
 import { QueryKey, useQuery, UseQueryResult } from '@tanstack/react-query';
 import { useContext } from 'react';
 
-import { Admin } from '@votingworks/api';
+import { CastVoteRecord } from '@votingworks/types';
 
 import { ServicesContext } from '../contexts/services_context';
 
 /**
- * Gets the query key for {@link useCvrFilesQuery}.
+ * Gets the query key for {@link useCvrsQuery}.
  */
 export function getCvrsQueryKey(): QueryKey {
   return ['cvrs'];
 }
 
 /**
- * Returns a metadata query for all imported CVR files, if any, for the current
- * election.
+ * Returns a query for all CVRs imported for the current election.
  */
-export function useCvrFilesQuery(): UseQueryResult<
-  Admin.CastVoteRecordFileRecord[]
-> {
+export function useCvrsQuery(): UseQueryResult<CastVoteRecord[]> {
   const { backend } = useContext(ServicesContext);
-  return useQuery(getCvrsQueryKey(), () => backend.getCvrs());
+  return useQuery(getCvrsQueryKey(), () => backend.getCvrs(), {
+    staleTime: Infinity,
+  });
 }
