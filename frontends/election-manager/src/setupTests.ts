7856dbed7342c36c53a982cde58b012aad3d768a
adghayes@gmail.com <adghayes@gmail.com>
2022-11-30 17:14:40 -0800

    test(em): use jest-styled-components

--- frontends/election-manager/src/setupTests.ts
+++ frontends/election-manager/src/setupTests.ts
@@ -3,6 +3,7 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom/extend-expect';
+import 'jest-styled-components';
 import { configure } from '@testing-library/react';
 import {
   expectTestToEndWithAllPrintsAsserted,
