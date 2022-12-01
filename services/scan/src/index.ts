2670f3babf3330d648dd3e7563a790937cb6dab3
Jonah Kagan <jonah@voting.works>
2022-12-01 13:15:55 -0800

    Only run central scanner backend

--- services/scan/src/index.ts
+++ services/scan/src/index.ts
@@ -1,21 +1,11 @@
-// Import the rest of our application.
-import { MockScannerClient, ScannerClient } from '@votingworks/plustek-sdk';
 import { Logger, LogSource, LogEventId } from '@votingworks/logging';
-import { ok, Result } from '@votingworks/types';
 import fs from 'fs';
 import * as dotenv from 'dotenv';
 import * as dotenvExpand from 'dotenv-expand';
-import {
-  MOCK_SCANNER_FILES,
-  MOCK_SCANNER_HTTP,
-  MOCK_SCANNER_PORT,
-  VX_MACHINE_TYPE,
-  NODE_ENV,
-} from './globals';
+import { MOCK_SCANNER_FILES, NODE_ENV } from './globals';
 import { LoopScanner, parseBatchesFromEnv } from './loop_scanner';
 import { BatchScanner } from './fujitsu_scanner';
 import * as server from './server';
-import { plustekMockServer } from './plustek_mock_server';
 
 // https://github.com/bkeepers/dotenv#what-other-env-files-can-i-use
 const dotEnvPath = '.env';
@@ -44,32 +34,7 @@ for (const dotenvFile of dotenvFiles) {
 
 const logger = new Logger(LogSource.VxScanService);
 
-async function createMockPlustekClient(): Promise<
-  Result<ScannerClient, Error>
-> {
-  const client = new MockScannerClient();
-  await client.connect();
-  const port = MOCK_SCANNER_PORT;
-  process.stdout.write(
-    `Starting mock plustek scanner API at http://localhost:${port}/mock\n`
-  );
-  process.stdout.write(
-    `→ Load paper: curl -X PUT -d '{"files":["/path/to/front.jpg", "/path/to/back.jpg"]}' -H 'Content-Type: application/json' http://localhost:${port}/mock\n`
-  );
-  process.stdout.write(
-    `→ Remove paper: curl -X DELETE http://localhost:${port}/mock\n`
-  );
-  plustekMockServer(client).listen(port);
-  return ok(client);
-}
-const createPlustekClient =
-  VX_MACHINE_TYPE === 'precinct-scanner' && MOCK_SCANNER_HTTP
-    ? createMockPlustekClient
-    : undefined;
-
 function getScanner(): BatchScanner | undefined {
-  if (VX_MACHINE_TYPE !== 'bsd') return undefined;
-
   const mockScannerFiles = parseBatchesFromEnv(MOCK_SCANNER_FILES);
   if (!mockScannerFiles) return undefined;
   process.stdout.write(
@@ -82,7 +47,7 @@ function getScanner(): BatchScanner | undefined {
 }
 
 async function main(): Promise<number> {
-  await server.start({ batchScanner: getScanner(), createPlustekClient });
+  await server.start({ batchScanner: getScanner() });
   return 0;
 }
 
