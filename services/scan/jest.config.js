2f68e0f58d48a104cb38c4dec0236ad454886cdb
Jonah Kagan <jonah@voting.works>
2022-12-01 14:53:44 -0800

    Bump test coverage

--- services/scan/jest.config.js
+++ services/scan/jest.config.js
@@ -17,6 +17,10 @@ module.exports = {
     '!src/types.ts',
     '!test/**/*',
   ],
+  // Note that these thresholds are based on CI, which skips some of the tests
+  // that run locally (e.g. in retry-scan/index.test.ts). Look for checks for
+  // process.env.CI to figure out which ones. This makes coverage lower on CI,
+  // so when updating these thresholds, it should be based on CI.
   coverageThreshold: {
     global: {
       statements: 89,
