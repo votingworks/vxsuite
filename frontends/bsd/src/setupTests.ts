a185123931f37c30e96962c78961c7ab50e41ae9
Andrew Hayes <Adghayes@gmail.com>
2022-12-06 16:17:47 -0800

    Use `jest-styled-components` (#2820)

--- frontends/bsd/src/setupTests.ts
+++ frontends/bsd/src/setupTests.ts
@@ -1,6 +1,7 @@
 import { configure } from '@testing-library/react';
 import fetchMock from 'fetch-mock';
 import jestFetchMock from 'jest-fetch-mock';
+import 'jest-styled-components';
 import { TextDecoder, TextEncoder } from 'util';
 
 configure({ asyncUtilTimeout: 5_000 });
