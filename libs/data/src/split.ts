64cb989b50da9fc130ff78eb973c92921c9e9519
Jonah Kagan <jonah@voting.works>
2023-01-26 14:28:39 -0800

    Update libs/data

--- libs/data/src/split.ts
+++ libs/data/src/split.ts
@@ -1,4 +1,4 @@
-import { assert } from '@votingworks/utils';
+import { assert } from '@votingworks/basics';
 import { createWriteStream, renameSync } from 'fs';
 import { join, parse } from 'path';
 import StreamChopper from 'stream-chopper';
