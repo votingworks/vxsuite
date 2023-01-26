3125be703bc686db4030b7ba9ed8515883055e02
Jonah Kagan <jonah@voting.works>
2023-01-26 11:03:28 -0800

    more sketching

--- libs/types/src/election-adt.ts
+++ libs/types/src/election-adt.ts
@@ -1,3 +1,15 @@
+import { throwIllegalValue } from '@votingworks/utils';
+import {
+  AdjudicationReason,
+  BallotLayout,
+  BallotStrings,
+  GridLayout,
+  MarkThresholds,
+  Translations,
+  Election as VxElectionData,
+  Party as VxPartyData,
+} from './election';
+
 export interface State {
   name(): string;
 }
@@ -32,7 +44,6 @@ export interface CandidateContest extends BaseContest {
   votesAllowed(): number; // Replaces seats
   candidates(): readonly Candidate[];
   allowWriteIns(): boolean;
-  rotation(): Rotation | undefined;
 }
 
 export interface Candidate {
@@ -43,11 +54,7 @@ export interface Candidate {
   // writeInIndex?: number;
 }
 
-export interface Rotation {
-  type: 'candidateShiftByPrecinctIndex';
-}
-
-export interface BallotMeasureContest {
+export interface BallotMeasureContest extends BaseContest {
   readonly type: 'ballot-measure';
   description(): string;
   shortTitle(): string | undefined;
@@ -78,19 +85,118 @@ export interface Election {
   contests(): readonly Contest[];
   ballotStyles(): readonly BallotStyle[];
 
-  // // Physical ballot
-  // ballotLayout(): BallotLayout | undefined;
-  // gridLayouts(): readonly GridLayout[] | undefined;
-  // seal(): string | undefined;
-  // sealUrl(): string | undefined;
-  // quickResultsReportingUrl(): string | undefined;
+  // The rest of the fields below are the same as in the original VXF election
+  // definition data structure. They are deprecated, since we will be moving
+  // these features to CDF in the future. For now, this data will be appended as
+  // an extra VX-specific blob at the end of the CDF. We don't both making
+  // accessors for them now, since they will change.
 
-  // // Localization
-  // _lang(): Translations | undefined;
-  // ballotStrings(): BallotStrings | undefined;
+  // Physical ballot
+  /** @deprecated */
+  ballotLayout?: BallotLayout;
+  /** @deprecated */
+  gridLayouts?: readonly GridLayout[];
+  /** @deprecated */
+  seal?: string;
+  /** @deprecated */
+  sealUrl?: string;
+  /** @deprecated */
+  quickResultsReportingUrl?: string;
 
-  // // Election settings
-  // markThresholds(): MarkThresholds | undefined;
-  // centralScanAdjudicationReasons(): readonly AdjudicationReason[] | undefined;
-  // precinctScanAdjudicationReasons(): readonly AdjudicationReason[] | undefined;
+  // Election settings
+  /** @deprecated */
+  markThresholds?: MarkThresholds;
+  /** @deprecated */
+  centralScanAdjudicationReasons?: readonly AdjudicationReason[];
+  /** @deprecated */
+  precinctScanAdjudicationReasons?: readonly AdjudicationReason[];
+
+  // Localization
+  /** @deprecated */
+  _lang?: Translations;
+  /** @deprecated */
+  ballotStrings?: BallotStrings;
+}
+
+function buildElectionFromVxf(electionData: VxElectionData): Election {
+  function buildParty(partyData: VxPartyData) {
+    return {
+      name: () => partyData.name,
+      abbreviation: () => partyData.abbrev,
+    };
+  }
+
+  return {
+    ...electionData,
+
+    title(): string {
+      return electionData.title;
+    },
+
+    date(): string {
+      return electionData.date;
+    },
+
+    state(): State {
+      const { state } = electionData;
+      return { name: () => state };
+    },
+
+    county(): County {
+      const { county } = electionData;
+      return { name: () => county.name };
+    },
+
+    districts(): readonly District[] {
+      return electionData.districts.map((district) => ({
+        name: () => district.name,
+      }));
+    },
+
+    precincts(): readonly Precinct[] {
+      return electionData.precincts.map((precinct) => ({
+        name: () => precinct.name,
+      }));
+    },
+
+    parties(): readonly Party[] {
+      return electionData.parties.map(buildParty);
+    },
+
+    contests(): readonly Contest[] {
+      return electionData.contests.map((contest) => {
+        switch (contest.type) {
+          case 'candidate': {
+            return {
+              type: 'candidate',
+              title: () => contest.title,
+              party: () => {
+                const partyData =
+                  contest.partyId &&
+                  electionData.parties.find(
+                    (party) => party.id === contest.partyId
+                  );
+                return partyData && buildParty(partyData);
+              },
+              votesAllowed: () => contest.seats,
+              candidates: () => [], // TODO
+              allowWriteIns: () => contest.allowWriteIns,
+            };
+          }
+          case 'yesno': {
+            throw new Error('not implemented');
+          }
+          case 'ms-either-neither': {
+            throw new Error('not implemented');
+          }
+          default:
+            throwIllegalValue(contest);
+        }
+      });
+    },
+
+    ballotStyles(): readonly BallotStyle[] {
+      return []; // TODO
+    },
+  };
 }
