f9a4a794edfa4c93526baac846a675d779cd1e54
Arsalan Sufi <arsalan@voting.works>
2023-01-31 11:36:13 -0500

    Update WebServiceCard to accept a custom base URL
    So that it can be used on the backend

--- libs/utils/src/Card/web_service_card.ts
+++ libs/utils/src/Card/web_service_card.ts
@@ -24,12 +24,21 @@ interface SuccessIndicationResponse {
  * Implements the `Card` API by accessing it through a web service.
  */
 export class WebServiceCard implements Card {
+  private readonly baseUrl: string;
+
+  constructor({ baseUrl }: { baseUrl?: string } = {}) {
+    this.baseUrl = baseUrl ?? '';
+  }
+
   /**
    * Reads basic information about the card, including whether one is present,
    * what its short value is and whether it has a long value.
    */
   async readSummary(): Promise<CardSummary> {
-    return unsafeParse(CardSummarySchema, await fetchJson('/card/read'));
+    return unsafeParse(
+      CardSummarySchema,
+      await fetchJson(`${this.baseUrl}/card/read`)
+    );
   }
 
   /**
@@ -40,7 +49,7 @@ export class WebServiceCard implements Card {
     schema: z.ZodSchema<T>
   ): Promise<Result<Optional<T>, SyntaxError | z.ZodError>> {
     const { longValue } = (await fetchJson(
-      '/card/read_long'
+      `${this.baseUrl}/card/read_long`
     )) as LongValueResponse;
     return longValue ? safeParseJson(longValue, schema) : ok(undefined);
   }
@@ -51,7 +60,7 @@ export class WebServiceCard implements Card {
    */
   async readLongString(): Promise<Optional<string>> {
     const { longValue } = (await fetchJson(
-      '/card/read_long'
+      `${this.baseUrl}/card/read_long`
     )) as LongValueResponse;
     return longValue || undefined;
   }
@@ -62,7 +71,7 @@ export class WebServiceCard implements Card {
    */
   async readLongUint8Array(): Promise<Optional<Uint8Array>> {
     const { longValue } = (await fetchJson(
-      '/card/read_long_b64'
+      `${this.baseUrl}/card/read_long_b64`
     )) as LongValueResponse;
     return longValue ? toByteArray(longValue) : undefined;
   }
@@ -71,7 +80,7 @@ export class WebServiceCard implements Card {
    * Writes a new short value to the card.
    */
   async writeShortValue(value: string): Promise<void> {
-    const { success } = (await fetchJson('/card/write', {
+    const { success } = (await fetchJson(`${this.baseUrl}/card/write`, {
       method: 'post',
       headers: { 'Content-Type': 'application/json' },
       body: value,
@@ -95,13 +104,16 @@ export class WebServiceCard implements Card {
    */
   async writeLongUint8Array(value: Uint8Array): Promise<void> {
     const longValueBase64 = fromByteArray(value);
-    const { success } = (await fetchJson('/card/write_long_b64', {
-      method: 'post',
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
-      },
-      body: `long_value=${encodeURIComponent(longValueBase64)}`,
-    })) as SuccessIndicationResponse;
+    const { success } = (await fetchJson(
+      `${this.baseUrl}/card/write_long_b64`,
+      {
+        method: 'post',
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
+        },
+        body: `long_value=${encodeURIComponent(longValueBase64)}`,
+      }
+    )) as SuccessIndicationResponse;
     if (!success) {
       throw new Error('Failed to write long value');
     }
@@ -114,15 +126,18 @@ export class WebServiceCard implements Card {
     shortValue,
     longValue,
   }: ShortAndLongValues): Promise<void> {
-    const { success } = (await fetchJson('/card/write_short_and_long', {
-      method: 'post',
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
-      },
-      body:
-        `short_value=${encodeURIComponent(shortValue)}&` +
-        `long_value=${encodeURIComponent(longValue)}`,
-    })) as SuccessIndicationResponse;
+    const { success } = (await fetchJson(
+      `${this.baseUrl}/card/write_short_and_long`,
+      {
+        method: 'post',
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
+        },
+        body:
+          `short_value=${encodeURIComponent(shortValue)}&` +
+          `long_value=${encodeURIComponent(longValue)}`,
+      }
+    )) as SuccessIndicationResponse;
     if (!success) {
       throw new Error('Failed to write short and long values');
     }
@@ -132,9 +147,12 @@ export class WebServiceCard implements Card {
    * Overrides card write protection.
    */
   async overrideWriteProtection(): Promise<void> {
-    const { success } = (await fetchJson('/card/write_protect_override', {
-      method: 'post',
-    })) as SuccessIndicationResponse;
+    const { success } = (await fetchJson(
+      `${this.baseUrl}/card/write_protect_override`,
+      {
+        method: 'post',
+      }
+    )) as SuccessIndicationResponse;
     if (!success) {
       throw new Error('Failed to override write protection');
     }
