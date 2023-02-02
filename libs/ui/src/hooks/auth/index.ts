6b9e4705f8ddfb0a4ea0e7ac2e190a251a3670c2
Arsalan Sufi <arsalan@voting.works>
2023-02-02 16:28:49 -0500

    Remove no longer used useDippedSmartCardAuth

--- libs/ui/src/hooks/auth/index.ts
+++ libs/ui/src/hooks/auth/index.ts
@@ -1,7 +1,4 @@
 /* istanbul ignore file */
-export * from './use_dipped_smart_card_auth';
-
-/** Being phased out */
 export * from './auth_helpers';
 export * from './use_inserted_smartcard_auth';
 export * from './use_dipped_smartcard_auth';
