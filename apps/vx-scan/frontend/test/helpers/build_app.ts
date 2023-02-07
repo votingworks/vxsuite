1c0224811f886998ec0d52d79525a0f9112bd213
Arsalan Sufi <arsalan@voting.works>
2023-02-07 10:40:35 -0500

    Update VxScan frontend test helpers

--- apps/vx-scan/frontend/test/helpers/build_app.ts
+++ apps/vx-scan/frontend/test/helpers/build_app.ts
@@ -1,15 +1,13 @@
 import { render, RenderResult } from '@testing-library/react';
 import { fakeLogger, Logger } from '@votingworks/logging';
-import { MemoryCard, MemoryHardware } from '@votingworks/utils';
+import { MemoryHardware } from '@votingworks/utils';
 import { App } from '../../src/app';
 
 export function buildApp(connectPrinter = false): {
-  card: MemoryCard;
   hardware: MemoryHardware;
   logger: Logger;
   renderApp: () => RenderResult;
 } {
-  const card = new MemoryCard();
   const hardware = MemoryHardware.build({
     connectPrinter,
     connectCardReader: true,
@@ -17,7 +15,7 @@ export function buildApp(connectPrinter = false): {
   });
   const logger = fakeLogger();
   function renderApp() {
-    return render(App({ card, hardware, logger }));
+    return render(App({ hardware, logger }));
   }
-  return { renderApp, card, hardware, logger };
+  return { renderApp, hardware, logger };
 }
