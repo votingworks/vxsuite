300af895bc7ae66e89330f9e4d635f2a35772c5c
Brian Donovan <1938+eventualbuddha@users.noreply.github.com>
2023-02-15 20:18:47 -0800

    feat(types): add side parameter to `mapSheet`

--- libs/types/src/hmpb.test.ts
+++ libs/types/src/hmpb.test.ts
@@ -2,6 +2,11 @@ import { mapSheet } from './hmpb';
 
 test('mapSheet sync', () => {
   expect(mapSheet([1, 2], (x) => x + 1)).toEqual([2, 3]);
+
+  const fn = jest.fn();
+  mapSheet([1, 2], fn);
+  expect(fn).toHaveBeenNthCalledWith(1, 1, 'front');
+  expect(fn).toHaveBeenNthCalledWith(2, 2, 'back');
 });
 
 test('mapSheet async', async () => {
