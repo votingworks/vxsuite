e87c03d37b8c1c8cc09c2acb8952056fe7a35a3c
arsalansufi <arsalan@voting.works>
2023-02-21 14:06:28 -0500

    Modify react-query query client default options (#3023)
    * Modify react-query query client default options
    
    To prevent errors when the backend is starting up or restarting
    
    * Update comment to note that this is a stopgap

--- libs/ui/src/react_query.test.ts
+++ libs/ui/src/react_query.test.ts
@@ -1,6 +1,6 @@
 import { assert } from '@votingworks/basics';
 
-import { QUERY_CLIENT_DEFAULT_OPTIONS } from './react_query';
+import { QUERY_CLIENT_DEFAULT_OPTIONS, shouldRetry } from './react_query';
 
 test('Relevant settings are consistent for queries and mutations', () => {
   const querySettings = QUERY_CLIENT_DEFAULT_OPTIONS.queries;
@@ -15,5 +15,23 @@ test('Relevant settings are consistent for queries and mutations', () => {
   expect(querySettings.retry).toEqual(mutationSettings.retry);
   expect(querySettings.useErrorBoundary).toEqual(
     mutationSettings.useErrorBoundary
+  );
+});
+
+test('Custom retry function', () => {
+  expect(
+    shouldRetry(0, new Error('Something something 504 something something'))
+  ).toEqual(true);
+  expect(
+    shouldRetry(3, new Error('Something something 504 something something'))
+  ).toEqual(true);
+  expect(
+    shouldRetry(4, new Error('Something something 504 something something'))
+  ).toEqual(false);
+  expect(
+    shouldRetry(0, new Error('Something something 500 something something'))
+  ).toEqual(false);
+  expect(shouldRetry(0, 'Something something 504 something something')).toEqual(
+    false
   );
 });
