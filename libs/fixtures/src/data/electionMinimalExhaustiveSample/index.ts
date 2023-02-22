d5db45bf3cb8c4496a087de2e0c28fc026b57229
Jonah Kagan <jonah@voting.works>
2023-02-22 15:58:06 -0800

    fixtures: remove unused SEMS data

--- libs/fixtures/src/data/electionMinimalExhaustiveSample/index.ts
+++ libs/fixtures/src/data/electionMinimalExhaustiveSample/index.ts
@@ -1,8 +1,5 @@
 import { asText as standardCvrsAsText } from './cvrFiles/standard.jsonl';
 import { asText as standardLiveCvrsAsText } from './cvrFiles/standard.live.jsonl';
-import { asText as partial1CvrsAsText } from './cvrFiles/partial1.jsonl';
-import { asText as partial2CvrsAsText } from './cvrFiles/partial2.jsonl';
-import { asText as semsDataAsText } from './semsFiles/standard.csv';
 import { asText as batchResultsCsvAsText } from './csvFiles/batchResults.csv';
 import { asText as finalResultsCsvAsText } from './csvFiles/finalResults.csv';
 
@@ -13,9 +10,6 @@ export * as partial2CvrFile from './cvrFiles/partial2.jsonl';
 
 export const cvrData = standardCvrsAsText();
 export const liveCvrsData = standardLiveCvrsAsText();
-export const semsData = semsDataAsText();
-export const semsDataPartial1 = partial1CvrsAsText();
-export const semsDataPartial2 = partial2CvrsAsText();
 export const batchCsvData = batchResultsCsvAsText();
 export const finalCsvData = finalResultsCsvAsText();
 
--- libs/fixtures/src/data/electionWithMsEitherNeither/index.ts
+++ libs/fixtures/src/data/electionWithMsEitherNeither/index.ts
@@ -1,10 +1,8 @@
 import { asText as standardCvrsAsText } from './cvrFiles/standard.jsonl';
 import { asText as testCvrsAsText } from './cvrFiles/test.jsonl';
-import { asText as semsDataAsText } from './semsFiles/standard.csv';
 
 export const cvrData = standardCvrsAsText();
 export const cvrTestData = testCvrsAsText();
-export const semsData = semsDataAsText();
 
 export {
   election,
