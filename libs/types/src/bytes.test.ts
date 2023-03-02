528b86b855f0407b784dbfdc92a48b34c148ed49
Arsalan Sufi <arsalan@voting.works>
2023-03-02 14:36:15 -0500

    Add asHexString helper for Byte type

--- libs/types/src/bytes.test.ts
+++ libs/types/src/bytes.test.ts
@@ -1,4 +1,4 @@
-import { isByte } from './byte';
+import { asHexString, isByte } from './byte';
 
 test('isByte', () => {
   expect(isByte(0)).toEqual(true);
@@ -12,4 +12,10 @@ test('isByte', () => {
   expect(isByte(0x100)).toEqual(false);
   expect(isByte(3.14)).toEqual(false);
   expect(isByte(Infinity)).toEqual(false);
+});
+
+test('asHexString', () => {
+  expect(asHexString(0x00)).toEqual('00');
+  expect(asHexString(0x01)).toEqual('01');
+  expect(asHexString(0x3f)).toEqual('3f');
 });
