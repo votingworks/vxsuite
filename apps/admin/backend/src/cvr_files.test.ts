be639fae046868869ff4f04e3a4550e64a0edec2
adghayes@gmail.com <adghayes@gmail.com>
2023-03-20 09:45:45 -0700

    add cast vote record validation and conversion code

--- apps/admin/backend/src/cvr_files.test.ts
+++ apps/admin/backend/src/cvr_files.test.ts
@@ -1,9 +1,14 @@
+/* eslint-disable vx/gts-identifiers */
 import { Buffer } from 'buffer';
 import { electionMinimalExhaustiveSampleDefinition } from '@votingworks/fixtures';
 import { fakeLogger, LogEventId } from '@votingworks/logging';
-import { getDisplayElectionHash } from '@votingworks/types';
+import { CVR, getDisplayElectionHash } from '@votingworks/types';
 import { createMockUsb } from '../test/app';
-import { listCastVoteRecordFilesOnUsb } from './cvr_files';
+import {
+  convertCastVoteRecordToLegacyFormat,
+  listCastVoteRecordFilesOnUsb,
+  validateCastVoteRecord,
+} from './cvr_files';
 
 const electionDefinition = electionMinimalExhaustiveSampleDefinition;
 const file = Buffer.from([]);
@@ -185,5 +190,382 @@ describe('list cast vote record files on USB drive', () => {
           'Error accessing cast vote record files on USB drive, which may be corrupted.',
       }
     );
+  });
+});
+
+const ballotImageLocation = 'file:./ballot-images/batch-1/test';
+const validCastVoteRecord: CVR.CVR = {
+  '@type': 'CVR.CVR',
+  BallotStyleId: '1M',
+  BallotStyleUnitId: 'precinct-1',
+  PartyIds: ['0'],
+  CreatingDeviceId: 'scanner',
+  ElectionId:
+    'e38b66442647f4bc87a08e3e4f0986db3c354a58b0790096b88c2e208e18cd08',
+  BatchId: 'batch-1',
+  vxBallotType: CVR.vxBallotType.Absentee,
+  BallotSheetId: '1',
+  CurrentSnapshotId: '0-modified',
+  UniqueId: '0',
+  BallotImage: [
+    {
+      '@type': 'CVR.ImageData',
+      Location: ballotImageLocation,
+    },
+    {
+      '@type': 'CVR.ImageData',
+    },
+  ],
+  CVRSnapshot: [
+    {
+      '@type': 'CVR.CVRSnapshot',
+      '@id': '0-modified',
+      Type: CVR.CVRType.Modified,
+      CVRContest: [
+        {
+          '@type': 'CVR.CVRContest',
+          ContestId: 'best-animal-mammal',
+          Overvotes: 0,
+          Undervotes: 1,
+          WriteIns: 0,
+          Status: [
+            CVR.ContestStatus.NotIndicated,
+            CVR.ContestStatus.Undervoted,
+          ],
+          CVRContestSelection: [],
+        },
+        {
+          '@type': 'CVR.CVRContest',
+          ContestId: 'zoo-council-mammal',
+          Overvotes: 0,
+          Undervotes: 3,
+          WriteIns: 0,
+          Status: [
+            CVR.ContestStatus.NotIndicated,
+            CVR.ContestStatus.Undervoted,
+          ],
+          CVRContestSelection: [],
+        },
+        {
+          '@type': 'CVR.CVRContest',
+          ContestId: 'new-zoo-either',
+          Overvotes: 0,
+          Undervotes: 0,
+          CVRContestSelection: [
+            {
+              '@type': 'CVR.CVRContestSelection',
+              ContestSelectionId: 'yes',
+              OptionPosition: 0,
+              SelectionPosition: [
+                {
+                  '@type': 'CVR.SelectionPosition',
+                  HasIndication: CVR.IndicationStatus.Yes,
+                  NumberVotes: 1,
+                  IsAllocable: CVR.AllocationStatus.Yes,
+                },
+              ],
+            },
+          ],
+        },
+        {
+          '@type': 'CVR.CVRContest',
+          ContestId: 'new-zoo-pick',
+          Overvotes: 0,
+          Undervotes: 0,
+          CVRContestSelection: [
+            {
+              '@type': 'CVR.CVRContestSelection',
+              ContestSelectionId: 'yes',
+              OptionPosition: 0,
+              SelectionPosition: [
+                {
+                  '@type': 'CVR.SelectionPosition',
+                  HasIndication: CVR.IndicationStatus.Yes,
+                  NumberVotes: 1,
+                  IsAllocable: CVR.AllocationStatus.Yes,
+                },
+              ],
+            },
+          ],
+        },
+        {
+          '@type': 'CVR.CVRContest',
+          ContestId: 'fishing',
+          Overvotes: 0,
+          Undervotes: 0,
+          CVRContestSelection: [
+            {
+              '@type': 'CVR.CVRContestSelection',
+              ContestSelectionId: 'yes',
+              OptionPosition: 0,
+              SelectionPosition: [
+                {
+                  '@type': 'CVR.SelectionPosition',
+                  HasIndication: CVR.IndicationStatus.Yes,
+                  NumberVotes: 1,
+                  IsAllocable: CVR.AllocationStatus.Yes,
+                },
+              ],
+            },
+          ],
+        },
+      ],
+    },
+  ],
+};
+
+describe('validateCastVoteRecord', () => {
+  test('handles valid CVR', () => {
+    const result = validateCastVoteRecord({
+      cvr: validCastVoteRecord,
+      electionDefinition,
+      reportBallotImageLocations: [ballotImageLocation],
+      reportBatchIds: ['batch-1'],
+    });
+
+    expect(result.isOk()).toBeTruthy();
+  });
+
+  test('error on invalid election', () => {
+    const result = validateCastVoteRecord({
+      cvr: {
+        ...validCastVoteRecord,
+        ElectionId: 'wrong',
+      },
+      electionDefinition,
+      reportBallotImageLocations: [ballotImageLocation],
+      reportBatchIds: ['batch-1'],
+    });
+
+    expect(result.err()).toEqual('invalid-election');
+  });
+
+  test('error on invalid ballot style', () => {
+    const result = validateCastVoteRecord({
+      cvr: {
+        ...validCastVoteRecord,
+        BallotStyleId: 'wrong',
+      },
+      electionDefinition,
+      reportBallotImageLocations: [ballotImageLocation],
+      reportBatchIds: ['batch-1'],
+    });
+
+    expect(result.err()).toEqual('invalid-ballot-style');
+  });
+
+  test('error on invalid precinct', () => {
+    const result = validateCastVoteRecord({
+      cvr: {
+        ...validCastVoteRecord,
+        BallotStyleUnitId: 'wrong',
+      },
+      electionDefinition,
+      reportBallotImageLocations: [ballotImageLocation],
+      reportBatchIds: ['batch-1'],
+    });
+
+    expect(result.err()).toEqual('invalid-precinct');
+  });
+
+  test('error on invalid batch', () => {
+    const result = validateCastVoteRecord({
+      cvr: {
+        ...validCastVoteRecord,
+        BatchId: 'wrong',
+      },
+      electionDefinition,
+      reportBallotImageLocations: [ballotImageLocation],
+      reportBatchIds: ['batch-1'],
+    });
+
+    expect(result.err()).toEqual('invalid-batch');
+  });
+
+  test('error on invalid sheet number', () => {
+    const result = validateCastVoteRecord({
+      cvr: {
+        ...validCastVoteRecord,
+        BallotSheetId: 'wrong',
+      },
+      electionDefinition,
+      reportBallotImageLocations: [ballotImageLocation],
+      reportBatchIds: ['batch-1'],
+    });
+
+    expect(result.err()).toEqual('invalid-sheet-number');
+  });
+
+  const validCastVoteRecordSnapshot = validCastVoteRecord.CVRSnapshot[0]!;
+
+  test('error on invalid contest id', () => {
+    const result = validateCastVoteRecord({
+      cvr: {
+        ...validCastVoteRecord,
+        CVRSnapshot: [
+          {
+            ...validCastVoteRecordSnapshot,
+            CVRContest: [{ '@type': 'CVR.CVRContest', ContestId: 'wrong' }],
+          },
+        ],
+      },
+      electionDefinition,
+      reportBallotImageLocations: [ballotImageLocation],
+      reportBatchIds: ['batch-1'],
+    });
+
+    expect(result.err()).toEqual('invalid-contest');
+  });
+
+  test('error on invalid contest option id', () => {
+    const result = validateCastVoteRecord({
+      cvr: {
+        ...validCastVoteRecord,
+        CVRSnapshot: [
+          {
+            ...validCastVoteRecordSnapshot,
+            CVRContest: [
+              {
+                '@type': 'CVR.CVRContest',
+                ContestId: 'best-animal-mammal',
+                CVRContestSelection: [
+                  {
+                    '@type': 'CVR.CVRContestSelection',
+                    ContestSelectionId: 'wrong',
+                    SelectionPosition: [],
+                  },
+                ],
+              },
+            ],
+          },
+        ],
+      },
+      electionDefinition,
+      reportBallotImageLocations: [ballotImageLocation],
+      reportBatchIds: ['batch-1'],
+    });
+
+    expect(result.err()).toEqual('invalid-contest-option');
+  });
+
+  test('error on unknown ballot image', () => {
+    const result = validateCastVoteRecord({
+      cvr: {
+        ...validCastVoteRecord,
+        BallotImage: [
+          {
+            '@type': 'CVR.ImageData',
+            Location: 'wrong',
+          },
+          {
+            '@type': 'CVR.ImageData',
+          },
+        ],
+      },
+      electionDefinition,
+      reportBallotImageLocations: [ballotImageLocation],
+      reportBatchIds: ['batch-1'],
+    });
+
+    expect(result.err()).toEqual('invalid-ballot-image-location');
+  });
+
+  test('error on mismatched write-in image', () => {
+    const result = validateCastVoteRecord({
+      cvr: {
+        ...validCastVoteRecord,
+        CVRSnapshot: [
+          {
+            ...validCastVoteRecordSnapshot,
+            CVRContest: [
+              {
+                '@type': 'CVR.CVRContest',
+                ContestId: 'best-animal-mammal',
+                CVRContestSelection: [
+                  {
+                    '@type': 'CVR.CVRContestSelection',
+                    ContestSelectionId: 'write-in-0',
+                    SelectionPosition: [
+                      {
+                        '@type': 'CVR.SelectionPosition',
+                        HasIndication: CVR.IndicationStatus.Yes,
+                        NumberVotes: 1,
+                        CVRWriteIn: {
+                          '@type': 'CVR.CVRWriteIn',
+                          WriteInImage: {
+                            '@type': 'CVR.ImageData',
+                            Location: 'wrong',
+                          },
+                        },
+                      },
+                    ],
+                  },
+                ],
+              },
+            ],
+          },
+        ],
+      },
+      electionDefinition,
+      reportBallotImageLocations: [ballotImageLocation],
+      reportBatchIds: ['batch-1'],
+    });
+
+    expect(result.err()).toEqual('invalid-write-in-image-location');
+  });
+
+  test('error on missing current snapshot', () => {
+    const result = validateCastVoteRecord({
+      cvr: {
+        ...validCastVoteRecord,
+        CVRSnapshot: [],
+      },
+      electionDefinition,
+      reportBallotImageLocations: [ballotImageLocation],
+      reportBatchIds: ['batch-1'],
+    });
+
+    expect(result.err()).toEqual('no-current-snapshot');
+  });
+});
+
+describe('convertCastVoteRecordToLegacyFormat', () => {
+  test('general conversion ', () => {
+    expect(
+      convertCastVoteRecordToLegacyFormat({
+        cvr: validCastVoteRecord,
+        isTestReport: true,
+        batchLabel: 'Batch 1',
+      })
+    ).toMatchObject({
+      _ballotId: '0',
+      _ballotStyleId: '1M',
+      _ballotType: CVR.vxBallotType.Absentee,
+      _batchId: 'batch-1',
+      _batchLabel: 'Batch 1',
+      _precinctId: 'precinct-1',
+      _scannerId: 'scanner',
+      _testBallot: true,
+      'best-animal-mammal': [],
+      fishing: ['yes'],
+      'new-zoo-either': ['yes'],
+      'new-zoo-pick': ['yes'],
+      'zoo-council-mammal': [],
+    });
+  });
+
+  test('precinct ballot type', () => {
+    expect(
+      convertCastVoteRecordToLegacyFormat({
+        cvr: {
+          ...validCastVoteRecord,
+          vxBallotType: CVR.vxBallotType.Precinct,
+        },
+        isTestReport: true,
+        batchLabel: 'Batch 1',
+      })
+    ).toMatchObject({
+      _ballotType: 'standard',
+    });
   });
 });
--- apps/admin/backend/src/cvr_files.ts
+++ apps/admin/backend/src/cvr_files.ts
@@ -2,9 +2,28 @@ import {
   FileSystemEntryType,
   listDirectoryOnUsbDrive,
 } from '@votingworks/backend';
-import { throwIllegalValue } from '@votingworks/basics';
+import {
+  assert,
+  err,
+  ok,
+  integers,
+  Result,
+  throwIllegalValue,
+  find,
+} from '@votingworks/basics';
 import { LogEventId, Logger } from '@votingworks/logging';
-import { ElectionDefinition } from '@votingworks/types';
+import {
+  AnyContest,
+  BallotId,
+  CastVoteRecord,
+  CastVoteRecordBallotType,
+  Contests,
+  CVR,
+  ElectionDefinition,
+  getBallotStyle,
+  getContests,
+  safeParseNumber,
+} from '@votingworks/types';
 import {
   generateElectionBasedSubfolderName,
   parseCastVoteRecordReportDirectoryName,
@@ -95,4 +114,283 @@ export async function listCastVoteRecordFilesOnUsb(
   return [...castVoteRecordFileMetadataList].sort(
     (a, b) => b.exportTimestamp.getTime() - a.exportTimestamp.getTime()
   );
+}
+
+// CVR Validation
+
+function getValidContestOptions(contest: AnyContest) {
+  if (contest.type === 'candidate') {
+    return [
+      ...contest.candidates.map((candidate) => candidate.id),
+      ...integers({ from: 0, through: contest.seats - 1 })
+        .map((num) => `write-in-${num}`)
+        .toArray(),
+    ];
+  }
+  return ['yes', 'no'];
+}
+
+type ContestReferenceError = 'invalid-contest' | 'invalid-contest-option';
+
+/**
+ * Checks whether all the contest and contest options referenced in a cast vote
+ * record are indeed a part of the specified election.
+ */
+function snapshotHasValidContestReferences(
+  snapshot: CVR.CVRSnapshot,
+  electionContests: Contests
+): Result<void, ContestReferenceError> {
+  // TODO: this field should probably be required
+  if (!snapshot.CVRContest) return ok();
+
+  for (const cvrContest of snapshot.CVRContest) {
+    const electionContest = electionContests.find(
+      (contest) => contest.id === cvrContest.ContestId
+    );
+    if (!electionContest) return err('invalid-contest');
+
+    const cvrContestSelections = cvrContest.CVRContestSelection;
+
+    if (!cvrContestSelections) return ok();
+
+    for (const cvrContestSelection of cvrContestSelections) {
+      // TODO: this field should probably be required
+      if (!cvrContestSelection.ContestSelectionId) return ok();
+      if (
+        !getValidContestOptions(electionContest).includes(
+          cvrContestSelection.ContestSelectionId
+        )
+      ) {
+        return err('invalid-contest-option');
+      }
+    }
+  }
+
+  return ok();
+}
+
+/**
+ * Checks whether any images referenced by particular write-ins are included at
+ * the top-level and are referenced by `CVR.BallotImage`
+ */
+function cvrHasValidWriteInImageReferences(cvr: CVR.CVR) {
+  const ballotImageReferences = cvr.BallotImage?.map(
+    (imageData) => imageData.Location
+  ).filter((location): location is string => location !== undefined);
+
+  if (!ballotImageReferences) return true;
+
+  const writeInImageReferences = cvr.CVRSnapshot.flatMap(
+    (snapshot) => snapshot.CVRContest
+  )
+    .filter(
+      (cvrContest): cvrContest is CVR.CVRContest => cvrContest !== undefined
+    )
+    .flatMap((cvrContest) => cvrContest.CVRContestSelection)
+    .filter(
+      (cvrContestSelection): cvrContestSelection is CVR.CVRContestSelection =>
+        cvrContestSelection !== undefined
+    )
+    .flatMap((cvrContestSelection) => cvrContestSelection.SelectionPosition)
+    .filter(
+      (selectionPosition): selectionPosition is CVR.SelectionPosition =>
+        selectionPosition !== undefined
+    )
+    .map(
+      (selectionPosition) =>
+        selectionPosition.CVRWriteIn?.WriteInImage?.Location
+    )
+    .filter((location): location is string => location !== undefined);
+
+  for (const writeInImageReference of writeInImageReferences) {
+    if (!ballotImageReferences.includes(writeInImageReference)) return false;
+  }
+
+  return true;
+}
+
+type CastVoteRecordValidationError =
+  | 'invalid-election'
+  | 'invalid-ballot-style'
+  | 'invalid-precinct'
+  | 'invalid-batch'
+  | 'invalid-sheet-number'
+  | ContestReferenceError
+  | 'invalid-ballot-image-location'
+  | 'invalid-write-in-image-location'
+  | 'no-current-snapshot';
+
+/**
+ * Checks if a parsed {@link CVR.CVR} is valid for a given election definition.
+ *
+ * @param cvr The parsed {@link CVR.CVR}
+ * @param electionDefinition The election context
+ * @param reportBallotImageLocations A list of images found in the report's
+ * folder, which is used to confirm the images referenced by the CVR do exist
+ */
+export function validateCastVoteRecord({
+  cvr,
+  electionDefinition,
+  reportBallotImageLocations,
+  reportBatchIds,
+}: {
+  cvr: CVR.CVR;
+  electionDefinition: ElectionDefinition;
+  reportBallotImageLocations: string[];
+  reportBatchIds: string[];
+}): Result<void, CastVoteRecordValidationError> {
+  const { election, electionHash } = electionDefinition;
+
+  if (cvr.ElectionId !== electionHash) {
+    return err('invalid-election');
+  }
+
+  if (
+    !election.ballotStyles.some(
+      (ballotStyle) => ballotStyle.id === cvr.BallotStyleId
+    )
+  ) {
+    return err('invalid-ballot-style');
+  }
+  const ballotStyle = getBallotStyle({
+    ballotStyleId: cvr.BallotStyleId,
+    election,
+  });
+  assert(ballotStyle);
+
+  if (
+    !election.precincts.some(
+      (precinct) => precinct.id === cvr.BallotStyleUnitId
+    )
+  ) {
+    return err('invalid-precinct');
+  }
+
+  if (!reportBatchIds.some((reportBatchId) => reportBatchId === cvr.BatchId)) {
+    return err('invalid-batch');
+  }
+
+  if (cvr.BallotSheetId) {
+    // Only applicable to hand-marked paper ballots
+    const parseBallotSheetIdResult = safeParseNumber(cvr.BallotSheetId);
+    if (parseBallotSheetIdResult.isErr()) {
+      // TODO: once layouts are in the election definition, we should check
+      // whether the sheet number is within range for the ballot style
+      return err('invalid-sheet-number');
+    }
+  }
+
+  for (const snapshot of cvr.CVRSnapshot) {
+    const contestValidation = snapshotHasValidContestReferences(
+      snapshot,
+      getContests({ ballotStyle, election })
+    );
+    if (contestValidation.isErr()) {
+      return contestValidation;
+    }
+  }
+
+  const ballotImageLocations = cvr.BallotImage?.map(
+    (imageData) => imageData.Location
+  ).filter((location): location is string => location !== undefined);
+
+  if (ballotImageLocations) {
+    for (const ballotImageLocation of ballotImageLocations) {
+      if (!reportBallotImageLocations.includes(ballotImageLocation)) {
+        return err('invalid-ballot-image-location');
+      }
+    }
+  }
+
+  if (!cvrHasValidWriteInImageReferences(cvr)) {
+    return err('invalid-write-in-image-location');
+  }
+
+  if (
+    !cvr.CVRSnapshot.find(
+      (snapshot) => snapshot['@id'] === cvr.CurrentSnapshotId
+    )
+  ) {
+    return err('no-current-snapshot');
+  }
+
+  return ok();
+}
+
+function cvrBallotTypeToLegacyBallotType(
+  ballotType: CVR.vxBallotType
+): CastVoteRecordBallotType {
+  switch (ballotType) {
+    case CVR.vxBallotType.Absentee:
+      return 'absentee';
+    case CVR.vxBallotType.Precinct:
+      return 'standard';
+    case CVR.vxBallotType.Provisional:
+      return 'provisional';
+    default:
+      throwIllegalValue(ballotType);
+  }
+}
+
+/**
+ * Converts the vote data in the CDF cast vote record into the simple
+ * dictionary of contest ids to contest selection ids that VxAdmin uses
+ * internally as a basis for tallying votes.
+ *
+ * This method assumes that if a contest selection is present, then it an
+ * indication. It ignores the possibility that there are multiple selection
+ * positions within the contest selection (only applicable to ranked choice and
+ * other forms of voting) or that the selection position has no indication.
+ */
+function convertCastVoteRecordVotesToLegacyVotes(
+  cvrSnapshot: CVR.CVRSnapshot
+): Record<string, string[]> {
+  const votes: Record<string, string[]> = {};
+  if (!cvrSnapshot.CVRContest) return votes;
+
+  for (const cvrContest of cvrSnapshot.CVRContest) {
+    if (!cvrContest.CVRContestSelection) continue;
+
+    const contestSelectionIds: string[] = [];
+    for (const cvrContestSelection of cvrContest.CVRContestSelection) {
+      if (!cvrContestSelection.ContestSelectionId) continue;
+
+      contestSelectionIds.push(cvrContestSelection.ContestSelectionId);
+    }
+
+    votes[cvrContest.ContestId] = contestSelectionIds;
+  }
+
+  return votes;
+}
+
+/**
+ * Converts a cast vote record in CDF format ({@link CVR.CVR}) into the legacy
+ * format still used for storage and tallying.
+ */
+export function convertCastVoteRecordToLegacyFormat({
+  cvr,
+  isTestReport,
+  batchLabel,
+}: {
+  cvr: CVR.CVR;
+  isTestReport: boolean;
+  batchLabel: string;
+}): CastVoteRecord {
+  const currentSnapshot = find(
+    cvr.CVRSnapshot,
+    (snapshot) => snapshot['@id'] === cvr.CurrentSnapshotId
+  );
+
+  return {
+    _ballotId: cvr.UniqueId as BallotId,
+    _precinctId: cvr.BallotStyleUnitId,
+    _batchId: cvr.BatchId,
+    _batchLabel: batchLabel,
+    _ballotStyleId: cvr.BallotStyleId,
+    _ballotType: cvrBallotTypeToLegacyBallotType(cvr.vxBallotType),
+    _scannerId: cvr.CreatingDeviceId,
+    _testBallot: isTestReport,
+    ...convertCastVoteRecordVotesToLegacyVotes(currentSnapshot),
+  };
 }
