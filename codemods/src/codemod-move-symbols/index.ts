7d97de723bf5e8b6d798dfbdedc134a44f544135
Brian Donovan <1938+eventualbuddha@users.noreply.github.com>
2023-03-23 09:33:08 -0700

    fix(codemods): misc `codemod-move-symbols` fixes
    - reference dependent packages in `tsconfig.json`
    - don't update references within the source or target packages
    - refer to packages by name rather than by path

--- codemods/src/codemod-move-symbols/index.ts
+++ codemods/src/codemod-move-symbols/index.ts
@@ -26,8 +26,9 @@
 import { constants, promises as fs } from 'fs';
 import { join, relative } from 'path';
 import { Project } from 'ts-morph';
+import { iter, Optional } from '@votingworks/basics';
 import { formatDurationNs } from '@votingworks/utils';
-import { getWorkspacePackageInfo } from '../pnpm';
+import { getWorkspacePackageInfo, PackageInfo } from '../pnpm';
 
 const VOTINGWORKS_PACKAGE_NAMESPACE = '@votingworks';
 
@@ -47,22 +48,31 @@ async function getTsConfigFilePath(cwd: string): Promise<string | undefined> {
   }
 }
 
-function fullyQualifiedPackageName(name: string): string {
-  return name.startsWith(VOTINGWORKS_PACKAGE_NAMESPACE)
-    ? name
-    : `${VOTINGWORKS_PACKAGE_NAMESPACE}/${name}`;
+function findPackageByName(
+  packages: Iterable<PackageInfo>,
+  name: string
+): Optional<PackageInfo> {
+  for (const pkg of packages) {
+    if (
+      pkg.name === name ||
+      pkg.name === `${VOTINGWORKS_PACKAGE_NAMESPACE}/${name}`
+    ) {
+      return pkg;
+    }
+  }
 }
 
 interface Movement {
   symbol: string;
-  source: string;
-  target: string;
+  source: PackageInfo;
+  target: PackageInfo;
 }
 
 export async function main(args: readonly string[]): Promise<number> {
   const monorepoRoot = join(__dirname, '../../..');
   const movements: Movement[] = [];
-  const packages: string[] = [];
+  const packages: PackageInfo[] = [];
+  const workspacePackageInfo = await getWorkspacePackageInfo(monorepoRoot);
 
   for (let i = 0; i < args.length; i += 1) {
     const arg = args[i] as string;
@@ -70,16 +80,25 @@ export async function main(args: readonly string[]): Promise<number> {
       const value = args[i + 1];
       i += 1;
       if (value && !value.startsWith('-')) {
-        packages.push(value);
+        const absolutePath = join(process.cwd(), value);
+        const pkg = iter(workspacePackageInfo.values()).find(
+          ({ path }) => path === absolutePath
+        );
+        if (!pkg) {
+          process.stderr.write(
+            `error: ${value} is not a valid package directory\n`
+          );
+          return 1;
+        }
+        packages.push(pkg);
       } else {
         process.stderr.write(`error: missing value for --dir\n`);
         return 1;
       }
     } else if (arg === '--all') {
-      const workspacePackageInfo = await getWorkspacePackageInfo(monorepoRoot);
       for (const pkg of workspacePackageInfo.values()) {
         if (pkg.name.startsWith(`${VOTINGWORKS_PACKAGE_NAMESPACE}/`)) {
-          packages.push(pkg.path);
+          packages.push(pkg);
         }
       }
     } else if (arg.startsWith('-')) {
@@ -88,10 +107,33 @@ export async function main(args: readonly string[]): Promise<number> {
     } else {
       const [symbol, source, target] = arg.split(':');
       if (symbol && source && target) {
+        const sourcePackage = findPackageByName(
+          workspacePackageInfo.values(),
+          source
+        );
+        const targetPackage = findPackageByName(
+          workspacePackageInfo.values(),
+          target
+        );
+
+        if (!sourcePackage) {
+          process.stderr.write(
+            `error: ${source} is not a valid package name\n`
+          );
+          return 1;
+        }
+
+        if (!targetPackage) {
+          process.stderr.write(
+            `error: ${target} is not a valid package name\n`
+          );
+          return 1;
+        }
+
         movements.push({
           symbol,
-          source: fullyQualifiedPackageName(source),
-          target: fullyQualifiedPackageName(target),
+          source: sourcePackage,
+          target: targetPackage,
         });
       } else {
         process.stderr.write(
@@ -102,14 +144,14 @@ export async function main(args: readonly string[]): Promise<number> {
     }
   }
 
-  for (const packageRoot of packages) {
-    const relativePackagePath = relative(monorepoRoot, packageRoot);
+  for (const pkg of packages) {
     const start = process.hrtime.bigint();
-    const tsConfigFilePath = await getTsConfigFilePath(packageRoot);
+    let updates = 0;
+    const tsConfigFilePath = await getTsConfigFilePath(pkg.path);
 
     if (!tsConfigFilePath) {
       process.stderr.write(
-        `${relativePackagePath}: could not locate a tsconfig.json file to use\n`
+        `${pkg.name}: could not locate a tsconfig.json file to use\n`
       );
       return 1;
     }
@@ -118,18 +160,28 @@ export async function main(args: readonly string[]): Promise<number> {
 
     for (const sourceFile of project.getSourceFiles()) {
       const filePath = sourceFile.getFilePath();
-      const relativeFilePath = relative(packageRoot, filePath);
+      const relativeFilePath = relative(pkg.path, filePath);
 
       if (relativeFilePath.startsWith('../')) {
         continue;
       }
 
       for (const { symbol, source, target } of movements) {
+        // skip if the source and target are the same
+        if (source.name === target.name) {
+          continue;
+        }
+
+        // skip the package we're moving the symbol from or to
+        if (pkg.name === source.name || pkg.name === target.name) {
+          continue;
+        }
+
         const importDeclarationForSource = sourceFile.getImportDeclaration(
           (importDeclaration) => {
             const moduleSpecifier = importDeclaration.getModuleSpecifierValue();
             return (
-              moduleSpecifier === source &&
+              moduleSpecifier === source.name &&
               importDeclaration
                 .getNamedImports()
                 .some((namedImport) => namedImport.getName() === symbol)
@@ -144,7 +196,7 @@ export async function main(args: readonly string[]): Promise<number> {
         const importDeclarationFromTarget = sourceFile.getImportDeclaration(
           (importDeclaration) => {
             const moduleSpecifier = importDeclaration.getModuleSpecifierValue();
-            return moduleSpecifier === target;
+            return moduleSpecifier === target.name;
           }
         );
 
@@ -168,7 +220,7 @@ export async function main(args: readonly string[]): Promise<number> {
         } else {
           sourceFile.addImportDeclaration({
             namedImports: [symbol],
-            moduleSpecifier: target,
+            moduleSpecifier: target.name,
           });
         }
 
@@ -183,16 +235,17 @@ export async function main(args: readonly string[]): Promise<number> {
         }
 
         process.stdout.write(
-          `${relativePackagePath}: ${relativeFilePath}: ${symbol}: ${source} → ${target}\n`
+          `${pkg.name}: ${relativeFilePath}: ${symbol}: ${source.name} → ${target.name}\n`
         );
+        updates += 1;
       }
     }
 
     await project.save();
     process.stdout.write(
-      `${relativePackagePath}: ✅ ${formatDurationNs(
+      `${pkg.name}: ✅ ${formatDurationNs(
         process.hrtime.bigint() - start
-      )}\n`
+      )} (${updates} updated)\n`
     );
   }
 
