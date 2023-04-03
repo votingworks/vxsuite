0da3a647c6445388c18f876423f97f5971372591
Kofi Ohene-Adu <kofi@voting.works>
2023-04-03 14:19:19 -0500

    [VVSG Design] Add light background behind seal images in dark mode themes

--- libs/ui/src/seal.tsx
+++ libs/ui/src/seal.tsx
@@ -1,25 +1,45 @@
+/* stylelint-disable order/properties-order, value-keyword-case */
 import React from 'react';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+
+import { ColorMode } from '@votingworks/types';
 
 const sealMaxWidth = '250px';
-const sealMinWidth = '56px';
+
+const DARK_COLOR_MODES: ReadonlySet<ColorMode> = new Set<ColorMode>([
+  'contrastHighDark',
+  'contrastLow',
+]);
+
+/**
+ * Adds a light background behind the seal image to provide contrast against
+ * dark backgrounds when using dark themes.
+ */
+const darkModeSealStyles = css`
+  background: ${(p) => p.theme.colors.foreground};
+  border-radius: 50%;
+  box-sizing: border-box;
+  padding: 0.125rem;
+`;
 
 const SealContainer = styled.div`
-  min-width: ${sealMinWidth};
   max-width: ${sealMaxWidth};
+  position: relative;
+  width: 100%;
+
+  ${(p) => DARK_COLOR_MODES.has(p.theme.colorMode) && darkModeSealStyles};
 `;
 
 const SealImage = styled.img`
-  min-width: ${sealMinWidth};
-  max-width: ${sealMaxWidth};
+  width: 100%;
 `;
 
-interface Props {
+export interface SealProps {
   seal?: string;
   sealUrl?: string;
 }
 
-export function Seal({ seal, sealUrl }: Props): JSX.Element {
+export function Seal({ seal, sealUrl }: SealProps): JSX.Element {
   return (
     <SealContainer
       aria-hidden
