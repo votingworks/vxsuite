693d69c8b73aac75ff8f0aec1aac66b7dbd86836
Brian Donovan <1938+eventualbuddha@users.noreply.github.com>
2023-04-06 17:37:11 -0700

    feat(test-utils): print a summary of suppressed output
    Per @kofi-q's suggestion.

--- libs/test-utils/src/console.ts
+++ libs/test-utils/src/console.ts
@@ -1,5 +1,94 @@
-/* eslint-disable no-console */
+import chalk from 'chalk';
 import { MaybePromise, Optional } from '@votingworks/basics';
+
+const capturedCallCountsByTest = new Map<
+  string,
+  Map<'log' | 'warn' | 'error', { count: number }>
+>();
+
+/**
+ * Strips ANSI escape codes from a string. When printing a box around a string
+ * with ANSI escape codes, the box will be the wrong size. This function removes
+ * the ANSI escape codes so that the box is the correct size.
+ */
+function stripAnsi(str: string): string {
+  // eslint-disable-next-line no-control-regex
+  return str.replace(/\x1b\[[0-9;]*m/g, '');
+}
+
+function printLinesInBox(lines: string[], out: NodeJS.WritableStream) {
+  const boxTopLeft = '┌';
+  const boxTopRight = '┐';
+  const boxTopAndBottom = '─';
+  const boxLeft = '│';
+  const boxRight = '│';
+  const boxBottomLeft = '└';
+  const boxBottomRight = '┘';
+
+  const longestLine = lines.reduce(
+    (longest, line) => Math.max(longest, stripAnsi(line).length),
+    0
+  );
+
+  const boxTop = `${boxTopLeft}${boxTopAndBottom.repeat(
+    longestLine + 2
+  )}${boxTopRight}`;
+  const boxBottom = `${boxBottomLeft}${boxTopAndBottom.repeat(
+    longestLine + 2
+  )}${boxBottomRight}`;
+
+  out.write(chalk.dim(`${boxTop}\n`));
+  for (const line of lines) {
+    out.write(
+      chalk.dim(
+        `${boxLeft} ${line}${' '.repeat(
+          longestLine - stripAnsi(line).length
+        )} ${boxRight}\n`
+      )
+    );
+  }
+  out.write(chalk.dim(`${boxBottom}\n`));
+}
+
+beforeAll(() => {
+  capturedCallCountsByTest.clear();
+});
+
+afterAll(() => {
+  const shouldPrintSummary = process.env['CI'] !== 'true';
+
+  if (!shouldPrintSummary) {
+    return;
+  }
+
+  const allSummaries = Array.from(capturedCallCountsByTest).flatMap(
+    ([testName, capturedCallCounts]) => {
+      const summaries = Array.from(capturedCallCounts.entries())
+        .filter(([, { count }]) => count > 0)
+        .map(([name, { count }]) => `${count} ${name}`);
+
+      return summaries.length > 0
+        ? [`${testName} (${summaries.join(', ')})`]
+        : [];
+    }
+  );
+
+  if (allSummaries.length > 0) {
+    printLinesInBox(
+      [
+        '🤫 Some tests suppressed console output:',
+        '',
+        ...allSummaries,
+        '',
+        `Run with ${chalk.italic(
+          'SUPPRESS_CONSOLE_OUTPUT=false'
+        )} to see the output.`,
+      ],
+      process.stderr
+    );
+    process.stderr.write('\n');
+  }
+});
 
 /**
  * Suppresses console output during the execution of a function. Resolves to the
@@ -16,9 +105,32 @@ export function suppressingConsoleOutput<T>(fn: () => T): T;
 export function suppressingConsoleOutput<T>(
   fn: () => MaybePromise<T>
 ): MaybePromise<T> {
-  jest.spyOn(console, 'log').mockReturnValue();
-  jest.spyOn(console, 'warn').mockReturnValue();
-  jest.spyOn(console, 'error').mockReturnValue();
+  if (process.env['SUPPRESS_CONSOLE_OUTPUT'] === 'false') {
+    return fn();
+  }
+
+  const { currentTestName } = expect.getState();
+  const capturedCallCounts =
+    capturedCallCountsByTest.get(currentTestName) ??
+    new Map([
+      ['log', { count: 0 }],
+      ['warn', { count: 0 }],
+      ['error', { count: 0 }],
+    ]);
+  capturedCallCountsByTest.set(currentTestName, capturedCallCounts);
+  const logStats = capturedCallCounts.get('log') as { count: number };
+  const warnStats = capturedCallCounts.get('warn') as { count: number };
+  const errorStats = capturedCallCounts.get('error') as { count: number };
+
+  jest.spyOn(console, 'log').mockImplementation(() => {
+    logStats.count += 1;
+  });
+  jest.spyOn(console, 'warn').mockImplementation(() => {
+    warnStats.count += 1;
+  });
+  jest.spyOn(console, 'error').mockImplementation(() => {
+    errorStats.count += 1;
+  });
 
   function cleanup() {
     jest.spyOn(console, 'log').mockRestore();
