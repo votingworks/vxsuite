78d05a28f4793d877569d4b59877064b962987f7
adghayes@gmail.com <adghayes@gmail.com>
2023-04-12 14:13:54 -0700

    remove random unused CVR fixture. at this point, there are still 5 legacy CVR fixtures in use

--- libs/fixtures/src/data/electionPrimary/index.ts
+++ libs/fixtures/src/data/electionPrimary/index.ts
@@ -1,7 +1,1 @@
-import { asText as standardCvrsAsText } from './cvrFiles/standard.txt';
-
-export * as cvrFile from './cvrFiles/standard.txt';
-
-export const cvrData = standardCvrsAsText();
-
 export { election, electionDefinition } from './electionPrimarySample.json';
