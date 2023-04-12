3e52cad129f1849f254bf4733a8bb35a1128d902
Arsalan Sufi <arsalan@voting.works>
2023-04-12 15:34:47 -0400

    Update sys admin card programming script to not be specific to dev

--- libs/auth/scripts/program_system_administrator_java_card.ts
+++ libs/auth/scripts/program_system_administrator_java_card.ts
@@ -1,27 +1,47 @@
 /* eslint-disable no-console */
+import { assert } from '@votingworks/basics';
+import { generatePin, hyphenatePin } from '@votingworks/utils';
+
 import { DEV_JURISDICTION } from '../src/certs';
+import { getRequiredEnvVar } from '../src/env_vars';
 import { JavaCard } from '../src/java_card';
 import { constructJavaCardConfig } from '../src/java_card_config';
 import { waitForReadyCardStatus } from './utils';
 
+const nodeEnv = getRequiredEnvVar('NODE_ENV');
+assert(
+  nodeEnv === 'development' || nodeEnv === 'production',
+  'NODE_ENV should be one of development or production'
+);
+const isProduction = nodeEnv === 'production';
+
 const initialJavaCardConfigurationScriptReminder = `
-Have you run \`./scripts/configure-dev-java-card\` on this card yet?
+${
+  isProduction
+    ? 'Have you run this card through the initial Java Card configuration script on VxCertifier yet?'
+    : 'Have you run `./scripts/configure-dev-java-card` on this card yet?'
+}
 If not, that's likely the cause of this error.
 Run that and then retry.
 `;
 
-async function programDevSystemAdministratorJavaCard(): Promise<void> {
+async function programSystemAdministratorJavaCard(): Promise<string> {
   const card = new JavaCard(
+    // Uses process.env.NODE_ENV to determine which config to use
     constructJavaCardConfig({ includeCardProgrammingConfig: true })
   );
   await waitForReadyCardStatus(card);
+
+  let jurisdiction = DEV_JURISDICTION;
+  let pin = '000000';
+  if (isProduction) {
+    jurisdiction = getRequiredEnvVar('VX_MACHINE_JURISDICTION');
+    pin = generatePin();
+  }
   try {
     await card.program({
-      user: {
-        role: 'system_administrator',
-        jurisdiction: DEV_JURISDICTION,
-      },
-      pin: '000000',
+      user: { role: 'system_administrator', jurisdiction },
+      pin,
     });
   } catch (error) {
     const errorMessage = error instanceof Error ? error.message : '';
@@ -29,20 +49,24 @@ async function programDevSystemAdministratorJavaCard(): Promise<void> {
       `${errorMessage}\n${initialJavaCardConfigurationScriptReminder}`
     );
   }
+  return pin;
 }
 
 /**
- * A script for programming a dev system administrator Java Card to bootstrap local development
- * with real smart cards. Once you have your first system administrator card, you can program all
- * other cards, including additional system administrator cards, through the VxAdmin UI.
+ * A script for programming a first system administrator Java Card to bootstrap both real usage and
+ * local development.
+ *
+ * Uses the NODE_ENV env var to determine whether to program a production or development card.
+ * Programming a production card requires additional production-machine-specific env vars.
  */
 export async function main(): Promise<void> {
+  let pin: string;
   try {
-    await programDevSystemAdministratorJavaCard();
+    pin = await programSystemAdministratorJavaCard();
   } catch (error) {
     console.error(error instanceof Error ? `❌ ${error.message}` : error);
     process.exit(1);
   }
-  console.log('✅ Done!');
+  console.log(`✅ Done! Card PIN is ${hyphenatePin(pin)}.`);
   process.exit(0);
 }
--- libs/auth/src/env.d.ts
+++ libs/auth/src/env.d.ts
@@ -2,6 +2,7 @@ declare namespace NodeJS {
   export interface ProcessEnv {
     NODE_ENV: 'development' | 'production' | 'test';
     VX_CONFIG_ROOT?: string;
+    VX_MACHINE_JURISDICTION?: string;
     VX_MACHINE_PRIVATE_KEY_PASSWORD?: string;
   }
 }
