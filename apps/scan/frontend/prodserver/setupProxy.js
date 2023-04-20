73948cacdfb9c28a425e38bf8577ed3629acf07d
Jonah Kagan <jonah@voting.works>
2023-04-20 14:10:32 -0700

    Install in VxScan

--- apps/scan/frontend/prodserver/setupProxy.js
+++ apps/scan/frontend/prodserver/setupProxy.js
@@ -13,5 +13,6 @@ const { createProxyMiddleware: proxy } = require('http-proxy-middleware');
  */
 module.exports = function (app) {
   app.use(proxy('/api', { target: 'http://localhost:3002/' }));
+  app.use(proxy('/dock', { target: 'http://localhost:3002/' }));
   app.use(proxy('/card', { target: 'http://localhost:3001/' }));
 };
