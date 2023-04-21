f6e6f90c144537ef55f45876bca79448806393ac
Kevin Shen <kevin@voting.works>
2023-04-21 13:47:07 -0700

    add tests

--- apps/admin/frontend/src/screens/unconfigured_screen.test.tsx
+++ apps/admin/frontend/src/screens/unconfigured_screen.test.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import {
+  electionFamousNames2021Fixtures,
   electionMinimalExhaustiveSampleFixtures,
   systemSettings,
 } from '@votingworks/fixtures';
 import userEvent from '@testing-library/user-event';
 import { zipFile } from '@votingworks/test-utils';
+import { DEFAULT_SYSTEM_SETTINGS } from '@votingworks/types';
 import { renderInAppContext } from '../../test/render_in_app_context';
 import { screen } from '../../test/react_testing_library';
 
@@ -54,4 +56,48 @@ test('handles an uploaded file', async () => {
   // election_manager (the parent component) handles advancing to the next screen so we
   // just need to test that loading is false and we rerender without the loading screen
   await screen.findByLabelText('Select Existing Setup Package Zip File');
+});
+
+test('uploads default system settings if loading only an election.json file', async () => {
+  const { electionDefinition } = electionMinimalExhaustiveSampleFixtures;
+
+  apiMock.expectConfigure(electionDefinition.electionData);
+  apiMock.expectSetSystemSettings(JSON.stringify(DEFAULT_SYSTEM_SETTINGS));
+
+  renderInAppContext(<UnconfiguredScreen />, {
+    apiMock,
+    electionDefinition: 'NONE',
+  });
+
+  const file = new File([electionDefinition.electionData], 'election.json');
+  const fileInput = await screen.findByLabelText(
+    'Select Existing Election Definition File'
+  );
+  userEvent.upload(fileInput, file);
+
+  await screen.findByText('Loading');
+  // election_manager (the parent component) handles advancing to the next screen so we
+  // just need to test that loading is false and we rerender without the loading screen
+  await screen.findByLabelText('Select Existing Election Definition File');
+});
+
+test('uploads default system settings if loading the default election', async () => {
+  const { electionDefinition } = electionFamousNames2021Fixtures;
+
+  apiMock.expectConfigure(electionDefinition.electionData);
+  apiMock.expectSetSystemSettings(JSON.stringify(DEFAULT_SYSTEM_SETTINGS));
+
+  renderInAppContext(<UnconfiguredScreen />, {
+    apiMock,
+    electionDefinition: 'NONE',
+  });
+
+  const loadDemoButton = await screen.findByText(
+    'Load Demo Election Definition'
+  );
+  userEvent.click(loadDemoButton);
+
+  // election_manager (the parent component) handles advancing to the next screen so we
+  // just need to test that loading is false and we rerender without the loading screen
+  await screen.findByLabelText('Select Existing Election Definition File');
 });
