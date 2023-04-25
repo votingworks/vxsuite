41beaab6dd61a5fe1bc2fa278081031240c2eb93
adghayes@gmail.com <adghayes@gmail.com>
2023-04-25 15:47:30 -0700

    perf testing stubs

---
+++ libs/cvr-fixture-generator/generate_admin_perf_fixtures.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+NUM_REPORTS=100
+RECORDS_PER_REPORT=10000
+
+echo "Generating $NUM_REPORTS cast vote record reports with $RECORDS_PER_REPORT records each..."
+
+for REPORT_IDX in $( seq 0 $((NUM_REPORTS - 1)) )
+do
+    ./bin/generate --ballotPackage ../fixtures/data/electionMinimalExhaustiveSample/ballot-package.zip --numBallots $RECORDS_PER_REPORT --outputPath ../../apps/admin/backend/perf-fixtures/$RECORDS_PER_REPORT/$REPORT_IDX --scannerNames scanner-$REPORT_IDX --ballotIdPrefix $REPORT_IDX --bmdBallots
+done
