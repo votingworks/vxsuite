e868c47851536f0b367dfc3b8d7023ba46a7d7cb
Arsalan Sufi <arsalan@voting.works>
2023-05-01 10:18:03 -0400

    Add auth settings to system settings type

--- libs/types/src/system_settings.ts
+++ libs/types/src/system_settings.ts
@@ -1,4 +1,19 @@
 import { z } from 'zod';
+
+import {
+  DEFAULT_INACTIVE_SESSION_TIME_LIMIT_MINUTES,
+  DEFAULT_NUM_INCORRECT_PIN_ATTEMPTS_ALLOWED_BEFORE_CARD_LOCKOUT,
+  DEFAULT_OVERALL_SESSION_TIME_LIMIT_HOURS,
+  DEFAULT_STARTING_CARD_LOCKOUT_DURATION_SECONDS,
+  InactiveSessionTimeLimitMinutes,
+  InactiveSessionTimeLimitMinutesSchema,
+  NumIncorrectPinAttemptsAllowedBeforeCardLockout,
+  NumIncorrectPinAttemptsAllowedBeforeCardLockoutSchema,
+  OverallSessionTimeLimitHours,
+  OverallSessionTimeLimitHoursSchema,
+  StartingCardLockoutDurationSeconds,
+  StartingCardLockoutDurationSecondsSchema,
+} from './auth';
 import { Id, Iso8601Timestamp } from './generic';
 
 /**
@@ -7,14 +22,29 @@ import { Id, Iso8601Timestamp } from './generic';
  */
 export interface SystemSettings {
   arePollWorkerCardPinsEnabled: boolean;
+  inactiveSessionTimeLimitMinutes: InactiveSessionTimeLimitMinutes;
+  numIncorrectPinAttemptsAllowedBeforeCardLockout: NumIncorrectPinAttemptsAllowedBeforeCardLockout;
+  overallSessionTimeLimitHours: OverallSessionTimeLimitHours;
+  startingCardLockoutDurationSeconds: StartingCardLockoutDurationSeconds;
 }
 
 export const SystemSettingsSchema: z.ZodType<SystemSettings> = z.object({
   arePollWorkerCardPinsEnabled: z.boolean(),
+  inactiveSessionTimeLimitMinutes: InactiveSessionTimeLimitMinutesSchema,
+  numIncorrectPinAttemptsAllowedBeforeCardLockout:
+    NumIncorrectPinAttemptsAllowedBeforeCardLockoutSchema,
+  overallSessionTimeLimitHours: OverallSessionTimeLimitHoursSchema,
+  startingCardLockoutDurationSeconds: StartingCardLockoutDurationSecondsSchema,
 });
 
 export const DEFAULT_SYSTEM_SETTINGS: SystemSettings = {
   arePollWorkerCardPinsEnabled: false,
+  inactiveSessionTimeLimitMinutes: DEFAULT_INACTIVE_SESSION_TIME_LIMIT_MINUTES,
+  numIncorrectPinAttemptsAllowedBeforeCardLockout:
+    DEFAULT_NUM_INCORRECT_PIN_ATTEMPTS_ALLOWED_BEFORE_CARD_LOCKOUT,
+  overallSessionTimeLimitHours: DEFAULT_OVERALL_SESSION_TIME_LIMIT_HOURS,
+  startingCardLockoutDurationSeconds:
+    DEFAULT_STARTING_CARD_LOCKOUT_DURATION_SECONDS,
 };
 
 /**
@@ -23,6 +53,9 @@ export const DEFAULT_SYSTEM_SETTINGS: SystemSettings = {
 export interface SystemSettingsDbRow {
   id: Id;
   created: Iso8601Timestamp;
-  // sqlite3 does not support booleans
-  arePollWorkerCardPinsEnabled: 0 | 1;
+  arePollWorkerCardPinsEnabled: 0 | 1; // sqlite3 does not support booleans
+  inactiveSessionTimeLimitMinutes: InactiveSessionTimeLimitMinutes;
+  numIncorrectPinAttemptsAllowedBeforeCardLockout: NumIncorrectPinAttemptsAllowedBeforeCardLockout;
+  overallSessionTimeLimitHours: OverallSessionTimeLimitHours;
+  startingCardLockoutDurationSeconds: StartingCardLockoutDurationSeconds;
 }
