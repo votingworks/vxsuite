76aaf6aae73ef5fa1b71fa0f5b8849b8d7e03851
Arsalan Sufi <arsalan@voting.works>
2023-05-11 10:14:34 -0400

    Reorder comment wording

--- libs/auth/src/tpm.ts
+++ libs/auth/src/tpm.ts
@@ -9,7 +9,7 @@ export const OPENSSL_TPM_ENGINE_NAME = 'tpm2tss';
 export const TPM_KEY_ID = '0x81000001';
 
 /**
- * The password of the TPM signing key. Not required for our security model, hence the dummy
- * password, just required by OpenSSL.
+ * The password of the TPM signing key. Not required for our security model, just required by
+ * OpenSSL, hence the dummy password.
  */
 export const TPM_KEY_PASSWORD = 'password';
