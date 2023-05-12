b57d9cdaaf74bf0de846659b342e2f8a921539a5
Brian Donovan <1938+eventualbuddha@users.noreply.github.com>
2023-05-11 17:31:49 -0700

    refactor(backend): use a more idiomatic loop construct

--- libs/backend/src/scan/interpretation/interpret_and_save_files.test.ts
+++ libs/backend/src/scan/interpretation/interpret_and_save_files.test.ts
@@ -28,7 +28,7 @@ test('interprets ballot images and saves images for storage', async () => {
     'InterpretedBmdPage',
     'BlankPage',
   ]);
-  for (const imagePath of result.map((page) => page.imagePath)) {
+  for (const { imagePath } of result) {
     await expect(loadImageData(imagePath)).resolves.toBeDefined();
   }
 });
