b2a6a1441f3982eab929267dd20c2cbd4aa3251a
Kofi Ohene-Adu <kofi@voting.works>
2023-05-15 16:37:53 -0500

    Remove now-unused components and react hook

--- apps/mark/frontend/src/components/contest_screen_layout.tsx
+++ apps/mark/frontend/src/components/contest_screen_layout.tsx
@@ -1,5 +1,4 @@
 import styled from 'styled-components';
-import { Scrollable, ScrollShadows } from '../config/types';
 
 export const ContentHeader = styled.div`
   padding: 0.25rem 0.5rem 0.5rem;
@@ -14,110 +13,6 @@ export const DistrictName = styled.div`
   font-size: 0.85rem;
   font-weight: 600;
 `;
-export const VariableContentContainer = styled.div<ScrollShadows>`
-  display: flex;
-  position: relative;
-  overflow: auto;
-  &::before,
-  &::after {
-    position: absolute;
-    z-index: 1;
-    width: 100%;
-    height: 0.25rem;
-    content: '';
-    transition: opacity 0.25s ease;
-  }
-  &::before {
-    top: 0;
-    opacity: ${({ showTopShadow }) =>
-      showTopShadow ? /* istanbul ignore next: Tested by Cypress */ 1 : 0};
-    background: linear-gradient(
-      to bottom,
-      rgb(177, 186, 190) 0%,
-      transparent 100%
-    );
-  }
-  &::after {
-    bottom: 0;
-    opacity: ${({ showBottomShadow }) =>
-      showBottomShadow ? /* istanbul ignore next: Tested by Cypress */ 1 : 0};
-    background: linear-gradient(
-      to bottom,
-      transparent 0%,
-      rgb(177, 186, 190) 100%
-    );
-  }
-`;
-export const ScrollControls = styled.div`
-  z-index: 2;
-  & > button {
-    position: absolute;
-    right: 1.5rem;
-    transform: opacity 1s linear;
-    visibility: visible;
-    opacity: 1;
-    outline: none;
-    box-shadow: 0 0 10px 3px rgba(0, 0, 0, 0.3);
-    width: 8rem;
-    height: 5rem;
-    padding: 0;
-    font-weight: 700;
-    transition: visibility 0s linear 0s, opacity 500ms;
-    &[disabled] {
-      visibility: hidden;
-      opacity: 0;
-      transition: visibility 0s linear 500ms, opacity 500ms;
-      pointer-events: none;
-    }
-    & > span {
-      position: relative;
-      pointer-events: none;
-    }
-    &::before {
-      position: absolute;
-      left: 50%;
-      margin-left: -1rem;
-      width: 2rem;
-      height: 2rem;
-      content: '';
-    }
-    &:first-child {
-      top: 0;
-      border-radius: 0 0 4rem 4rem;
-      & > span {
-        top: -1.25rem;
-      }
-      &::before {
-        bottom: 0.75rem;
-        background: url('/images/arrow-up.svg') no-repeat;
-      }
-    }
-    &:last-child {
-      bottom: 0;
-      border-radius: 4rem 4rem 0 0;
-      & > span {
-        top: 1.25rem;
-      }
-      &::before {
-        top: 0.75rem;
-        background: url('/images/arrow-down.svg') no-repeat;
-      }
-    }
-  }
-`;
-export const ScrollContainer = styled.div`
-  flex: 1;
-  overflow: auto;
-`;
-export const ScrollableContentWrapper = styled.div<Scrollable>`
-  margin: 0 auto;
-  width: 100%;
-  padding: 0.25rem 20px 0;
-  padding-right: ${({ isScrollable }) =>
-    isScrollable
-      ? /* istanbul ignore next: Tested by Cypress */ '11rem'
-      : undefined};
-`;
 export const ContestDescription = styled.div`
   padding: 0 10px;
 `;
@@ -128,5 +23,4 @@ export const ChoicesGrid = styled.div.attrs({
   display: grid;
   grid-auto-rows: minmax(auto, 1fr);
   grid-gap: 0.5rem;
-  margin-bottom: 1rem;
 `;
--- apps/mark/frontend/src/hooks/use_current_text_size.tsx
+++
@@ -1,7 +0,0 @@
-import { useCurrentTheme } from '@votingworks/ui';
-
-/** Returns the current base text size, in pixels, of the HTML document. */
-export function useCurrentTextSizePx(): number {
-  const currentTheme = useCurrentTheme();
-  return currentTheme.sizes.fontDefault;
-}
