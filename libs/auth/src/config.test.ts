de08e9a524388fbf11861eb7c488cb7e424c9d53
Arsalan Sufi <arsalan@voting.works>
2023-05-17 16:57:04 -0400

    Update config.test.ts

--- libs/auth/src/config.test.ts
+++ libs/auth/src/config.test.ts
@@ -1,7 +1,12 @@
 import { mockOf } from '@votingworks/test-utils';
 import { isVxDev } from '@votingworks/utils';
 
-import { constructJavaCardConfig, JavaCardConfig } from './java_card_config';
+import {
+  constructArtifactAuthenticatorConfig,
+  constructJavaCardConfig,
+  ArtifactAuthenticatorConfig,
+  JavaCardConfig,
+} from './config';
 
 jest.mock('@votingworks/utils', (): typeof import('@votingworks/utils') => ({
   ...jest.requireActual('@votingworks/utils'),
@@ -9,41 +14,30 @@ jest.mock('@votingworks/utils', (): typeof import('@votingworks/utils') => ({
 }));
 
 beforeEach(() => {
-  process.env.NODE_ENV = 'test';
-  process.env.VX_CONFIG_ROOT = '/vx/config';
-
+  (process.env.NODE_ENV as string) = 'test';
+  (process.env.VX_CONFIG_ROOT as string) = '/vx/config';
   mockOf(isVxDev).mockImplementation(() => false);
 });
 
 test.each<{
-  nodeEnv: 'development' | 'production' | 'test';
+  nodeEnv: NodeJS.ProcessEnv['NODE_ENV'];
   isVxDev?: true;
-  input: Parameters<typeof constructJavaCardConfig>[0];
+  machineType: NonNullable<NodeJS.ProcessEnv['VX_MACHINE_TYPE']>;
   expectedOutput: JavaCardConfig;
 }>([
   {
     nodeEnv: 'development',
-    input: undefined,
+    machineType: 'admin',
     expectedOutput: {
-      vxCertAuthorityCertPath: expect.stringContaining(
-        '/certs/dev/vx-cert-authority-cert.pem'
-      ),
-    },
-  },
-  {
-    nodeEnv: 'production',
-    input: undefined,
-    expectedOutput: {
-      vxCertAuthorityCertPath: expect.stringContaining(
-        '/certs/prod/vx-cert-authority-cert.pem'
-      ),
-    },
-  },
-  {
-    nodeEnv: 'production',
-    isVxDev: true,
-    input: undefined,
-    expectedOutput: {
+      cardProgrammingConfig: {
+        vxAdminCertAuthorityCertPath: expect.stringContaining(
+          '/certs/dev/vx-admin-cert-authority-cert.pem'
+        ),
+        vxAdminPrivateKey: {
+          source: 'file',
+          path: expect.stringContaining('/certs/dev/vx-admin-private-key.pem'),
+        },
+      },
       vxCertAuthorityCertPath: expect.stringContaining(
         '/certs/dev/vx-cert-authority-cert.pem'
       ),
@@ -51,7 +45,40 @@ test.each<{
   },
   {
     nodeEnv: 'development',
-    input: { includeCardProgrammingConfig: true },
+    machineType: 'scan',
+    expectedOutput: {
+      vxCertAuthorityCertPath: expect.stringContaining(
+        '/certs/dev/vx-cert-authority-cert.pem'
+      ),
+    },
+  },
+  {
+    nodeEnv: 'production',
+    machineType: 'admin',
+    expectedOutput: {
+      cardProgrammingConfig: {
+        vxAdminCertAuthorityCertPath:
+          '/vx/config/vx-admin-cert-authority-cert.pem',
+        vxAdminPrivateKey: { source: 'tpm' },
+      },
+      vxCertAuthorityCertPath: expect.stringContaining(
+        '/certs/prod/vx-cert-authority-cert.pem'
+      ),
+    },
+  },
+  {
+    nodeEnv: 'production',
+    machineType: 'scan',
+    expectedOutput: {
+      vxCertAuthorityCertPath: expect.stringContaining(
+        '/certs/prod/vx-cert-authority-cert.pem'
+      ),
+    },
+  },
+  {
+    nodeEnv: 'production',
+    isVxDev: true,
+    machineType: 'admin',
     expectedOutput: {
       cardProgrammingConfig: {
         vxAdminCertAuthorityCertPath: expect.stringContaining(
@@ -69,15 +96,80 @@ test.each<{
   },
   {
     nodeEnv: 'production',
-    input: { includeCardProgrammingConfig: true },
+    isVxDev: true,
+    machineType: 'scan',
     expectedOutput: {
-      cardProgrammingConfig: {
-        vxAdminCertAuthorityCertPath:
-          '/vx/config/vx-admin-cert-authority-cert.pem',
-        vxAdminPrivateKey: {
-          source: 'tpm',
-        },
+      vxCertAuthorityCertPath: expect.stringContaining(
+        '/certs/dev/vx-cert-authority-cert.pem'
+      ),
+    },
+  },
+])(
+  'constructJavaCardConfig - nodeEnv = $nodeEnv, isVxDev = $isVxDev, machineType = $machineType',
+  ({ nodeEnv, isVxDev: isVxDevResult, machineType, expectedOutput }) => {
+    (process.env.NODE_ENV as string) = nodeEnv;
+    (process.env.VX_MACHINE_TYPE as string) = machineType;
+    mockOf(isVxDev).mockImplementation(() => isVxDevResult ?? false);
+
+    expect(constructJavaCardConfig()).toEqual(expectedOutput);
+  }
+);
+
+test.each<{
+  nodeEnv: NodeJS.ProcessEnv['NODE_ENV'];
+  isVxDev?: true;
+  machineType: NonNullable<NodeJS.ProcessEnv['VX_MACHINE_TYPE']>;
+  expectedOutput: ArtifactAuthenticatorConfig;
+}>([
+  {
+    nodeEnv: 'development',
+    machineType: 'admin',
+    expectedOutput: {
+      signingMachineCertPath: expect.stringContaining(
+        '/certs/dev/vx-admin-cert-authority-cert.pem'
+      ),
+      signingMachinePrivateKey: {
+        source: 'file',
+        path: expect.stringContaining('/certs/dev/vx-admin-private-key.pem'),
       },
+      vxCertAuthorityCertPath: expect.stringContaining(
+        '/certs/dev/vx-cert-authority-cert.pem'
+      ),
+    },
+  },
+  {
+    nodeEnv: 'development',
+    machineType: 'scan',
+    expectedOutput: {
+      signingMachineCertPath: expect.stringContaining(
+        '/certs/dev/vx-scan-cert.pem'
+      ),
+      signingMachinePrivateKey: {
+        source: 'file',
+        path: expect.stringContaining('/certs/dev/vx-scan-private-key.pem'),
+      },
+      vxCertAuthorityCertPath: expect.stringContaining(
+        '/certs/dev/vx-cert-authority-cert.pem'
+      ),
+    },
+  },
+  {
+    nodeEnv: 'production',
+    machineType: 'admin',
+    expectedOutput: {
+      signingMachineCertPath: '/vx/config/vx-admin-cert-authority-cert.pem',
+      signingMachinePrivateKey: { source: 'tpm' },
+      vxCertAuthorityCertPath: expect.stringContaining(
+        '/certs/prod/vx-cert-authority-cert.pem'
+      ),
+    },
+  },
+  {
+    nodeEnv: 'production',
+    machineType: 'scan',
+    expectedOutput: {
+      signingMachineCertPath: '/vx/config/vx-scan-cert.pem',
+      signingMachinePrivateKey: { source: 'tpm' },
       vxCertAuthorityCertPath: expect.stringContaining(
         '/certs/prod/vx-cert-authority-cert.pem'
       ),
@@ -86,16 +178,31 @@ test.each<{
   {
     nodeEnv: 'production',
     isVxDev: true,
-    input: { includeCardProgrammingConfig: true },
+    machineType: 'admin',
     expectedOutput: {
-      cardProgrammingConfig: {
-        vxAdminCertAuthorityCertPath: expect.stringContaining(
-          '/certs/dev/vx-admin-cert-authority-cert.pem'
-        ),
-        vxAdminPrivateKey: {
-          source: 'file',
-          path: expect.stringContaining('/certs/dev/vx-admin-private-key.pem'),
-        },
+      signingMachineCertPath: expect.stringContaining(
+        '/certs/dev/vx-admin-cert-authority-cert.pem'
+      ),
+      signingMachinePrivateKey: {
+        source: 'file',
+        path: expect.stringContaining('/certs/dev/vx-admin-private-key.pem'),
+      },
+      vxCertAuthorityCertPath: expect.stringContaining(
+        '/certs/dev/vx-cert-authority-cert.pem'
+      ),
+    },
+  },
+  {
+    nodeEnv: 'production',
+    isVxDev: true,
+    machineType: 'scan',
+    expectedOutput: {
+      signingMachineCertPath: expect.stringContaining(
+        '/certs/dev/vx-scan-cert.pem'
+      ),
+      signingMachinePrivateKey: {
+        source: 'file',
+        path: expect.stringContaining('/certs/dev/vx-scan-private-key.pem'),
       },
       vxCertAuthorityCertPath: expect.stringContaining(
         '/certs/dev/vx-cert-authority-cert.pem'
@@ -103,10 +210,12 @@ test.each<{
     },
   },
 ])(
-  'constructJavaCardConfig - nodeEnv = $nodeEnv, isVxDev = $isVxDev, input = $input',
-  ({ nodeEnv, isVxDev: isVxDevResult = false, input, expectedOutput }) => {
-    process.env.NODE_ENV = nodeEnv;
-    mockOf(isVxDev).mockImplementation(() => isVxDevResult);
-    expect(constructJavaCardConfig(input)).toEqual(expectedOutput);
+  'constructArtifactAuthenticatorConfig - nodeEnv = $nodeEnv, isVxDev = $isVxDev, machineType = $machineType',
+  ({ nodeEnv, isVxDev: isVxDevResult, machineType, expectedOutput }) => {
+    (process.env.NODE_ENV as string) = nodeEnv;
+    (process.env.VX_MACHINE_TYPE as string) = machineType;
+    mockOf(isVxDev).mockImplementation(() => isVxDevResult ?? false);
+
+    expect(constructArtifactAuthenticatorConfig()).toEqual(expectedOutput);
   }
 );
