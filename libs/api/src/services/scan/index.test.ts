e918bfe4f20890f9374451600c179c31c1f28180
Brian Donovan <1938+eventualbuddha@users.noreply.github.com>
2023-05-18 05:19:39 -0700

    chore(api): remove unused types/schemas

--- libs/api/src/services/scan/index.test.ts
+++
@@ -1,9 +0,0 @@
-import { safeParse } from '@votingworks/types';
-import { GetPrecinctSelectionConfigResponseSchema } from '.';
-
-test('sanity check', () => {
-  safeParse(GetPrecinctSelectionConfigResponseSchema, {
-    status: 'ok',
-    precinctId: 'abc',
-  }).unsafeUnwrap();
-});
