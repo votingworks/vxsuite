187a4b1ad32ed6757e17140b2dc006f8c197efe5
Kevin Shen <kevin@voting.works>
2023-05-22 13:26:14 -0700

    update print width to 1700 and extend CLI

--- libs/custom-paper-handler/src/test/fixtures/ballot_fixture.ts
+++ libs/custom-paper-handler/src/test/fixtures/ballot_fixture.ts
@@ -1,5 +1,5 @@
 import fs from 'fs';
 import path from 'path';
 
-const filename = path.join(__dirname, 'ballot.pdf');
+const filename = path.join(__dirname, 'ballot_1700.pdf');
 export const ballotFixture: Uint8Array = fs.readFileSync(filename);
