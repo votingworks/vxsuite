748cc380c0ad992c6aa6b180a3282cddf2a8eed4
Arsalan Sufi <arsalan@voting.works>
2023-05-23 16:10:20 -0400

    Update VxMark backend tests

--- apps/mark/backend/src/server.test.ts
+++ apps/mark/backend/src/server.test.ts
@@ -1,17 +1,22 @@
 import { fakeLogger } from '@votingworks/logging';
 import tmp from 'tmp';
-import { buildMockInsertedSmartCardAuth } from '@votingworks/auth';
+import {
+  buildMockArtifactAuthenticator,
+  buildMockInsertedSmartCardAuth,
+} from '@votingworks/auth';
 import { PORT } from './globals';
 import { start } from './server';
 import { createWorkspace } from './util/workspace';
 
 test('can start server', () => {
   const auth = buildMockInsertedSmartCardAuth();
+  const artifactAuthenticator = buildMockArtifactAuthenticator();
   const logger = fakeLogger();
   const workspace = createWorkspace(tmp.dirSync().name);
 
   const server = start({
     auth,
+    artifactAuthenticator,
     logger,
     port: PORT,
     workspace,
