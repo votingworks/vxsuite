8cb4b087114ab36e91e34e24d889aef26390a5b9
Arsalan Sufi <arsalan@voting.works>
2023-05-23 16:14:52 -0400

    Have ballot package import helper verify ballot package signatures

--- libs/types/src/errors.ts
+++ libs/types/src/errors.ts
@@ -4,4 +4,5 @@
 export type BallotPackageConfigurationError =
   | 'no_ballot_package_on_usb_drive'
   | 'auth_required_before_ballot_package_load'
+  | 'ballot_package_authentication_error'
   | 'election_hash_mismatch';
