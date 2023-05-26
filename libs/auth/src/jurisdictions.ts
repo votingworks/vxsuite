af84ed3eacc63a06b30ea4761cfea16854b37e1a
Arsalan Sufi <arsalan@voting.works>
2023-05-26 15:18:03 -0400

    Explain why DEV_JURISDICTION looks the way it does
    st. != saint LOL

--- libs/auth/src/jurisdictions.ts
+++ libs/auth/src/jurisdictions.ts
@@ -1,4 +1,5 @@
 /**
- * The jurisdiction in all dev certs
+ * The jurisdiction in all dev certs. Follows the format of a prod cert jurisdiction
+ * ({state-2-letter-abbreviation}.{county-or-town}).
  */
 export const DEV_JURISDICTION = 'st.dev-jurisdiction';
