b437ab21502b5a6d7a2b52fcc67b80e349c0f340
Brian Donovan <1938+eventualbuddha@users.noreply.github.com>
2023-05-31 16:28:50 -0700

    chore(mark): remove unused code
    These things were tested but not used outside the tests.

--- apps/mark/frontend/src/components/no_print.tsx
+++
@@ -1,7 +0,0 @@
-import styled from 'styled-components';
-
-export const NoPrint = styled.span`
-  @media print {
-    display: none;
-  }
-`;
--- apps/mark/frontend/src/utils/number_with_commas.ts
+++
@@ -1,3 +0,0 @@
-export function numberWithCommas(x: number): string {
-  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
-}
