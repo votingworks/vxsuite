3eacfe5fcdb53c6f09352e5d8097514e3b849ed9
adghayes@gmail.com <adghayes@gmail.com>
2023-05-31 16:45:24 -0700

    update utilities & app to new types

--- apps/admin/backend/src/util/manual_tallies.test.ts
+++ apps/admin/backend/src/util/manual_tallies.test.ts
@@ -1,232 +1,85 @@
 import { electionMinimalExhaustiveSampleDefinition } from '@votingworks/fixtures';
-import { buildSpecificManualTally } from '@votingworks/utils';
-import { writeInCandidate as genericWriteInCandidate } from '@votingworks/types';
-import { combineManualTallies } from './manual_tallies';
-import { WriteInCandidateRecord } from '../types';
+import {
+  buildManualResultsFixture,
+  buildSpecificManualTally,
+} from '@votingworks/utils';
+import { convertResultsToDeprecatedTally } from './manual_tallies';
 
 const electionDefinition = electionMinimalExhaustiveSampleDefinition;
 const { election } = electionDefinition;
-test('combineManualTallies', () => {
-  const tally1 = buildSpecificManualTally(election, 10, {
-    'best-animal-mammal': {
-      overvotes: 1,
-      undervotes: 0,
-      ballots: 10,
-      officialOptionTallies: {
-        horse: 4,
-        otter: 3,
-        fox: 2,
-      },
-    },
-    fishing: {
-      overvotes: 1,
-      undervotes: 1,
-      ballots: 10,
-      officialOptionTallies: {
-        yes: 4,
-        no: 4,
-      },
-    },
-  });
 
-  const tally2 = buildSpecificManualTally(election, 15, {
-    'best-animal-mammal': {
-      overvotes: 0,
-      undervotes: 3,
-      ballots: 15,
-      officialOptionTallies: {
-        horse: 1,
-        otter: 9,
-        fox: 0,
+test('convertResultsToDeprecatedTally', () => {
+  const manualResults = buildManualResultsFixture({
+    election,
+    ballotCount: 10,
+    contestResultsSummaries: {
+      fishing: {
+        type: 'yesno',
+        ballots: 10,
+        overvotes: 3,
+        undervotes: 2,
+        yesTally: 5,
+        noTally: 0,
       },
-      writeInOptionTallies: {
-        chimera: {
-          count: 2,
-          candidate: {
-            name: 'Chimera',
-            id: 'chimera',
-            isWriteIn: true,
-          },
-        },
-      },
-    },
-    fishing: {
-      overvotes: 0,
-      undervotes: 5,
-      ballots: 15,
-      officialOptionTallies: {
-        yes: 9,
-        no: 1,
-      },
-    },
-  });
-
-  const tally3 = buildSpecificManualTally(election, 5, {
-    'best-animal-mammal': {
-      overvotes: 0,
-      undervotes: 0,
-      ballots: 5,
-      officialOptionTallies: {
-        horse: 1,
-        otter: 1,
-        fox: 1,
-      },
-      writeInOptionTallies: {
-        rapidash: {
-          count: 2,
-          candidate: {
-            name: 'Rapidash',
-            id: 'rapidash',
-            isWriteIn: true,
-          },
-        },
-      },
-    },
-    fishing: {
-      overvotes: 0,
-      undervotes: 0,
-      ballots: 5,
-      officialOptionTallies: {
-        yes: 4,
-        no: 1,
-      },
-    },
-  });
-
-  const writeInCandidates: WriteInCandidateRecord[] = [
-    {
-      contestId: 'best-animal-mammal',
-      electionId: 'uuid',
-      id: 'chimera',
-      name: 'Chimera',
-    },
-    {
-      contestId: 'best-animal-mammal',
-      electionId: 'uuid',
-      id: 'rapidash',
-      name: 'Rapidash',
-    },
-  ];
-
-  // without merging write-ins
-  expect(
-    combineManualTallies({
-      manualTallies: [tally1, tally2, tally3],
-      election,
-      writeInCandidates,
-      mergeWriteIns: false,
-    })
-  ).toMatchObject(
-    buildSpecificManualTally(election, 30, {
-      'best-animal-mammal': {
-        overvotes: 1,
-        undervotes: 3,
-        ballots: 30,
+      'zoo-council-mammal': {
+        type: 'candidate',
+        ballots: 10,
+        overvotes: 3,
+        undervotes: 2,
         officialOptionTallies: {
-          horse: 6,
-          otter: 13,
-          fox: 3,
+          zebra: 8,
+          lion: 6,
+          kangaroo: 7,
+          elephant: 2,
         },
         writeInOptionTallies: {
-          rapidash: {
-            count: 2,
-            candidate: {
-              name: 'Rapidash',
-              id: 'rapidash',
-              isWriteIn: true,
-            },
-          },
-          chimera: {
-            count: 2,
-            candidate: {
-              name: 'Chimera',
-              id: 'chimera',
-              isWriteIn: true,
-            },
+          somebody: {
+            id: 'somebody',
+            name: 'Somebody',
+            tally: 2,
           },
         },
       },
-      fishing: {
-        overvotes: 1,
-        undervotes: 6,
-        ballots: 30,
-        officialOptionTallies: {
-          yes: 17,
-          no: 6,
-        },
-      },
-    })
+    },
+  });
+
+  const convertedManualResults = convertResultsToDeprecatedTally(
+    election,
+    manualResults
   );
 
-  // with merging write-ins
-  expect(
-    combineManualTallies({
-      manualTallies: [tally1, tally2, tally3],
-      election,
-      writeInCandidates,
-      mergeWriteIns: true,
-    })
-  ).toMatchObject(
-    buildSpecificManualTally(election, 30, {
-      'best-animal-mammal': {
-        overvotes: 1,
-        undervotes: 3,
-        ballots: 30,
+  expect(convertedManualResults).toEqual(
+    buildSpecificManualTally(election, 10, {
+      fishing: {
+        ballots: 10,
+        overvotes: 3,
+        undervotes: 2,
         officialOptionTallies: {
-          horse: 6,
-          otter: 13,
-          fox: 3,
+          yes: 5,
+          no: 0,
+        },
+      },
+      'zoo-council-mammal': {
+        ballots: 10,
+        overvotes: 3,
+        undervotes: 2,
+        officialOptionTallies: {
+          zebra: 8,
+          lion: 6,
+          kangaroo: 7,
+          elephant: 2,
         },
         writeInOptionTallies: {
-          [genericWriteInCandidate.id]: {
-            count: 4,
-            candidate: genericWriteInCandidate,
+          somebody: {
+            candidate: {
+              id: 'somebody',
+              name: 'Somebody',
+              isWriteIn: true,
+            },
+            count: 2,
           },
         },
       },
-      fishing: {
-        overvotes: 1,
-        undervotes: 6,
-        ballots: 30,
-        officialOptionTallies: {
-          yes: 17,
-          no: 6,
-        },
-      },
-    })
-  );
-
-  // order independence, merging write-ins
-  expect(
-    combineManualTallies({
-      manualTallies: [tally1, tally2, tally3],
-      election,
-      writeInCandidates,
-      mergeWriteIns: true,
-    })
-  ).toEqual(
-    combineManualTallies({
-      manualTallies: [tally3, tally2, tally1],
-      election,
-      writeInCandidates,
-      mergeWriteIns: true,
-    })
-  );
-
-  // order independence, no merging write-ins
-  expect(
-    combineManualTallies({
-      manualTallies: [tally1, tally2, tally3],
-      election,
-      writeInCandidates,
-      mergeWriteIns: false,
-    })
-  ).toEqual(
-    combineManualTallies({
-      manualTallies: [tally3, tally2, tally1],
-      election,
-      writeInCandidates,
-      mergeWriteIns: false,
     })
   );
 });
--- apps/admin/backend/src/util/manual_tallies.ts
+++ apps/admin/backend/src/util/manual_tallies.ts
@@ -1,225 +1,77 @@
 import {
   BallotStyleId,
-  Candidate,
-  CandidateContest,
-  CandidateId,
-  ContestOptionTally,
   ContestTally,
+  ContestTallyMeta,
   Dictionary,
   Election,
   Id,
   ManualTally,
   PartyId,
+  Tabulation,
   TallyCategory,
   VotingMethod,
-  YesNoContest,
-  writeInCandidate as genericWriteInCandidate,
 } from '@votingworks/types';
-import { Optional, assert, assertDefined } from '@votingworks/basics';
+import { Optional, assert } from '@votingworks/basics';
 import {
-  getEmptyCandidateContestTally,
+  combineManualElectionResults,
   getEmptyManualTally,
-  getEmptyYesNoContestTally,
 } from '@votingworks/utils';
 import { Store } from '../store';
-import {
-  ServerFullElectionManualTally,
-  WriteInCandidateRecord,
-} from '../types';
+import { ServerFullElectionManualTally } from '../types';
 
-function combineManualYesNoContestTallies({
-  contest,
-  contestTallies,
-}: {
-  contest: YesNoContest;
-  contestTallies: ContestTally[];
-}): ContestTally {
-  if (contestTallies.length === 0) {
-    return getEmptyYesNoContestTally(contest);
-  }
+/**
+ * Takes manual results in the new data format {@link Tabulation.ManualElectionResult}
+ * and converts it to the old format {@link ManualTally}.
+ */
+export function convertResultsToDeprecatedTally(
+  election: Election,
+  manualResults: Tabulation.ManualElectionResults
+): ManualTally {
+  const contestTallies: ManualTally['contestTallies'] = {};
 
-  let overvotes = 0;
-  let undervotes = 0;
-  let ballots = 0;
-  let yesCount = 0;
-  let noCount = 0;
-  for (const contestTally of contestTallies) {
-    overvotes += contestTally.metadata.overvotes;
-    undervotes += contestTally.metadata.undervotes;
-    ballots += contestTally.metadata.ballots;
-    yesCount += assertDefined(contestTally.tallies['yes']).tally;
-    noCount += assertDefined(contestTally.tallies['no']).tally;
-  }
+  for (const contest of election.contests) {
+    const contestResults = manualResults.contestResults[contest.id];
+    assert(contestResults);
 
-  return {
-    contest,
-    tallies: {
-      yes: {
+    const metadata: ContestTallyMeta = {
+      overvotes: contestResults.overvotes,
+      undervotes: contestResults.undervotes,
+      ballots: contestResults.ballots,
+    };
+
+    const tallies: ContestTally['tallies'] = {};
+    if (contestResults.contestType === 'yesno') {
+      tallies['yes'] = {
         option: ['yes'],
-        tally: yesCount,
-      },
-      no: {
+        tally: contestResults.yesTally,
+      };
+      tallies['no'] = {
         option: ['no'],
-        tally: noCount,
-      },
-    },
-    metadata: {
-      undervotes,
-      overvotes,
-      ballots,
-    },
-  };
-}
-
-function combineManualCandidateContestTallies({
-  contest,
-  contestTallies,
-  writeInCandidates,
-  mergeWriteIns,
-}: {
-  contest: CandidateContest;
-  contestTallies: ContestTally[];
-  writeInCandidates: WriteInCandidateRecord[];
-  mergeWriteIns: boolean;
-}): ContestTally {
-  if (contestTallies.length === 0) {
-    return getEmptyCandidateContestTally(contest);
-  }
-
-  const contestWriteInCandidates = writeInCandidates.filter(
-    (c) => c.contestId === contest.id
-  );
-
-  // iterate through all the tallies, summing counts
-  let overvotes = 0;
-  let undervotes = 0;
-  let ballots = 0;
-  const officialCandidateCounts: Record<CandidateId, number> = {};
-  const writeInCandidateCounts: Record<Id, number> = {};
-  for (const contestTally of contestTallies) {
-    overvotes += contestTally.metadata.overvotes;
-    undervotes += contestTally.metadata.undervotes;
-    ballots += contestTally.metadata.ballots;
-    for (const candidate of contest.candidates) {
-      const candidateTally = contestTally.tallies[candidate.id];
-      if (candidateTally) {
-        officialCandidateCounts[candidate.id] =
-          (officialCandidateCounts[candidate.id] ?? 0) + candidateTally.tally;
-      }
-    }
-    for (const writeInCandidate of contestWriteInCandidates) {
-      const writeInCandidateTally = contestTally.tallies[writeInCandidate.id];
-      if (writeInCandidateTally) {
-        writeInCandidateCounts[writeInCandidate.id] =
-          (writeInCandidateCounts[writeInCandidate.id] ?? 0) +
-          writeInCandidateTally.tally;
-      }
-    }
-  }
-
-  // format official candidate counts for the ContestTally
-  const combinedCandidateTallies: Dictionary<ContestOptionTally> = {};
-  for (const candidate of contest.candidates) {
-    combinedCandidateTallies[candidate.id] = {
-      option: candidate,
-      tally: officialCandidateCounts[candidate.id] ?? 0,
-    };
-  }
-
-  // if merging write-ins, combine all write-in votes under generic write-in
-  if (mergeWriteIns) {
-    const totalWriteInCount = Object.values(writeInCandidateCounts).reduce(
-      (acc, cur) => acc + cur,
-      0
-    );
-    combinedCandidateTallies[genericWriteInCandidate.id] = {
-      option: genericWriteInCandidate,
-      tally: totalWriteInCount,
-    };
-  } else {
-    // if not merging write-ins, format write-in counts for the combined ContestTally.
-    // only includes if write-in candidates seen in at least one of the contest tallies
-    for (const writeInCandidate of contestWriteInCandidates) {
-      const numVotesForWriteInCandidate =
-        writeInCandidateCounts[writeInCandidate.id];
-      if (numVotesForWriteInCandidate !== undefined) {
-        combinedCandidateTallies[writeInCandidate.id] = {
-          option: {
-            id: writeInCandidate.id,
-            name: writeInCandidate.name,
-            isWriteIn: true,
-          },
-          tally: numVotesForWriteInCandidate,
+        tally: contestResults.noTally,
+      };
+    } else {
+      assert(contest.type === 'candidate');
+      for (const candidateTally of Object.values(contestResults.tallies)) {
+        const { tally, ...candidate } = candidateTally;
+        tallies[candidateTally.id] = {
+          option:
+            contest.candidates.find((c) => c.id === candidateTally.id) ??
+            candidate,
+          tally,
         };
       }
     }
+
+    contestTallies[contest.id] = {
+      contest,
+      metadata,
+      tallies,
+    };
   }
 
   return {
-    contest,
-    tallies: combinedCandidateTallies,
-    metadata: {
-      undervotes,
-      overvotes,
-      ballots,
-    },
-  };
-}
-
-/**
- * Combine a list of manual tallies into a single manual tally.
- */
-export function combineManualTallies({
-  manualTallies,
-  election,
-  writeInCandidates,
-  mergeWriteIns,
-}: {
-  manualTallies: ManualTally[];
-  election: Election;
-  writeInCandidates: WriteInCandidateRecord[];
-  mergeWriteIns: boolean;
-}): ManualTally {
-  const combinedContestTallies: Dictionary<ContestTally> = {};
-
-  for (const contest of election.contests) {
-    const contestTallies = manualTallies
-      .map((manualTally) => manualTally.contestTallies[contest.id])
-      .filter(
-        (contestTally): contestTally is ContestTally =>
-          contestTally !== undefined
-      );
-
-    // the contest may not be relevant for this set of manual tallies
-    if (contestTallies.length === 0) {
-      continue;
-    }
-
-    if (contest.type === 'yesno') {
-      combinedContestTallies[contest.id] = combineManualYesNoContestTallies({
-        contest,
-        contestTallies,
-      });
-    } else {
-      combinedContestTallies[contest.id] = combineManualCandidateContestTallies(
-        {
-          contest,
-          contestTallies,
-          writeInCandidates,
-          mergeWriteIns,
-        }
-      );
-    }
-  }
-
-  const combinedNumberOfBallotsCounted = manualTallies.reduce(
-    (acc, cur) => acc + cur.numberOfBallotsCounted,
-    0
-  );
-
-  return {
-    numberOfBallotsCounted: combinedNumberOfBallotsCounted,
-    contestTallies: combinedContestTallies,
+    numberOfBallotsCounted: manualResults.ballotCount,
+    contestTallies,
   };
 }
 
@@ -239,29 +91,28 @@ export function buildFullElectionManualTallyFromStore(
 
   // TODO: wire up frontend so it can handle multiple ballot types. for now
   // we are only providing precinct manual tallies to frontend tallying
-  const manualTallyRecords = store.getManualTallies({
+  const manualResultsRecords = store.getManualResults({
     electionId,
     ballotType: 'precinct',
   });
-  if (manualTallyRecords.length === 0) return undefined;
-
-  const writeInCandidates = store.getWriteInCandidates({ electionId });
+  if (manualResultsRecords.length === 0) return undefined;
 
   // calculate manual tallies for each precinct
   const manualTalliesByPrecinct: Dictionary<ManualTally> = {};
   for (const precinct of election.precincts) {
-    const precinctManualTallies = manualTallyRecords
+    const precinctManualResults = manualResultsRecords
       .filter(({ precinctId }) => precinctId === precinct.id)
-      .map(({ manualTally }) => manualTally);
+      .map(({ manualResults }) => manualResults);
     manualTalliesByPrecinct[precinct.id] =
-      precinctManualTallies.length === 0
+      precinctManualResults.length === 0
         ? getEmptyManualTally(election)
-        : combineManualTallies({
-            manualTallies: [...precinctManualTallies],
+        : convertResultsToDeprecatedTally(
             election,
-            writeInCandidates,
-            mergeWriteIns: false,
-          });
+            combineManualElectionResults({
+              election,
+              allManualResults: precinctManualResults,
+            })
+          );
   }
 
   const ballotStyleIdsByPartyId: Record<PartyId, BallotStyleId[]> = {};
@@ -278,26 +129,30 @@ export function buildFullElectionManualTallyFromStore(
   for (const [partyId, ballotStyleIds] of Object.entries(
     ballotStyleIdsByPartyId
   )) {
-    const partyManualTallies = manualTallyRecords
+    const partyManualResults = manualResultsRecords
       .filter(({ ballotStyleId }) => ballotStyleIds.includes(ballotStyleId))
-      .map(({ manualTally }) => manualTally);
-    manualTalliesByParty[partyId] = combineManualTallies({
-      manualTallies: [...partyManualTallies],
+      .map(({ manualResults }) => manualResults);
+    manualTalliesByParty[partyId] = convertResultsToDeprecatedTally(
       election,
-      writeInCandidates,
-      mergeWriteIns: false,
-    });
+      combineManualElectionResults({
+        election,
+        allManualResults: partyManualResults,
+      })
+    );
   }
 
-  const overallTally = combineManualTallies({
-    manualTallies: manualTallyRecords.map(({ manualTally }) => manualTally),
+  const overallTally = convertResultsToDeprecatedTally(
     election,
-    writeInCandidates,
-    mergeWriteIns: false,
-  });
+    combineManualElectionResults({
+      election,
+      allManualResults: manualResultsRecords.map(
+        ({ manualResults }) => manualResults
+      ),
+    })
+  );
 
   let oldestCreatedAt = new Date();
-  for (const { createdAt } of manualTallyRecords) {
+  for (const { createdAt } of manualResultsRecords) {
     const createdAtDate = new Date(createdAt);
     if (createdAtDate < oldestCreatedAt) oldestCreatedAt = createdAtDate;
   }
@@ -314,53 +169,48 @@ export function buildFullElectionManualTallyFromStore(
 }
 
 /**
- * The manual tally entry form allows creating new write-in candidates. These
- * are included in the manual tally from the frontend prefaced by
+ * The manual results entry form allows creating new write-in candidates. These
+ * are included in the manual results from the frontend prefaced by
  * `temp-write-in-`. This method creates the new write-in candidates in the
- * database, substitutes the ids in the passed `ManualTally`, and strips out
- * any write-in references with zero votes. Edits the `ManualTally` in place.
+ * database, substitutes the ids in the passed `ManualElectionResults`, and strips out
+ * any write-in references with zero votes. Edits the `ManualElectionResults` in place.
  */
 export function handleEnteredWriteInCandidateData({
-  manualTally,
+  manualResults,
   electionId,
   store,
 }: {
-  manualTally: ManualTally;
+  manualResults: Tabulation.ManualElectionResults;
   electionId: Id;
   store: Store;
-}): ManualTally {
-  for (const contestTally of Object.values(manualTally.contestTallies)) {
-    assert(contestTally);
-    if (contestTally.contest.type === 'candidate') {
-      for (const [optionId, optionTally] of Object.entries(
-        contestTally.tallies
+}): Tabulation.ManualElectionResults {
+  for (const contestResults of Object.values(manualResults.contestResults)) {
+    if (contestResults.contestType === 'candidate') {
+      for (const [candidateId, candidateTally] of Object.entries(
+        contestResults.tallies
       )) {
-        assert(optionTally);
-        const candidate = optionTally?.option as Candidate;
-        if (candidate.isWriteIn) {
-          if (optionTally.tally === 0) {
+        if (candidateTally.isWriteIn) {
+          if (candidateTally.tally === 0) {
             // if any write-in candidate has no votes, remove them from tally
-            delete contestTally.tallies[optionId];
-          } else if (candidate.id.startsWith('temp-write-in-')) {
+            delete contestResults.tallies[candidateId];
+          } else if (candidateId.startsWith('temp-write-in-')) {
             // for temp-write-in candidates, create records and substitute ids
             const writeInCandidateRecord = store.addWriteInCandidate({
               electionId,
-              contestId: contestTally.contest.id,
-              name: candidate.name,
+              contestId: contestResults.contestId,
+              name: candidateTally.name,
             });
-            contestTally.tallies[writeInCandidateRecord.id] = {
-              ...optionTally,
-              option: {
-                ...candidate,
-                id: writeInCandidateRecord.id,
-              },
+            contestResults.tallies[writeInCandidateRecord.id] = {
+              // eslint-disable-next-line vx/gts-spread-like-types
+              ...candidateTally,
+              id: writeInCandidateRecord.id,
             };
-            delete contestTally.tallies[candidate.id];
+            delete contestResults.tallies[candidateId];
           }
         }
       }
     }
   }
 
-  return manualTally;
+  return manualResults;
 }
