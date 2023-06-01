53d140117d1e7506e7d4a37b4b4ad82b550fb31b
Brian Donovan <1938+eventualbuddha@users.noreply.github.com>
2023-06-01 09:29:39 -0700

    chore(mark): remove unused code (#3533)
    These things were tested but not used outside the tests.

--- apps/mark/frontend/src/components/no_print.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from '../../test/react_testing_library';
-
-import { NoPrint } from './no_print';
-
-it('renders NoPrint', () => {
-  const { container } = render(<NoPrint>foo</NoPrint>);
-  expect(container.firstChild).toMatchSnapshot();
-});
