ecd0f03874c03de82176b424b0670ae06a0088ba
Brian Donovan <1938+eventualbuddha@users.noreply.github.com>
2023-06-01 16:25:53 -0700

    ci(central-scan): use `v8` coverage
    Continuation of https://github.com/votingworks/vxsuite/pull/3539.

--- apps/central-scan/backend/src/central_scanner_app.ts
+++ apps/central-scan/backend/src/central_scanner_app.ts
@@ -134,7 +134,8 @@ function buildApi({
       }
     },
 
-    /* istanbul ignore next - only used by Cypress */
+    /* c8 ignore start */
+    // This is only used in Cypress tests.
     async configureWithSampleBallotPackageForIntegrationTest(): Promise<void> {
       const { electionGridLayoutNewHampshireAmherstFixtures } = await import(
         '@votingworks/fixtures'
@@ -147,6 +148,7 @@ function buildApi({
       importer.configure(electionDefinition, TEST_JURISDICTION);
       store.setSystemSettings(systemSettings);
     },
+    /* c8 ignore stop */
 
     async configureFromBallotPackageOnUsbDrive(): Promise<
       Result<ElectionDefinition, BallotPackageConfigurationError>
