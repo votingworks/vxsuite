e70b357f939741041857d7d273574afc53b72089
adghayes@gmail.com <adghayes@gmail.com>
2023-06-13 14:22:51 -0700

    backend

--- apps/admin/backend/src/app.exports.test.ts
+++ apps/admin/backend/src/app.exports.test.ts
@@ -4,6 +4,7 @@ import {
 } from '@votingworks/fixtures';
 import {
   BooleanEnvironmentVariableName,
+  buildManualResultsFixture,
   getFeatureFlagMock,
 } from '@votingworks/utils';
 import { tmpNameSync } from 'tmp';
@@ -102,4 +103,121 @@ test('sems export', async () => {
   expect(talliesByPrecinct['precinct-2']).toEqual(
     talliesByPrecinct['precinct-1']
   );
+});
+
+test('precinct / voting method results export', async () => {
+  const { electionDefinition, castVoteRecordReport } =
+    electionMinimalExhaustiveSampleFixtures;
+
+  const { apiClient, auth, logger } = buildTestEnvironment();
+  await configureMachine(apiClient, auth, electionDefinition);
+  mockElectionManagerAuth(auth, electionDefinition.electionHash);
+
+  const loadFileResult = await apiClient.addCastVoteRecordFile({
+    path: castVoteRecordReport.asDirectoryPath(),
+  });
+  loadFileResult.assertOk('load file failed');
+
+  const path = tmpNameSync();
+  const exportResult = await apiClient.exportResultsCsv({ path });
+  expect(exportResult.isOk()).toEqual(true);
+  expect(readFileSync(path, 'utf-8').toString()).toMatchSnapshot();
+  expect(logger.log).toHaveBeenLastCalledWith(
+    LogEventId.FileSaved,
+    'election_manager',
+    {
+      disposition: 'success',
+      filename: path,
+      message: `Saved csv results to ${path} on the USB drive.`,
+    }
+  );
+
+  // mock a failure
+  const offLimitsPath = '/root/hidden';
+  const failedExportResult = await apiClient.exportResultsCsv({
+    path: offLimitsPath,
+  });
+  expect(failedExportResult.isErr()).toEqual(true);
+  expect(logger.log).toHaveBeenLastCalledWith(
+    LogEventId.FileSaved,
+    'election_manager',
+    {
+      disposition: 'failure',
+      filename: offLimitsPath,
+      message: `Failed to save csv results to ${offLimitsPath} on the USB drive.`,
+    }
+  );
+});
+
+test('precinct / voting method results export - wia and manual data', async () => {
+  const { electionDefinition, castVoteRecordReport } =
+    electionGridLayoutNewHampshireAmherstFixtures;
+  const { election } = electionDefinition;
+
+  const { apiClient, auth } = buildTestEnvironment();
+  await configureMachine(apiClient, auth, electionDefinition);
+  mockElectionManagerAuth(auth, electionDefinition.electionHash);
+
+  const loadFileResult = await apiClient.addCastVoteRecordFile({
+    path: castVoteRecordReport.asDirectoryPath(),
+  });
+  loadFileResult.assertOk('load file failed');
+
+  const candidateContestId =
+    'State-Representatives-Hillsborough-District-34-b1012d38';
+
+  // export without wia and manual data
+  const path = tmpNameSync();
+  const exportResult = await apiClient.exportResultsCsv({ path });
+  expect(exportResult.isOk()).toEqual(true);
+  const fileContentForCandidateContest = readFileSync(path, 'utf-8')
+    .toString()
+    .split('\n')
+    .filter((line) => line.includes(candidateContestId))
+    .join('\n');
+  expect(fileContentForCandidateContest).toMatchSnapshot();
+
+  // add manual data
+  await apiClient.setManualResults({
+    precinctId: election.precincts[0]!.id,
+    votingMethod: 'absentee',
+    ballotStyleId: election.ballotStyles[0]!.id,
+    manualResults: buildManualResultsFixture({
+      election,
+      ballotCount: 10,
+      contestResultsSummaries: {
+        [candidateContestId]: {
+          type: 'candidate',
+          ballots: 10,
+          overvotes: 0,
+          undervotes: 0,
+          officialOptionTallies: {
+            'Obadiah-Carrigan-5c95145a': 10,
+          },
+        },
+      },
+    }),
+  });
+
+  // adjudicate write-ins
+  const writeIns = await apiClient.getWriteIns({
+    contestId: candidateContestId,
+  });
+  for (const writeIn of writeIns) {
+    await apiClient.adjudicateWriteIn({
+      writeInId: writeIn.id,
+      type: 'invalid',
+    });
+  }
+
+  // export with wia and manual data
+  const path2 = tmpNameSync();
+  const exportResult2 = await apiClient.exportResultsCsv({ path: path2 });
+  expect(exportResult2.isOk()).toEqual(true);
+  const fileContentForCandidateContest2 = readFileSync(path2, 'utf-8')
+    .toString()
+    .split('\n')
+    .filter((line) => line.includes(candidateContestId))
+    .join('\n');
+  expect(fileContentForCandidateContest2).toMatchSnapshot();
 });
---
+++ apps/admin/backend/src/exports/csv_results.ts
@@ -0,0 +1,207 @@
+// eslint-disable-next-line import/no-unresolved
+import { stringify } from 'csv-stringify/sync';
+import {
+  Election,
+  writeInCandidate,
+  Precinct,
+  Contest,
+  Tabulation,
+} from '@votingworks/types';
+import { assert, throwIllegalValue } from '@votingworks/basics';
+import { getEmptyElectionResults } from '@votingworks/utils';
+import { Readable } from 'stream';
+import { WriteInCandidateRecord } from '../types';
+
+function buildCsvRow(
+  precinct: Precinct,
+  votingMethod: string,
+  votes: number | undefined,
+  contest: Contest,
+  selection: string,
+  selectionId?: string
+): string {
+  return stringify([
+    [
+      `${contest.title}`,
+      contest.id,
+      `${selection}`,
+      selectionId ?? '',
+      `${precinct.name}`,
+      precinct.id,
+      votingMethod,
+      votes?.toString() ?? '0',
+    ],
+  ]);
+}
+
+function getVotingMethodLabel(votingMethod: Tabulation.VotingMethod): string {
+  switch (votingMethod) {
+    case 'absentee':
+      return 'Absentee';
+    case 'precinct':
+      return 'Precinct';
+    /* c8 ignore next 4 */
+    case 'provisional':
+      return 'Provisional';
+    default:
+      throwIllegalValue(votingMethod);
+  }
+}
+
+const INCLUDED_VOTING_METHODS: Tabulation.VotingMethod[] = [
+  'absentee',
+  'precinct',
+];
+
+function* generateRows({
+  electionResultsByPrecinctAndVotingMethod,
+  election,
+  writeInCandidates,
+}: {
+  electionResultsByPrecinctAndVotingMethod: Tabulation.GroupedElectionResults;
+  election: Election;
+  writeInCandidates: WriteInCandidateRecord[];
+}): Generator<string> {
+  for (const precinct of election.precincts) {
+    for (const votingMethod of INCLUDED_VOTING_METHODS) {
+      const electionResults =
+        Object.values(electionResultsByPrecinctAndVotingMethod).find(
+          (er) =>
+            er.precinctId === precinct.id && er.votingMethod === votingMethod
+        ) || getEmptyElectionResults(election);
+
+      for (const contest of election.contests) {
+        const contestWriteInCandidates = writeInCandidates.filter(
+          (c) => c.contestId === contest.id
+        );
+        const contestResults = electionResults.contestResults[contest.id];
+        assert(contestResults !== undefined);
+
+        if (contest.type === 'candidate') {
+          assert(contestResults.contestType === 'candidate');
+
+          // official candidate rows
+          for (const candidate of contest.candidates) {
+            yield buildCsvRow(
+              precinct,
+              getVotingMethodLabel(votingMethod),
+              contestResults.tallies[candidate.id]?.tally,
+              contest,
+              candidate.name,
+              candidate.id
+            );
+          }
+
+          // generic write-in row
+          if (contest.allowWriteIns) {
+            const tally = contestResults.tallies[writeInCandidate.id]?.tally;
+            if (tally) {
+              yield buildCsvRow(
+                precinct,
+                getVotingMethodLabel(votingMethod),
+                tally,
+                contest,
+                writeInCandidate.name,
+                writeInCandidate.id
+              );
+            }
+          }
+
+          // adjudicated write-in rows
+          for (const contestWriteInCandidate of contestWriteInCandidates) {
+            const tally =
+              contestResults.tallies[contestWriteInCandidate.id]?.tally;
+            if (tally) {
+              yield buildCsvRow(
+                precinct,
+                getVotingMethodLabel(votingMethod),
+                tally,
+                contest,
+                contestWriteInCandidate.name,
+                contestWriteInCandidate.id
+              );
+            }
+          }
+        } else if (contest.type === 'yesno') {
+          assert(contestResults.contestType === 'yesno');
+          yield buildCsvRow(
+            precinct,
+            getVotingMethodLabel(votingMethod),
+            contestResults.yesTally,
+            contest,
+            'Yes',
+            contest.yesOption?.id
+          );
+          yield buildCsvRow(
+            precinct,
+            getVotingMethodLabel(votingMethod),
+            contestResults.noTally,
+            contest,
+            'No',
+            contest.noOption?.id
+          );
+        }
+
+        yield buildCsvRow(
+          precinct,
+          getVotingMethodLabel(votingMethod),
+          contestResults.overvotes,
+          contest,
+          'Overvotes'
+        );
+
+        yield buildCsvRow(
+          precinct,
+          getVotingMethodLabel(votingMethod),
+          contestResults.undervotes,
+          contest,
+          'Undervotes'
+        );
+      }
+    }
+  }
+}
+
+/**
+ * Converts a tally for an election to a CSV file (represented as a string) of tally results
+ * broken down by voting method and precinct.
+ */
+export function generateResultsCsv({
+  electionResultsByPrecinctAndVotingMethod,
+  election,
+  writeInCandidates,
+}: {
+  electionResultsByPrecinctAndVotingMethod: Tabulation.GroupedElectionResults;
+  election: Election;
+  writeInCandidates: WriteInCandidateRecord[];
+}): NodeJS.ReadableStream {
+  // TODO(https://github.com/votingworks/vxsuite/issues/2631): Omit the voting method column for
+  // elections where we can't distinguish between absentee/precinct ballots (e.g. NH). Punted as
+  // out-of-scope for the NH pilot.
+  const headers = [
+    'Contest',
+    'Contest ID',
+    'Selection',
+    'Selection ID',
+    'Precinct',
+    'Precinct ID',
+    'Voting Method',
+    'Votes',
+  ];
+
+  const headerRow = stringify([headers]);
+
+  function* generateResultsCsvRows() {
+    yield headerRow;
+
+    for (const dataRow of generateRows({
+      electionResultsByPrecinctAndVotingMethod,
+      election,
+      writeInCandidates,
+    })) {
+      yield dataRow;
+    }
+  }
+
+  return Readable.from(generateResultsCsvRows());
+}
