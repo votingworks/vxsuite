826e40c5364fd5c4b45f2e1dfda98f62125430e0
adghayes@gmail.com <adghayes@gmail.com>
2023-06-16 15:33:38 -0700

    add AdminTallyReport component

---
+++ libs/ui/src/reports/admin_tally_report.test.tsx
@@ -0,0 +1,131 @@
+import React from 'react';
+import { electionMinimalExhaustiveSampleDefinition } from '@votingworks/fixtures';
+import {
+  buildElectionResultsFixture,
+  buildManualResultsFixture,
+  getEmptyElectionResults,
+} from '@votingworks/utils';
+import { hasTextAcrossElements } from '@votingworks/test-utils';
+import { render, screen } from '../../test/react_testing_library';
+import { AdminTallyReport } from './admin_tally_report';
+
+const { election } = electionMinimalExhaustiveSampleDefinition;
+
+function queryForContest(contestId: string) {
+  return screen.queryByTestId(`results-table-${contestId}`);
+}
+
+test('includes indicated contests', () => {
+  // all contests but first
+  const [excludedContest, ...includedContests] = election.contests;
+  render(
+    <AdminTallyReport
+      title="Title"
+      election={election}
+      contests={includedContests}
+      scannedElectionResults={getEmptyElectionResults(election, true)}
+    />
+  );
+
+  for (const contest of includedContests) {
+    expect(queryForContest(contest.id)).toBeInTheDocument();
+  }
+  expect(queryForContest(excludedContest.id)).not.toBeInTheDocument();
+});
+
+test('includes subtitle', () => {
+  render(
+    <AdminTallyReport
+      title="Title"
+      subtitle="Subtitle"
+      election={election}
+      contests={election.contests}
+      scannedElectionResults={getEmptyElectionResults(election, true)}
+    />
+  );
+  screen.getByRole('heading', { name: 'Title' });
+  screen.getByRole('heading', { name: 'Subtitle' });
+});
+
+test('includes specified date', () => {
+  render(
+    <AdminTallyReport
+      title="Title"
+      subtitle="Subtitle"
+      election={election}
+      contests={election.contests}
+      scannedElectionResults={getEmptyElectionResults(election, true)}
+      generatedAtTime={new Date('2020-01-01')}
+    />
+  );
+  screen.getByText(
+    'This report was created on Wednesday, January 1, 2020 at 12:00:00 AM UTC.'
+  );
+});
+
+const scannedElectionResults = buildElectionResultsFixture({
+  election,
+  cardCounts: {
+    bmd: 790,
+    hmpb: [],
+  },
+  contestResultsSummaries: {
+    fishing: {
+      type: 'yesno',
+      ballots: 791,
+      overvotes: 3,
+      undervotes: 88,
+      yesTally: 550,
+      noTally: 150,
+    },
+  },
+  includeGenericWriteIn: true,
+});
+
+test('with only scanned results', () => {
+  render(
+    <AdminTallyReport
+      title="Title"
+      subtitle="Subtitle"
+      election={election}
+      contests={election.contests}
+      scannedElectionResults={scannedElectionResults}
+    />
+  );
+  screen.getByText(/791 ballots cast/);
+  expect(screen.getAllByText(/0 ballots cast/)).toHaveLength(
+    election.contests.length - 1
+  );
+
+  expect(screen.queryAllByText('manual')).toHaveLength(0);
+});
+
+const manualElectionResults = buildManualResultsFixture({
+  election,
+  ballotCount: 100,
+  contestResultsSummaries: {
+    fishing: {
+      type: 'yesno',
+      ballots: 100,
+      overvotes: 0,
+      undervotes: 0,
+      yesTally: 50,
+      noTally: 50,
+    },
+  },
+});
+
+test('with scanned and manual results', () => {
+  render(
+    <AdminTallyReport
+      title="Title"
+      subtitle="Subtitle"
+      election={election}
+      contests={election.contests}
+      scannedElectionResults={scannedElectionResults}
+      manualElectionResults={manualElectionResults}
+    />
+  );
+  screen.getByText(hasTextAcrossElements('Ballots Cast791100891'));
+  expect(screen.getAllByText('manual')).toHaveLength(election.contests.length);
+});
