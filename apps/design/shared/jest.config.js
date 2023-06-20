a82b41857e4dade34b6a4160b9148843baee8a3c
Jonah Kagan <jonah@voting.works>
2023-06-20 14:35:44 -0700

    Add apps/design/shared package skeleton
    There's some code that will need to be shared between the frontend and
    backend (e.g. the logic for laying out a ballot). In the past, we've
    created packages under /libs for logic like this. Since this code is
    specific to apps/design, I thought I'd experiment with keeping it under
    the app directory structure.

---
+++ apps/design/shared/jest.config.js
@@ -0,0 +1,18 @@
+const shared = require('../../../jest.config.shared');
+
+/**
+ * @type {import('@jest/types').Config.InitialOptions}
+ */
+module.exports = {
+  ...shared,
+  coverageThreshold: {
+    /* Experimental package, don't enforce coverage yet */
+    global: {
+      statements: 0,
+      branches: 0,
+      functions: 0,
+      lines: 0,
+    },
+  },
+  testEnvironment: 'jsdom',
+};
