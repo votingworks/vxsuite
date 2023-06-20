b9a2940166f915cddfb984cedaa62c8bf9f6a548
Jonah Kagan <jonah@voting.works>
2023-06-20 15:30:57 -0700

    Clean up frontend config

--- apps/design/frontend/vite.config.ts
+++ apps/design/frontend/vite.config.ts
@@ -73,6 +73,14 @@ export default defineConfig(async (env) => {
 
     plugins: [react()],
 
+    // Configure the Vite dev server to proxy API requests to the dev backend server
+    server: {
+      proxy: {
+        '/api': 'http://localhost:3002',
+      },
+      port: 3000,
+    },
+
     // Pass some environment variables to the client in `import.meta.env`.
     envPrefix,
   };
