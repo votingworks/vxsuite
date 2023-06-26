56af406e0f0cf9b30016ecac1da04cb8590b511d
adghayes@gmail.com <adghayes@gmail.com>
2023-06-26 01:54:30 -0700

    admin/frontend - prune some random files

--- apps/admin/frontend/src/utils/pdf_to_images.ts
+++
@@ -1,6 +0,0 @@
-import { pdfToImages, setPdfRenderWorkerSrc } from '@votingworks/image-utils';
-
-// Configure browser PDF rendering to use the worker.
-setPdfRenderWorkerSrc('/pdfjs-dist/pdf.worker.js');
-
-export { pdfToImages };
