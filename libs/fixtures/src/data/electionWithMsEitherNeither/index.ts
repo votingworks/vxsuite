7b3270293cb425d431160fdbb5de9d1278b27262
adghayes@gmail.com <adghayes@gmail.com>
2023-06-26 02:05:35 -0700

    libs/fixtures - remove cvr fixture

--- libs/fixtures/src/data/electionWithMsEitherNeither/index.ts
+++ libs/fixtures/src/data/electionWithMsEitherNeither/index.ts
@@ -1,9 +1,3 @@
-import { asText as standardCvrsAsText } from './legacy-cvr-files/standard.jsonl';
-import { asText as testCvrsAsText } from './legacy-cvr-files/test.jsonl';
-
-export const legacyCvrData = standardCvrsAsText();
-export const legacyCvrTestData = testCvrsAsText();
-
 export {
   election,
   electionDefinition,
