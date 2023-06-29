9e8dc7f36c6642cc83f9a71156bfaba8fa2c2cfe
Brian Donovan <1938+eventualbuddha@users.noreply.github.com>
2023-06-29 10:05:34 -0700

    style: do not require `React` to be in scope
    Per the docs on `react/react-in-jsx-scope` [1], the rule is only needed when not using the `react-jsx` runtime, which we are using. So we disable it by extending the `jsx-runtime` config as described in the note at the bottom.
    
    [1]: https://github.com/jsx-eslint/eslint-plugin-react/blob/master/docs/rules/react-in-jsx-scope.md

--- libs/eslint-plugin-vx/src/configs/react.ts
+++ libs/eslint-plugin-vx/src/configs/react.ts
@@ -12,6 +12,7 @@ export = {
       .filter((name) => name !== 'plugin:prettier/recommended'),
     'airbnb/hooks',
     'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
     'plugin:jsx-a11y/recommended',
     'plugin:prettier/recommended', // Enables eslint-plugin-prettier and displays prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
   ],
