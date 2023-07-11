bc40f412eb367d578c630b0bd16d3dd5a593b0d9
Jonah Kagan <jonah@voting.works>
2023-07-11 10:10:38 -0700

    Basic UI components
    - A bunch of layout components that either didn't exist in libs/ui or
      needed to be tweaked. Again, since this is prototype code, I'm not
    trying to integrate it into the rest of VxSuite.
    
    - A multi-select component that is a list of labeled checkboxes.
    
    - A router-based tabs component set.

---
+++ apps/design/frontend/src/layout.tsx
@@ -0,0 +1,100 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Icons, Table as UiTable } from '@votingworks/ui';
+import styled from 'styled-components';
+import { Route } from './routes';
+
+export const Row = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
+
+export const Column = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const Form = styled.form`
+  p {
+    margin-bottom: 1.5rem;
+  }
+  button {
+    min-width: 8rem;
+  }
+`;
+
+export const Input = styled.input`
+  border: 1px solid #cccccc;
+  background: #ffffff;
+  padding: 0.35rem 0.5rem;
+  line-height: 1.25;
+  border-radius: 0.25rem;
+  width: 100%;
+  max-width: 18rem;
+`;
+
+export function FormField({
+  label,
+  children,
+}: {
+  label: string;
+  children: React.ReactNode;
+}): JSX.Element {
+  return (
+    <div style={{ marginBottom: '1.5rem' }}>
+      <label
+        style={{ display: 'block', marginBottom: '0.2rem', fontWeight: 'bold' }}
+      >
+        {label}
+      </label>
+      {children}
+    </div>
+  );
+}
+
+export const FormActionsRow = styled(Row)`
+  gap: 0.5rem;
+`;
+
+export const Table = styled(UiTable)`
+  td:last-child {
+    text-align: right;
+    padding-right: 0;
+  }
+`;
+
+export const TableActionsRow = styled(Row)`
+  margin-bottom: 1rem;
+  gap: 0.5rem;
+`;
+
+export const NestedTr = styled.tr`
+  td:first-child {
+    padding-left: 3rem;
+  }
+`;
+
+export function Breadcrumbs({ routes }: { routes: Route[] }): JSX.Element {
+  return (
+    <Row style={{ gap: '0.5rem' }}>
+      {routes.map((route, index) => {
+        if (index === routes.length - 1) {
+          return route.label;
+        }
+        return (
+          <React.Fragment key={route.path}>
+            <Link to={route.path}>{route.label}</Link>
+            {index < routes.length - 1 && <Icons.RightChevron />}
+          </React.Fragment>
+        );
+      })}
+    </Row>
+  );
+}
+
+export const Card = styled.div`
+  padding: 1rem;
+  border: ${(p) => p.theme.sizes.bordersRem.hairline}rem solid
+    ${(p) => p.theme.colors.foregroundDisabled};
+  border-radius: 0.2rem;
+`;
---
+++ apps/design/frontend/src/tabs.tsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import { Button } from '@votingworks/ui';
+import styled from 'styled-components';
+import { useHistory, useLocation } from 'react-router-dom';
+import { Row } from './layout';
+
+interface Tab {
+  label: string;
+  path: string;
+}
+
+interface TabBarProps {
+  tabs: Tab[];
+}
+
+const TabRow = styled(Row)`
+  gap: 0.5rem;
+  border-bottom: 2px solid ${({ theme }) => theme.colors.accentPrimary};
+`;
+
+const TabButton = styled(Button)`
+  min-width: 8rem;
+  text-align: left;
+  border-radius: 0.25rem 0.25rem 0 0;
+  border-bottom-width: 0;
+  span {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+`;
+
+export function TabBar({ tabs }: TabBarProps): JSX.Element {
+  const location = useLocation();
+  const history = useHistory();
+  return (
+    <TabRow>
+      {tabs.map((tab) => {
+        const isActive = location.pathname === tab.path;
+        return (
+          <TabButton
+            key={tab.path}
+            variant={isActive ? 'primary' : undefined}
+            onPress={() => history.push(tab.path)}
+          >
+            {tab.label}
+          </TabButton>
+        );
+      })}
+    </TabRow>
+  );
+}
+
+export const TabPanel = styled.section`
+  padding: 1rem 0;
+`;
