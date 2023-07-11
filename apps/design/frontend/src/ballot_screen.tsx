b6b8de9e31b22c737ad86ecc916d301a78b51d97
Jonah Kagan <jonah@voting.works>
2023-07-11 10:29:44 -0700

    BallotsScreen
    Shows the auto-generated ballot styles based on the precinct/split and
    district data. Mimics the layout of the list of precincts and splits and
    shows each, since the most meticulous users will proof each one (rather
    than proofing by ballot style). I imagine later adding the ability to
    group by ballot style on this page.
    
    Also updates the ballot viewer to match with the overall aesthetic.

---
+++ apps/design/frontend/src/ballot_screen.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { useParams } from 'react-router-dom';
+import { assertDefined } from '@votingworks/basics';
+import { getPrecinctById, getBallotStyle } from '@votingworks/types';
+import { Screen } from '@votingworks/ui';
+import { getElection } from './api';
+import { BallotViewer } from './ballot_viewer';
+
+export function BallotScreen(): JSX.Element | null {
+  const { electionId, ballotStyleId, precinctId } = useParams<{
+    electionId: string;
+    ballotStyleId: string;
+    precinctId: string;
+  }>();
+  const getElectionQuery = getElection.useQuery(electionId);
+
+  if (!getElectionQuery.isSuccess) {
+    return null; // Initial loading state
+  }
+
+  const { election } = getElectionQuery.data;
+  const precinct = getPrecinctById({ election, precinctId });
+  const ballotStyle = getBallotStyle({ election, ballotStyleId });
+
+  return (
+    <Screen>
+      <BallotViewer
+        election={election}
+        precinct={assertDefined(precinct)}
+        ballotStyle={assertDefined(ballotStyle)}
+      />
+    </Screen>
+  );
+}
