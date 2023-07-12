0bde286f8818b1a5ff251c6ce8bb9ef39e6cbf73
Kofi Ohene-Adu <kofi@voting.works>
2023-07-12 11:17:04 -0500

    [VxScan] Reset voter display settings after each session (#3706)

--- apps/scan/backend/src/types.ts
+++ apps/scan/backend/src/types.ts
@@ -20,27 +20,30 @@ export interface PageInterpretationWithAdjudication<
   contestIds?: readonly string[];
 }
 
-export type PrecinctScannerState =
-  | 'connecting'
-  | 'disconnected'
-  | 'no_paper'
-  | 'ready_to_scan'
-  | 'scanning'
-  | 'returning_to_rescan'
-  | 'ready_to_accept'
-  | 'accepting'
-  | 'accepted'
-  | 'needs_review'
-  | 'accepting_after_review'
-  | 'returning'
-  | 'returned'
-  | 'rejecting'
-  | 'rejected'
-  | 'jammed'
-  | 'both_sides_have_paper'
-  | 'recovering_from_error'
-  | 'double_sheet_jammed'
-  | 'unrecoverable_error';
+export const PRECINCT_SCANNER_STATES = [
+  'connecting',
+  'disconnected',
+  'no_paper',
+  'ready_to_scan',
+  'scanning',
+  'returning_to_rescan',
+  'ready_to_accept',
+  'accepting',
+  'accepted',
+  'needs_review',
+  'accepting_after_review',
+  'returning',
+  'returned',
+  'rejecting',
+  'rejected',
+  'jammed',
+  'both_sides_have_paper',
+  'recovering_from_error',
+  'double_sheet_jammed',
+  'unrecoverable_error',
+] as const;
+
+export type PrecinctScannerState = typeof PRECINCT_SCANNER_STATES[number];
 
 export type InvalidInterpretationReason =
   | 'invalid_test_mode'
