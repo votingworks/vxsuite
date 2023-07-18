0b9ce7f6df384864bddeb7ca24180e8569bc7133
Kofi Ohene-Adu <kofi@voting.works>
2023-07-18 12:02:07 -0500

    [VxMarkOld] Fit CastBallotPage to landscape layout (#3730)

--- libs/mark-flow-ui/src/pages/cast_ballot_page.test.tsx
+++ libs/mark-flow-ui/src/pages/cast_ballot_page.test.tsx
@@ -1,9 +1,10 @@
-import { render } from '../../test/react_testing_library';
+import { render, screen } from '../../test/react_testing_library';
 import { CastBallotPage } from './cast_ballot_page';
 
 it('renders CastBallotPage', () => {
-  const { container } = render(
-    <CastBallotPage hidePostVotingInstructions={jest.fn()} />
-  );
-  expect(container.firstChild).toMatchSnapshot();
+  render(<CastBallotPage hidePostVotingInstructions={jest.fn()} />);
+
+  screen.getByRole('heading', { name: /almost done/i });
+  screen.getByText(/verify your official ballot/i);
+  screen.getByText(/scan your official ballot/i);
 });
