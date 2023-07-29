c936696c2c7c76d05b17d666467182b5f7e60602
adghayes@gmail.com <adghayes@gmail.com>
2023-07-29 14:27:53 -0700

    admin/integration-testing: skip test files that we aren't running any tests in anyway

--- apps/admin/integration-testing/cypress.config.ts
+++ apps/admin/integration-testing/cypress.config.ts
@@ -13,6 +13,14 @@ export default defineConfig({
     setupNodeEvents(on) {
       on('task', { readMostRecentFile });
     },
+    specPattern: [
+      'cypress/e2e/**/*.cy.ts',
+      // we're skipping all tests within these files because we don't have USB
+      // mocking - by skipping the whole file we get the performance benefits
+      '!cypress/e2e/candidate_contest_tallies.cy.ts',
+      '!cypress/e2e/tallies.cy.ts',
+      '!cypress/e2e/yes_no_contest_tallies.cy.ts',
+    ],
     baseUrl: 'http://localhost:3000',
   },
 });
