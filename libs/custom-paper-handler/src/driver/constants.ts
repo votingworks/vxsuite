cb5c2a638e7199358448a14c8965a2d04d0bc774
Kevin Shen <kevin@voting.works>
2023-07-31 12:46:53 -0700

    Update libs/custom-paper-handler/src/driver/constants.ts
    Co-authored-by: Brian Donovan <1938+eventualbuddha@users.noreply.github.com>

--- libs/custom-paper-handler/src/driver/constants.ts
+++ libs/custom-paper-handler/src/driver/constants.ts
@@ -32,15 +32,83 @@ export const PRINTING_DENSITY_CODES: Record<PrintingDensity, Uint8> = {
   '+12.5%': 0x05,
   '+25%': 0x06,
 };
-// Exhaustive list of scan types whose data blocks will be 8 bpp.
+// Exhaustive list of scan types.
 // Manual reference: "Start scan ticket" 0x1C 0x53 0x50 0x53
-export enum ScanTypes8BitsPerPixel {
+export enum ScanTypes {
   NA = 0x00,
+  /**
+   * Red light scan (8bpp)
+   */
   RED = 0x01,
+  /**
+   * Green light scan (8bpp)
+   */
   GREEN = 0x02,
+  /**
+   * Blue light scan (8bpp)
+   */
   BLUE = 0x03,
+  /**
+   * Ultraviolet light scan (8bpp)
+   */
   ULTRAVIOLET = 0x04,
+  /**
+   * White light scan (8bpp)
+   */
   GRAY = 0x05,
+  /**
+   * first line RED, second line GREEN, third line BLUE (24bpp)
+   */
+  RGB_RAW = 0x06,
+  /**
+   * first line RED, second line GREEN, third line BLUE, fourth line UV (32bpp)
+   */
+  RGBU_RAW = 0x07,
+  /**
+   * Black and white with red light (1bpp)
+   */
+  BW_RED = 0x08,
+  /**
+   * Black and white with green light (1bpp)
+   */
+  BW_GREEN = 0x09,
+  /**
+   * Black and white with blue light (1bpp)
+   */
+  BW_BLUE = 0x0a,
+  /**
+   * Black and white with ultraviolet light (1bpp)
+   */
+  BW_ULTRAVIOLET = 0x0b,
+  /**
+   * Black and white with white light (1bpp)
+   */
+  BW = 0x0c,
+}
+
+export function getBitsPerPixelForScanType(scanType: ScanTypes): number {
+  switch (scanType) {
+    case ScanTypes.NA:
+      return 0;
+    case ScanTypes.RED:
+    case ScanTypes.GREEN:
+    case ScanTypes.BLUE:
+    case ScanTypes.ULTRAVIOLET:
+    case ScanTypes.GRAY:
+      return 8;
+    case ScanTypes.BW_RED:
+    case ScanTypes.BW_GREEN:
+    case ScanTypes.BW_BLUE:
+    case ScanTypes.BW_ULTRAVIOLET:
+    case ScanTypes.BW:
+      return 1;
+    case ScanTypes.RGB_RAW:
+      return 24;
+    case ScanTypes.RGBU_RAW:
+      return 32;
+    default:
+      throwIllegalValue(scanType);
+  }
 }
 export enum PrintModeDotDensity {
   SINGLE_DOT_24 = 32,
