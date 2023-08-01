0cf66527283f4ee8f6997b9cf352b4d553a693c1
adghayes@gmail.com <adghayes@gmail.com>
2023-08-01 14:28:33 -0700

    remove legacy cvr files from fixtures

--- libs/fixtures/src/data/electionMultiPartyPrimary/index.ts
+++ libs/fixtures/src/data/electionMultiPartyPrimary/index.ts
@@ -1,10 +1,6 @@
-import { asText as standardCvrAsText } from './legacy-cvr-files/standard.jsonl';
 import { asText as batchResultsCsvAsText } from './csvFiles/batchResults.csv';
 import { asText as semsDataAsText } from './semsFiles/standard.csv';
 
-export * as legacyCvrFile from './legacy-cvr-files/standard.jsonl';
-
-export const legacyCvrData = standardCvrAsText();
 export const csvData = batchResultsCsvAsText();
 export const semsData = semsDataAsText();
 
