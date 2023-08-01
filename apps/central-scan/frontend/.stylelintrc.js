38e15d47d5b99e3f09e397b58e1d8674487b77eb
adghayes@gmail.com <adghayes@gmail.com>
2023-08-01 07:51:17 -0700

    update configurations

--- apps/central-scan/frontend/.stylelintrc.js
+++ apps/central-scan/frontend/.stylelintrc.js
@@ -1,15 +1,5 @@
-// Config for stylelint parsing CSS in Styled Components
-// Note the `--fix` flag doesn't yet work for CSS-in-JS
+const shared = require('../../../stylelint.shared');
+
 module.exports = {
-  processors: ['stylelint-processor-styled-components'],
-  extends: [
-    'stylelint-config-palantir',
-    'stylelint-config-prettier',
-    'stylelint-config-styled-components',
-  ],
-  rules: {
-    'selector-max-id': 1,
-    'selector-max-universal': 1,
-    'order/properties-alphabetical-order': null,
-  },
+  ...shared
 }
