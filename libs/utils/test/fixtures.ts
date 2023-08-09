4f9dd8c584982131d71c52b65bcb478868ac1d12
adghayes@gmail.com <adghayes@gmail.com>
2023-08-09 12:19:34 -0700

    add utility for determining which splits to include

---
+++ libs/utils/test/fixtures.ts
@@ -0,0 +1,139 @@
+import { electionMinimalExhaustiveSampleDefinition } from '@votingworks/fixtures';
+import {
+  AnyContest,
+  DistrictId,
+  ElectionDefinition,
+  PartyId,
+} from '@votingworks/types';
+
+function contestFactory(
+  id: string,
+  districtId: string,
+  partyId?: string
+): AnyContest {
+  return {
+    id,
+    districtId: districtId as DistrictId,
+    partyId: partyId as PartyId,
+    title: 'Mock Contest',
+    seats: 1,
+    allowWriteIns: true,
+    type: 'candidate',
+    candidates: [],
+  };
+}
+
+// election definition with more complex precinct & ballot style structure for testing
+export const complexBallotStyleElectionDefinition: ElectionDefinition = {
+  ...electionMinimalExhaustiveSampleDefinition,
+  electionHash: 'extended-election-hash',
+  election: {
+    ...electionMinimalExhaustiveSampleDefinition.election,
+    districts: [
+      {
+        id: 'congressional-1' as DistrictId,
+        name: 'Congressional District 1',
+      },
+      {
+        id: 'congressional-2' as DistrictId,
+        name: 'Congressional District 2',
+      },
+      {
+        id: 'town' as DistrictId,
+        name: 'Town',
+      },
+      {
+        id: 'water-1' as DistrictId,
+        name: 'Water District 1',
+      },
+      {
+        id: 'water-2' as DistrictId,
+        name: 'Water District 2',
+      },
+    ],
+    contests: [
+      contestFactory('congressional-1-mammal', 'congressional-1', '0'),
+      contestFactory('congressional-1-fish', 'congressional-1', '1'),
+      contestFactory('congressional-2-mammal', 'congressional-2', '0'),
+      contestFactory('congressional-2-fish', 'congressional-2', '1'),
+      contestFactory('town-mammal', 'town', '0'),
+      contestFactory('town-fish', 'town', '1'),
+      contestFactory('water-1-mammal', 'water-1', '0'),
+      contestFactory('water-1-fish', 'water-1', '1'),
+      contestFactory('water-2-mammal', 'water-2', '0'),
+      contestFactory('water-2-fish', 'water-2', '1'),
+    ],
+    precincts: [
+      {
+        id: 'c1-w1-1',
+        name: 'Precinct 1',
+      },
+      {
+        id: 'c1-w1-2',
+        name: 'Precinct 2',
+      },
+      {
+        id: 'c1-w2-1',
+        name: 'Precinct 3',
+      },
+      {
+        id: 'c2-w1-1',
+        name: 'Precinct 4',
+      },
+      {
+        id: 'c2-w2-1',
+        name: 'Precinct 5',
+      },
+    ],
+    ballotStyles: [
+      {
+        id: 'c1-w1-mammal',
+        districts: ['town', 'congressional-1', 'water-1'] as DistrictId[],
+        partyId: '0' as PartyId,
+        precincts: ['c1-w1-1', 'c1-w1-2'],
+      },
+      {
+        id: 'c1-w2-mammal',
+        districts: ['town', 'congressional-1', 'water-2'] as DistrictId[],
+        partyId: '0' as PartyId,
+        precincts: ['c1-w2-1'],
+      },
+      {
+        id: 'c2-w1-mammal',
+        districts: ['town', 'congressional-2', 'water-1'] as DistrictId[],
+        partyId: '0' as PartyId,
+        precincts: ['c2-w1-1'],
+      },
+      {
+        id: 'c2-w2-mammal',
+        districts: ['town', 'congressional-2', 'water-2'] as DistrictId[],
+        partyId: '0' as PartyId,
+        precincts: ['c2-w2-1'],
+      },
+      {
+        id: 'c1-w1-fish',
+        districts: ['town', 'congressional-1', 'water-1'] as DistrictId[],
+        partyId: '1' as PartyId,
+        precincts: ['c1-w1-1', 'c1-w1-2'],
+      },
+      {
+        id: 'c1-w2-fish',
+        districts: ['town', 'congressional-1', 'water-2'] as DistrictId[],
+        partyId: '1' as PartyId,
+        precincts: ['c1-w2-1'],
+      },
+      {
+        id: 'c2-w1-fish',
+        districts: ['town', 'congressional-2', 'water-1'] as DistrictId[],
+        partyId: '1' as PartyId,
+        precincts: ['c2-w1-1'],
+      },
+      {
+        id: 'c2-w2-fish',
+        districts: ['town', 'congressional-2', 'water-2'] as DistrictId[],
+        partyId: '1' as PartyId,
+        precincts: ['c2-w2-1'],
+      },
+    ],
+  },
+};
