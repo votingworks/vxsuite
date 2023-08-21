953ef4c2fbbef2c7dfd699c325e473216243e1a6
Jonah Kagan <jonah@voting.works>
2023-08-21 14:21:48 -0700

    VxDesign: Temporarily disable tabulation settings screen
    It'll be easier to update this once adjudication reasons are also moved
    to system settings (my next project).

--- apps/design/frontend/src/tabulation_screen.tsx
+++ apps/design/frontend/src/tabulation_screen.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { useState } from 'react';
 import { H1, H2, Button, Icons } from '@votingworks/ui';
 import { useParams } from 'react-router-dom';
@@ -8,12 +9,12 @@ import { ElectionIdParams } from './routes';
 import { MultiSelect } from './multiselect';
 import { updateElection, getElection } from './api';
 
-type TabulationSettings = Pick<
-  Election,
-  | 'precinctScanAdjudicationReasons'
-  | 'centralScanAdjudicationReasons'
-  | 'markThresholds'
->;
+// type TabulationSettings = Pick<
+//   Election,
+//   | 'precinctScanAdjudicationReasons'
+//   | 'centralScanAdjudicationReasons'
+//   | 'markThresholds'
+// >;
 
 export function TabulationForm({
   electionId,
@@ -22,122 +23,123 @@ export function TabulationForm({
   electionId: Id;
   savedElection: Election;
 }): JSX.Element {
-  const [isEditing, setIsEditing] = useState(false);
-  const [tabulationSettings, setTabulationSettings] =
-    useState<TabulationSettings>(savedElection);
-  const updateElectionMutation = updateElection.useMutation();
+  return <div>Temporarily disabled</div>;
+  // const [isEditing, setIsEditing] = useState(false);
+  // const [tabulationSettings, setTabulationSettings] =
+  //   useState<TabulationSettings>(savedElection);
+  // const updateElectionMutation = updateElection.useMutation();
 
-  function onSaveButtonPress() {
-    updateElectionMutation.mutate(
-      { electionId, election: { ...savedElection, ...tabulationSettings } },
-      { onSuccess: () => setIsEditing(false) }
-    );
-  }
+  // function onSaveButtonPress() {
+  //   updateElectionMutation.mutate(
+  //     { electionId, election: { ...savedElection, ...tabulationSettings } },
+  //     { onSuccess: () => setIsEditing(false) }
+  //   );
+  // }
 
-  const adjudicationReasonOptions = [
-    { label: 'Overvote', value: AdjudicationReason.Overvote },
-    { label: 'Undervote', value: AdjudicationReason.Undervote },
-    { label: 'Marginal Mark', value: AdjudicationReason.MarginalMark },
-    { label: 'Blank Ballot', value: AdjudicationReason.BlankBallot },
-  ];
+  // const adjudicationReasonOptions = [
+  //   { label: 'Overvote', value: AdjudicationReason.Overvote },
+  //   { label: 'Undervote', value: AdjudicationReason.Undervote },
+  //   { label: 'Marginal Mark', value: AdjudicationReason.MarginalMark },
+  //   { label: 'Blank Ballot', value: AdjudicationReason.BlankBallot },
+  // ];
 
-  return (
-    <Form>
-      <Row style={{ gap: '1rem', marginBottom: '1rem' }}>
-        <Card style={{ minWidth: '16rem' }}>
-          <H2>Adjudication Reasons</H2>
-          <FormField label="VxScan">
-            <MultiSelect
-              options={adjudicationReasonOptions}
-              value={
-                (tabulationSettings.precinctScanAdjudicationReasons ??
-                  []) as string[]
-              }
-              onChange={(value) =>
-                setTabulationSettings({
-                  ...tabulationSettings,
-                  precinctScanAdjudicationReasons:
-                    value as AdjudicationReason[],
-                })
-              }
-              disabled={!isEditing}
-            />
-          </FormField>
-          <FormField label="VxCentralScan">
-            <MultiSelect
-              options={adjudicationReasonOptions}
-              value={
-                (tabulationSettings.centralScanAdjudicationReasons ??
-                  []) as string[]
-              }
-              onChange={(value) =>
-                setTabulationSettings({
-                  ...tabulationSettings,
-                  centralScanAdjudicationReasons: value as AdjudicationReason[],
-                })
-              }
-              disabled={!isEditing}
-            />
-          </FormField>
-        </Card>
-        <Card style={{ minWidth: '16rem' }}>
-          <H2>Mark Thresholds</H2>
-          <FormField label="Definite Mark Threshold">
-            <Input
-              type="number"
-              value={tabulationSettings.markThresholds?.definite ?? ''}
-              onChange={(e) =>
-                setTabulationSettings({
-                  ...tabulationSettings,
-                  markThresholds: {
-                    ...(tabulationSettings.markThresholds || { marginal: 0 }),
-                    definite: e.target.valueAsNumber,
-                  },
-                })
-              }
-              step={0.01}
-              min={0}
-              max={1}
-              disabled={!isEditing}
-            />
-          </FormField>
-          <FormField label="Marginal Mark Threshold">
-            <Input
-              type="number"
-              value={tabulationSettings.markThresholds?.marginal ?? ''}
-              onChange={(e) =>
-                setTabulationSettings({
-                  ...tabulationSettings,
-                  markThresholds: {
-                    ...(tabulationSettings.markThresholds || { definite: 0 }),
-                    marginal: e.target.valueAsNumber,
-                  },
-                })
-              }
-              step={0.01}
-              min={0}
-              max={1}
-              disabled={!isEditing}
-            />
-          </FormField>
-        </Card>
-      </Row>
-      {isEditing ? (
-        <FormActionsRow>
-          <Button onPress={() => setIsEditing(false)}>Cancel</Button>
-          <Button onPress={onSaveButtonPress} variant="primary">
-            <Icons.Checkmark /> Save
-          </Button>
-        </FormActionsRow>
-      ) : (
-        <FormActionsRow>
-          <Button variant="primary" onPress={() => setIsEditing(true)}>
-            <Icons.Edit /> Edit
-          </Button>
-        </FormActionsRow>
-      )}
-    </Form>
-  );
+  // return (
+  //   <Form>
+  //     <Row style={{ gap: '1rem', marginBottom: '1rem' }}>
+  //       <Card style={{ minWidth: '16rem' }}>
+  //         <H2>Adjudication Reasons</H2>
+  //         <FormField label="VxScan">
+  //           <MultiSelect
+  //             options={adjudicationReasonOptions}
+  //             value={
+  //               (tabulationSettings.precinctScanAdjudicationReasons ??
+  //                 []) as string[]
+  //             }
+  //             onChange={(value) =>
+  //               setTabulationSettings({
+  //                 ...tabulationSettings,
+  //                 precinctScanAdjudicationReasons:
+  //                   value as AdjudicationReason[],
+  //               })
+  //             }
+  //             disabled={!isEditing}
+  //           />
+  //         </FormField>
+  //         <FormField label="VxCentralScan">
+  //           <MultiSelect
+  //             options={adjudicationReasonOptions}
+  //             value={
+  //               (tabulationSettings.centralScanAdjudicationReasons ??
+  //                 []) as string[]
+  //             }
+  //             onChange={(value) =>
+  //               setTabulationSettings({
+  //                 ...tabulationSettings,
+  //                 centralScanAdjudicationReasons: value as AdjudicationReason[],
+  //               })
+  //             }
+  //             disabled={!isEditing}
+  //           />
+  //         </FormField>
+  //       </Card>
+  //       <Card style={{ minWidth: '16rem' }}>
+  //         <H2>Mark Thresholds</H2>
+  //         <FormField label="Definite Mark Threshold">
+  //           <Input
+  //             type="number"
+  //             value={tabulationSettings.markThresholds?.definite ?? ''}
+  //             onChange={(e) =>
+  //               setTabulationSettings({
+  //                 ...tabulationSettings,
+  //                 markThresholds: {
+  //                   ...(tabulationSettings.markThresholds || { marginal: 0 }),
+  //                   definite: e.target.valueAsNumber,
+  //                 },
+  //               })
+  //             }
+  //             step={0.01}
+  //             min={0}
+  //             max={1}
+  //             disabled={!isEditing}
+  //           />
+  //         </FormField>
+  //         <FormField label="Marginal Mark Threshold">
+  //           <Input
+  //             type="number"
+  //             value={tabulationSettings.markThresholds?.marginal ?? ''}
+  //             onChange={(e) =>
+  //               setTabulationSettings({
+  //                 ...tabulationSettings,
+  //                 markThresholds: {
+  //                   ...(tabulationSettings.markThresholds || { definite: 0 }),
+  //                   marginal: e.target.valueAsNumber,
+  //                 },
+  //               })
+  //             }
+  //             step={0.01}
+  //             min={0}
+  //             max={1}
+  //             disabled={!isEditing}
+  //           />
+  //         </FormField>
+  //       </Card>
+  //     </Row>
+  //     {isEditing ? (
+  //       <FormActionsRow>
+  //         <Button onPress={() => setIsEditing(false)}>Cancel</Button>
+  //         <Button onPress={onSaveButtonPress} variant="primary">
+  //           <Icons.Checkmark /> Save
+  //         </Button>
+  //       </FormActionsRow>
+  //     ) : (
+  //       <FormActionsRow>
+  //         <Button variant="primary" onPress={() => setIsEditing(true)}>
+  //           <Icons.Edit /> Edit
+  //         </Button>
+  //       </FormActionsRow>
+  //     )}
+  //   </Form>
+  // );
 }
 
 export function TabulationScreen(): JSX.Element | null {
