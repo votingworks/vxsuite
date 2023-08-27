4c19c50d9a75cef15a29d6d78b48876146f4c2a5
Arsalan Sufi <arsalan@voting.works>
2023-08-27 16:22:56 -0400

    Update libs/auth tests

--- libs/auth/src/artifact_authentication.test.ts
+++ libs/auth/src/artifact_authentication.test.ts
@@ -1,37 +1,85 @@
 import fs from 'fs';
 import path from 'path';
 import { dirSync } from 'tmp';
-import { err, ok } from '@votingworks/basics';
+import { assert, err, ok } from '@votingworks/basics';
+import { CastVoteRecordExportMetadata, CVR } from '@votingworks/types';
 
 import { getTestFilePath } from '../test/utils';
 import {
-  Artifact,
+  ArtifactToExport,
+  ArtifactToImport,
   authenticateArtifactUsingSignatureFile,
-  writeSignatureFile,
+  prepareSignatureFile,
 } from './artifact_authentication';
 import { ArtifactAuthenticationConfig } from './config';
-import * as shell from './shell';
 
 let tempDirectoryPath: string;
-let tempFile1Path: string;
-let tempFile2Path: string;
-let tempSubDirectoryPath: string;
-let tempFile3Path: string;
+let mockElectionPackage: {
+  artifactToExport: ArtifactToExport;
+  artifactToImport: ArtifactToImport;
+};
+let mockCastVoteRecords: {
+  artifactToExport: ArtifactToExport;
+  artifactToImport: ArtifactToImport;
+};
 
 beforeEach(() => {
   tempDirectoryPath = dirSync().name;
-  tempFile1Path = path.join(tempDirectoryPath, 'file-1.txt');
-  tempFile2Path = path.join(tempDirectoryPath, 'file-2.txt');
-  tempSubDirectoryPath = path.join(tempDirectoryPath, 'sub-dir');
-  tempFile3Path = path.join(tempSubDirectoryPath, 'file-3.txt');
-  fs.writeFileSync(tempFile1Path, 'abcd');
-  fs.writeFileSync(tempFile2Path, 'efgh');
-  fs.mkdirSync(tempSubDirectoryPath);
-  fs.writeFileSync(tempFile3Path, 'ijkl');
+
+  // Prepare mock election package
+  const mockElectionPackagePath = path.join(
+    tempDirectoryPath,
+    'election-package.zip'
+  );
+  fs.writeFileSync(mockElectionPackagePath, 'abcd');
+  mockElectionPackage = {
+    artifactToExport: {
+      type: 'election_package',
+      filePath: mockElectionPackagePath,
+    },
+    artifactToImport: {
+      type: 'election_package',
+      filePath: mockElectionPackagePath,
+    },
+  };
+
+  // Prepare mock cast vote records
+  const mockCastVoteRecordsPath = path.join(
+    tempDirectoryPath,
+    'cast-vote-records'
+  );
+  fs.mkdirSync(mockCastVoteRecordsPath);
+  fs.mkdirSync(path.join(mockCastVoteRecordsPath, '1'));
+  fs.writeFileSync(path.join(mockCastVoteRecordsPath, '1', 'a'), 'abcd');
+  fs.writeFileSync(path.join(mockCastVoteRecordsPath, '1', 'b'), 'efgh');
+  fs.mkdirSync(path.join(mockCastVoteRecordsPath, '2'));
+  fs.writeFileSync(path.join(mockCastVoteRecordsPath, '2', 'a'), 'ijkl');
+  fs.writeFileSync(path.join(mockCastVoteRecordsPath, '2', 'b'), 'mnop');
+  const mockCastVoteRecordExportMetadata: CastVoteRecordExportMetadata = {
+    arePollsClosed: true,
+    castVoteRecordReportMetadata: {} as unknown as CVR.CastVoteRecordReport,
+    castVoteRecordRootHash: 'abcd1234',
+  };
+  fs.writeFileSync(
+    path.join(mockCastVoteRecordsPath, 'metadata.json'),
+    JSON.stringify(mockCastVoteRecordExportMetadata)
+  );
+  mockCastVoteRecords = {
+    artifactToExport: {
+      type: 'cast_vote_records',
+      context: 'export',
+      directoryName: path.basename(mockCastVoteRecordsPath),
+      metadataFileContents: JSON.stringify(mockCastVoteRecordExportMetadata),
+    },
+    artifactToImport: {
+      type: 'cast_vote_records',
+      context: 'import',
+      directoryPath: mockCastVoteRecordsPath,
+    },
+  };
 });
 
 afterEach(() => {
-  jest.restoreAllMocks(); // Clear spies
   fs.rmSync(tempDirectoryPath, { recursive: true });
 });
 
@@ -66,7 +114,10 @@ const vxScanTestConfig: ArtifactAuthenticationConfig = {
  * being used before they're assigned to in the beforeEach block, so we use functions to defer
  * variable access.
  */
-type ArtifactGenerator = () => Artifact;
+type ArtifactGenerator = () => {
+  artifactToExport: ArtifactToExport;
+  artifactToImport: ArtifactToImport;
+};
 
 test.each<{
   description: string;
@@ -75,35 +126,36 @@ test.each<{
   importingMachineConfig: ArtifactAuthenticationConfig;
 }>([
   {
-    description: 'ballot package',
-    artifactGenerator: () => ({
-      type: 'ballot_package',
-      path: tempFile1Path,
-    }),
-    exportingMachineConfig: vxAdminTestConfig,
-    importingMachineConfig: vxScanTestConfig,
-  },
-  {
     description: 'cast vote records',
-    artifactGenerator: () => ({
-      type: 'cast_vote_records',
-      path: tempDirectoryPath,
-    }),
+    artifactGenerator: () => mockCastVoteRecords,
     exportingMachineConfig: vxScanTestConfig,
     importingMachineConfig: vxAdminTestConfig,
   },
+  {
+    description: 'election package',
+    artifactGenerator: () => mockElectionPackage,
+    exportingMachineConfig: vxAdminTestConfig,
+    importingMachineConfig: vxScanTestConfig,
+  },
 ])(
-  'Writing signature file and authenticating artifact using signature file - $description',
+  'Preparing signature file and authenticating artifact using signature file - $description',
   async ({
     artifactGenerator,
     exportingMachineConfig,
     importingMachineConfig,
   }) => {
-    const artifact = artifactGenerator();
-    await writeSignatureFile(artifact, undefined, exportingMachineConfig);
+    const { artifactToExport, artifactToImport } = artifactGenerator();
+    const signatureFile = await prepareSignatureFile(
+      artifactToExport,
+      exportingMachineConfig
+    );
+    fs.writeFileSync(
+      path.join(tempDirectoryPath, signatureFile.fileName),
+      signatureFile.fileContents
+    );
     expect(
       await authenticateArtifactUsingSignatureFile(
-        artifact,
+        artifactToImport,
         importingMachineConfig
       )
     ).toEqual(ok());
@@ -118,34 +170,35 @@ test.each<{
   tamperFn: () => void;
 }>([
   {
-    description: 'ballot package',
-    artifactGenerator: () => ({
-      type: 'ballot_package',
-      path: tempFile1Path,
-    }),
+    description: 'election package',
+    artifactGenerator: () => mockElectionPackage,
     exportingMachineConfig: vxAdminTestConfig,
     importingMachineConfig: vxScanTestConfig,
-    tamperFn: () => fs.appendFileSync(tempFile1Path, 'e'),
+    tamperFn: () => {
+      assert(mockElectionPackage.artifactToImport.type === 'election_package');
+      fs.appendFileSync(mockElectionPackage.artifactToImport.filePath, 'e');
+    },
   },
   {
-    description: 'cast vote records, file is modified',
-    artifactGenerator: () => ({
-      type: 'cast_vote_records',
-      path: tempDirectoryPath,
-    }),
+    description: 'cast vote records',
+    artifactGenerator: () => mockCastVoteRecords,
     exportingMachineConfig: vxScanTestConfig,
     importingMachineConfig: vxAdminTestConfig,
-    tamperFn: () => fs.appendFileSync(tempFile1Path, 'e'),
-  },
-  {
-    description: 'cast vote records, file is deleted',
-    artifactGenerator: () => ({
-      type: 'cast_vote_records',
-      path: tempDirectoryPath,
-    }),
-    exportingMachineConfig: vxScanTestConfig,
-    importingMachineConfig: vxAdminTestConfig,
-    tamperFn: () => fs.rmSync(tempFile2Path),
+    tamperFn: () => {
+      assert(mockCastVoteRecords.artifactToImport.type === 'cast_vote_records');
+      const metadataFilePath = path.join(
+        mockCastVoteRecords.artifactToImport.directoryPath,
+        'metadata.json'
+      );
+      const metadataFileContents = fs
+        .readFileSync(metadataFilePath)
+        .toString('utf-8');
+      const metadataFileContentsAltered = JSON.stringify({
+        ...JSON.parse(metadataFileContents),
+        castVoteRecordRootHash: 'efgh5678',
+      });
+      fs.writeFileSync(metadataFilePath, metadataFileContentsAltered);
+    },
   },
 ])(
   'Detecting that an artifact has been tampered with - $description',
@@ -155,40 +208,21 @@ test.each<{
     importingMachineConfig,
     tamperFn,
   }) => {
-    const artifact = artifactGenerator();
-    await writeSignatureFile(artifact, undefined, exportingMachineConfig);
+    const { artifactToExport, artifactToImport } = artifactGenerator();
+    const signatureFile = await prepareSignatureFile(
+      artifactToExport,
+      exportingMachineConfig
+    );
+    fs.writeFileSync(
+      path.join(tempDirectoryPath, signatureFile.fileName),
+      signatureFile.fileContents
+    );
     tamperFn();
     expect(
       await authenticateArtifactUsingSignatureFile(
-        artifact,
+        artifactToImport,
         importingMachineConfig
       )
-    ).toEqual(
-      err(
-        new Error(`Error authenticating ${artifact.path} using signature file`)
-      )
-    );
+    ).toEqual(err(expect.any(Error)));
   }
 );
-
-test('Writing signature file to a USB drive', async () => {
-  const mockUsbDriveMountPoint = path.join(tempDirectoryPath, 'usb-drive');
-  fs.mkdirSync(mockUsbDriveMountPoint);
-  jest.spyOn(shell, 'runCommand');
-
-  const config: ArtifactAuthenticationConfig = {
-    ...vxScanTestConfig,
-    isFileOnRemovableDeviceOverride: (filePath: string) =>
-      filePath.startsWith(mockUsbDriveMountPoint),
-  };
-  await writeSignatureFile(
-    { type: 'cast_vote_records', path: tempDirectoryPath },
-    mockUsbDriveMountPoint,
-    config
-  );
-  expect(shell.runCommand).toHaveBeenCalledWith([
-    'sync',
-    '-f',
-    expect.stringMatching(new RegExp(`^${mockUsbDriveMountPoint}`)),
-  ]);
-});
