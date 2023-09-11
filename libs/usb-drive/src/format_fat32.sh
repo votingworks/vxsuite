157baf72c89f1ee2f9fc98031256fbebc962ca5b
adghayes@gmail.com <adghayes@gmail.com>
2023-09-11 11:30:55 -0700

    @eventualbuddha's script security changes

--- libs/usb-drive/src/format_fat32.sh
+++ libs/usb-drive/src/format_fat32.sh
@@ -3,14 +3,18 @@
 set -euo pipefail
 
 usage () {
-    echo 'Usage: format.sh <device> <label>'
+    echo 'Usage: format_fat32.sh <device> <label>'
     exit 1
 }
 
 ROOT_DEVICE_REGEX=^/dev/sd[a-z]$
 
-DEVICE=$1
-LABEL=$2
+DEVICE=${1:-}
+LABEL=${2:-}
+
+if [[ -z $DEVICE || -z $LABEL ]]; then
+    usage
+fi
 
 if ! [[ $DEVICE =~ $ROOT_DEVICE_REGEX ]]; then
     echo "error: \"${DEVICE}\" is an invalid device. a valid device is of the form /dev/sd[a-z]" 
@@ -23,11 +27,11 @@ if [[ ${#LABEL} -gt 11 ]]; then
 fi
 
 # partition the device with a single FAT32 (type=c) partition
-echo 'type=c' | sfdisk --wipe always --wipe-partitions always $DEVICE
+echo 'type=c' | sfdisk --wipe always --wipe-partitions always "${DEVICE}"
 
 # there may be a delay before the partition can be recognized by the OS, particularly
 # when there is simultaneous polling (e.g. via lsblk). this pause allows for the delay
 sleep 0.01
 
 # format the partition with a FAT32 filesystem, which must be done as root
-sudo mkfs.fat -F 32 -n $LABEL ${DEVICE}1
+sudo mkfs.fat -F 32 -n "${LABEL}" "${DEVICE}1"
