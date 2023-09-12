c3ae0162a43490ce7d52e7625170161c1a10e7d1
Arsalan Sufi <arsalan@voting.works>
2023-09-12 10:42:31 -0400

    Add libs/backend helpers for importing CVRs
    Where these helpers focus on reading in CVR exports but leave the
    actual persisting logic to consumers

--- libs/backend/src/cast_vote_records/index.ts
+++ libs/backend/src/cast_vote_records/index.ts
@@ -2,5 +2,6 @@
 export { buildCVRContestsFromVotes } from './build_cast_vote_record';
 export { buildCastVoteRecordReportMetadata } from './build_report_metadata';
 export * from './export';
+export * from './import';
 export * from './legacy_export';
 export * from './legacy_import';
