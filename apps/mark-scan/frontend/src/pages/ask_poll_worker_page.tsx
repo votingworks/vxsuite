d1772c25687e7d038b1c4895544402a9640cabe2
Kevin Shen <kevin@voting.works>
2023-09-14 16:01:57 -0700

    add reusable ask_poll_worker_page

---
+++ apps/mark-scan/frontend/src/pages/ask_poll_worker_page.tsx
@@ -0,0 +1,71 @@
+import React from 'react';
+
+import { Screen, H1, Main, Button, Text } from '@votingworks/ui';
+
+import { InsertedSmartCardAuth } from '@votingworks/types';
+import { isCardlessVoterAuth } from '@votingworks/utils';
+
+import { ButtonFooter } from '../components/button_footer';
+
+interface Props {
+  authStatus:
+    | InsertedSmartCardAuth.CardlessVoterLoggedIn
+    | InsertedSmartCardAuth.PollWorkerLoggedIn;
+  cardlessVoterContent: {
+    body: JSX.Element;
+  };
+  pollWorkerContent: {
+    headerText: string;
+    body: JSX.Element;
+    buttonText: string;
+    onButtonPress: () => Promise<void>;
+  };
+}
+
+// Component with 2 stages
+// 1. Voter stage: tells the voter to ask a poll worker for help. Does not allow the voter to advance the state.
+// 2. Poll worker stage: after auth, gives instructions and a button to advance the state.
+export function AskPollWorkerPage(props: Props): JSX.Element | null {
+  const { authStatus, cardlessVoterContent, pollWorkerContent } = props;
+
+  let mainContents = (
+    <React.Fragment>
+      <H1>{pollWorkerContent.headerText}</H1>
+      {pollWorkerContent.body}
+    </React.Fragment>
+  );
+  let button = (
+    <Button onPress={() => pollWorkerContent.onButtonPress()}>
+      {pollWorkerContent.buttonText}
+    </Button>
+  );
+
+  if (isCardlessVoterAuth(authStatus)) {
+    mainContents = (
+      <React.Fragment>
+        <H1>
+          <span aria-label="Ask a Poll Worker for Help">
+            Ask a Poll Worker for Help
+          </span>
+        </H1>
+        {cardlessVoterContent.body}
+      </React.Fragment>
+    );
+    button = (
+      // onPress is a required prop but we want the button to no op
+      // eslint-disable-next-line @typescript-eslint/no-empty-function
+      <Button disabled onPress={() => {}}>
+        Insert a Poll Worker Card to Continue
+      </Button>
+    );
+  }
+
+  return (
+    <Screen>
+      <Main padded centerChild>
+        <Text center>{mainContents}</Text>
+      </Main>
+      <ButtonFooter>{button}</ButtonFooter>
+    </Screen>
+  );
+}
