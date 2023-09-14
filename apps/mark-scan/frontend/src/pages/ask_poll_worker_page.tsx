4623d67852c60373a8dce9e0ac1d797b8cb14c53
Kevin Shen <kevin@voting.works>
2023-09-14 16:10:06 -0700

    generalize ask_poll_worker_page

--- apps/mark-scan/frontend/src/pages/ask_poll_worker_page.tsx
+++ apps/mark-scan/frontend/src/pages/ask_poll_worker_page.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-
 import { Screen, H1, Main, Button, Text } from '@votingworks/ui';
 
 import { InsertedSmartCardAuth } from '@votingworks/types';
@@ -25,7 +24,7 @@ interface Props {
 // Component with 2 stages
 // 1. Voter stage: tells the voter to ask a poll worker for help. Does not allow the voter to advance the state.
 // 2. Poll worker stage: after auth, gives instructions and a button to advance the state.
-export function AskPollWorkerPage(props: Props): JSX.Element | null {
+export function AskPollWorkerPage(props: Props): JSX.Element {
   const { authStatus, cardlessVoterContent, pollWorkerContent } = props;
 
   let mainContents = (
--- apps/mark-scan/frontend/src/pages/ballot_invalidated_page.tsx
+++ apps/mark-scan/frontend/src/pages/ballot_invalidated_page.tsx
@@ -1,14 +1,10 @@
-/* istanbul ignore file - placeholder component that will change */
-import React, { useContext } from 'react';
-
-import { Screen, H1, Main, Button, Text } from '@votingworks/ui';
+import { useContext } from 'react';
 
 import { InsertedSmartCardAuth } from '@votingworks/types';
-import { isCardlessVoterAuth } from '@votingworks/utils';
-import { confirmInvalidateBallot } from '../api';
 
+import { confirmInvalidateBallot } from '../api';
 import { BallotContext } from '../contexts/ballot_context';
-import { ButtonFooter } from '../components/button_footer';
+import { AskPollWorkerPage } from './ask_poll_worker_page';
 
 interface Props {
   authStatus:
@@ -16,9 +12,7 @@ interface Props {
     | InsertedSmartCardAuth.PollWorkerLoggedIn;
 }
 
-export function BallotInvalidatedPage({
-  authStatus,
-}: Props): JSX.Element | null {
+export function BallotInvalidatedPage({ authStatus }: Props): JSX.Element {
   const { resetBallot, endVoterSession } = useContext(BallotContext);
 
   const confirmInvalidateBallotMutation = confirmInvalidateBallot.useMutation();
@@ -31,48 +25,29 @@ export function BallotInvalidatedPage({
     confirmInvalidateBallotMutation.mutate(undefined);
   }
 
-  let mainContents = (
-    <React.Fragment>
-      <H1>Remove Ballot</H1>
-      <p>
-        Remove the ballot and press below to start a new voter session. Remember
-        to spoil the old ballot.
-      </p>
-    </React.Fragment>
-  );
-  let button = (
-    <Button onPress={onPressContinue}>Start a New Voter Session</Button>
-  );
-  if (isCardlessVoterAuth(authStatus)) {
-    mainContents = (
-      <React.Fragment>
-        <H1>
-          <span aria-label="Ask a Poll Worker for Help">
-            Ask a Poll Worker for Help
-          </span>
-        </H1>
-        <p>
-          You have indicated your ballot needs changes. Please alert a poll
-          worker to invalidate your incorrect ballot and restart your voting
-          session.
-        </p>
-      </React.Fragment>
-    );
-    button = (
-      // onPress is a required prop but we want the button to no op
-      // eslint-disable-next-line @typescript-eslint/no-empty-function
-      <Button disabled onPress={() => {}}>
-        Insert a Poll Worker Card to Continue
-      </Button>
-    );
-  }
-
   return (
-    <Screen>
-      <Main padded centerChild>
-        <Text center>{mainContents}</Text>
-      </Main>
-      <ButtonFooter>{button}</ButtonFooter>
-    </Screen>
+    <AskPollWorkerPage
+      authStatus={authStatus}
+      cardlessVoterContent={{
+        body: (
+          <p>
+            You have indicated your ballot needs changes. Please alert a poll
+            worker to invalidate your incorrect ballot and restart your voting
+            session.
+          </p>
+        ),
+      }}
+      pollWorkerContent={{
+        headerText: 'Remove Ballot',
+        body: (
+          <p>
+            Remove the ballot and press below to start a new voter session.
+            Remember to spoil the old ballot.
+          </p>
+        ),
+        buttonText: 'Start a New Voter Session',
+        onButtonPress: onPressContinue,
+      }}
+    />
   );
 }
