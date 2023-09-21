49fc5b327ae92ce0b3bb6041cce55e917b972a53
Adam McManus <adam.mcmanus@gmail.com>
2023-09-21 10:08:12 -0500

    ordering

--- libs/monorepo-utils/src/circleci.ts
+++ libs/monorepo-utils/src/circleci.ts
@@ -178,9 +178,9 @@ commands:
       - run:
           name: Setup Dependencies
           command: |
+            pnpm install --frozen-lockfile
             pnpm --dir libs/ballot-interpreter install:rust-addon
             pnpm --dir libs/ballot-interpreter build:rust-addon
-            pnpm install --frozen-lockfile
       - save_cache:
           key:
             dotcache-cache-{{checksum ".circleci/config.yml" }}-{{ checksum
