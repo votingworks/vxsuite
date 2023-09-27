66d44cf2542189ce46a04f590258a1c47be7cc0e
Kofi Ohene-Adu <kofi@voting.works>
2023-09-27 14:34:41 -0500

    Add get/set store methods and shared app logic

--- libs/backend/src/ui_strings/index.ts
+++ libs/backend/src/ui_strings/index.ts
@@ -2,3 +2,5 @@
 export * from './ui_strings_api';
 export * from './ui_strings_api_test_runner';
 export * from './ui_strings_store';
+export * from './ui_strings_machine_configuration';
+export * from './ui_strings_machine_configuration_test_runner';
--- libs/backend/src/ui_strings/ui_strings_store.ts
+++ libs/backend/src/ui_strings/ui_strings_store.ts
@@ -1,17 +1,29 @@
 /* istanbul ignore file - tested via VxSuite apps. */
 
+import { Optional } from '@votingworks/basics';
 import { Client as DbClient } from '@votingworks/db';
 import {
   LanguageCode,
   LanguageCodeSchema,
   safeParse,
+  safeParseJson,
+  UiStringTranslations,
+  UiStringTranslationsSchema,
 } from '@votingworks/types';
 
 /** Store interface for UI String API endpoints. */
 export interface UiStringsStore {
   // TODO(kofi): Fill out.
   addLanguage(code: LanguageCode): void;
+
   getLanguages(): LanguageCode[];
+
+  getUiStrings(languageCode: LanguageCode): UiStringTranslations | null;
+
+  setUiStrings(input: {
+    languageCode: LanguageCode;
+    data: UiStringTranslations;
+  }): void;
 }
 
 /** Creates a shareable implementation of the {@link UiStringsStore}. */
@@ -31,6 +43,43 @@ export function createUiStringStore(dbClient: DbClient): UiStringsStore {
 
       return result.map((row) =>
         safeParse(LanguageCodeSchema, row.code).unsafeUnwrap()
+      );
+    },
+
+    getUiStrings(languageCode) {
+      const row = dbClient.one(
+        `
+        select
+          data
+        from ui_strings
+        where
+          language_code = ?
+      `,
+        languageCode
+      ) as Optional<{ data: string }>;
+
+      if (!row) {
+        return null;
+      }
+
+      return safeParseJson(row.data, UiStringTranslationsSchema).unsafeUnwrap();
+    },
+
+    setUiStrings(input) {
+      const { languageCode, data } = input;
+
+      this.addLanguage(languageCode);
+
+      dbClient.run(
+        `
+          insert or replace into ui_strings (
+            language_code,
+            data
+          ) values
+            (?, ?)
+        `,
+        languageCode,
+        JSON.stringify(data)
       );
     },
   };
