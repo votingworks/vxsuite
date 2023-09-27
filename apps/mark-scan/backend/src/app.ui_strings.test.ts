0e9d1e0e7af40158926d819c0b42575e84ee62c8
Kofi Ohene-Adu <kofi@voting.works>
2023-09-27 14:39:55 -0500

    [Mark/Scan/Mark-Scan] Use test runner

--- apps/mark-scan/backend/src/app.ui_strings.test.ts
+++ apps/mark-scan/backend/src/app.ui_strings.test.ts
@@ -1,26 +1,89 @@
+import os from 'os';
 import tmp from 'tmp';
 
-import { createMockUsb, runUiStringApiTests } from '@votingworks/backend';
+import {
+  MockUsb,
+  createMockUsb,
+  runUiStringApiTests,
+  runUiStringMachineConfigurationTests,
+} from '@votingworks/backend';
 import { fakeLogger } from '@votingworks/logging';
-
 import { buildMockInsertedSmartCardAuth } from '@votingworks/auth';
+import {
+  safeParseElectionDefinitionExtended,
+  testCdfBallotDefinition,
+} from '@votingworks/types';
+import {
+  BooleanEnvironmentVariableName,
+  getFeatureFlagMock,
+} from '@votingworks/utils';
+
+import {
+  fakeElectionManagerUser,
+  fakeSessionExpiresAt,
+} from '@votingworks/test-utils';
 import { Store } from './store';
 import { createWorkspace } from './util/workspace';
-import { buildApi } from './app';
+import { Api, buildApi } from './app';
+
+const mockFeatureFlagger = getFeatureFlagMock();
+
+jest.mock('@votingworks/utils', (): typeof import('@votingworks/utils') => {
+  return {
+    ...jest.requireActual('@votingworks/utils'),
+    isFeatureFlagEnabled: (flag) => mockFeatureFlagger.isEnabled(flag),
+  };
+});
 
 const store = Store.memoryStore();
 const workspace = createWorkspace(tmp.dirSync().name, { store });
+const mockAuth = buildMockInsertedSmartCardAuth();
+const electionPackage = safeParseElectionDefinitionExtended(
+  JSON.stringify(testCdfBallotDefinition)
+).unsafeUnwrap();
 
-afterAll(() => {
+afterEach(() => {
   workspace.reset();
 });
 
 runUiStringApiTests({
-  api: buildApi(
-    buildMockInsertedSmartCardAuth(),
-    createMockUsb().mock,
-    fakeLogger(),
-    workspace
-  ),
+  api: buildApi(mockAuth, createMockUsb().mock, fakeLogger(), workspace),
   store: store.getUiStringsStore(),
+});
+
+describe('configureMachine', () => {
+  let mockUsbDrive: MockUsb;
+  let api: Api;
+
+  beforeEach(() => {
+    mockFeatureFlagger.resetFeatureFlags();
+    mockFeatureFlagger.enableFeatureFlag(
+      BooleanEnvironmentVariableName.SKIP_BALLOT_PACKAGE_AUTHENTICATION
+    );
+
+    mockUsbDrive = createMockUsb();
+    api = buildApi(
+      mockAuth,
+      mockUsbDrive.mock,
+      fakeLogger(),
+      workspace,
+      undefined,
+      os.tmpdir()
+    );
+
+    mockAuth.getAuthStatus.mockImplementation(() =>
+      Promise.resolve({
+        status: 'logged_in',
+        user: fakeElectionManagerUser(electionPackage.electionDefinition),
+        sessionExpiresAt: fakeSessionExpiresAt(),
+      })
+    );
+  });
+
+  runUiStringMachineConfigurationTests({
+    electionPackage,
+    getMockUsbDrive: () => mockUsbDrive,
+    runConfigureMachine: () => api.configureBallotPackageFromUsb(),
+    store: store.getUiStringsStore(),
+  });
 });
