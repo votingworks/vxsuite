4da88b2a8bf8aac88e7e0d128c54ed51af0d8c52
Arsalan Sufi <arsalan@voting.works>
2023-09-29 15:37:41 -0400

    Remove check from helper that occasionally fails

--- apps/scan/backend/test/helpers/custom_helpers.ts
+++ apps/scan/backend/test/helpers/custom_helpers.ts
@@ -21,7 +21,7 @@ import {
 import * as grout from '@votingworks/grout';
 import { getImageChannelCount } from '@votingworks/image-utils';
 import { Logger, fakeLogger } from '@votingworks/logging';
-import { SheetInterpretation, SheetOf, mapSheet } from '@votingworks/types';
+import { SheetOf, mapSheet } from '@votingworks/types';
 import { Application } from 'express';
 import { Server } from 'http';
 import { AddressInfo } from 'net';
@@ -228,29 +228,18 @@ export async function scanBallot(
       initialBallotsCounted === 0 || (await apiClient.getConfig()).isTestMode,
   });
 
-  const interpretation: SheetInterpretation = {
-    type: 'ValidSheet',
-  };
-
   mockScanner.scan.mockResolvedValueOnce(ok(await ballotImages.completeBmd()));
   await apiClient.scanBallot();
   mockScanner.getStatus.mockResolvedValue(ok(mocks.MOCK_READY_TO_EJECT));
   await waitForStatus(apiClient, {
     state: 'ready_to_accept',
-    interpretation,
     ballotsCounted: initialBallotsCounted,
     canUnconfigure: true,
-  });
-  await apiClient.acceptBallot();
-  mockScanner.getStatus.mockResolvedValue(ok(mocks.MOCK_NO_PAPER));
-  await waitForStatus(apiClient, {
-    ballotsCounted: initialBallotsCounted + 1,
-    state: 'accepted',
-    interpretation,
-    canUnconfigure: true,
+    interpretation: { type: 'ValidSheet' },
   });
 
-  // Wait for transition back to no paper
+  await apiClient.acceptBallot();
+  mockScanner.getStatus.mockResolvedValue(ok(mocks.MOCK_NO_PAPER));
   await waitForStatus(apiClient, {
     state: 'no_paper',
     ballotsCounted: initialBallotsCounted + 1,
