fc0ae1d1db9d0188b2ee9c28163edbbd27c2f731
adghayes@gmail.com <adghayes@gmail.com>
2023-10-10 13:34:34 +0300

    remove confusing test assertion

--- apps/scan/backend/src/scanners/custom/app_config.test.ts
+++ apps/scan/backend/src/scanners/custom/app_config.test.ts
@@ -118,19 +118,8 @@ test("fails to configure if there's no ballot package on the usb drive", async (
 });
 
 test('fails to configure ballot package if logged out', async () => {
-  await withApp({}, async ({ apiClient, mockUsbDrive, mockAuth }) => {
+  await withApp({}, async ({ apiClient, mockAuth }) => {
     mockLoggedOut(mockAuth);
-    mockUsbDrive.insertUsbDrive({
-      'ballot-packages': {
-        'test-ballot-package.zip': await createBallotPackageZipArchive({
-          electionDefinition: electionGeneralDefinition,
-        }),
-      },
-    });
-    // TODO: Remove assertion once #4045, and it doesn't need to be polled to pass
-    expect(await apiClient.getUsbDriveStatus()).toMatchObject({
-      status: 'mounted',
-    });
     expect(await apiClient.configureFromBallotPackageOnUsbDrive()).toEqual(
       err('auth_required_before_ballot_package_load')
     );
