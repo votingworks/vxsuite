467ffb99dd6590151b432fb212bcc0a943e1953c
Kevin Shen <kevin@voting.works>
2023-10-11 10:19:47 -0700

    increase frontend auth polling interval

--- apps/mark-scan/frontend/src/constants.ts
+++ apps/mark-scan/frontend/src/constants.ts
@@ -1,2 +1,4 @@
-/* istanbul ignore next */
+/* istanbul ignore file */
 export const STATE_MACHINE_POLLING_INTERVAL_MS = 300;
+// Overrides shorter libs/ui polling interval
+export const AUTH_STATUS_POLLING_INTERVAL_MS_OVERRIDE = 300;
--- apps/mark-scan/frontend/test/helpers/timers.ts
+++ apps/mark-scan/frontend/test/helpers/timers.ts
@@ -1,9 +1,9 @@
 import { advanceTimers as advanceTimersBase } from '@votingworks/test-utils';
-import { AUTH_STATUS_POLLING_INTERVAL_MS } from '@votingworks/ui';
 import { waitFor } from '../react_testing_library';
+import { AUTH_STATUS_POLLING_INTERVAL_MS_OVERRIDE } from '../../src/constants';
 
 export function advanceTimers(seconds = 0): void {
-  advanceTimersBase(seconds || AUTH_STATUS_POLLING_INTERVAL_MS / 1000);
+  advanceTimersBase(seconds || AUTH_STATUS_POLLING_INTERVAL_MS_OVERRIDE / 1000);
 }
 
 export async function advanceTimersAndPromises(seconds = 0): Promise<void> {
