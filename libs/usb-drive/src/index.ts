5da0d1c98aff7e64815a12f2a5a498a8e2bc4a80
adghayes@gmail.com <adghayes@gmail.com>
2023-10-12 19:50:37 +0300

    fixup! rebase oopsie fix

--- libs/usb-drive/src/index.ts
+++ libs/usb-drive/src/index.ts
@@ -3,3 +3,4 @@ export * from './mocks/memory_usb_drive';
 export * from './mocks/file_usb_drive';
 export * from './types';
 export * from './usb_drive';
+export type { MockFileTree } from './mocks/helpers';
