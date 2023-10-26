c9633c5800b6ec9e07c4dfc39ff5998e52e278e5
adghayes@gmail.com <adghayes@gmail.com>
2023-10-26 13:23:26 -0700

    remove from full election tally page and extract component

--- apps/admin/frontend/src/screens/reporting/full_election_tally_report_screen.tsx
+++ apps/admin/frontend/src/screens/reporting/full_election_tally_report_screen.tsx
@@ -1,100 +1,33 @@
-import React, { useContext, useState } from 'react';
+import { useContext } from 'react';
 import { isElectionManagerAuth } from '@votingworks/utils';
 import { assert } from '@votingworks/basics';
-import { Button, Modal, P } from '@votingworks/ui';
+import { P } from '@votingworks/ui';
 
-import styled from 'styled-components';
 import { AppContext } from '../../contexts/app_context';
 
 import { NavigationScreen } from '../../components/navigation_screen';
 
-import { getCastVoteRecordFileMode, markResultsOfficial } from '../../api';
-import { Loading } from '../../components/loading';
 import { TallyReportViewer } from '../../components/reporting/tally_report_viewer';
 import { ReportBackButton } from '../../components/reporting/shared';
 
 const SCREEN_TITLE = 'Full Election Tally Report';
 
-const TopButtonBar = styled.div`
-  display: flex;
-  gap: 1rem;
-`;
-
 export function FullElectionTallyReportScreen(): JSX.Element {
-  const { electionDefinition, isOfficialResults, auth } =
-    useContext(AppContext);
+  const { electionDefinition, auth } = useContext(AppContext);
   assert(electionDefinition);
   assert(isElectionManagerAuth(auth));
 
-  const [isMarkOfficialModalOpen, setIsMarkOfficialModalOpen] = useState(false);
-
-  const markResultsOfficialMutation = markResultsOfficial.useMutation();
-  const castVoteRecordFileModeQuery = getCastVoteRecordFileMode.useQuery();
-
-  function closeMarkOfficialModal() {
-    setIsMarkOfficialModalOpen(false);
-  }
-  function openMarkOfficialModal() {
-    setIsMarkOfficialModalOpen(true);
-  }
-  function markOfficial() {
-    setIsMarkOfficialModalOpen(false);
-    markResultsOfficialMutation.mutate();
-  }
-
-  if (!castVoteRecordFileModeQuery.isSuccess) {
-    return (
-      <NavigationScreen title={SCREEN_TITLE}>
-        <Loading />
-      </NavigationScreen>
-    );
-  }
-
-  const canMarkResultsOfficial =
-    castVoteRecordFileModeQuery.data !== 'unlocked' && !isOfficialResults;
-
   return (
-    <React.Fragment>
-      <NavigationScreen title={SCREEN_TITLE}>
-        <TopButtonBar>
-          <ReportBackButton />{' '}
-          <Button
-            disabled={!canMarkResultsOfficial}
-            onPress={openMarkOfficialModal}
-          >
-            Mark Tally Results as Official
-          </Button>
-        </TopButtonBar>
-        <TallyReportViewer
-          filter={{}}
-          groupBy={{}}
-          disabled={false}
-          autoPreview
-        />
-      </NavigationScreen>
-      {isMarkOfficialModalOpen && (
-        <Modal
-          title="Mark Unofficial Tally Results as Official Tally Results?"
-          content={
-            <React.Fragment>
-              <P>
-                Have all CVR files been loaded? Once results are marked as
-                official, no additional CVR files can be loaded.
-              </P>
-              <P>Have all unofficial tally reports been reviewed?</P>
-            </React.Fragment>
-          }
-          actions={
-            <React.Fragment>
-              <Button variant="primary" onPress={markOfficial}>
-                Mark Tally Results as Official
-              </Button>
-              <Button onPress={closeMarkOfficialModal}>Cancel</Button>
-            </React.Fragment>
-          }
-          onOverlayClick={closeMarkOfficialModal}
-        />
-      )}
-    </React.Fragment>
+    <NavigationScreen title={SCREEN_TITLE}>
+      <P>
+        <ReportBackButton />
+      </P>
+      <TallyReportViewer
+        filter={{}}
+        groupBy={{}}
+        disabled={false}
+        autoPreview
+      />
+    </NavigationScreen>
   );
 }
