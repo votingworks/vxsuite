40bbabb21228238b1250538cd429f3330c98aa60
Jonah Kagan <jonah@voting.works>
2023-10-31 15:43:32 -0700

    Add color prop to Icons

--- libs/ui/src/icons.stories.tsx
+++ libs/ui/src/icons.stories.tsx
@@ -1,11 +1,20 @@
 import { Meta } from '@storybook/react';
 import styled from 'styled-components';
 
-import { Icons } from './icons';
+import { ICON_COLORS, IconComponent, IconProps, Icons } from './icons';
 import { H1, H5, P } from './typography';
 
-const meta: Meta = {
+const meta: Meta<IconComponent> = {
   title: 'libs-ui/Icons',
+  args: {
+    color: 'default',
+  },
+  argTypes: {
+    color: {
+      type: 'select',
+      options: [undefined, ...ICON_COLORS],
+    },
+  },
 };
 export default meta;
 
@@ -16,35 +25,29 @@ const StyledCodeBlock = styled.code`
 
 const sampleCode = `import {Icons} from "@votingworks/ui";
 
-<P color="warning">
-  <Icons.Warning /> You have been warned.
-</P>
+<Icons.Warning color="warning" /> You have been warned.
 `;
 
-function renderIcon([name, Component]: [string, () => JSX.Element]) {
-  if (typeof Component !== 'function') {
-    return null;
-  }
-
-  return (
-    <P key={name}>
-      <Component /> &mdash; {name}
-    </P>
-  );
-}
-
-export function icons(): JSX.Element {
+export function icons(props: IconProps): JSX.Element {
   return (
     <div>
       <H1>Icons</H1>
       <H5 as="h2">Usage:</H5>
       <StyledCodeBlock>{sampleCode}</StyledCodeBlock>
       <br />
-      <P color="warning">
-        <Icons.Warning /> You have been warned.
+      <P>
+        <Icons.Warning color="warning" /> You have been warned.
       </P>
       <H5 as="h2">Icon List:</H5>
-      {Object.entries(Icons).map(renderIcon)}
+      {Object.entries(Icons)
+        .filter(([, Icon]) => typeof Icon === 'function')
+        .map(([name, Icon]) => {
+          return (
+            <P key={name}>
+              <Icon {...props} /> &mdash; {name}
+            </P>
+          );
+        })}
     </div>
   );
 }
