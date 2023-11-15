5dda179b261d0edcc3a7ef9c893aa7c00414003d
Arsalan Sufi <arsalan@voting.works>
2023-11-15 14:58:32 -0500

    Update InvalidCardScreen to support jurisdiction mismatches

--- libs/ui/src/invalid_card_screen.tsx
+++ libs/ui/src/invalid_card_screen.tsx
@@ -1,47 +1,75 @@
 import { DippedSmartCardAuth, InsertedSmartCardAuth } from '@votingworks/types';
 
-import { fontSizeTheme } from './themes';
 import { Main } from './main';
 import { Prose } from './prose';
-import { Screen } from './screen';
 import { RotateCardImage } from './rotate_card_image';
+import { Screen } from './screen';
+import { fontSizeTheme } from './themes';
 import { H1, P } from './typography';
 
-type LoggedOutReason =
-  | DippedSmartCardAuth.LoggedOut['reason']
-  | InsertedSmartCardAuth.LoggedOut['reason'];
+type ReasonAndContext = Pick<
+  DippedSmartCardAuth.LoggedOut | InsertedSmartCardAuth.LoggedOut,
+  'reason' | 'cardJurisdiction' | 'cardUserRole' | 'machineJurisdiction'
+>;
 
 export interface Props {
-  reason: LoggedOutReason;
+  reasonAndContext: ReasonAndContext;
   recommendedAction?: string;
 }
 
 export function InvalidCardScreen({
-  reason,
+  reasonAndContext,
   recommendedAction: recommendedActionOverride,
 }: Props): JSX.Element {
+  const { cardJurisdiction, cardUserRole, machineJurisdiction, reason } =
+    reasonAndContext;
+
   let graphic: JSX.Element | null = null;
   let heading = 'Invalid Card';
   let errorDescription = '';
   let recommendedAction =
-    'Please insert a valid Election Manager or System Administrator card.';
+    recommendedActionOverride ?? 'Please insert a valid card.';
 
-  if (reason === 'card_error') {
-    graphic = <RotateCardImage />;
-    // The only cause we currently know of for a card error
-    heading = 'Card is Backwards';
-    recommendedAction = 'Remove the card, turn it around, and insert it again.';
-  }
-
-  if (reason === 'election_manager_wrong_election') {
-    errorDescription =
-      'The inserted Election Manager card is programmed for another election ' +
-      'and cannot be used to unlock this machine.';
-  }
-
-  if (reason === 'machine_not_configured') {
-    errorDescription =
-      'This machine is unconfigured and cannot be unlocked with this card.';
+  switch (reason) {
+    case 'card_error': {
+      graphic = <RotateCardImage />;
+      // The only cause we currently know of for a card error
+      heading = 'Card is Backwards';
+      recommendedAction =
+        'Remove the card, turn it around, and insert it again.';
+      break;
+    }
+    case 'machine_not_configured': {
+      errorDescription =
+        'This machine is unconfigured and cannot be unlocked with this card.';
+      break;
+    }
+    case 'wrong_election': {
+      const cardString = (() => {
+        switch (cardUserRole) {
+          case 'election_manager':
+            return 'Election Manager card';
+          case 'poll_worker':
+            return 'Poll Worker card';
+          /* istanbul ignore next */
+          default:
+            return 'card';
+        }
+      })();
+      errorDescription =
+        `The inserted ${cardString} is programmed for another election ` +
+        'and cannot be used to unlock this machine.';
+      break;
+    }
+    case 'wrong_jurisdiction': {
+      errorDescription =
+        `The inserted card’s jurisdiction (${cardJurisdiction}) does not match ` +
+        `this machine’s jurisdiction (${machineJurisdiction}).`;
+      break;
+    }
+    default: {
+      break;
+    }
   }
 
   return (
@@ -51,7 +79,7 @@ export function InvalidCardScreen({
         <Prose textCenter themeDeprecated={fontSizeTheme.medium}>
           <H1>{heading}</H1>
           <P>
-            {errorDescription} {recommendedActionOverride || recommendedAction}
+            {errorDescription} {recommendedAction}
           </P>
         </Prose>
       </Main>
