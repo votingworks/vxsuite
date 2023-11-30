3d4b4ee4d234e9480ff3c37a4b504333ee672676
Andrew Hayes <Adghayes@gmail.com>
2023-11-29 17:20:34 -0800

    unmount phantom drives before mounting drive (#4306)

--- libs/usb-drive/scripts/mount.sh
+++ libs/usb-drive/scripts/mount.sh
@@ -22,6 +22,17 @@ fi
 DEVICE=$1
 MOUNTPOINT=/media/vx/usb-drive
 
+# If a drive was previously removed without being ejected first, it may leave a
+# "phantom" mounted drive - a mount entry for an inaccessible file system. Although
+# "phantom" drives do not cause problems for our application code, the system's 
+# file picker will confusingly show multiple drives with the same name. Thus,
+# before mounting, we check for a (probably "phantom") mounted drive and
+# unmount it if it exists.
+if [[  $(findmnt) =~ "$MOUNTPOINT" ]]; then
+    SCRIPTS_DIRECTORY="$(dirname "${BASH_SOURCE[0]}")"
+    "${SCRIPTS_DIRECTORY}/unmount.sh" "$MOUNTPOINT"
+fi
+
 # The mount point will already exist in production but possibly not in development
 if ! [[ -e $MOUNTPOINT ]]; then
     mkdir -p $MOUNTPOINT 
