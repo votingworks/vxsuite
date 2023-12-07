730f57bc37b4dee65f30e19a18fa961aa3664ce7
Kofi Ohene-Adu <kofi@voting.works>
2023-12-07 15:07:34 -0600

    [i18n/Audio] Add react clients for UI String audio APIs (#4366)

--- libs/ui/src/hooks/ui_strings_api.test.tsx
+++ libs/ui/src/hooks/ui_strings_api.test.tsx
@@ -1,4 +1,4 @@
-import { LanguageCode } from '@votingworks/types';
+import { LanguageCode, UiStringAudioClips } from '@votingworks/types';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import React from 'react';
 import { act } from 'react-dom/test-utils';
@@ -92,4 +92,116 @@ test('getUiStrings', async () => {
   await waitFor(() => expect(result.current.isLoading).toEqual(false));
   expect(result.current.data).toEqual(null);
   expect(mockApiClient.getUiStrings).toHaveBeenLastCalledWith({ languageCode });
+});
+
+test('getAudioClip', async () => {
+  const { ENGLISH, SPANISH } = LanguageCode;
+
+  // Simulate initial machine state:
+  mockApiClient.getAudioClips.mockResolvedValue([]);
+
+  const { result } = renderHook(
+    () => ({
+      en1: api.getAudioClip.useQuery({ id: 'en1', languageCode: ENGLISH }),
+      es1: api.getAudioClip.useQuery({ id: 'es1', languageCode: SPANISH }),
+      es2: api.getAudioClip.useQuery({ id: 'es2', languageCode: SPANISH }),
+    }),
+    { wrapper: QueryWrapper }
+  );
+
+  // Expect a batch call for `ENGLISH` clips:
+  await waitFor(() => expect(result.current.en1.isSuccess).toEqual(true));
+  expect(result.current.en1.data).toBeNull();
+  expect(mockApiClient.getAudioClips).toHaveBeenCalledWith({
+    audioIds: ['en1'],
+    languageCode: ENGLISH,
+  });
+
+  // Expect another batch call for `SPANISH` clips:
+  await waitFor(() => expect(result.current.es1.isSuccess).toEqual(true));
+  expect(result.current.es1.data).toBeNull();
+  expect(result.current.es2.data).toBeNull();
+  expect(mockApiClient.getAudioClips).toHaveBeenCalledWith({
+    audioIds: ['es1', 'es2'],
+    languageCode: SPANISH,
+  });
+
+  expect(mockApiClient.getAudioClips).toHaveBeenCalledTimes(2);
+
+  // Simulate configuring an election:
+  const [clipEnglish1, clipSpanish1, clipSpanish2]: UiStringAudioClips = [
+    { dataBase64: 'ABC==', id: 'en1', languageCode: ENGLISH },
+    { dataBase64: 'DEF==', id: 'es1', languageCode: SPANISH },
+    { dataBase64: 'EDF==', id: 'es2', languageCode: SPANISH },
+  ];
+  await act(async () => {
+    mockApiClient.getAudioClips.mockImplementation((input) => {
+      if (input.languageCode === ENGLISH) {
+        return Promise.resolve([clipEnglish1]);
+      }
+
+      if (input.languageCode === SPANISH) {
+        return Promise.resolve([clipSpanish1, clipSpanish2]);
+      }
+
+      return Promise.resolve([]);
+    });
+    await api.onMachineConfigurationChange(queryClient);
+  });
+
+  await waitFor(() => expect(result.current.en1.isLoading).toEqual(false));
+  expect(result.current.en1.data).toEqual(clipEnglish1);
+
+  await waitFor(() => expect(result.current.es1.isLoading).toEqual(false));
+  expect(result.current.es1.data).toEqual(clipSpanish1);
+  expect(result.current.es2.data).toEqual(clipSpanish2);
+
+  // Simulate unconfiguring an election:
+  await act(async () => {
+    mockApiClient.getAudioClips.mockResolvedValue([]);
+    await api.onMachineConfigurationChange(queryClient);
+  });
+
+  await waitFor(() => expect(result.current.en1.isLoading).toEqual(false));
+  await waitFor(() => expect(result.current.es1.isLoading).toEqual(false));
+  expect(result.current.en1.data).toEqual(null);
+  expect(result.current.es1.data).toEqual(null);
+  expect(result.current.es2.data).toEqual(null);
+});
+
+test('getAudioIds', async () => {
+  const languageCode = LanguageCode.SPANISH;
+
+  // Simulate initial machine state:
+  mockApiClient.getUiStringAudioIds.mockResolvedValueOnce(null);
+
+  const { result } = renderHook(() => api.getAudioIds.useQuery(languageCode), {
+    wrapper: QueryWrapper,
+  });
+
+  await waitFor(() => expect(result.current.isSuccess).toEqual(true));
+  expect(result.current.data).toEqual(null);
+  expect(mockApiClient.getUiStringAudioIds).toHaveBeenCalledWith({
+    languageCode,
+  });
+
+  // Simulate configuring an election:
+  await act(async () => {
+    mockApiClient.getUiStringAudioIds.mockResolvedValueOnce({
+      foo: ['bar', 'baz'],
+    });
+    await api.onMachineConfigurationChange(queryClient);
+  });
+
+  await waitFor(() => expect(result.current.isLoading).toEqual(false));
+  expect(result.current.data).toEqual({ foo: ['bar', 'baz'] });
+
+  // Simulate unconfiguring an election:
+  await act(async () => {
+    mockApiClient.getUiStringAudioIds.mockResolvedValueOnce(null);
+    await api.onMachineConfigurationChange(queryClient);
+  });
+
+  await waitFor(() => expect(result.current.isLoading).toEqual(false));
+  expect(result.current.data).toEqual(null);
 });
--- libs/ui/src/hooks/ui_strings_api.ts
+++ libs/ui/src/hooks/ui_strings_api.ts
@@ -1,3 +1,5 @@
+import * as batcher from '@yornaath/batshit';
+
 import { QueryClient, QueryKey, useQuery } from '@tanstack/react-query';
 import { LanguageCode, UiStringsApi } from '@votingworks/types';
 import * as grout from '@votingworks/grout';
@@ -10,7 +12,56 @@ export type UiStringsApiClient = {
 };
 
 function createReactQueryApi(getApiClient: () => UiStringsApiClient) {
+  function createBatchAudioClipsClient(languageCode: LanguageCode) {
+    return batcher.create({
+      fetcher: (queries: Array<{ id: string }>) =>
+        getApiClient().getAudioClips({
+          audioIds: queries.map((q) => q.id),
+          languageCode,
+        }),
+
+      resolver: (clips, query) =>
+        clips.find((clip) => clip.id === query.id) || null,
+    });
+  }
+
+  const batchAudioClipsClients = new Map<
+    LanguageCode,
+    ReturnType<typeof createBatchAudioClipsClient>
+  >();
+
+  function getBatchAudioClipsClient(languageCode: LanguageCode) {
+    const existingBatchClient = batchAudioClipsClients.get(languageCode);
+    if (existingBatchClient) {
+      return existingBatchClient;
+    }
+
+    const newBatchClient = createBatchAudioClipsClient(languageCode);
+
+    batchAudioClipsClients.set(languageCode, newBatchClient);
+    return newBatchClient;
+  }
+
   return {
+    getAudioClip: {
+      queryKeyPrefix: 'getAudioClip',
+
+      getQueryKey(params: {
+        id: string;
+        languageCode: LanguageCode;
+      }): QueryKey {
+        return [this.queryKeyPrefix, params.languageCode, params.id];
+      },
+
+      useQuery(params: { id: string; languageCode: LanguageCode }) {
+        const batchClient = getBatchAudioClipsClient(params.languageCode);
+
+        return useQuery(this.getQueryKey(params), () =>
+          batchClient.fetch({ id: params.id })
+        );
+      },
+    },
+
     getAvailableLanguages: {
       getQueryKey(): QueryKey {
         return ['getAvailableLanguages'];
@@ -41,6 +92,22 @@ function createReactQueryApi(getApiClient: () => UiStringsApiClient) {
       },
     },
 
+    getAudioIds: {
+      queryKeyPrefix: 'getAudioIds',
+
+      getQueryKey(languageCode: LanguageCode): QueryKey {
+        return [this.queryKeyPrefix, languageCode];
+      },
+
+      useQuery(languageCode: LanguageCode) {
+        const apiClient = getApiClient();
+
+        return useQuery(this.getQueryKey(languageCode), () =>
+          apiClient.getUiStringAudioIds({ languageCode })
+        );
+      },
+    },
+
     async onMachineConfigurationChange(
       queryClient: QueryClient
     ): Promise<void> {
@@ -48,9 +115,9 @@ function createReactQueryApi(getApiClient: () => UiStringsApiClient) {
         this.getAvailableLanguages.getQueryKey()
       );
       await queryClient.invalidateQueries([this.getUiStrings.queryKeyPrefix]);
+      await queryClient.invalidateQueries([this.getAudioIds.queryKeyPrefix]);
+      await queryClient.invalidateQueries([this.getAudioClip.queryKeyPrefix]);
     },
-
-    // TODO(kofi): Fill out.
   };
 }
 
