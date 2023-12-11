97b1ea33e5fc02549fec7ef3709566da24748320
Brian Donovan <1938+eventualbuddha@users.noreply.github.com>
2023-12-11 10:18:44 -0800

    chore(mark): remove `REPORT_PRINTING_TIMEOUT_SECONDS`
    This has been unused since 99ac4481f8d8e7ce0f730db4ff06f78bb3c682a9.

--- apps/mark/frontend/src/config/globals.ts
+++ apps/mark/frontend/src/config/globals.ts
@@ -1,6 +1,5 @@
 export const BALLOT_PRINTING_TIMEOUT_SECONDS = 5;
 export const BALLOT_INSTRUCTIONS_TIMEOUT_SECONDS = 30;
-export const REPORT_PRINTING_TIMEOUT_SECONDS = 4;
 export const CHECK_ICON = '✓';
 export const WRITE_IN_CANDIDATE_MAX_LENGTH = 40;
 export const QUIT_KIOSK_IDLE_SECONDS = 5 * 60; // 5 minutes
