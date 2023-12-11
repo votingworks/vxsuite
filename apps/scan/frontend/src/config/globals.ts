c1f6900cff5bbea64b53381d7d1d13950caf093b
Brian Donovan <1938+eventualbuddha@users.noreply.github.com>
2023-12-11 10:41:12 -0800

    chore: remove various unused globals

--- apps/scan/frontend/src/config/globals.ts
+++ apps/scan/frontend/src/config/globals.ts
@@ -1,13 +1,2 @@
-export const FONT_SIZES = [18, 24, 28, 32];
-export const DEFAULT_FONT_SIZE = 1;
-export const LARGE_DISPLAY_FONT_SIZE = 3;
-export const SECURITY_PIN_LENGTH = 6;
-
 export const POLLING_INTERVAL_FOR_SCANNER_STATUS_MS = 500;
-export const POLLING_INTERVAL_FOR_USB = 2000;
-export const CARD_POLLING_INTERVAL = 200;
-export const STATUS_POLLING_EXTRA_CHECKS = 2;
-
 export const BALLOT_BAG_CAPACITY = 7000;
-
-export const TEXT_SIZE = 1;
