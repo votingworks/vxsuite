0612e3a826ac8546dfca7a8dbadd3f6003d31c38
Jonah Kagan <jonah@voting.works>
2023-12-11 11:50:20 -0800

    Add `Contest.termDescription` to CDF using `Office.Term`

--- libs/types/src/cdf/ballot-definition/convert.ts
+++ libs/types/src/cdf/ballot-definition/convert.ts
@@ -32,6 +32,10 @@ function dateTimeString(date: Date) {
   return `${isoString.split('.')[0]}Z`;
 }
 
+function officeId(contestId: Vxf.ContestId): string {
+  return `office-${contestId}`;
+}
+
 export function convertVxfElectionToCdfBallotDefinition(
   vxfElection: Vxf.Election
 ): Cdf.BallotDefinition {
@@ -217,8 +221,29 @@ export function convertVxfElectionToCdfBallotDefinition(
     );
   }
 
+  // In order to include contest term descriptions in the CDF, we need to
+  // create Office objects, since that's where Terms are defined.
+  const offices = vxfElection.contests
+    .filter(
+      (contest): contest is Vxf.CandidateContest =>
+        contest.type === 'candidate' && contest.termDescription !== undefined
+    )
+    .map(
+      (contest): Cdf.Office => ({
+        '@type': 'BallotDefinition.Office',
+        '@id': officeId(contest.id),
+        Name: text(contest.title), // Not used, but required
+        Term: {
+          '@type': 'BallotDefinition.Term',
+          Label: assertDefined(contest.termDescription),
+        },
+      })
+    );
+
   return {
     '@type': 'BallotDefinition.BallotDefinition',
+
+    Office: offices,
 
     Election: [
       {
@@ -278,6 +303,9 @@ export function convertVxfElectionToCdfBallotDefinition(
                 ],
                 PrimaryPartyIds: contest.partyId
                   ? [contest.partyId]
+                  : undefined,
+                OfficeIds: contest.termDescription
+                  ? [officeId(contest.id)]
                   : undefined,
               };
 
@@ -453,6 +481,10 @@ export function convertCdfBallotDefinitionToVxfElection(
   const election = cdfBallotDefinition.Election[0];
   const gpUnits = cdfBallotDefinition.GpUnit;
   const ballotFormat = cdfBallotDefinition.BallotFormat[0];
+  const offices =
+    cdfBallotDefinition.Office ??
+    /* istanbul ignore next */
+    [];
 
   const state = find(
     gpUnits,
@@ -582,6 +614,9 @@ export function convertCdfBallotDefinitionToVxfElection(
             partyId: contest.PrimaryPartyIds
               ? (contest.PrimaryPartyIds[0] as Vxf.PartyId)
               : undefined,
+            termDescription: offices.find(
+              (office) => office['@id'] === officeId(contest['@id'])
+            )?.Term.Label,
           };
         }
         case 'BallotDefinition.BallotMeasureContest': {
