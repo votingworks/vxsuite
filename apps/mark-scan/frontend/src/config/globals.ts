ae1663824148786443a09218af7522650ee4da3f
Brian Donovan <1938+eventualbuddha@users.noreply.github.com>
2023-12-11 10:33:07 -0800

    chore(mark): remove unused `WRITE_IN_CANDIDATE_MAX_LENGTH`
    This is unused as of ac751a6c1cbce327586c7dffdaf2f0a153efe420.

--- apps/mark-scan/frontend/src/config/globals.ts
+++ apps/mark-scan/frontend/src/config/globals.ts
@@ -1,5 +1,4 @@
 export const BALLOT_PRINTING_TIMEOUT_SECONDS = 5;
 export const BALLOT_INSTRUCTIONS_TIMEOUT_SECONDS = 30;
-export const WRITE_IN_CANDIDATE_MAX_LENGTH = 40;
 export const QUIT_KIOSK_IDLE_SECONDS = 5 * 60; // 5 minutes
 export const SECURITY_PIN_LENGTH = 6;
