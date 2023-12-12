3d8c721cedd78c66c4e51e690b2c03880c3d7fbb
vx <vx@vx.local>
2023-12-11 20:00:16 -0500

    tests

--- apps/mark-scan/accessible-controller/src/controllerd.rs
+++ apps/mark-scan/accessible-controller/src/controllerd.rs
@@ -2,24 +2,29 @@ use serialport::{self, SerialPort};
 use std::{fmt::Display, io, thread, time::Duration};
 use uinput::{event::keyboard, Device};
 
+const POLL_INTERVAL_MS: u64 = 10;
 const UINPUT_PATH: &str = "/dev/uinput";
 const DEVICE_PATH: &str = "/dev/ttyACM1";
 const DEVICE_BAUD_RATE: u32 = 9600;
 const EXPECTED_PACKET_LEGNTH: usize = 7;
 
-#[derive(Debug, thiserror::Error)]
+#[derive(Debug, PartialEq, thiserror::Error)]
 enum ParseError {
     #[error("Unexpected command data length: {0}")]
     UnexpectedPacketSize(usize),
+    #[error("Unexpected data length reported: {0}")]
+    UnexpectedDataLength(u16),
     #[error("Invalid command received: {0}")]
     InvalidCommand(u8),
     #[error("Invalid action received: {0}")]
     InvalidAction(u8),
+    #[error("Error occurred when sending keypress event")]
+    KeypressError(),
     #[error("Button value invalid: {0}")]
     Button(#[from] ButtonError),
 }
 
-#[derive(Debug, thiserror::Error)]
+#[derive(Debug, PartialEq, thiserror::Error)]
 struct ButtonError {
     invalid_code: u8,
 }
@@ -101,12 +106,18 @@ impl TryFrom<u8> for Action {
     }
 }
 
-fn send_key(device: &mut Device, key: keyboard::Key) -> () {
+fn send_key(device: &mut Device, key: keyboard::Key) -> Result<(), ParseError> {
     let keypress_result = device.click(&key);
+    if keypress_result.is_ok() {}
     match keypress_result {
-        Ok(_) => device.synchronize().unwrap(),
-        // We want the user to be able to retry a button press so we just log the error and continue
-        Err(error) => eprintln!("Error sending keypress event: {:?}", error),
+        Ok(_) => {
+            device.synchronize().unwrap();
+            return Ok(());
+        }
+        Err(error) => {
+            eprintln!("Error sending keypress event: {:?}", error);
+            return Err(ParseError::KeypressError());
+        }
     }
 }
 
@@ -118,6 +129,9 @@ fn handle_command(device: &mut Device, data: &[u8]) -> Result<(), ParseError> {
     // 1 byte key status
     // 2 bytes CRC validation value
 
+    // TODO check CRC16 value
+    // println!("Received data {:02X?}", data);
+
     if data.len() != EXPECTED_PACKET_LEGNTH {
         return Err(ParseError::UnexpectedPacketSize(data.len()));
     }
@@ -126,34 +140,36 @@ fn handle_command(device: &mut Device, data: &[u8]) -> Result<(), ParseError> {
 
     let n_data_bytes = ((data[1] as u16) << 8) | data[2] as u16;
     if n_data_bytes != 0x0002 {
-        eprintln!("Unexpected data length specified {}", n_data_bytes);
+        return Err(ParseError::UnexpectedDataLength(n_data_bytes));
     }
 
-    println!("Button value recieved: {:0x}", data[3]);
     let button: Button = data[3].try_into()?;
     let action: Action = data[4].try_into()?;
+    let key: keyboard::Key;
     match action {
         Action::Pressed => match button {
             Button::Select => {
-                send_key(device, keyboard::Key::Enter);
+                key = keyboard::Key::Enter;
             }
             Button::Left => {
-                send_key(device, keyboard::Key::Left);
+                key = keyboard::Key::Left;
             }
             Button::Right => {
-                send_key(device, keyboard::Key::Right);
+                key = keyboard::Key::Right;
             }
             Button::Up => {
-                send_key(device, keyboard::Key::Up);
+                key = keyboard::Key::Up;
             }
             Button::Down => {
-                send_key(device, keyboard::Key::Down);
+                key = keyboard::Key::Down;
             }
             Button::Help => {
-                send_key(device, keyboard::Key::Q);
+                key = keyboard::Key::Q;
             }
             _ => {
-                println!("Unhandled button {:#02x}", data[3]);
+                return Err(ParseError::Button(ButtonError {
+                    invalid_code: data[3],
+                }));
             }
         },
         Action::Released => {
@@ -161,20 +177,21 @@ fn handle_command(device: &mut Device, data: &[u8]) -> Result<(), ParseError> {
             return Ok(());
         }
     }
-    Ok(())
+
+    return send_key(device, key);
 }
 
 fn validate_connection(port: &mut Box<dyn SerialPort>) {
     let echo_command: Vec<u8> = vec![
         CommandId::Echo as u8,
-        0x00,
+        0x00, // u16 data size; fixed value of 5 bytes
         0x05,
-        0x01,
+        0x01, // 5 bytes of arbitrary data
         0x02,
         0x03,
         0x04,
         0x05,
-        0xfc,
+        0xfc, // CRC16_XModem value of data packet up to this point
         0xbd,
     ];
     match port.write(&echo_command) {
@@ -182,31 +199,31 @@ fn validate_connection(port: &mut Box<dyn SerialPort>) {
         Err(error) => eprintln!("{:?}", error),
     }
 
-    let mut serial_buf: Vec<u8> = vec![0; 1000];
+    let mut serial_buf: Vec<u8> = vec![0; 20];
 
-    match port.read(serial_buf.as_mut_slice()) {
-        Ok(size) => {
-            let echo_response = &serial_buf[..size];
-            if echo_command != echo_response {
-                panic!(
-                    "Recieved different response from echo command: {:x?}",
-                    echo_response
-                )
+    // Allow 1s for echo response
+    for _n in 1..(1000 / POLL_INTERVAL_MS) {
+        match port.read(serial_buf.as_mut_slice()) {
+            Ok(size) => {
+                let echo_response = &serial_buf[..size];
+                if echo_command != echo_response {
+                    panic!(
+                        "Recieved different response from echo command: {:x?}",
+                        echo_response
+                    )
+                }
+
+                println!("Received valid echo command response");
+                return;
             }
+            Err(ref e) if e.kind() == io::ErrorKind::TimedOut => (),
+            Err(e) => eprintln!("Error reading echo response: {:?}", e),
         }
-        Err(e) => eprintln!("Error reading echo response: {:?}", e),
     }
 }
 
-fn main() {
-    // Open the serial port
-    let port = serialport::new(DEVICE_PATH, DEVICE_BAUD_RATE)
-        .timeout(Duration::from_millis(10))
-        .open();
-
-    println!("Opened controller serial port at {DEVICE_PATH}");
-
-    let mut device = uinput::open(UINPUT_PATH)
+fn create_virtual_device() -> Device {
+    return uinput::open(UINPUT_PATH)
         .unwrap()
         .name("Accessible Controller Daemon Virtual Device")
         .unwrap()
@@ -214,15 +231,25 @@ fn main() {
         .unwrap()
         .create()
         .unwrap();
+}
+
+fn main() {
+    // Open the serial port
+    let port = serialport::new(DEVICE_PATH, DEVICE_BAUD_RATE)
+        .timeout(Duration::from_millis(POLL_INTERVAL_MS))
+        .open();
+
+    println!("Opened controller serial port at {DEVICE_PATH}");
+
+    // Create virtual device for keypress events
+    let mut device = create_virtual_device();
     println!("Created virtual device");
 
-    // Wait for device to register
+    // Wait for virtual device to register
     thread::sleep(Duration::from_secs(1));
 
     match port {
         Ok(mut port) => {
-            let mut serial_buf: Vec<u8> = vec![0; 1000];
-
             validate_connection(&mut port);
 
             println!(
@@ -230,6 +257,7 @@ fn main() {
                 &DEVICE_PATH, &DEVICE_BAUD_RATE
             );
 
+            let mut serial_buf: Vec<u8> = vec![0; 1000];
             loop {
                 match port.read(serial_buf.as_mut_slice()) {
                     Ok(size) => handle_command(&mut device, &serial_buf[..size]).unwrap(),
@@ -243,5 +271,53 @@ fn main() {
             eprintln!("Failed to open \"{}\". Error: {}", DEVICE_PATH, e);
             ::std::process::exit(1);
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_handle_command_packet_length_error() {
+        let mut device = create_virtual_device();
+        let bad_data = [0x01];
+        let result = handle_command(&mut device, &bad_data);
+        assert!(result.is_err());
+        assert_eq!(
+            result,
+            Err(ParseError::UnexpectedPacketSize(bad_data.len()))
+        );
+    }
+
+    #[test]
+    fn test_handle_command_data_length() {
+        let bad_data_length: u8 = 0x03;
+        let mut device = create_virtual_device();
+        let bad_data = [0x30, 0x00, bad_data_length, 0x00, 0x00, 0x00, 0x00];
+        let result = handle_command(&mut device, &bad_data);
+        assert!(result.is_err());
+        assert_eq!(
+            result,
+            Err(ParseError::UnexpectedDataLength(bad_data_length as u16))
+        );
+    }
+
+    #[test]
+    fn test_handle_command_success() {
+        let mut device = create_virtual_device();
+        thread::sleep(Duration::from_millis(200));
+
+        let data = [
+            0x30,
+            0x00,
+            0x02,
+            Button::Help as u8,
+            Action::Pressed as u8,
+            0x00,
+            0x00,
+        ];
+        let result = handle_command(&mut device, &data);
+        assert!(result.is_ok());
     }
 }
