3e7994279ec6fb28dd422189ad571c174b001c2b
Andrew Hayes <adghayes@gmail.com>
2023-12-13 13:35:24 -0800

    file generation and backend endpoint

---
+++ apps/admin/backend/src/app.err_cdf_export.test.ts
@@ -0,0 +1,408 @@
+import {
+  electionGridLayoutNewHampshireAmherstFixtures,
+  electionTwoPartyPrimaryFixtures,
+} from '@votingworks/fixtures';
+import {
+  BooleanEnvironmentVariableName,
+  buildManualResultsFixture,
+  getFeatureFlagMock,
+} from '@votingworks/utils';
+import { tmpNameSync } from 'tmp';
+import { readFileSync } from 'fs';
+import { LogEventId } from '@votingworks/logging';
+import {
+  CVR,
+  CandidateContest,
+  ResultsReporting,
+  Tabulation,
+  safeParse,
+  safeParseJson,
+} from '@votingworks/types';
+import { assert, assertDefined, find } from '@votingworks/basics';
+import { Client } from '@votingworks/grout';
+import { modifyCastVoteRecordExport } from '@votingworks/backend';
+import {
+  buildTestEnvironment,
+  configureMachine,
+  mockElectionManagerAuth,
+} from '../test/app';
+import { Api } from '.';
+
+jest.setTimeout(60_000);
+
+// mock SKIP_CVR_ELECTION_HASH_CHECK to allow us to use old cvr fixtures
+const featureFlagMock = getFeatureFlagMock();
+jest.mock('@votingworks/utils', () => {
+  return {
+    ...jest.requireActual('@votingworks/utils'),
+    isFeatureFlagEnabled: (flag: BooleanEnvironmentVariableName) =>
+      featureFlagMock.isEnabled(flag),
+  };
+});
+
+beforeEach(() => {
+  jest.restoreAllMocks();
+  featureFlagMock.enableFeatureFlag(
+    BooleanEnvironmentVariableName.SKIP_CVR_ELECTION_HASH_CHECK
+  );
+  featureFlagMock.enableFeatureFlag(
+    BooleanEnvironmentVariableName.SKIP_CAST_VOTE_RECORDS_AUTHENTICATION
+  );
+});
+
+afterEach(() => {
+  featureFlagMock.resetFeatureFlags();
+});
+
+it('logs success if export succeeds', async () => {
+  const { electionDefinition } = electionTwoPartyPrimaryFixtures;
+
+  const { apiClient, auth, logger } = buildTestEnvironment();
+  await configureMachine(apiClient, auth, electionDefinition);
+  mockElectionManagerAuth(auth, electionDefinition.electionHash);
+
+  const offLimitsPath = '/root/hidden';
+  const failedExportResult = await apiClient.exportCdfElectionResultsReport({
+    path: offLimitsPath,
+  });
+  expect(failedExportResult.isErr()).toEqual(true);
+  expect(logger.log).toHaveBeenLastCalledWith(
+    LogEventId.FileSaved,
+    'election_manager',
+    {
+      disposition: 'failure',
+      filename: offLimitsPath,
+      message: `Failed to save CDF election results report JSON file to ${offLimitsPath} on the USB drive.`,
+    }
+  );
+});
+
+it('logs failure if export fails', async () => {
+  const { electionDefinition } = electionTwoPartyPrimaryFixtures;
+
+  const { apiClient, auth, logger } = buildTestEnvironment();
+  await configureMachine(apiClient, auth, electionDefinition);
+  mockElectionManagerAuth(auth, electionDefinition.electionHash);
+
+  const path = tmpNameSync();
+  const exportResult = await apiClient.exportCdfElectionResultsReport({
+    path,
+  });
+  expect(exportResult.isOk()).toEqual(true);
+  expect(logger.log).toHaveBeenLastCalledWith(
+    LogEventId.FileSaved,
+    'election_manager',
+    {
+      disposition: 'success',
+      filename: path,
+      message: `Saved CDF election results report JSON file to ${path} on the USB drive.`,
+    }
+  );
+});
+
+async function getCurrentReport(
+  apiClient: Client<Api>
+): Promise<ResultsReporting.ElectionReport> {
+  const path = tmpNameSync();
+
+  const exportResult = await apiClient.exportCdfElectionResultsReport({ path });
+  exportResult.assertOk('CDF results report export failed');
+
+  const json = readFileSync(path, 'utf-8').toString();
+
+  return safeParse(
+    ResultsReporting.ElectionReportSchema,
+    safeParseJson(json).unsafeUnwrap()
+  ).unsafeUnwrap();
+}
+
+it('exports results and metadata accurately', async () => {
+  const { electionDefinition, castVoteRecordExport } =
+    electionGridLayoutNewHampshireAmherstFixtures;
+  const { election } = electionDefinition;
+
+  const { apiClient, auth } = buildTestEnvironment();
+  await configureMachine(apiClient, auth, electionDefinition);
+  mockElectionManagerAuth(auth, electionDefinition.electionHash);
+
+  // add CVR data
+  const loadFileResult = await apiClient.addCastVoteRecordFile({
+    path: castVoteRecordExport.asDirectoryPath(),
+  });
+  loadFileResult.assertOk('load file failed');
+
+  // adjudicate a write-in
+  const candidateContestId =
+    'State-Representatives-Hillsborough-District-34-b1012d38';
+  const officialCandidateId = 'Obadiah-Carrigan-5c95145a';
+  const writeInCandidate1 = await apiClient.addWriteInCandidate({
+    contestId: candidateContestId,
+    name: 'Mr. Jerry',
+  });
+  const [writeInId1, writeInId2] = await apiClient.getWriteInAdjudicationQueue({
+    contestId: candidateContestId,
+  });
+  assert(writeInId1 !== undefined);
+  assert(writeInId2 !== undefined);
+  await apiClient.adjudicateWriteIn({
+    writeInId: writeInId1,
+    type: 'write-in-candidate',
+    candidateId: writeInCandidate1.id,
+  });
+  await apiClient.adjudicateWriteIn({
+    writeInId: writeInId2,
+    type: 'official-candidate',
+    candidateId: officialCandidateId,
+  });
+
+  // add manual data
+  const writeInCandidate2 = await apiClient.addWriteInCandidate({
+    contestId: candidateContestId,
+    name: 'Mr. Kennedy',
+  });
+  await apiClient.setManualResults({
+    precinctId: election.precincts[0]!.id,
+    votingMethod: 'absentee',
+    ballotStyleId: election.ballotStyles[0]!.id,
+    manualResults: buildManualResultsFixture({
+      election,
+      ballotCount: 10,
+      contestResultsSummaries: {
+        [candidateContestId]: {
+          type: 'candidate',
+          ballots: 10,
+          overvotes: 1,
+          undervotes: 1,
+          officialOptionTallies: {
+            [officialCandidateId]: 5,
+          },
+          writeInOptionTallies: {
+            [writeInCandidate1.id]: {
+              name: writeInCandidate1.name,
+              tally: 2,
+            },
+            [writeInCandidate2.id]: {
+              name: writeInCandidate2.name,
+              tally: 1,
+            },
+          },
+        },
+      },
+    }),
+  });
+
+  const { Party, Election, GpUnit, ...reportMetadata } =
+    await getCurrentReport(apiClient);
+
+  expect(reportMetadata).toMatchObject({
+    '@type': 'ElectionResults.ElectionReport',
+    Format: 'summary-contest',
+    GeneratedDate: expect.anything(),
+    IsTest: true,
+    Issuer: 'Test Ballot',
+    IssuerAbbreviation: '00701',
+    SequenceEnd: 1,
+    SequenceStart: 1,
+    Status: 'unofficial-complete',
+    VendorApplicationId: 'VxAdmin, version dev',
+  });
+  expect(GpUnit?.map((gpUnit) => gpUnit['@id'])).toEqual([
+    'nh',
+    '00701',
+    'town-id-00701-precinct-id-',
+  ]);
+  expect(Party?.map((gpUnit) => gpUnit['@id'])).toEqual([
+    'Democratic-aea20adb',
+    'Republican-f0167ce7',
+    'OC-3a386d2b',
+  ]);
+  assert(Election);
+  const { Candidate, Contest, BallotCounts, ...electionMetadata } =
+    assertDefined(Election[0]);
+  expect(electionMetadata).toEqual({
+    '@type': 'ElectionResults.Election',
+    ElectionScopeId: 'nh',
+    EndDate: '2022-07-12T12:00:00.000-04:00',
+    Name: {
+      '@type': 'ElectionResults.InternationalizedText',
+      Text: [
+        {
+          '@type': 'ElectionResults.LanguageString',
+          Content: 'General Election',
+          Language: 'en',
+        },
+      ],
+    },
+    StartDate: '2022-07-12T12:00:00.000-04:00',
+    Type: 'general',
+  });
+  expect(BallotCounts).toHaveLength(1);
+  expect(BallotCounts![0]).toEqual({
+    '@type': 'ElectionResults.BallotCounts',
+    BallotsCast: 194, // includes manual ballot count
+    GpUnitId: '00701',
+    Type: 'total',
+  });
+  const expectedOfficialCandidateIds = election.contests
+    .filter(
+      (contest): contest is CandidateContest => contest.type === 'candidate'
+    )
+    .flatMap((contest) => contest.candidates)
+    .map((candidate) => candidate.id)
+    .sort();
+  expect(Candidate?.map((c) => c['@id']).sort()).toEqual(
+    [
+      ...expectedOfficialCandidateIds,
+      writeInCandidate1.id,
+      writeInCandidate2.id,
+      Tabulation.GENERIC_WRITE_IN_ID,
+    ].sort()
+  );
+
+  assert(Contest);
+  const ballotMeasureContestId =
+    'Shall-there-be-a-convention-to-amend-or-revise-the-constitution--15e8b5bc';
+  const ballotMeasureContest = find(
+    Contest,
+    (c) => c['@id'] === ballotMeasureContestId
+  );
+
+  function expectedCount(num: number) {
+    return expect.objectContaining({
+      '@type': 'ElectionResults.VoteCounts',
+      Count: num,
+      GpUnitId: 'town-id-00701-precinct-id-',
+      Type: 'total',
+    });
+  }
+
+  expect(ballotMeasureContest).toEqual({
+    '@id':
+      'Shall-there-be-a-convention-to-amend-or-revise-the-constitution--15e8b5bc',
+    '@type': 'ElectionResults.BallotMeasureContest',
+    ContestSelection: [
+      {
+        '@id':
+          'Shall-there-be-a-convention-to-amend-or-revise-the-constitution--15e8b5bc-option-yes',
+        '@type': 'ElectionResults.BallotMeasureSelection',
+        Selection: {
+          '@type': 'ElectionResults.InternationalizedText',
+          Text: [
+            {
+              '@type': 'ElectionResults.LanguageString',
+              Content: 'Yes',
+              Language: 'en',
+            },
+          ],
+        },
+        VoteCounts: [expectedCount(2)],
+      },
+      {
+        '@id':
+          'Shall-there-be-a-convention-to-amend-or-revise-the-constitution--15e8b5bc-option-no',
+        '@type': 'ElectionResults.BallotMeasureSelection',
+        Selection: {
+          '@type': 'ElectionResults.InternationalizedText',
+          Text: [
+            {
+              '@type': 'ElectionResults.LanguageString',
+              Content: 'No',
+              Language: 'en',
+            },
+          ],
+        },
+        VoteCounts: [expectedCount(2)],
+      },
+    ],
+    ElectionDistrictId: 'town-id-00701-precinct-id-',
+    Name: 'Constitutional Amendment Question #1',
+    OtherCounts: [
+      {
+        '@type': 'ElectionResults.OtherCounts',
+        GpUnitId: 'town-id-00701-precinct-id-',
+        Overvotes: 2,
+        Undervotes: 178,
+      },
+    ],
+  });
+
+  const candidateContest = find(
+    Contest,
+    (c) => c['@id'] === candidateContestId
+  );
+
+  expect(candidateContest).toEqual({
+    '@id': 'State-Representatives-Hillsborough-District-34-b1012d38',
+    '@type': 'ElectionResults.CandidateContest',
+    ContestSelection: expect.arrayContaining([
+      {
+        '@id': 'Obadiah-Carrigan-5c95145a',
+        '@type': 'ElectionResults.CandidateSelection',
+        VoteCounts: [expectedCount(66)],
+      },
+      {
+        '@id': 'write-in',
+        '@type': 'ElectionResults.CandidateSelection',
+        IsWriteIn: true,
+        VoteCounts: [expectedCount(54)],
+      },
+      {
+        '@id': writeInCandidate1.id,
+        '@type': 'ElectionResults.CandidateSelection',
+        IsWriteIn: true,
+        VoteCounts: [expectedCount(3)],
+      },
+      {
+        '@id': writeInCandidate2.id,
+        '@type': 'ElectionResults.CandidateSelection',
+        IsWriteIn: true,
+        VoteCounts: [expectedCount(1)],
+      },
+    ]),
+    ElectionDistrictId: 'town-id-00701-precinct-id-',
+    Name: 'State Representatives  Hillsborough District 34',
+    OtherCounts: [
+      {
+        '@type': 'ElectionResults.OtherCounts',
+        GpUnitId: 'town-id-00701-precinct-id-',
+        Overvotes: 31,
+        Undervotes: 13,
+      },
+    ],
+    VotesAllowed: 3,
+  });
+});
+
+it('marks report as certified when official, as primary when primary, and as non-test when official files loaded', async () => {
+  const { electionDefinition, castVoteRecordExport } =
+    electionTwoPartyPrimaryFixtures;
+
+  const { apiClient, auth } = buildTestEnvironment();
+  await configureMachine(apiClient, auth, electionDefinition);
+  mockElectionManagerAuth(auth, electionDefinition.electionHash);
+
+  // add CVR data, as non-test file
+  const loadFileResult = await apiClient.addCastVoteRecordFile({
+    path: await modifyCastVoteRecordExport(
+      castVoteRecordExport.asDirectoryPath(),
+      {
+        castVoteRecordReportMetadataModifier: (
+          castVoteRecordReportMetadata
+        ) => ({
+          ...castVoteRecordReportMetadata,
+          OtherReportType: undefined,
+          ReportType: [CVR.ReportType.OriginatingDeviceExport],
+        }),
+      }
+    ),
+  });
+  loadFileResult.assertOk('load file failed');
+
+  await apiClient.markResultsOfficial();
+
+  const { IsTest, Election, Status } = await getCurrentReport(apiClient);
+
+  expect(IsTest).toEqual(false);
+  expect(Election?.[0]?.Type).toEqual(ResultsReporting.ElectionType.Primary);
+  expect(Status).toEqual(ResultsReporting.ResultsStatus.Certified);
+});
---
+++ apps/admin/backend/src/util/err_cdf.ts
@@ -0,0 +1,277 @@
+import {
+  CandidateContest,
+  Election,
+  ResultsReporting,
+  Tabulation,
+  YesNoContest,
+} from '@votingworks/types';
+import { getBallotCount } from '@votingworks/utils';
+import { assert } from '@votingworks/basics';
+import { MachineConfig, WriteInCandidateRecord } from '../types';
+
+function getVendorApplicationId(machineConfig: MachineConfig): string {
+  return `VxAdmin, version ${machineConfig.codeVersion}`;
+}
+
+function getStateId(election: Election): string {
+  return election.state.toLowerCase().replaceAll(' ', '-');
+}
+
+function asInternationalizedText(
+  text: string
+): ResultsReporting.InternationalizedText {
+  return {
+    '@type': 'ElectionResults.InternationalizedText',
+    Text: [
+      {
+        '@type': 'ElectionResults.LanguageString',
+        Language: 'en',
+        Content: text,
+      },
+    ],
+  };
+}
+
+function getParties(election: Election): ResultsReporting.Party[] {
+  return election.parties.map((party) => ({
+    '@type': 'ElectionResults.Party',
+    '@id': party.id,
+    Name: asInternationalizedText(party.name),
+    Abbreviation: asInternationalizedText(party.abbrev),
+  }));
+}
+
+function getElectionType(election: Election): ResultsReporting.ElectionType {
+  return election.type === 'general'
+    ? ResultsReporting.ElectionType.General
+    : ResultsReporting.ElectionType.Primary;
+}
+
+function getOfficialCandidates(
+  election: Election
+): ResultsReporting.Candidate[] {
+  const candidates = election.contests
+    .filter(
+      (contest): contest is CandidateContest => contest.type === 'candidate'
+    )
+    .flatMap((contest) => contest.candidates);
+
+  return candidates.map((candidate) => ({
+    '@type': 'ElectionResults.Candidate',
+    '@id': candidate.id,
+    /* c8 ignore next 1 -- trivial fallthrough case */
+    PartyId: candidate.partyIds?.[0],
+    BallotName: asInternationalizedText(candidate.name),
+  }));
+}
+
+const PENDING_WRITE_IN_CANDIDATE: ResultsReporting.Candidate = {
+  '@type': 'ElectionResults.Candidate',
+  '@id': Tabulation.GENERIC_WRITE_IN_ID,
+  BallotName: asInternationalizedText(Tabulation.GENERIC_WRITE_IN_NAME),
+};
+
+function getWriteInCandidates(
+  writeInCandidates: WriteInCandidateRecord[]
+): ResultsReporting.Candidate[] {
+  return [
+    ...writeInCandidates.map((candidate) => ({
+      '@type': 'ElectionResults.Candidate' as const,
+      '@id': candidate.id,
+      BallotName: asInternationalizedText(candidate.name),
+    })),
+    PENDING_WRITE_IN_CANDIDATE,
+  ];
+}
+
+function getBallotMeasureContest(
+  contest: YesNoContest,
+  results: Tabulation.YesNoContestResults
+): ResultsReporting.BallotMeasureContest {
+  return {
+    '@type': 'ElectionResults.BallotMeasureContest',
+    '@id': contest.id,
+    Name: contest.title,
+    ElectionDistrictId: contest.districtId,
+    ContestSelection: [
+      {
+        '@type': 'ElectionResults.BallotMeasureSelection',
+        '@id': contest.yesOption.id,
+        Selection: asInternationalizedText(contest.yesOption.label),
+        VoteCounts: [
+          {
+            '@type': 'ElectionResults.VoteCounts',
+            Count: results.yesTally,
+            GpUnitId: contest.districtId,
+            Type: ResultsReporting.CountItemType.Total,
+          },
+        ],
+      },
+      {
+        '@type': 'ElectionResults.BallotMeasureSelection',
+        '@id': contest.noOption.id,
+        Selection: asInternationalizedText(contest.noOption.label),
+        VoteCounts: [
+          {
+            '@type': 'ElectionResults.VoteCounts',
+            Count: results.noTally,
+            GpUnitId: contest.districtId,
+            Type: ResultsReporting.CountItemType.Total,
+          },
+        ],
+      },
+    ],
+    OtherCounts: [
+      {
+        '@type': 'ElectionResults.OtherCounts',
+        GpUnitId: contest.districtId,
+        Overvotes: results.overvotes,
+        Undervotes: results.undervotes,
+      },
+    ],
+  };
+}
+
+function getCandidateContest(
+  contest: CandidateContest,
+  results: Tabulation.CandidateContestResults
+): ResultsReporting.CandidateContest {
+  return {
+    '@type': 'ElectionResults.CandidateContest',
+    '@id': contest.id,
+    Name: contest.title,
+    ElectionDistrictId: contest.districtId,
+    VotesAllowed: results.votesAllowed,
+    ContestSelection: Object.values(results.tallies).map((candidateTally) => ({
+      '@type': 'ElectionResults.CandidateSelection',
+      '@id': candidateTally.id,
+      IsWriteIn: candidateTally.isWriteIn,
+      VoteCounts: [
+        {
+          '@type': 'ElectionResults.VoteCounts',
+          GpUnitId: contest.districtId,
+          Count: candidateTally.tally,
+          Type: ResultsReporting.CountItemType.Total,
+        },
+      ],
+    })),
+    OtherCounts: [
+      {
+        '@type': 'ElectionResults.OtherCounts',
+        GpUnitId: contest.districtId,
+        Overvotes: results.overvotes,
+        Undervotes: results.undervotes,
+      },
+    ],
+  };
+}
+
+type ReportContest =
+  | ResultsReporting.BallotMeasureContest
+  | ResultsReporting.CandidateContest;
+
+function getContests(
+  election: Election,
+  electionResults: Tabulation.ElectionResults
+): ReportContest[] {
+  const reportContests: ReportContest[] = [];
+
+  for (const contest of election.contests) {
+    const contestResults = electionResults.contestResults[contest.id];
+    assert(contestResults);
+    if (contest.type === 'yesno') {
+      assert(contestResults.contestType === 'yesno');
+      reportContests.push(getBallotMeasureContest(contest, contestResults));
+    } else {
+      assert(contestResults.contestType === 'candidate');
+      reportContests.push(getCandidateContest(contest, contestResults));
+    }
+  }
+
+  return reportContests;
+}
+
+/**
+ *
+ */
+export function getElectionResultsReport({
+  election,
+  electionResults,
+  writeInCandidates,
+  isTestMode,
+  machineConfig,
+  isOfficialResults,
+}: {
+  election: Election;
+  electionResults: Tabulation.ElectionResults;
+  writeInCandidates: WriteInCandidateRecord[];
+  isTestMode: boolean;
+  machineConfig: MachineConfig;
+  isOfficialResults: boolean;
+}): ResultsReporting.ElectionReport {
+  const stateId = getStateId(election);
+
+  return {
+    '@type': 'ElectionResults.ElectionReport',
+    Format: ResultsReporting.ReportDetailLevel.SummaryContest,
+    IsTest: isTestMode,
+    SequenceStart: 1,
+    SequenceEnd: 1,
+    // CDF DateWithTimeZone format doesn't allow milliseconds, so remove them
+    GeneratedDate: new Date().toISOString().replace(/\.\d{3}/, ''),
+    Issuer: election.county.name,
+    IssuerAbbreviation: election.county.id,
+    VendorApplicationId: getVendorApplicationId(machineConfig),
+    Status: isOfficialResults
+      ? ResultsReporting.ResultsStatus.Certified
+      : ResultsReporting.ResultsStatus.UnofficialComplete,
+    Party: getParties(election),
+    Election: [
+      {
+        '@type': 'ElectionResults.Election',
+        StartDate: election.date,
+        EndDate: election.date,
+        Name: asInternationalizedText(election.title),
+        ElectionScopeId: stateId,
+        Type: getElectionType(election),
+        BallotCounts: [
+          {
+            '@type': 'ElectionResults.BallotCounts',
+            Type: ResultsReporting.CountItemType.Total,
+            GpUnitId: election.county.id,
+            BallotsCast: getBallotCount(electionResults.cardCounts),
+          },
+        ],
+        Candidate: [
+          ...getOfficialCandidates(election),
+          ...getWriteInCandidates(writeInCandidates),
+        ],
+        Contest: getContests(election, electionResults),
+      },
+    ],
+    GpUnit: [
+      {
+        '@type': 'ElectionResults.ReportingUnit',
+        '@id': stateId,
+        Name: asInternationalizedText(election.state),
+        Type: ResultsReporting.ReportingUnitType.State,
+        ComposingGpUnitIds: [election.county.id],
+      },
+      {
+        '@type': 'ElectionResults.ReportingUnit',
+        '@id': election.county.id,
+        Name: asInternationalizedText(election.county.name),
+        Type: ResultsReporting.ReportingUnitType.County,
+        ComposingGpUnitIds: election.districts.map((district) => district.id),
+      },
+      ...election.districts.map(
+        (district): ResultsReporting.ReportingUnit => ({
+          '@type': 'ElectionResults.ReportingUnit',
+          '@id': district.id,
+          Name: asInternationalizedText(district.name),
+          Type: ResultsReporting.ReportingUnitType.Other,
+        })
+      ),
+    ],
+  };
+}
