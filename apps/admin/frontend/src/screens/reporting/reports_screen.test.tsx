78ab6fc40b34cec8451921ca2a8ff1465222fe38
Andrew Hayes <Adghayes@gmail.com>
2023-12-14 12:22:40 -0800

    hide WIA report when no write-in contests (#4424) (#4429)

--- apps/admin/frontend/src/screens/reporting/reports_screen.test.tsx
+++ apps/admin/frontend/src/screens/reporting/reports_screen.test.tsx
@@ -1,5 +1,6 @@
 import { electionTwoPartyPrimaryDefinition } from '@votingworks/fixtures';
 import { hasTextAcrossElements } from '@votingworks/test-utils';
+import { ElectionDefinition } from '@votingworks/types';
 import { ReportsScreen } from './reports_screen';
 import { renderInAppContext } from '../../../test/render_in_app_context';
 import { ApiMock, createApiMock } from '../../../test/helpers/mock_api_client';
@@ -68,5 +69,51 @@ describe('ballot count summary text', () => {
         '3,000 test ballots have been counted for Example Primary Election.'
       )
     );
+  });
+});
+
+describe('showing WIA report link', () => {
+  const BUTTON_TEXT = 'Unofficial Write-In Adjudication Report';
+
+  test('shown when election has write-in contests', async () => {
+    apiMock.expectGetCastVoteRecordFileMode('test');
+    apiMock.expectGetCardCounts({}, [getMockCardCounts(3000)]);
+
+    renderInAppContext(<ReportsScreen />, {
+      electionDefinition,
+      apiMock,
+    });
+
+    await screen.findButton(BUTTON_TEXT);
+  });
+
+  test('not shown when election does not write-in contests', async () => {
+    apiMock.expectGetCastVoteRecordFileMode('test');
+    apiMock.expectGetCardCounts({}, [getMockCardCounts(3000)]);
+
+    const electionDefinitionWithoutWriteIns: ElectionDefinition = {
+      ...electionDefinition,
+      election: {
+        ...electionDefinition.election,
+        contests: electionDefinition.election.contests.map((contest) => {
+          if (contest.type === 'candidate') {
+            return {
+              ...contest,
+              allowWriteIns: false,
+            };
+          }
+
+          return contest;
+        }),
+      },
+    };
+
+    renderInAppContext(<ReportsScreen />, {
+      electionDefinition: electionDefinitionWithoutWriteIns,
+      apiMock,
+    });
+
+    await screen.findButton('Full Election Tally Report');
+    expect(screen.queryByText(BUTTON_TEXT)).not.toBeInTheDocument();
   });
 });
