a12c78d02e2f65d21ff6c0803d3adbe673ee11aa
Brian Donovan <1938+eventualbuddha@users.noreply.github.com>
2023-12-19 10:23:03 -0800

    feat(basics): add `lines` iterator helper
    Turns an arbitrary string or string iterable into an iterable that yields lines of text.

---
+++ libs/basics/src/iterators/lines.ts
@@ -0,0 +1,136 @@
+import { Buffer } from 'buffer';
+import { AsyncIteratorPlusImpl } from './async_iterator_plus';
+import { IteratorPlusImpl } from './iterator_plus';
+import { AsyncIteratorPlus, IteratorPlus } from './types';
+
+interface ToString {
+  toString(): string;
+}
+
+function matchAllNewlines(str: string): Iterable<RegExpMatchArray> {
+  return str.matchAll(/\r?\n/g);
+}
+
+function linesSync(iterable: Iterable<ToString>): IteratorPlus<string> {
+  let buffer = '';
+  return new IteratorPlusImpl(
+    (function* gen() {
+      let hasSeenAnyChunks = false;
+
+      for (const element of iterable) {
+        const chunk = element.toString();
+        let offset = 0;
+        for (const newlineMatch of matchAllNewlines(chunk)) {
+          /* istanbul ignore next */
+          if (typeof newlineMatch.index !== 'number') {
+            throw new Error('expected index');
+          }
+
+          const line = buffer + chunk.slice(offset, newlineMatch.index);
+          buffer = '';
+          offset = newlineMatch.index + newlineMatch[0].length;
+          yield line;
+        }
+
+        buffer += chunk.slice(offset);
+        hasSeenAnyChunks = true;
+      }
+
+      if (hasSeenAnyChunks) {
+        yield buffer;
+      }
+    })()
+  );
+}
+
+function linesAsync(
+  iterable: AsyncIterable<ToString>
+): AsyncIteratorPlus<string> {
+  let buffer = '';
+  return new AsyncIteratorPlusImpl(
+    (async function* gen() {
+      let hasSeenAnyChunks = false;
+
+      for await (const element of iterable) {
+        const chunk = element.toString();
+        let offset = 0;
+        for (const newlineMatch of matchAllNewlines(chunk)) {
+          /* istanbul ignore next */
+          if (typeof newlineMatch.index !== 'number') {
+            throw new Error('expected index');
+          }
+
+          const line = buffer + chunk.slice(offset, newlineMatch.index);
+          buffer = '';
+          offset = newlineMatch.index + newlineMatch[0].length;
+          yield line;
+        }
+
+        buffer += chunk.slice(offset);
+        hasSeenAnyChunks = true;
+      }
+
+      if (hasSeenAnyChunks) {
+        yield buffer;
+      }
+    })()
+  );
+}
+
+/**
+ * Pipes elements from `iterable` by collecting them together into a string
+ * until a full line is formed, then yielding that line. The final line may or
+ * may not include a newline.
+ *
+ * @example
+ *
+ * ```ts
+ * const file = fs.createReadStream('file.txt', { encoding: 'utf8' });
+ *
+ * expect(await iter(file).lines().toArray()).toEqual([
+ *   'line 1\n',
+ *   'line 2\n',
+ *   …
+ * ]);
+ * ```
+ */
+export function lines(
+  iterable: AsyncIterable<ToString>
+): AsyncIteratorPlus<string>;
+
+/**
+ * Pipes elements from `iterable` by collecting them together into a string
+ * until a full line is formed, then yielding that line. The final line may or
+ * may not include a newline.
+ *
+ * @example
+ *
+ * ```ts
+ * const input = ['hello', ' world', '!', '\n', 'goodbye', 'world'];
+ *
+ * expect(lines(input).toArray()).toEqual([
+ *   'hello world!\n',
+ *   'goodbye world'
+ * ]);
+ * ```
+ */
+export function lines(
+  iterable: ToString | Iterable<ToString>
+): IteratorPlus<string>;
+
+/**
+ * Pipes elements from `iterable` by collecting them together into a string
+ * until a full line is formed, then yielding that line. The final line may or
+ * may not include a newline.
+ */
+export function lines(
+  iterable: ToString | Iterable<ToString> | AsyncIterable<ToString>
+): IteratorPlus<string> | AsyncIteratorPlus<string> {
+  return typeof iterable === 'string' || Buffer.isBuffer(iterable)
+    ? linesSync([iterable.toString()])
+    : Symbol.iterator in iterable
+    ? linesSync(iterable)
+    : Symbol.asyncIterator in iterable
+    ? linesAsync(iterable)
+    : linesSync([iterable.toString()]);
+}
