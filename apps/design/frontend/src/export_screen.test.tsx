b343832e0a31d824d53b489ecb60ae0f6d826a43
Arsalan Sufi <arsalan@voting.works>
2024-01-03 15:21:58 -0500

    Update frontend tests

--- apps/design/frontend/src/export_screen.test.tsx
+++ apps/design/frontend/src/export_screen.test.tsx
@@ -1,5 +1,7 @@
 import { Buffer } from 'buffer';
 import fileDownload from 'js-file-download';
+import userEvent from '@testing-library/user-event';
+import { mockOf } from '@votingworks/test-utils';
 import {
   provideApi,
   createMockApiClient,
@@ -10,14 +12,21 @@ import { render, screen, waitFor } from '../test/react_testing_library';
 import { withRoute } from '../test/routing_helpers';
 import { ExportScreen } from './export_screen';
 import { routes } from './routes';
+import { downloadFile } from './utils';
 
 jest.mock('js-file-download');
 const fileDownloadMock = jest.mocked(fileDownload);
+
+jest.mock('./utils', (): typeof import('./utils') => ({
+  ...jest.requireActual('./utils'),
+  downloadFile: jest.fn(),
+}));
 
 let apiMock: MockApiClient;
 
 beforeEach(() => {
   apiMock = createMockApiClient();
+  apiMock.getElectionPackage.expectCallWith({ electionId }).resolves({});
 });
 
 afterEach(() => {
@@ -36,25 +45,6 @@ function renderScreen() {
   );
 }
 
-test('export election package', async () => {
-  renderScreen();
-  await screen.findByRole('heading', { name: 'Export' });
-
-  apiMock.exportElectionPackage.expectCallWith({ electionId }).resolves({
-    zipContents: Buffer.from('fake-zip-contents'),
-    electionHash: '1234567890abcdef',
-  });
-
-  screen.getButton('Export Election Package').click();
-
-  await waitFor(() => {
-    expect(fileDownloadMock).toHaveBeenCalledWith(
-      Buffer.from('fake-zip-contents'),
-      'election-package-1234567890.zip'
-    );
-  });
-});
-
 test('export all ballots', async () => {
   renderScreen();
   await screen.findByRole('heading', { name: 'Export' });
@@ -64,7 +54,7 @@ test('export all ballots', async () => {
     electionHash: '1234567890abcdef',
   });
 
-  screen.getButton('Export All Ballots').click();
+  userEvent.click(screen.getButton('Export All Ballots'));
 
   await waitFor(() => {
     expect(fileDownloadMock).toHaveBeenCalledWith(
@@ -83,7 +73,7 @@ test('export test decks', async () => {
     electionHash: '1234567890abcdef',
   });
 
-  screen.getButton('Export Test Decks').click();
+  userEvent.click(screen.getButton('Export Test Decks'));
 
   await waitFor(() => {
     expect(fileDownloadMock).toHaveBeenCalledWith(
@@ -91,4 +81,91 @@ test('export test decks', async () => {
       'test-decks-1234567890.zip'
     );
   });
+});
+
+test('export election package', async () => {
+  renderScreen();
+  await screen.findByRole('heading', { name: 'Export' });
+
+  const taskCreatedAt = new Date();
+  apiMock.exportElectionPackage.expectCallWith({ electionId }).resolves();
+  apiMock.getElectionPackage.expectRepeatedCallsWith({ electionId }).resolves({
+    task: {
+      createdAt: taskCreatedAt,
+      id: '1',
+      payload: JSON.stringify({ electionId }),
+      taskName: 'generate_election_package',
+    },
+  });
+  userEvent.click(screen.getButton('Export Election Package'));
+
+  await screen.findByText('Exporting Election Package...');
+  expect(screen.queryByText('Export Election Package')).not.toBeInTheDocument();
+
+  apiMock.getElectionPackage.expectCallWith({ electionId }).resolves({
+    task: {
+      completedAt: new Date(taskCreatedAt.getTime() + 2000),
+      createdAt: taskCreatedAt,
+      id: '1',
+      payload: JSON.stringify({ electionId }),
+      startedAt: new Date(taskCreatedAt.getTime() + 1000),
+      taskName: 'generate_election_package',
+    },
+    url: 'http://localhost:1234/election-package-1234567890.zip',
+  });
+
+  await screen.findByText('Export Election Package', undefined, {
+    timeout: 2000,
+  });
+  expect(
+    screen.queryByText('Exporting Election Package...')
+  ).not.toBeInTheDocument();
+
+  await waitFor(() => {
+    expect(mockOf(downloadFile)).toHaveBeenCalledWith(
+      'http://localhost:1234/election-package-1234567890.zip'
+    );
+  });
+});
+
+test('export election package error handling', async () => {
+  renderScreen();
+  await screen.findByRole('heading', { name: 'Export' });
+
+  const taskCreatedAt = new Date();
+  apiMock.exportElectionPackage.expectCallWith({ electionId }).resolves();
+  apiMock.getElectionPackage.expectRepeatedCallsWith({ electionId }).resolves({
+    task: {
+      createdAt: taskCreatedAt,
+      id: '1',
+      payload: JSON.stringify({ electionId }),
+      taskName: 'generate_election_package',
+    },
+  });
+  userEvent.click(screen.getButton('Export Election Package'));
+
+  await screen.findByText('Exporting Election Package...');
+  expect(screen.queryByText('Export Election Package')).not.toBeInTheDocument();
+
+  apiMock.getElectionPackage.expectCallWith({ electionId }).resolves({
+    task: {
+      completedAt: new Date(taskCreatedAt.getTime() + 2000),
+      createdAt: taskCreatedAt,
+      error: 'Whoa!',
+      id: '1',
+      payload: JSON.stringify({ electionId }),
+      startedAt: new Date(taskCreatedAt.getTime() + 1000),
+      taskName: 'generate_election_package',
+    },
+  });
+
+  await screen.findByText('Export Election Package', undefined, {
+    timeout: 2000,
+  });
+  expect(
+    screen.queryByText('Exporting Election Package...')
+  ).not.toBeInTheDocument();
+
+  await screen.findByText('An unexpected error occurred. Please try again.');
+  expect(mockOf(downloadFile)).not.toHaveBeenCalled();
 });
