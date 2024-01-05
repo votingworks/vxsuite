29e81559a5b1c6f261a233b6d26bf5ab746b3a13
Arsalan Sufi <arsalan@voting.works>
2024-01-05 15:38:25 -0500

    Translate app strings and add to election packages

--- apps/design/backend/src/worker/generate_election_package.ts
+++ apps/design/backend/src/worker/generate_election_package.ts
@@ -1,26 +1,76 @@
 import { createWriteStream } from 'fs';
+import fs from 'fs/promises';
 import JsZip from 'jszip';
 import path from 'path';
 import { pipeline } from 'stream/promises';
+import { z } from 'zod';
+import { assertDefined } from '@votingworks/basics';
 import { layOutAllBallotStyles } from '@votingworks/hmpb-layout';
 import {
   BallotType,
   ElectionPackageFileName,
   getDisplayElectionHash,
   Id,
+  LanguageCode,
+  safeParseJson,
+  UiStringsPackage,
 } from '@votingworks/types';
 
 import { PORT } from '../globals';
+import { GoogleCloudTranslator } from '../language_and_audio/translator';
 import { WorkerContext } from './context';
 
+async function translateAppStrings(
+  translator: GoogleCloudTranslator
+): Promise<UiStringsPackage> {
+  const appStringsCatalogFileContents = await fs.readFile(
+    path.join(
+      __dirname,
+      // TODO: Account for system version
+      '../../../../../libs/ui/src/ui_strings/app_strings_catalog/latest.json'
+    ),
+    'utf-8'
+  );
+  const appStringsCatalog = safeParseJson(
+    appStringsCatalogFileContents,
+    z.record(z.string())
+  ).unsafeUnwrap();
+
+  const appStringKeys = Object.keys(appStringsCatalog).sort();
+  const appStringsInEnglish = appStringKeys.map(
+    (key) => appStringsCatalog[key]
+  );
+
+  const appStrings: UiStringsPackage = {};
+  for (const languageCode of Object.values(LanguageCode)) {
+    appStrings[languageCode] = {};
+    const appStringsInLanguage =
+      languageCode === LanguageCode.ENGLISH
+        ? appStringsInEnglish
+        : await translator.translateText(appStringsInEnglish, languageCode);
+    for (const [i, key] of appStringKeys.entries()) {
+      assertDefined(appStrings[languageCode])[key] = appStringsInLanguage[i];
+    }
+  }
+  return appStrings;
+}
+
 export async function generateElectionPackage(
-  { workspace }: WorkerContext,
+  { translator, workspace }: WorkerContext,
   { electionId }: { electionId: Id }
 ): Promise<void> {
   const { assetDirectoryPath, store } = workspace;
 
   const { election, layoutOptions, systemSettings } =
     store.getElection(electionId);
+
+  const zip = new JsZip();
+
+  const appStrings = await translateAppStrings(translator);
+  zip.file(
+    ElectionPackageFileName.APP_STRINGS,
+    JSON.stringify(appStrings, null, 2)
+  );
 
   const { electionDefinition } = layOutAllBallotStyles({
     election,
@@ -30,8 +80,8 @@ export async function generateElectionPackage(
     ballotMode: 'test',
     layoutOptions,
   }).unsafeUnwrap();
-  const zip = new JsZip();
   zip.file(ElectionPackageFileName.ELECTION, electionDefinition.electionData);
+
   zip.file(
     ElectionPackageFileName.SYSTEM_SETTINGS,
     JSON.stringify(systemSettings, null, 2)
