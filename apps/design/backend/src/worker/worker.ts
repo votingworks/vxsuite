9e60591ce2d9214a72c07321545b7f7ac1e7e38e
Arsalan Sufi <arsalan@voting.works>
2024-01-16 14:49:48 -0500

    Requeue interrupted background tasks on VxDesign worker startup

--- apps/design/backend/src/worker/worker.ts
+++ apps/design/backend/src/worker/worker.ts
@@ -34,7 +34,14 @@ export async function processNextBackgroundTaskIfAny(
   /* eslint-enable no-console */
 }
 
+/**
+ * Starts the VxDesign background worker. Note that, as currently implemented, it's only safe to
+ * run one instance of the worker.
+ */
 export function start(context: WorkerContext): void {
+  // Requeue any tasks that were previously interrupted, say, because the worker process crashed
+  context.workspace.store.requeueInterruptedBackgroundTasks();
+
   process.nextTick(async () => {
     // eslint-disable-next-line no-constant-condition
     while (true) {
