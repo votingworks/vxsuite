f6d33b9fc86f0a527bde2c0812713a2351318d48
Andrew Hayes <adghayes@gmail.com>
2024-01-22 14:16:11 -0800

    add device attach and unattach logging to backend

---
+++ libs/backend/src/detect_devices.test.ts
@@ -0,0 +1,40 @@
+import { LogEventId, fakeLogger } from '@votingworks/logging';
+import { usb } from 'usb';
+import { detectDevices } from './detect_devices';
+
+test('detectDevices', () => {
+  const logger = fakeLogger();
+
+  detectDevices({ logger });
+
+  const device = {
+    deviceDescriptor: {
+      idVendor: 1,
+      idProduct: 2,
+    },
+  } as unknown as usb.Device;
+
+  usb.emit('attach', device);
+  expect(logger.log).toHaveBeenCalledTimes(1);
+  expect(logger.log).toHaveBeenLastCalledWith(
+    LogEventId.DeviceAttached,
+    'system',
+    {
+      message: 'Device attached. Vendor ID: 1, Product ID: 2',
+      productId: 2,
+      vendorId: 1,
+    }
+  );
+
+  usb.emit('detach', device);
+  expect(logger.log).toHaveBeenCalledTimes(2);
+  expect(logger.log).toHaveBeenLastCalledWith(
+    LogEventId.DeviceUnattached,
+    'system',
+    {
+      message: 'Device unattached. Vendor ID: 1, Product ID: 2',
+      productId: 2,
+      vendorId: 1,
+    }
+  );
+});
