ffc2a43f7c1b601877beacbcf2f224afed71e581
Kofi Ohene-Adu <kofi@voting.works>
2024-01-30 13:20:25 -0600

    Update catalog build/lint scripts and test CI failure

---
+++ libs/ui/scripts/finalize_app_strings_catalog.ts
@@ -0,0 +1,39 @@
+import fs from 'fs';
+
+import {
+  UiStringTranslations,
+  UiStringTranslationsSchema,
+  safeParseJson,
+} from '@votingworks/types';
+import { generateNumberStringsCatalog } from '../src/ui_strings/number_strings';
+
+const APP_STRINGS_CATALOG_FILE_PATH =
+  './src/ui_strings/app_strings_catalog/latest.json';
+
+function addNumberStrings(
+  baseAppStringsCatalog: UiStringTranslations
+): UiStringTranslations {
+  return {
+    ...baseAppStringsCatalog,
+    ...generateNumberStringsCatalog(),
+  };
+}
+
+export function main(): void {
+  let appStringsCatalog = safeParseJson(
+    fs.readFileSync(APP_STRINGS_CATALOG_FILE_PATH).toString('utf8'),
+    UiStringTranslationsSchema
+  ).unsafeUnwrap();
+
+  appStringsCatalog = addNumberStrings(appStringsCatalog);
+
+  const sortedAppStringsCatalog: UiStringTranslations = {};
+  for (const appStringKey of Object.keys(appStringsCatalog).sort()) {
+    sortedAppStringsCatalog[appStringKey] = appStringsCatalog[appStringKey];
+  }
+
+  fs.writeFileSync(
+    APP_STRINGS_CATALOG_FILE_PATH,
+    `${JSON.stringify(sortedAppStringsCatalog, undefined, 2)}\n`
+  );
+}
