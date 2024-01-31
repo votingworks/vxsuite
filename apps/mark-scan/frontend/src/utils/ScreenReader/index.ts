486470445fbe0f8b369daeb59ab4847d7a83c4d4
Kofi Ohene-Adu <kofi@voting.works>
2024-01-31 14:41:12 -0600

    Remove dead code from old screen reader

--- apps/mark-scan/frontend/src/utils/ScreenReader/index.ts
+++
@@ -1,8 +0,0 @@
-/* istanbul ignore file */
-import { SpeechSynthesisTextToSpeech } from './speech_synthesis_text_to_speech';
-import { KioskTextToSpeech } from './kiosk_text_to_speech';
-import { AriaScreenReader } from './aria_screen_reader';
-import type { ScreenReader } from '../../config/types';
-
-export type { ScreenReader };
-export { AriaScreenReader, SpeechSynthesisTextToSpeech, KioskTextToSpeech };
