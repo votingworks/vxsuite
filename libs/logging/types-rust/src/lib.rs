062311cb448424399005fc9452e53a151cf23d4b
Kevin Shen <kevin@voting.works>
2024-02-01 14:47:13 -0800

    add Rust logging crate

---
+++ libs/logging/types-rust/src/lib.rs
@@ -0,0 +1,19 @@
+use std::{io::stdout, sync::OnceLock};
+pub(crate) static APP_NAME: OnceLock<String> = OnceLock::new();
+
+pub fn print_log(log: Log) {
+    match serde_json::to_writer(stdout(), &log) {
+        Ok(_) => println!(),
+        Err(e) => eprintln!("Error serializing log: {e}"),
+    }
+}
+
+pub fn set_app_name(app_name: String) {
+    APP_NAME.set(app_name).unwrap();
+}
+
+mod log_event_enums;
+pub use self::log_event_enums::EventId;
+
+pub mod types;
+pub use self::types::{Disposition, EventType, Log, User};
