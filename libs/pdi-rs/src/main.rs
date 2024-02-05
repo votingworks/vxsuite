b7dadee5315705f7714635c88b5738010badb25a
Arsalan Sufi <arsalan@voting.works>
2024-02-05 15:37:29 -0500

    WIP

--- libs/pdi-rs/src/main.rs
+++ libs/pdi-rs/src/main.rs
@@ -1,5 +1,7 @@
 use clap::Parser;
+use serde_json::Result;
 use std::{
+    io::{self, Write},
     process::exit,
     time::{Duration, Instant},
 };
@@ -43,53 +45,195 @@ fn setup_logging(config: &Config) -> color_eyre::Result<()> {
     Ok(())
 }
 
+#[derive(Debug, serde::Deserialize)]
+#[serde(tag = "commandType")]
+enum Command {
+    #[serde(rename = "exit")]
+    Exit,
+
+    #[serde(rename = "connect")]
+    Connect,
+
+    #[serde(rename = "enable_scanning")]
+    EnableScanning,
+}
+
+#[derive(Debug, serde::Serialize)]
+#[serde(tag = "outgoingType")]
+enum Outgoing {
+    #[serde(rename = "ok")]
+    Ok,
+
+    #[serde(rename = "error")]
+    Err { message: String },
+
+    #[serde(rename = "scan_complete")]
+    ScanComplete { image_data: String },
+}
+
+fn wrap_outgoing(outgoing: &Outgoing) -> color_eyre::Result<()> {
+    serde_json::to_writer(io::stdout(), outgoing)?;
+    let mut stdout = io::stdout().lock();
+    stdout.write_all(b"\n")?;
+    Ok(())
+}
+
 fn main_scan_loop() -> color_eyre::Result<()> {
     let config = Config::parse();
     setup(&config)?;
 
-    let Ok(mut client) = PdiClient::open() else {
-        tracing::error!("failed to open device");
-        exit(-1);
-    };
+    // let Ok(mut client) = PdiClient::open() else {
+    //     tracing::error!("failed to open device");
+    //     exit(-1);
+    // };
 
-    println!("send_connect result: {:?}", client.send_connect());
-    println!(
-        "send_enable_scan_commands result: {:?}",
-        client.send_enable_scan_commands()
-    );
+    // println!("send_connect result: {:?}", client.send_connect());
+    // println!(
+    //     "send_enable_scan_commands result: {:?}",
+    //     client.send_enable_scan_commands()
+    // );
+
+    let (stdin_tx, stdin_rx) = std::sync::mpsc::channel();
+
+    std::thread::spawn(move || {
+        loop {
+            let mut buffer = String::new();
+            match io::stdin().read_line(&mut buffer) {
+                Ok(_) => {
+                    let command = serde_json::from_str::<Command>(&buffer).unwrap();
+                    stdin_tx.send(command).unwrap();
+                    // println!("received message: {command:?}");
+                }
+
+                Err(e) => {
+                    tracing::error!("failed to read from stdin: {e:?}");
+                }
+            }
+        }
+    });
+
+    let mut client: Option<PdiClient> = None;
 
     loop {
-        println!("waiting for begin scan…");
-        loop {
-            match client.await_event(Instant::now() + Duration::from_millis(10)) {
-                Err(pdiscan::client::Error::RecvTimeout(_)) => {}
-                Err(e) => return Err(e.into()),
-                Ok(_) => {}
-            }
+        match stdin_rx.try_recv() {
+            Ok(command) => {
+                // println!("received message: {command:?}");
 
-            if client.begin_scan_rx.try_recv().is_ok() {
-                break;
+                match (&mut client, command) {
+                    (_, Command::Exit) => {
+                        serde_json::to_writer(io::stdout(), &Outgoing::Ok)?;
+                        exit(0)
+                    }
+                    (Some(_), Command::Connect) => {
+                        wrap_outgoing(&Outgoing::Err {
+                            message: "already connected".to_string(),
+                        })?;
+                    }
+                    (None, Command::Connect) => {
+                        client = match PdiClient::open() {
+                            Ok(mut client) => {
+                                match client.send_connect() {
+                                    Ok(_) => {
+                                        wrap_outgoing(&Outgoing::Ok)?;
+                                    }
+                                    Err(e) => {
+                                        eprintln!("send_connect() error: {e:?}");
+                                        wrap_outgoing(&Outgoing::Err {
+                                            message: e.to_string(),
+                                        })?;
+                                    }
+                                }
+                                Some(client)
+                            }
+                            Err(e) => {
+                                eprintln!("open() error: {e:?}");
+                                wrap_outgoing(&Outgoing::Err {
+                                    message: e.to_string(),
+                                })?;
+                                None
+                            }
+                        };
+                    }
+                    (Some(client), Command::EnableScanning) => {
+                        match client.send_enable_scan_commands() {
+                            Ok(_) => {
+                                wrap_outgoing(&Outgoing::Ok)?;
+                            }
+                            Err(e) => {
+                                wrap_outgoing(&Outgoing::Err {
+                                    message: e.to_string(),
+                                })?;
+                            }
+                        }
+                    }
+                    (None, Command::EnableScanning) => {
+                        wrap_outgoing(&Outgoing::Err {
+                            message: "scanner not connected".to_string(),
+                        })?;
+                    }
+                }
+            }
+            Err(std::sync::mpsc::TryRecvError::Empty) => {}
+            Err(std::sync::mpsc::TryRecvError::Disconnected) => {
+                tracing::error!("stdin channel disconnected");
+                exit(-1);
             }
         }
 
-        println!("waiting for end scan…");
-        loop {
+        if let Some(client) = &mut client {
             match client.await_event(Instant::now() + Duration::from_millis(10)) {
                 Err(pdiscan::client::Error::RecvTimeout(_)) => {}
-                Err(e) => return Err(e.into()),
+                Err(e) => {
+                    tracing::error!("error: {e:?}");
+                    exit(-1);
+                }
                 Ok(_) => {}
             }
 
-            if client.end_scan_rx.try_recv().is_ok() {
-                break;
+            if let Ok(_) = client.begin_scan_rx.try_recv() {
+                // println!("begin scan");
+            }
+
+            if let Ok(_) = client.end_scan_rx.try_recv() {
+                wrap_outgoing(&Outgoing::ScanComplete {
+                    image_data: "".to_string(),
+                })?;
             }
         }
-
-        // std::thread::sleep(std::time::Duration::from_millis(10));
-        println!("accepting document…");
-        client.eject_document(pdiscan::protocol::types::EjectMotion::ToRear)?;
-        // client.get_test_string(std::time::Duration::from_millis(200))?;
     }
+
+    // loop {
+    //     println!("waiting for begin scan…");
+    //     loop {
+    //         match client.await_event(Instant::now() + Duration::from_millis(10)) {
+    //             Err(pdiscan::client::Error::RecvTimeout(_)) => {}
+    //             Err(e) => return Err(e.into()),
+    //             Ok(_) => {}
+    //         }
+
+    //         if client.begin_scan_rx.try_recv().is_ok() {
+    //             break;
+    //         }
+    //     }
+
+    //     println!("waiting for end scan…");
+    //     loop {
+    //         match client.await_event(Instant::now() + Duration::from_millis(10)) {
+    //             Err(pdiscan::client::Error::RecvTimeout(_)) => {}
+    //             Err(e) => return Err(e.into()),
+    //             Ok(_) => {}
+    //         }
+
+    //         if client.end_scan_rx.try_recv().is_ok() {
+    //             break;
+    //         }
+    //     }
+
+    //     // std::thread::sleep(std::time::Duration::from_millis(10));
+    //     println!("accepting document…");
+    //     client.eject_document(pdiscan::protocol::types::EjectMotion::ToRear)?;
+    //     // client.get_test_string(std::time::Duration::from_millis(200))?;
+    // }
 }
 
 pub fn main() -> color_eyre::Result<()> {
