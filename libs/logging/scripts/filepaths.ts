815943e0457bfbc095ad1da84fb233c6d9ae823b
Kevin Shen <kevin@voting.works>
2024-02-06 13:29:13 -0800

    use dirname

--- libs/logging/scripts/filepaths.ts
+++ libs/logging/scripts/filepaths.ts
@@ -1,10 +1,8 @@
-export const configDir = './config';
+export const configDir = `${__dirname}/../config`;
 export const configFilepath = `${configDir}/log_event_details.toml`;
 
-export const logEventIdsTemplateFilepath =
-  './scripts/log_event_ids.ts.template';
-export const logEventIdsOutputFilepath = './src/log_event_ids.ts';
+export const logEventIdsTemplateFilepath = `${__dirname}/log_event_ids.ts.template`;
+export const logEventIdsOutputFilepath = `${__dirname}/../src/log_event_ids.ts`;
 
-export const rustEnumsTemplateFilepath =
-  './scripts/log_event_enums.rs.template';
-export const rustEnumsOutputFilepath = './types-rust/src/log_event_enums.rs';
+export const rustEnumsTemplateFilepath = `${__dirname}/log_event_enums.rs.template`;
+export const rustEnumsOutputFilepath = `${__dirname}/../types-rust/src/log_event_enums.rs`;
