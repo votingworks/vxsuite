00690d879a69ba069c6157d77b78b9f29167648e
Brian Donovan <1938+eventualbuddha@users.noreply.github.com>
2024-02-07 15:15:26 -0800

    feat(logging): add `log!` macro to reduce logging boilerplate
    Makes a couple other small changes at clippy's behest.

--- libs/logging/types-rust/src/lib.rs
+++ libs/logging/types-rust/src/lib.rs
@@ -26,8 +26,63 @@ pub fn print_log(log: &Log) {
     };
 }
 
+/// Log a message with the given event id.
+///
+/// # Example
+///
+/// ```
+/// use vx_logging::{log, set_app_name, Disposition, EventId};
+///
+/// set_app_name("my-app");
+///
+/// // shorthand versions: event ID and optional message
+/// log!(EventId::MachineBootComplete);
+/// log!(EventId::AuthLogin, "User {user} logged in", user = "test-user");
+///
+/// // full version: specify any set of fields by name
+/// log!(
+///    message: format!("User {user} blocked!", user = "bad-user"),
+///    event_id: EventId::AuthLogin,
+///    disposition: Disposition::Failure
+/// );
+/// ```
+#[macro_export]
+macro_rules! log {
+    ($event_id:expr) => {
+        $crate::print_log(&$crate::Log {
+            event_id: $event_id,
+            ..Default::default()
+        });
+    };
+    ($event_id:expr, $($format_args:tt)*) => {
+        $crate::print_log(&$crate::Log {
+            event_id: $event_id,
+            message: format!($($format_args)*),
+            ..Default::default()
+        });
+    };
+    ($($arg:tt)*) => {
+        $crate::print_log(&$crate::Log {
+            $($arg)*,
+            ..Default::default()
+        });
+    };
+}
+
+/// Set the app name to be used in logs for this process.
+///
+/// # Example
+///
+/// ```
+/// use vx_logging::{log, set_app_name, EventId};
+///
+/// set_app_name("my-app");
+/// log!(EventId::AuthLogin, "User {user} logged in", user = "test-user");
+/// ```
 pub fn set_app_name(app_name: impl Into<String>) {
-    APP_NAME.set(app_name.into()).unwrap();
+    if let Err(e) = APP_NAME.set(app_name.into()) {
+        eprintln!("Error setting app name: {e}");
+    }
 }
 
 mod log_event_enums;
@@ -35,3 +90,37 @@ pub use self::log_event_enums::EventId;
 
 pub mod types;
 pub use self::types::{Disposition, EventType, Log, User};
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_set_app_name() {
+        set_app_name("test-app");
+        assert_eq!(APP_NAME.get().unwrap(), "test-app");
+    }
+
+    #[test]
+    fn test_print_log() {
+        set_app_name("test-app");
+
+        print_log(&Log::default());
+    }
+
+    #[test]
+    fn test_log_macro() {
+        set_app_name("test-app");
+
+        log!(EventId::MachineBootComplete);
+
+        let user = "test-user";
+        log!(EventId::AuthLogin, "somebody logged in: {user}");
+
+        log!(
+            message: format!("here is a log message: {}", "what!"),
+            event_id: EventId::AuthLogin,
+            disposition: Disposition::Failure
+        );
+    }
+}
