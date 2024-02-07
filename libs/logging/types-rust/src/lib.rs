dcb07e15672a3acac12b80f5b6eb4bc5294983fa
Brian Donovan <1938+eventualbuddha@users.noreply.github.com>
2024-02-07 14:58:39 -0800

    fix(logging): ensure log JSON is followed immediately by newline
    Without this a multithreaded application could interleave log messages.

--- libs/logging/types-rust/src/lib.rs
+++ libs/logging/types-rust/src/lib.rs
@@ -1,11 +1,29 @@
+use std::io::Write;
 use std::{io::stdout, sync::OnceLock};
 pub(crate) static APP_NAME: OnceLock<String> = OnceLock::new();
 
-pub fn print_log(log: Log) {
-    match serde_json::to_writer(stdout(), &log) {
-        Ok(_) => println!(),
+/// Prints a log to stdout in JSON format. You probably want to use the `log!` macro instead of
+/// calling this directly.
+///
+/// # Example
+///
+/// ```
+/// use vx_logging::{print_log, Log, EventId, set_app_name};
+///
+/// set_app_name("my-app");
+/// print_log(&Log {
+///    event_id: EventId::AuthLogin,
+///    message: format!("User {user} logged in", user = "test-user"),
+///    ..Default::default()
+/// });
+pub fn print_log(log: &Log) {
+    let mut stdout = stdout().lock();
+    match serde_json::to_writer(&mut stdout, &log) {
+        Ok(()) => {
+            let _ = writeln!(&mut stdout);
+        }
         Err(e) => eprintln!("Error serializing log: {e}"),
-    }
+    };
 }
 
 pub fn set_app_name(app_name: impl Into<String>) {
