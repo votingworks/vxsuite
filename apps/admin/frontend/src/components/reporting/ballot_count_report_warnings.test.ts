40e052edf5082add0e0841ef54ccd0cda8baf91e
Andrew Hayes <adghayes@gmail.com>
2024-02-08 11:46:35 -0800

    frontend: remove utils moved to backend

--- apps/admin/frontend/src/components/reporting/ballot_count_report_warnings.test.ts
+++ apps/admin/frontend/src/components/reporting/ballot_count_report_warnings.test.ts
@@ -1,31 +1,5 @@
-import { getEmptyCardCounts } from '@votingworks/utils';
-import {
-  BallotCountReportWarning,
-  getBallotCountReportWarning,
-  getBallotCountReportWarningText,
-} from './ballot_count_report_warnings';
-
-describe('getBallotCountReportWarning', () => {
-  test('does give warning when there are no reports', () => {
-    expect(
-      getBallotCountReportWarning({
-        allCardCounts: [],
-      })
-    ).toEqual({
-      type: 'no-reports-match-filter',
-    });
-  });
-
-  test("doesn't give a warning on zero report", () => {
-    expect(
-      getBallotCountReportWarning({
-        allCardCounts: [getEmptyCardCounts()],
-      })
-    ).toEqual({
-      type: 'none',
-    });
-  });
-});
+import type { BallotCountReportWarning } from '@votingworks/admin-backend';
+import { getBallotCountReportWarningText } from './ballot_count_report_warnings';
 
 test('getBallotCountReportWarningText', () => {
   const testCase: Array<{
--- apps/admin/frontend/src/components/reporting/ballot_count_report_warnings.ts
+++ apps/admin/frontend/src/components/reporting/ballot_count_report_warnings.ts
@@ -1,26 +1,4 @@
-import { Tabulation } from '@votingworks/types';
-
-export type BallotCountReportWarning =
-  | {
-      type: 'none';
-    }
-  | {
-      type: 'no-reports-match-filter';
-    };
-
-export function getBallotCountReportWarning({
-  allCardCounts,
-}: {
-  allCardCounts: Tabulation.GroupList<Tabulation.CardCounts>;
-}): BallotCountReportWarning {
-  if (allCardCounts.length === 0) {
-    return {
-      type: 'no-reports-match-filter',
-    };
-  }
-
-  return { type: 'none' };
-}
+import type { BallotCountReportWarning } from '@votingworks/admin-backend';
 
 interface BallotCountReportWarningProps {
   ballotCountReportWarning: BallotCountReportWarning;
