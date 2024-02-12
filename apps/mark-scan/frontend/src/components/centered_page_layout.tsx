0760283348faee31a2bc1589e4254a3f917a16d3
Kevin Shen <kevin@voting.works>
2024-02-12 11:11:02 -0800

    allow Align prop to be passed to CenteredPageLayout

--- apps/mark-scan/frontend/src/components/centered_page_layout.tsx
+++ apps/mark-scan/frontend/src/components/centered_page_layout.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { Font, H1, Main, ReadOnLoad, Screen } from '@votingworks/ui';
+import { Align, Font, H1, Main, ReadOnLoad, Screen } from '@votingworks/ui';
 import { VoterScreen, ButtonFooter } from '@votingworks/mark-flow-ui';
 
 export interface CenteredPageLayoutProps {
@@ -8,15 +8,16 @@ export interface CenteredPageLayoutProps {
   children: React.ReactNode;
   title?: React.ReactNode;
   voterFacing: boolean;
+  textAlign?: Align;
 }
 
 export function CenteredPageLayout(
   props: CenteredPageLayoutProps
 ): JSX.Element {
-  const { buttons, children, title, voterFacing } = props;
+  const { buttons, children, title, textAlign, voterFacing } = props;
 
   const mainContent = (
-    <Font align="center">
+    <Font align={textAlign || 'center'}>
       {title && <H1>{title}</H1>}
       {children}
     </Font>
