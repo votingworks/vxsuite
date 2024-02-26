6b7ed6bd28927f305bbdc908600bf937d00aa4d4
Kofi Ohene-Adu <kofi@voting.works>
2024-02-26 14:07:25 -0600

    Compact the misvote warning components in larger size modes

--- apps/scan/frontend/src/components/misvote_warnings/warning_details.tsx
+++ apps/scan/frontend/src/components/misvote_warnings/warning_details.tsx
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 
 import { appStrings } from '@votingworks/ui';
+import { SizeMode } from '@votingworks/types';
 import { ContestList } from './contest_list';
 import { useLayoutConfig } from './use_layout_config_hook';
 import { MisvoteWarningsProps } from './types';
@@ -9,11 +10,19 @@ interface ContainerProps {
   numCardsPerRow: number;
 }
 
+const CONTENT_SPACING_VALUES_REM: Readonly<Record<SizeMode, number>> = {
+  desktop: 0.5, // unused
+  touchSmall: 0.5,
+  touchMedium: 0.5,
+  touchLarge: 0.25,
+  touchExtraLarge: 0.25,
+};
+
 const Container = styled.div<ContainerProps>`
   display: grid;
   flex-direction: column;
   grid-template-columns: repeat(${(p) => p.numCardsPerRow}, 1fr);
-  grid-gap: 0.5rem;
+  grid-gap: ${(p) => CONTENT_SPACING_VALUES_REM[p.theme.sizeMode]}rem;
 `;
 
 export function WarningDetails(props: MisvoteWarningsProps): JSX.Element {
