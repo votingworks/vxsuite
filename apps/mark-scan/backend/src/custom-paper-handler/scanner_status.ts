56f258683578817bd3c08484b3328b8d7ce49ed0
Kevin Shen <kevin@voting.works>
2024-02-27 18:57:39 -0800

    factor statuses out of application_driver and test application_driver

---
+++ apps/mark-scan/backend/src/custom-paper-handler/scanner_status.ts
@@ -0,0 +1,60 @@
+import { PaperHandlerStatus } from '@votingworks/custom-paper-handler';
+
+export function isPaperReadyToLoad(
+  paperHandlerStatus: PaperHandlerStatus
+): boolean {
+  return (
+    paperHandlerStatus.paperInputLeftInnerSensor &&
+    paperHandlerStatus.paperInputLeftOuterSensor &&
+    paperHandlerStatus.paperInputRightInnerSensor &&
+    paperHandlerStatus.paperInputRightOuterSensor
+  );
+}
+
+export function isPaperJammed(paperHandlerStatus: PaperHandlerStatus): boolean {
+  return paperHandlerStatus.paperJam;
+}
+
+export function isPaperInScanner(
+  paperHandlerStatus: PaperHandlerStatus
+): boolean {
+  return (
+    paperHandlerStatus.paperPreCisSensor ||
+    paperHandlerStatus.paperPostCisSensor ||
+    paperHandlerStatus.preHeadSensor ||
+    paperHandlerStatus.paperOutSensor ||
+    paperHandlerStatus.parkSensor ||
+    paperHandlerStatus.scanInProgress
+  );
+}
+
+export function isPaperInInput(
+  paperHandlerStatus: PaperHandlerStatus
+): boolean {
+  return (
+    paperHandlerStatus.paperInputLeftInnerSensor ||
+    paperHandlerStatus.paperInputLeftOuterSensor ||
+    paperHandlerStatus.paperInputRightInnerSensor ||
+    paperHandlerStatus.paperInputRightOuterSensor
+  );
+}
+
+export function isPaperInOutput(
+  paperHandlerStatus: PaperHandlerStatus
+): boolean {
+  // From experimentation both of these are true when paper is in rear output
+  return (
+    paperHandlerStatus.ticketPresentInOutput ||
+    paperHandlerStatus.paperOutSensor
+  );
+}
+
+export function isPaperAnywhere(
+  paperHandlerStatus: PaperHandlerStatus
+): boolean {
+  return (
+    isPaperInInput(paperHandlerStatus) ||
+    isPaperInOutput(paperHandlerStatus) ||
+    isPaperInScanner(paperHandlerStatus)
+  );
+}
---
+++ apps/mark-scan/backend/src/custom-paper-handler/test_utils.ts
@@ -0,0 +1,66 @@
+import {
+  DEFAULT_PAPER_HANDLER_STATUS,
+  PaperHandlerStatus,
+} from '@votingworks/custom-paper-handler';
+import { readFile } from '@votingworks/fs';
+import { join } from 'path';
+import { Buffer } from 'buffer';
+
+const BALLOT_FIXTURE_FRONT_PATH = join(
+  __dirname,
+  'fixtures',
+  'ballot-pdf-data.bin'
+);
+export const MAX_FIXTURE_FILE_SIZE_BYTES = 1024 * 1024 * 50;
+
+export async function readBallotFixture(): Promise<Buffer> {
+  const pdfDataResult = await readFile(BALLOT_FIXTURE_FRONT_PATH, {
+    maxSize: MAX_FIXTURE_FILE_SIZE_BYTES,
+  });
+  return pdfDataResult.unsafeUnwrap();
+}
+
+export function getDefaultPaperHandlerStatus(): PaperHandlerStatus {
+  return { ...DEFAULT_PAPER_HANDLER_STATUS };
+}
+
+export function getPaperParkedStatus(): PaperHandlerStatus {
+  return { ...DEFAULT_PAPER_HANDLER_STATUS, parkSensor: true };
+}
+
+export function getPaperInsideStatus(): PaperHandlerStatus {
+  return { ...DEFAULT_PAPER_HANDLER_STATUS, paperPreCisSensor: true };
+}
+
+export function getPaperInFrontStatus(): PaperHandlerStatus {
+  return {
+    ...DEFAULT_PAPER_HANDLER_STATUS,
+    paperInputLeftInnerSensor: true,
+    paperInputLeftOuterSensor: true,
+    paperInputRightInnerSensor: true,
+    paperInputRightOuterSensor: true,
+  };
+}
+
+export function getPaperInRearStatus(): PaperHandlerStatus {
+  return {
+    ...DEFAULT_PAPER_HANDLER_STATUS,
+    ticketPresentInOutput: true,
+    paperOutSensor: true,
+  };
+}
+
+export function getPaperJammedStatus(): PaperHandlerStatus {
+  return {
+    ...DEFAULT_PAPER_HANDLER_STATUS,
+    ...getPaperInFrontStatus(),
+    paperJam: true,
+  };
+}
+
+export function getJammedButNoPaperStatus(): PaperHandlerStatus {
+  return {
+    ...DEFAULT_PAPER_HANDLER_STATUS,
+    paperJam: true,
+  };
+}
