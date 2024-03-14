2fd41fc549e02189e68536284991f2e3ad06855a
Brian Donovan <1938+eventualbuddha@users.noreply.github.com>
2024-03-14 16:20:59 -0700

    chore(image-utils): remove unused module `debug`

--- libs/image-utils/src/index.ts
+++ libs/image-utils/src/index.ts
@@ -1,7 +1,6 @@
 /* istanbul ignore file - this file should only have exports, no logic to test */
 export * from './crop';
 export * from './count';
-export * from './debug';
 export * from './image_data';
 export * from './outline';
 export * from './pdf_to_images';
