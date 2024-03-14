0483dd3af754f4b4d42c8760fd333abbab0aefbb
Brian Donovan <1938+eventualbuddha@users.noreply.github.com>
2024-03-14 16:18:53 -0700

    chore(image-utils): remove unused module `geometry`

--- libs/image-utils/src/index.ts
+++ libs/image-utils/src/index.ts
@@ -2,7 +2,6 @@
 export * from './crop';
 export * from './count';
 export * from './debug';
-export * from './geometry';
 export * from './image_data';
 export * from './outline';
 export * from './pdf_to_images';
