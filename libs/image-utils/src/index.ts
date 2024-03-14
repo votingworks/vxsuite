6a2354a7e6d62d9488a5f1a517682385cf9d6aba
Brian Donovan <1938+eventualbuddha@users.noreply.github.com>
2024-03-14 16:22:56 -0700

    chore(image-utils): remove unused module `count`

--- libs/image-utils/src/index.ts
+++ libs/image-utils/src/index.ts
@@ -1,6 +1,5 @@
 /* istanbul ignore file - this file should only have exports, no logic to test */
 export * from './crop';
-export * from './count';
 export * from './image_data';
 export * from './pdf_to_images';
 export * from './jest_pdf_snapshot';
