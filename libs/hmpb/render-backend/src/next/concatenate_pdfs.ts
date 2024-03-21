d12951504e5d1700fdc7aa473b1065312d4c55a8
Jonah Kagan <jonah@voting.works>
2024-03-21 16:55:40 -0700

    Add helper to concat pdfs
    We used to concatenate laid out ballot documents before creating PDFs
    (e.g. for test decks). It's too complicated to try to concatenate
    RenderDocuments, since they are living DOM with stylesheets and such.
    It's much easier to do concatenation with the resulting static PDFs.

---
+++ libs/hmpb/render-backend/src/next/concatenate_pdfs.ts
@@ -0,0 +1,17 @@
+import { Buffer } from 'buffer';
+import { PDFDocument } from 'pdf-lib';
+
+export async function concatenatePdfs(pdfs: Buffer[]): Promise<Buffer> {
+  const combinedPdf = await PDFDocument.create();
+  for (const pdf of pdfs) {
+    const pdfDoc = await PDFDocument.load(pdf);
+    const copiedPages = await combinedPdf.copyPages(
+      pdfDoc,
+      pdfDoc.getPageIndices()
+    );
+    for (const page of copiedPages) {
+      combinedPdf.addPage(page);
+    }
+  }
+  return Buffer.from(await combinedPdf.save());
+}
