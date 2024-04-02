5e6f8a42659015c87ed73f748c19d32fad88b224
Brian Donovan <1938+eventualbuddha@users.noreply.github.com>
2024-04-02 09:33:29 -0700

    fix(ballot-interpreter): updates for `neon` v1.0.0

--- libs/ballot-interpreter/src/hmpb-rust/js/serialization.rs
+++ libs/ballot-interpreter/src/hmpb-rust/js/serialization.rs
@@ -10,7 +10,7 @@ pub fn to_js<'a, T: Serialize, C: Context<'a>>(
     let json = serde_json::to_string(&value)
         .or_else(|err| cx.throw_error(format!("serialization as JSON failed: {err}")))?;
     let json = cx.string(json);
-    let json_global = cx.global().get::<JsObject, _, _>(cx, "JSON")?;
+    let json_global = cx.global::<JsObject>("JSON")?;
     let json_parse: Handle<JsFunction> = json_global.get(cx, "parse")?;
     json_parse.call_with(cx).arg(json).apply(cx)
 }
@@ -20,7 +20,7 @@ pub fn from_js<'a, T: DeserializeOwned, C: Context<'a>>(
     cx: &mut C,
     js_value: Handle<'a, JsValue>,
 ) -> NeonResult<T> {
-    let json_global = cx.global().get::<JsObject, _, _>(cx, "JSON")?;
+    let json_global = cx.global::<JsObject>("JSON")?;
     let json_stringify: Handle<JsFunction> = json_global.get(cx, "stringify")?;
     let json: Handle<JsString> = json_stringify.call_with(cx).arg(js_value).apply(cx)?;
     let json = json.value(cx);
