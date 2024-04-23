6ccbe4361edb854fa7a93df513535500c795db36
Kevin Shen <kevin@voting.works>
2024-04-23 17:02:34 -0400

    factor out mark_scan_device_diagnostic_section

--- libs/ui/src/diagnostics/mark_scan_readiness_report.tsx
+++ libs/ui/src/diagnostics/mark_scan_readiness_report.tsx
@@ -1,29 +1,40 @@
 import { ThemeProvider } from 'styled-components';
 import { ComputerSection, ComputerSectionProps } from './computer_section';
 import {
-  ConfigurationSectionProps,
   ConfigurationSection,
+  ConfigurationSectionProps,
 } from './configuration_section';
-import {
-  MarkScanControllerSection,
-  MarkScanControllerSectionProps,
-} from './mark_scan_controller_section';
 import { makeTheme } from '../themes/make_theme';
 import { PrintedReport } from '../reports/layout';
 import { ReadinessReportHeader } from './report_header';
+import {
+  MarkScanDeviceDiagnosticSection,
+  MarkScanDeviceDiagnosticSectionProps,
+} from './mark_scan_device_diagnostic_section';
 
-type ReportContentsProps = ComputerSectionProps &
-  MarkScanControllerSectionProps &
-  ConfigurationSectionProps;
+type NonpresentationalSectionProps = Omit<
+  MarkScanDeviceDiagnosticSectionProps,
+  'diagnosticType' | 'title'
+>;
+
+type ReportContentsProps = ConfigurationSectionProps &
+  ComputerSectionProps & {
+    accessibleControllerProps: NonpresentationalSectionProps;
+  };
 
 export function MarkScanReadinessReportContents(
   props: ReportContentsProps
 ): JSX.Element {
+  const { accessibleControllerProps } = props;
   return (
     <div>
       <ConfigurationSection {...props} expectPrecinctSelection />
       <ComputerSection {...props} />
-      <MarkScanControllerSection {...props} />
+      <MarkScanDeviceDiagnosticSection
+        {...accessibleControllerProps}
+        diagnosticType="mark-scan-accessible-controller"
+        title="Accessible Controller"
+      />
     </div>
   );
 }
