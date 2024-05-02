9532560b6a6d476a2981a035d2e68d37ad9fdfaf
Arsalan Sufi <arsalan@voting.works>
2024-05-02 16:22:10 -0400

    Get libs/basics building

--- libs/basics/src/iterators/naturals.ts
+++
@@ -1,9 +0,0 @@
-import { integers } from './integers';
-import { IteratorPlus } from './types';
-
-/**
- * Builds an infinite generator starting at 1 yielding successive integers.
- */
-export function naturals(): IteratorPlus<number> {
-  return integers({ from: 1 });
-}
