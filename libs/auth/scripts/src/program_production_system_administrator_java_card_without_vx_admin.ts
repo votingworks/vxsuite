fa11c2e912d3017649a3c3c132dda9c519289ac9
Arsalan Sufi <arsalan@voting.works>
2024-05-03 11:42:36 -0400

    Separate auth script TS source files from executables

---
+++ libs/auth/scripts/src/program_production_system_administrator_java_card_without_vx_admin.ts
@@ -0,0 +1,87 @@
+import fs from 'fs/promises';
+import path from 'path';
+import tmp from 'tmp';
+import { extractErrorMessage } from '@votingworks/basics';
+
+import {
+  CERT_EXPIRY_IN_DAYS,
+  constructMachineCertSubject,
+} from '../../src/certs';
+import { PROD_VX_CERT_AUTHORITY_CERT_PATH } from '../../src/config';
+import { createCert } from '../../src/cryptography';
+import { getRequiredEnvVar } from '../../src/env_vars';
+import { JavaCard } from '../../src/java_card';
+import {
+  generatePrivateKey,
+  programSystemAdministratorJavaCard,
+} from './utils';
+
+const jurisdiction = getRequiredEnvVar('VX_MACHINE_JURISDICTION');
+const vxPrivateKeyPath = getRequiredEnvVar('VX_PRIVATE_KEY_PATH');
+
+async function instantiateJavaCardWithOneOffVxAdminPrivateKeyAndCertAuthorityCert(
+  tempDirectoryPath: string
+): Promise<JavaCard> {
+  const vxAdminPrivateKey = await generatePrivateKey();
+  const vxAdminPrivateKeyPath = path.join(
+    tempDirectoryPath,
+    'vx-admin-private-key.pem'
+  );
+  await fs.writeFile(vxAdminPrivateKeyPath, vxAdminPrivateKey);
+
+  const vxAdminCertAuthorityCert = await createCert({
+    certKeyInput: {
+      type: 'private',
+      key: { source: 'file', path: vxAdminPrivateKeyPath },
+    },
+    certSubject: constructMachineCertSubject('admin', jurisdiction),
+    certType: 'cert_authority_cert',
+    expiryInDays: CERT_EXPIRY_IN_DAYS.MACHINE_VX_CERT,
+    signingCertAuthorityCertPath: PROD_VX_CERT_AUTHORITY_CERT_PATH,
+    signingPrivateKey: { source: 'file', path: vxPrivateKeyPath },
+  });
+  const vxAdminCertAuthorityCertPath = path.join(
+    tempDirectoryPath,
+    'vx-admin-cert-authority-cert.pem'
+  );
+  await fs.writeFile(vxAdminCertAuthorityCertPath, vxAdminCertAuthorityCert);
+
+  const card = new JavaCard({
+    cardProgrammingConfig: {
+      vxAdminCertAuthorityCertPath,
+      vxAdminPrivateKey: { source: 'file', path: vxAdminPrivateKeyPath },
+    },
+    vxCertAuthorityCertPath: PROD_VX_CERT_AUTHORITY_CERT_PATH,
+  });
+
+  return card;
+}
+
+/**
+ * A script for programming a production system administrator Java Card without a VxAdmin. This is
+ * useful for preparing backup cards after we've shipped a VxAdmin.
+ */
+export async function main(): Promise<void> {
+  let exitCode: number = 0;
+  let tempDirectory: tmp.DirResult | undefined;
+  try {
+    tempDirectory = tmp.dirSync({ unsafeCleanup: true });
+    const card =
+      await instantiateJavaCardWithOneOffVxAdminPrivateKeyAndCertAuthorityCert(
+        tempDirectory.name
+      );
+    await programSystemAdministratorJavaCard({
+      card,
+      jurisdiction,
+      isProduction: true,
+    });
+  } catch (error) {
+    console.error(`❌ ${extractErrorMessage(error)}`);
+    exitCode = 1;
+  } finally {
+    // Delete temp directory and contained files
+    tempDirectory?.removeCallback();
+  }
+  // Smart card scripts require an explicit exit, even on success, or else they hang
+  process.exit(exitCode);
+}
---
+++ libs/auth/scripts/src/utils.ts
@@ -0,0 +1,77 @@
+import { Buffer } from 'buffer';
+import { sleep } from '@votingworks/basics';
+import { generatePin, hyphenatePin } from '@votingworks/utils';
+
+import { ResponseApduError } from '../../src/apdu';
+import { CardStatusReady, StatefulCard } from '../../src/card';
+import { openssl } from '../../src/cryptography';
+import { JavaCard } from '../../src/java_card';
+
+/**
+ * Generates an ECC private key and returns the private key contents in a buffer. The key is not
+ * stored in a TPM.
+ */
+export async function generatePrivateKey(): Promise<Buffer> {
+  return await openssl(['ecparam', '-genkey', '-name', 'prime256v1', '-noout']);
+}
+
+/**
+ * Waits for a card to have a ready status
+ */
+export async function waitForReadyCardStatus<T>(
+  card: StatefulCard<T>,
+  waitTimeSeconds = 3
+): Promise<CardStatusReady<T>> {
+  let cardStatus = await card.getCardStatus();
+  let remainingWaitTimeSeconds = waitTimeSeconds;
+  while (cardStatus.status !== 'ready' && remainingWaitTimeSeconds > 0) {
+    await sleep(1000);
+    cardStatus = await card.getCardStatus();
+    remainingWaitTimeSeconds -= 1;
+  }
+  if (cardStatus.status !== 'ready') {
+    throw new Error(`Card status not "ready" after ${waitTimeSeconds} seconds`);
+  }
+  return cardStatus;
+}
+
+/**
+ * Programs a system administrator Java Card
+ */
+export async function programSystemAdministratorJavaCard({
+  card,
+  isProduction,
+  jurisdiction,
+}: {
+  card: JavaCard;
+  isProduction: boolean;
+  jurisdiction: string;
+}): Promise<void> {
+  const initialJavaCardConfigurationScriptReminder = `
+${
+  isProduction
+    ? 'Have you run this card through the configure-java-card script yet?'
+    : 'Have you run this card through the configure-dev-java-card script yet?'
+}
+If not, that's likely the cause of this error.
+Run that and then retry.
+`;
+
+  await waitForReadyCardStatus(card);
+
+  const pin = isProduction ? generatePin() : '000000';
+  try {
+    await card.program({
+      user: { role: 'system_administrator', jurisdiction },
+      pin,
+    });
+  } catch (error) {
+    if (error instanceof ResponseApduError) {
+      throw new Error(
+        `${error.message}\n${initialJavaCardConfigurationScriptReminder}`
+      );
+    }
+    throw error;
+  }
+  console.log(`✅ Done! Card PIN is ${hyphenatePin(pin)}.`);
+}
