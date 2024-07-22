1906bfe799037a93ddb4ab0f690a7fb5e2dd0681
Caroline Mars <caroline@voting.works>
2024-07-22 16:35:36 -0700

    reset language with voter display settings on vxscan

--- apps/scan/frontend/src/components/voter_settings_manager.tsx
+++ apps/scan/frontend/src/components/voter_settings_manager.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import {
   VoterSettingsManagerContext,
+  useCurrentLanguage,
+  useLanguageControls,
   useQueryChangeListener,
 } from '@votingworks/ui';
 import { DefaultTheme, ThemeContext } from 'styled-components';
+import { LanguageCode } from '@votingworks/types';
 import { getAuthStatus, getScannerStatus } from '../api';
 
 /**
  * Side-effect component for monitoring for auth and voter session changes and
- * resetting/restoring voter voter settings as needed.
+ * resetting/restoring voter display settings and language choice as needed.
  */
 export function VoterSettingsManager(): JSX.Element | null {
   const voterSettingsManager = React.useContext(VoterSettingsManagerContext);
@@ -17,8 +20,15 @@ export function VoterSettingsManager(): JSX.Element | null {
   const authStatusQuery = getAuthStatus.useQuery();
   const scannerStatusQuery = getScannerStatus.useQuery();
 
+  const { reset: resetLanguage, setLanguage } = useLanguageControls();
+  const currentLanguage = useCurrentLanguage();
+
   const [voterSessionTheme, setVoterSessionTheme] =
     React.useState<DefaultTheme | null>(null);
+
+  const [voterLanguage, setVoterLanguage] = React.useState<LanguageCode | null>(
+    null
+  );
 
   useQueryChangeListener(authStatusQuery, {
     select: ({ status }) => status,
@@ -26,26 +36,39 @@ export function VoterSettingsManager(): JSX.Element | null {
       // Reset to default theme when election official logs in:
       if (previousStatus === 'logged_out') {
         setVoterSessionTheme(currentTheme);
+        setVoterLanguage(currentLanguage);
         voterSettingsManager.resetThemes();
+        resetLanguage();
       }
 
       // Reset to previous voter settings when election official logs out:
       if (newStatus === 'logged_out' && voterSessionTheme) {
         voterSettingsManager.setColorMode(voterSessionTheme.colorMode);
         voterSettingsManager.setSizeMode(voterSessionTheme.sizeMode);
+        if (voterLanguage) {
+          setLanguage(voterLanguage);
+        }
         setVoterSessionTheme(null);
+        setVoterLanguage(null);
       }
     },
   });
 
-  // [VVSG 2.0 7.1-A] Reset to default theme when voter is done scanning. We
+  // [VVSG 2.0 7.1-A] Reset to default theme and language when voter is done scanning. We
   // have chosen to interpret that as whenever paper leaves the scanner (either
   // into the ballot box, or retrieved by the user after a ballot rejection).
   useQueryChangeListener(scannerStatusQuery, {
     select: ({ state }) => state,
     onChange: (newState, previousState) => {
-      if (previousState && newState === 'no_paper') {
+      // If we transition from paused to no_paper we are just returning from an election official screen
+      if (
+        previousState &&
+        previousState !== 'no_paper' &&
+        previousState !== 'paused' &&
+        newState === 'no_paper'
+      ) {
         voterSettingsManager.resetThemes();
+        resetLanguage();
       }
     },
   });
