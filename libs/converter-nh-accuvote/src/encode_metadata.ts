5256291b1a3ee3c0bcb62fdef26a25f1cafdd327
Brian Donovan <1938+eventualbuddha@users.noreply.github.com>
2024-07-22 15:36:51 -0700

    fix(converter-nh-accuvote): draw QR code with CMYK black

--- libs/converter-nh-accuvote/src/encode_metadata.ts
+++ libs/converter-nh-accuvote/src/encode_metadata.ts
@@ -1,40 +1,7 @@
-import { assertDefined, iter } from '@votingworks/basics';
+import { iter } from '@votingworks/basics';
 import { QrCodeData, encodeMetadataInQrCode } from '@votingworks/hmpb-layout';
 import { BallotMetadata, Election, SheetOf } from '@votingworks/types';
-import { Buffer } from 'buffer';
-import { createCanvas } from 'canvas';
-import { PDFDocument } from 'pdf-lib';
-
-async function qrCodeDataToPng(data: QrCodeData): Promise<Buffer> {
-  // QR codes are supposed to be surrounded by 4 modules of white space
-  const marginModules = 4;
-  const ppi = 200;
-  const maxSizeInches = 1;
-  const maxSizePixels = ppi * maxSizeInches;
-  const totalModules = data.length + marginModules * 2;
-  const moduleSize = Math.floor(maxSizePixels / totalModules);
-  const sizePixels = totalModules * moduleSize;
-  const canvas = createCanvas(sizePixels, sizePixels);
-  const ctx = canvas.getContext('2d');
-  ctx.fillStyle = 'white';
-  ctx.fillRect(0, 0, sizePixels, sizePixels);
-  ctx.fillStyle = 'black';
-  for (let x = 0; x < data.length; x += 1) {
-    for (let y = 0; y < data.length; y += 1) {
-      if (assertDefined(data[x])[y] === 1) {
-        ctx.fillRect(
-          (x + marginModules) * moduleSize,
-          (y + marginModules) * moduleSize,
-          moduleSize,
-          moduleSize
-        );
-      }
-    }
-  }
-  return new Promise((resolve, reject) => {
-    canvas.toBuffer((err, result) => (err ? reject(err) : resolve(result)));
-  });
-}
+import { PDFDocument, PDFPage, cmyk } from 'pdf-lib';
 
 /**
  * Encode ballot page metadata into either a QR code or a series of timing marks.
@@ -42,15 +9,34 @@ async function qrCodeDataToPng(data: QrCodeData): Promise<Buffer> {
 export function encodeMetadata(
   election: Election,
   metadata: BallotMetadata
-): Promise<SheetOf<Buffer>> {
+): Promise<SheetOf<QrCodeData>> {
   return Promise.all([
-    qrCodeDataToPng(
-      encodeMetadataInQrCode(election, { ...metadata, pageNumber: 1 })
-    ),
-    qrCodeDataToPng(
-      encodeMetadataInQrCode(election, { ...metadata, pageNumber: 2 })
-    ),
+    encodeMetadataInQrCode(election, { ...metadata, pageNumber: 1 }),
+    encodeMetadataInQrCode(election, { ...metadata, pageNumber: 2 }),
   ]);
+}
+
+function qrCodeToSvgPath(qrCode: QrCodeData, { scale = 1 } = {}): string {
+  return iter(qrCode)
+    .flatMap((col, x) =>
+      iter(col).filterMap((bit, y) =>
+        bit
+          ? `M${x * scale},${y * scale}h${scale}v${scale}h-${scale}z`
+          : undefined
+      )
+    )
+    .join();
+}
+
+function drawQrCodeOnPage(page: PDFPage, qrCode: QrCodeData): void {
+  const svgPath = qrCodeToSvgPath(qrCode, {
+    scale: 1.2,
+  });
+  page.drawSvgPath(svgPath, {
+    x: 21,
+    y: 38,
+    color: cmyk(0, 0, 0, 1),
+  });
 }
 
 /**
@@ -64,13 +50,8 @@ export async function addQrCodeMetadataToBallotPdf(
 ): Promise<void> {
   const qrCodes = await encodeMetadata(election, metadata);
   const pages = document.getPages();
-  for (const [page, qrCode] of iter(pages).zip(qrCodes).toArray()) {
-    const qrCodeEmbed = await document.embedPng(qrCode);
-    const qrCodeDimensions = qrCodeEmbed.scale(0.22);
-    page.drawImage(qrCodeEmbed, {
-      ...qrCodeDimensions,
-      x: 16,
-      y: 0,
-    });
+
+  for (const [page, qrCode] of iter(pages).zip(qrCodes)) {
+    drawQrCodeOnPage(page, qrCode);
   }
 }
