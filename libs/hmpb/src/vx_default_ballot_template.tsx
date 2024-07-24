votingworks/vxsuite

commit 1f45370f3a2b7eeffa3410c13b2df4efce7c61b4

Fix layout bugs for monolingual ballots  (#5025)
* Fix layout bugs for monolingual ballots

- Don't render two write-in labels in the same language side-by-side
- Render contest votes allowed and term on separate lines

* Regenerate HMPB fixtures

* Update snapshots

libs/hmpb/src/vx_default_ballot_template.tsx
@@ -211,8 +211,7 @@ function Header({
 function WriteInLabel() {
   return (
     <span>
-      {hmpbStrings.hmpbWriteIn} /{' '}
-      <InEnglish>{hmpbStrings.hmpbWriteIn}</InEnglish>
+      <DualLanguageText>{hmpbStrings.hmpbWriteIn}</DualLanguageText>
     </span>
   );
 }
@@ -529,10 +528,12 @@ function CandidateContest({
         <DualLanguageText delimiter="/">
           <h3>{electionStrings.contestTitle(contest)}</h3>
         </DualLanguageText>
-        <DualLanguageText delimiter="/">{voteForText}</DualLanguageText>
+        <DualLanguageText delimiter="/">
+          <div>{voteForText}</div>
+        </DualLanguageText>
         {contest.termDescription && (
           <DualLanguageText delimiter="/">
-            {electionStrings.contestTerm(contest)}
+            <div>{electionStrings.contestTerm(contest)}</div>
           </DualLanguageText>
         )}
       </ContestHeader>

commit 56f685eab845c8d18a7109b3fe234b216b0058ea

hmpb: Add missing delimiter for write-in label
Accidentally broken in https://github.com/votingworks/vxsuite/pull/5025

libs/hmpb/src/vx_default_ballot_template.tsx
@@ -211,7 +211,9 @@ function Header({
 function WriteInLabel() {
   return (
     <span>
-      <DualLanguageText>{hmpbStrings.hmpbWriteIn}</DualLanguageText>
+      <DualLanguageText delimiter="/">
+        {hmpbStrings.hmpbWriteIn}
+      </DualLanguageText>
     </span>
   );
 }