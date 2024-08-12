405a111fa336e81a336df828c58b75b49af465b6
Andrew Hayes <Adghayes@gmail.com>
2024-08-12 09:51:28 -0700

    allow spaces and underscores in usb drive mount points (#5243)

--- libs/usb-drive/scripts/unmount.sh
+++ libs/usb-drive/scripts/unmount.sh
@@ -13,15 +13,15 @@ fi
 
 MOUNTPOINT=$1
 
-MOUNTPOINT_REGEX=^/media/[a-Z0-9-]+/[a-Z0-9-]+$
+MOUNTPOINT_REGEX=^/media/[[:alnum:]\ -_]+/[[:alnum:]\ -_]+$
 
-if ! [[ $MOUNTPOINT =~ $MOUNTPOINT_REGEX ]]; then
+if ! [[ "$MOUNTPOINT" =~ $MOUNTPOINT_REGEX ]]; then
     echo "unmount.sh: mount point \"${MOUNTPOINT}\" is not a valid mounted USB drive"
     exit 1
 fi
 
 # Run sync before unmounting to force any cached file data to be flushed to the
 # removable drive. Used to prevent incomplete file transfers.
-sync -f $MOUNTPOINT
+sync -f "$MOUNTPOINT"
 
-umount $MOUNTPOINT
+umount "$MOUNTPOINT"
