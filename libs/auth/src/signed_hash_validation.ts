8fb68903bd870ca6febfd8a7c5125619cca311c8
Arsalan Sufi <arsalan@voting.works>
2024-08-23 14:16:31 -0400

    Add system hash, software version, and combined election hash to SHV

--- libs/auth/src/signed_hash_validation.ts
+++ libs/auth/src/signed_hash_validation.ts
@@ -1,95 +1,123 @@
+import { existsSync } from 'node:fs';
 import fs from 'node:fs/promises';
 import { Readable } from 'node:stream';
-import { assert } from '@votingworks/basics';
+import { assert, sleep } from '@votingworks/basics';
+import {
+  formatElectionHashes,
+  SignedHashValidationQrCodeValue,
+} from '@votingworks/types';
 
 import {
   constructSignedHashValidationConfig,
   SignedHashValidationConfig,
 } from './config';
 import { signMessage } from './cryptography';
-import { FileKey, TpmKey } from './keys';
+import { runCommand } from './shell';
 import { constructPrefixedMessage } from './signatures';
 
-const SIGNED_HASH_VALIDATION_MESSAGE_PAYLOAD_SEPARATOR = '/';
 const SIGNED_HASH_VALIDATION_QR_CODE_VALUE_SEPARATOR = ';';
+const SIGNED_HASH_VALIDATION_MESSAGE_PAYLOAD_SEPARATOR = '/';
+const SYSTEM_HASH_DISPLAY_LENGTH = 10;
+
+const HASHING_SCRIPT_PATH = '/vx/admin/admin-functions/hash-signature.sh';
+
+async function computeSystemHash(): Promise<string> {
+  /* istanbul ignore next */
+  if (existsSync(HASHING_SCRIPT_PATH)) {
+    const stdout = await runCommand([
+      'sudo',
+      HASHING_SCRIPT_PATH,
+      'noninteractive',
+    ]);
+    return stdout.toString('utf-8');
+  }
+
+  // Mock system hash computation in dev
+  await sleep(2000);
+  return 'UNVERIFIED';
+}
+
+interface ElectionRecord {
+  electionDefinition: { ballotHash: string };
+  electionPackageHash: string;
+}
 
 interface SignedHashValidationMachineState {
+  electionRecord?: ElectionRecord;
   machineId: string;
-  ballotHash?: string;
+  softwareVersion: string;
 }
 
 /**
  * Signed hash validation allows election officials to check that a VxSuite machine hasn't been
  * tampered with.
  */
-export class SignedHashValidation {
-  private readonly machineCertPath: string;
-  private readonly machinePrivateKey: FileKey | TpmKey;
+export async function generateSignedHashValidationQrCodeValue(
+  machineState: SignedHashValidationMachineState,
+  /* istanbul ignore next */
+  config: SignedHashValidationConfig = constructSignedHashValidationConfig()
+): Promise<SignedHashValidationQrCodeValue> {
+  const { electionRecord, machineId, softwareVersion } = machineState;
+  const systemHash = (await computeSystemHash()).slice(
+    0,
+    SYSTEM_HASH_DISPLAY_LENGTH
+  );
+  const combinedElectionHash = electionRecord
+    ? formatElectionHashes(
+        electionRecord.electionDefinition.ballotHash,
+        electionRecord.electionPackageHash
+      )
+    : '';
+  const date = new Date();
 
-  constructor(
-    // Support specifying a custom config for tests
-    /* istanbul ignore next */
-    input: SignedHashValidationConfig = constructSignedHashValidationConfig()
-  ) {
-    this.machineCertPath = input.machineCertPath;
-    this.machinePrivateKey = input.machinePrivateKey;
-  }
-
-  async generateQrCodeValue({
+  const messagePayloadComponents: string[] = [
+    systemHash,
+    softwareVersion,
     machineId,
-    ballotHash,
-  }: SignedHashValidationMachineState): Promise<{
-    qrCodeValue: string;
+    combinedElectionHash,
+    date.toISOString(),
+  ];
+  const messagePayload = messagePayloadComponents.join(
+    SIGNED_HASH_VALIDATION_MESSAGE_PAYLOAD_SEPARATOR
+  );
+  const message = constructPrefixedMessage(
+    'shv1', // Signed hash validation, version 1
+    messagePayload
+  );
+
+  const messageSignature = await signMessage({
+    message: Readable.from(message),
+    signingPrivateKey: config.machinePrivateKey,
+  });
+
+  const machineCert = await fs.readFile(config.machineCertPath, 'utf-8');
+
+  const qrCodeValueParts: string[] = [
+    message,
+    messageSignature.toString('base64'),
+    machineCert
+      // Remove the standard PEM header and footer to make the QR code as small as possible
+      .replace('-----BEGIN CERTIFICATE-----', '')
+      .replace('-----END CERTIFICATE-----', ''),
+  ];
+  assert(
+    qrCodeValueParts.every(
+      (part) => !part.includes(SIGNED_HASH_VALIDATION_QR_CODE_VALUE_SEPARATOR)
+    ),
+    `Found ${SIGNED_HASH_VALIDATION_QR_CODE_VALUE_SEPARATOR} separator within part of QR code value`
+  );
+  const qrCodeValue = qrCodeValueParts.join(
+    SIGNED_HASH_VALIDATION_QR_CODE_VALUE_SEPARATOR
+  );
+
+  return {
+    qrCodeValue,
     signatureInputs: {
-      machineId: string;
-      date: Date;
-      ballotHashPrefix?: string;
-    };
-  }> {
-    const date = new Date();
-    const ballotHashPrefix = ballotHash?.slice(0, 10) ?? '';
-    const messagePayload = [
+      combinedElectionHash,
+      date,
       machineId,
-      date.toISOString(),
-      ballotHashPrefix,
-    ].join(SIGNED_HASH_VALIDATION_MESSAGE_PAYLOAD_SEPARATOR);
-    const message = constructPrefixedMessage(
-      'lc', // Live Check (legacy feature name)
-      messagePayload
-    );
-
-    const messageSignature = await signMessage({
-      message: Readable.from(message),
-      signingPrivateKey: this.machinePrivateKey,
-    });
-
-    const machineCert = await fs.readFile(this.machineCertPath, 'utf-8');
-
-    const qrCodeValueParts: string[] = [
-      message,
-      messageSignature.toString('base64'),
-      machineCert
-        // Remove the standard PEM header and footer to make the QR code as small as possible
-        .replace('-----BEGIN CERTIFICATE-----', '')
-        .replace('-----END CERTIFICATE-----', ''),
-    ];
-    assert(
-      qrCodeValueParts.every(
-        (part) => !part.includes(SIGNED_HASH_VALIDATION_QR_CODE_VALUE_SEPARATOR)
-      ),
-      `Found ${SIGNED_HASH_VALIDATION_QR_CODE_VALUE_SEPARATOR} separator within part of QR code value`
-    );
-    const qrCodeValue = qrCodeValueParts.join(
-      SIGNED_HASH_VALIDATION_QR_CODE_VALUE_SEPARATOR
-    );
-
-    return {
-      qrCodeValue,
-      signatureInputs: {
-        machineId,
-        date,
-        ballotHashPrefix,
-      },
-    };
-  }
+      softwareVersion,
+      systemHash,
+    },
+  };
 }
