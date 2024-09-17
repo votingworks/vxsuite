47cc2e10d97078e0871f11211860b0d9f1ca30c4
Kofi <kofi@voting.works>
2024-09-17 11:57:25 -0500

    [MarkScan] Switch audio to speakers and play alarm sound when cover is open (#5377)

---
+++ apps/mark-scan/backend/src/audio/outputs.ts
@@ -0,0 +1,41 @@
+/* istanbul ignore file */
+import { execFile } from '@votingworks/backend';
+import { NODE_ENV } from '../globals';
+
+const PULSE_AUDIO_SINK_ID_VSAP_SOUND_CARD = '0';
+
+/**
+ * Audio output port names for VSAP BMD sound card. These are fairly
+ * hardware-specific and not guaranteed to work in other environments.
+ */
+export enum AudioOutput {
+  SPEAKER = 'analog-output-speaker',
+  HEADPHONES = 'analog-output-headphones',
+}
+
+/**
+ * Sets the active audio output port.
+ * NOTE: This is only guaranteed to work on production hardware.
+ */
+export async function setAudioOutput(outputName: AudioOutput): Promise<void> {
+  if (NODE_ENV !== 'production') {
+    return;
+  }
+
+  let errorOutput: string;
+
+  try {
+    ({ stderr: errorOutput } = await execFile('sudo', [
+      '/vx/code/app-scripts/pactl.sh',
+      'set-sink-port',
+      PULSE_AUDIO_SINK_ID_VSAP_SOUND_CARD,
+      outputName,
+    ]));
+  } catch (error) {
+    throw new Error(`Unable to set audio output: ${error}}`);
+  }
+
+  if (errorOutput) {
+    throw new Error(`Unable to set audio output: ${errorOutput}}`);
+  }
+}
