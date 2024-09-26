a3170e468c5aeaf7f3ae28c088633451953754cd
Brian Donovan <1938+eventualbuddha@users.noreply.github.com>
2024-09-26 10:39:17 -0700

    chore(deps): update `typescript` to v5.6.2
    Changes in v5.6: https://devblogs.microsoft.com/typescript/announcing-typescript-5-6/
    
    I don't see anything that dramatically affects us. The change in `assert.test.ts` is due to the "Disallowed Nullish and Truthy Checks" change. Once we update Electron and NodeJS to versions that support it, the "Iterator Helper Methods" changes will remove some of the need for `iter` in `libs/basics`, but we can't use them yet.

--- libs/res-to-ts/src/utils/assert.test.ts
+++ libs/res-to-ts/src/utils/assert.test.ts
@@ -3,5 +3,5 @@ import { assert } from './assert';
 test('assert', () => {
   expect(() => assert(true, 'true')).not.toThrow();
   expect(() => assert(false, 'false')).toThrow('false');
-  expect(() => assert(!{})).toThrow('Assertion failed');
+  expect(() => assert(false)).toThrow('Assertion failed');
 });
