94806d3de8a57c9b7f63e1d0967a8ed97d928bab
Jonah Kagan <jonah@voting.works>
2024-10-01 12:29:55 -0700

    Create separate modal component for removing official results
    It's simple enough that there's not much use in having an abstracted
    confirm modal component, and I'll be changing the "remove CVRs" case to
    be more complex soon.
    
    Also, there's no need to disable the button when there are no results since you can't mark results
    official without having some results.

--- apps/admin/frontend/src/screens/tally/tally_screen.tsx
+++ apps/admin/frontend/src/screens/tally/tally_screen.tsx
@@ -1,27 +1,15 @@
 import React, { useContext, useState } from 'react';
-
 import { isElectionManagerAuth } from '@votingworks/utils';
-import { assert, throwIllegalValue } from '@votingworks/basics';
+import { assert } from '@votingworks/basics';
 import { Button, Icons, H3, RouterTabBar } from '@votingworks/ui';
 import { Redirect, Route, Switch } from 'react-router-dom';
-import { ResultsFileType } from '../../config/types';
-
 import { AppContext } from '../../contexts/app_context';
-
 import { NavigationScreen } from '../../components/navigation_screen';
-import { ConfirmRemovingFileModal } from './confirm_removing_file_modal';
-import {
-  clearCastVoteRecordFiles,
-  deleteAllManualResults,
-  getCastVoteRecordFileMode,
-  getCastVoteRecordFiles,
-  getManualResultsMetadata,
-} from '../../api';
-import { Loading } from '../../components/loading';
 import { OfficialResultsCard } from '../../components/official_results_card';
 import { CastVoteRecordsTab } from './cast_vote_records_tab';
 import { ManualTalliesTab } from './manual_tallies_tab';
 import { routerPaths } from '../../router_paths';
+import { ConfirmRemoveAllResultsModal } from './confirm_remove_all_results_modal';
 
 export function TallyScreen(): JSX.Element | null {
   const { electionDefinition, isOfficialResults, auth } =
@@ -29,56 +17,10 @@ export function TallyScreen(): JSX.Element | null {
   assert(electionDefinition);
   assert(isElectionManagerAuth(auth));
 
-  const clearCastVoteRecordFilesMutation =
-    clearCastVoteRecordFiles.useMutation();
-  const deleteAllManualTalliesMutation = deleteAllManualResults.useMutation();
-
-  const [confirmingRemoveFileType, setConfirmingRemoveFileType] =
-    useState<ResultsFileType>();
-
-  function beginConfirmRemoveFiles(fileType: ResultsFileType) {
-    setConfirmingRemoveFileType(fileType);
-  }
-  function cancelConfirmingRemoveFiles() {
-    setConfirmingRemoveFileType(undefined);
-  }
-  function confirmRemoveFiles(fileType: ResultsFileType) {
-    switch (fileType) {
-      case ResultsFileType.CastVoteRecord:
-        clearCastVoteRecordFilesMutation.mutate();
-        break;
-      case ResultsFileType.All:
-        deleteAllManualTalliesMutation.mutate();
-        clearCastVoteRecordFilesMutation.mutate();
-        break;
-      /** istanbul ignore next */
-      default:
-        throwIllegalValue(fileType);
-    }
-    setConfirmingRemoveFileType(undefined);
-  }
-
-  const castVoteRecordFileModeQuery = getCastVoteRecordFileMode.useQuery();
-  const castVoteRecordFilesQuery = getCastVoteRecordFiles.useQuery();
-  const manualTallyMetadataQuery = getManualResultsMetadata.useQuery();
-
-  if (
-    !castVoteRecordFilesQuery.isSuccess ||
-    !castVoteRecordFileModeQuery.isSuccess ||
-    !manualTallyMetadataQuery.isSuccess
-  ) {
-    return (
-      <NavigationScreen title="Tally">
-        <Loading isFullscreen />
-      </NavigationScreen>
-    );
-  }
-
-  const manualTallyMetadata = manualTallyMetadataQuery.data;
-  const hasManualTally = manualTallyMetadata.length > 0;
-
-  const castVoteRecordFileList = castVoteRecordFilesQuery.data;
-  const hasAnyFiles = castVoteRecordFileList.length > 0 || hasManualTally;
+  const [
+    isConfirmRemoveAllResultsModalOpen,
+    setIsConfirmRemoveAllResultsModalOpen,
+  ] = useState(false);
 
   return (
     <React.Fragment>
@@ -90,12 +32,11 @@ export function TallyScreen(): JSX.Element | null {
               Election Results Marked as Official
             </H3>
             <Button
-              disabled={!hasAnyFiles}
-              onPress={() => beginConfirmRemoveFiles(ResultsFileType.All)}
+              onPress={() => setIsConfirmRemoveAllResultsModalOpen(true)}
               icon="Delete"
               color="danger"
             >
-              Clear All Results
+              Remove All Results
             </Button>
           </OfficialResultsCard>
         )}
@@ -127,11 +68,10 @@ export function TallyScreen(): JSX.Element | null {
           <Redirect from={routerPaths.tally} to={routerPaths.tallyCvrs} />
         </Switch>
       </NavigationScreen>
-      {confirmingRemoveFileType && (
-        <ConfirmRemovingFileModal
-          fileType={confirmingRemoveFileType}
-          onConfirm={confirmRemoveFiles}
-          onCancel={cancelConfirmingRemoveFiles}
+
+      {isConfirmRemoveAllResultsModalOpen && (
+        <ConfirmRemoveAllResultsModal
+          onClose={() => setIsConfirmRemoveAllResultsModalOpen(false)}
         />
       )}
     </React.Fragment>
