efbe5a8d1b309fe032912b5138a0c581bcfe7f41
Nikhil Bhatia <nikhil@voting.works>
2024-10-03 10:36:18 -0700

    Add alert when usb is disconnected before polls are closed

--- apps/scan/frontend/src/screens/insert_usb_screen.tsx
+++ apps/scan/frontend/src/screens/insert_usb_screen.tsx
@@ -1,7 +1,12 @@
 import { CenteredLargeProse, H1, P } from '@votingworks/ui';
+import React from 'react';
 import { ScreenMainCenterChild } from '../components/layout';
+import { useSound } from '../utils/use_sound';
 
 export function InsertUsbScreen(): JSX.Element {
+  const playAlarm = useSound('alarm');
+  React.useEffect(() => playAlarm(), [playAlarm]);
+
   return (
     <ScreenMainCenterChild voterFacing={false}>
       <CenteredLargeProse>
