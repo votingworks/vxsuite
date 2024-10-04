d6d2e7be6b27be2d872a691dc7ae1a691c1f59e6
Nikhil Bhatia <nikhil@voting.works>
2024-10-03 17:23:58 -0700

    VxScan: Alert on usb removal before polls have closed (#5469)
    * Add alert when usb is disconnected before polls are closed
    
    * Add test to validate alarm plays on usb disconnect

--- apps/scan/frontend/src/utils/use_sound.ts
+++ apps/scan/frontend/src/utils/use_sound.ts
@@ -1,6 +1,8 @@
 import useSoundLib from 'use-sound';
 
-export function useSound(sound: 'success' | 'warning' | 'error'): () => void {
+export function useSound(
+  sound: 'success' | 'warning' | 'error' | 'alarm'
+): () => void {
   const [playSound] = useSoundLib(`/sounds/${sound}.mp3`);
   return playSound;
 }
