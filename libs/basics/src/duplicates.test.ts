e0530b0f25437394ef2a29242d6aca3f18391d8b
Brian Donovan <1938+eventualbuddha@users.noreply.github.com>
2024-12-10 14:15:21 -0800

    test(basics): convert tests from `jest` to `vitest` (#5690)
    The motivation for this is not so much any particular problem with `jest` as the desire to move from targeting CommonJS modules (CJS) to EcmaScript modules (ESM) in our build system:
    - CJS modules interoperate poorly with ESM packages from NPM, of which there are a growing number. This has caused us to use older versions of various packages instead of the latest.
    - Faster native-speed tooling for building TypeScript tends to have poor or non-existent support for CJS. Targeting CJS therefore locks us into using slower build tools.
    
    `vitest` has a very similar API and DX to `jest`, but has much better support for ESM. It uses faster native-speed tooling-esbuild at the moment-though the gains appear to be [uneven][perf]. So I wouldn't necessarily consider this to be a net performance win.
    
    [perf]: https://dev.to/thejaredwilcurt/vitest-vs-jest-benchmarks-on-a-5-year-old-real-work-spa-4mf1

--- libs/basics/src/duplicates.test.ts
+++ libs/basics/src/duplicates.test.ts
@@ -1,3 +1,4 @@
+import { expect, test } from 'vitest';
 import { duplicates } from './duplicates';
 
 test('duplicates', () => {
--- libs/basics/src/map_object.test.ts
+++ libs/basics/src/map_object.test.ts
@@ -1,3 +1,4 @@
+import { expect, test } from 'vitest';
 import { mapObject } from './map_object';
 
 test('mapObject', () => {
