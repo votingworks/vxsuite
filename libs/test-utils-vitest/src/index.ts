99684b7bac39b0a0dd4ecd3c29e3d989a11c5905
Jonah Kagan <jonah@voting.works>
2024-12-10 08:49:13 -0800

    Create libs/test-utils-vitest (#5703)
    * Create libs/test-utils-vitest
    
    Copies all files from libs/test-utils that rely on `jest` and migrate them to use `vitest` instead. This will enable any packages that currently use libs/test-utils to be migrated to vitest by using the vitest version of these utils. Eventually, when we're done migrating and there are no more jest tests, we can move these back into libs/test-utils, replacing the jest versions.
    
    * Regenerate CircleCI config

---
+++ libs/test-utils-vitest/src/index.ts
@@ -0,0 +1,6 @@
+export * from './advance_timers';
+export * from './child_process';
+export * from './console';
+export * from './mock_kiosk';
+export * from './mock_of';
+export * from './mock_use_audio_controls';
