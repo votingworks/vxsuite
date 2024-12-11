a5329112cb4b5aabb860647302428f557165f5e9
Brian Donovan <1938+eventualbuddha@users.noreply.github.com>
2024-12-11 15:50:24 -0800

    test(dev-dock/frontend): move from `jest` to `vitest` (#5734)

--- libs/dev-dock/frontend/src/setupTests.ts
+++ libs/dev-dock/frontend/src/setupTests.ts
@@ -1,7 +1,7 @@
-import '@testing-library/jest-dom/extend-expect';
+import { afterEach, expect } from 'vitest';
+import matchers from '@testing-library/jest-dom/matchers';
+import { cleanup } from '@testing-library/react';
 
-// styled-components version 5.3.1 and above requires this remapping for jest
-// environments, reference: https://github.com/styled-components/styled-components/issues/3570
-jest.mock('styled-components', () =>
-  jest.requireActual('styled-components/dist/styled-components.browser.cjs.js')
-);
+expect.extend(matchers);
+
+afterEach(cleanup);
