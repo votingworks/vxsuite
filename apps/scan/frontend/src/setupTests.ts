a266d230a02766df9fd34a503ea27390a7c29f3f
Jonah Kagan <jonah@voting.works>
2024-12-11 10:47:50 -0800

    Update setupTests.ts

--- apps/scan/frontend/src/setupTests.ts
+++ apps/scan/frontend/src/setupTests.ts
@@ -1,15 +1,13 @@
-import 'jest-styled-components';
-import '@testing-library/jest-dom/extend-expect';
+import { afterEach, expect } from 'vitest';
+import matchers from '@testing-library/jest-dom/matchers';
 import { TextDecoder, TextEncoder } from 'node:util';
-import { configure } from '../test/react_testing_library';
+import { cleanup, configure } from '../test/react_testing_library';
+
+expect.extend(matchers);
 
 configure({ asyncUtilTimeout: 5_000 });
 
-// styled-components version 5.3.1 and above requires this remapping for jest
-// environments, reference: https://github.com/styled-components/styled-components/issues/3570
-jest.mock('styled-components', () =>
-  jest.requireActual('styled-components/dist/styled-components.browser.cjs.js')
-);
-
 globalThis.TextDecoder = TextDecoder as typeof globalThis.TextDecoder;
 globalThis.TextEncoder = TextEncoder as typeof globalThis.TextEncoder;
+
+afterEach(cleanup);
