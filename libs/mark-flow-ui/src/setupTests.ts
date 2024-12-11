332cc8d24c6695002755d7c63da59d642c616ced
Jonah Kagan <jonah@voting.works>
2024-12-11 15:59:49 -0800

    mark-flow-ui: Style tweak in setupTests.ts

--- libs/mark-flow-ui/src/setupTests.ts
+++ libs/mark-flow-ui/src/setupTests.ts
@@ -1,8 +1,7 @@
 import { afterEach, beforeEach, expect, vi } from 'vitest';
-import * as matchers from '@testing-library/jest-dom/matchers';
+import matchers from '@testing-library/jest-dom/matchers';
 import { cleanup, configure } from '@testing-library/react';
 
-// eslint-disable-next-line vx/gts-direct-module-export-access-only
 expect.extend(matchers);
 
 configure({ asyncUtilTimeout: 5_000 });
