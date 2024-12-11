0302b23b48e56dd441c1036f32df5436f840159e
Brian Donovan <1938+eventualbuddha@users.noreply.github.com>
2024-12-11 15:36:47 -0800

    refactor(dev-dock): use default import to avoid lint

--- libs/dev-dock/frontend/src/setupTests.ts
+++ libs/dev-dock/frontend/src/setupTests.ts
@@ -1,8 +1,7 @@
 import { afterEach, expect } from 'vitest';
-import * as matchers from '@testing-library/jest-dom/matchers';
+import matchers from '@testing-library/jest-dom/matchers';
 import { cleanup } from '@testing-library/react';
 
-// eslint-disable-next-line vx/gts-direct-module-export-access-only
 expect.extend(matchers);
 
 afterEach(cleanup);
