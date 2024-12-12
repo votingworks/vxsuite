b188587a31c24281068ef21e02f0a75ffa336ef0
Jonah Kagan <jonah@voting.works>
2024-12-12 09:12:46 -0800

    Update affected coverage thresholds

--- libs/dev-dock/frontend/vitest.config.ts
+++ libs/dev-dock/frontend/vitest.config.ts
@@ -4,5 +4,11 @@ export default defineConfig({
   test: {
     environment: 'jsdom',
     setupFiles: ['src/setupTests.ts'],
+    coverage: {
+      thresholds: {
+        lines: 87,
+        branches: 71,
+      },
+    },
   },
 });
