b36f8e806c03230158f1733df4dcebb75558cca8
Arsalan Sufi <arsalan@voting.works>
2024-12-17 10:42:36 -0500

    Allow specifying a passphrase file for a private key file
    And reorganize OpenSSL key param logic

--- libs/auth/src/keys.ts
+++ libs/auth/src/keys.ts
@@ -1,4 +1,6 @@
+import { Buffer } from 'node:buffer';
 import { z } from 'zod';
+import { throwIllegalValue } from '@votingworks/basics';
 
 /**
  * An SSL key stored in a file
@@ -6,6 +8,7 @@ import { z } from 'zod';
 export interface FileKey {
   source: 'file';
   path: string;
+  passphraseFilePath?: string;
 }
 
 /**
@@ -29,6 +32,7 @@ export interface TpmKey {
 export const FileKeySchema: z.ZodSchema<FileKey> = z.object({
   source: z.literal('file'),
   path: z.string(),
+  passphraseFilePath: z.string().optional(),
 });
 
 /**
@@ -45,3 +49,50 @@ export const InlineKeySchema: z.ZodSchema<InlineKey> = z.object({
 export const TpmKeySchema: z.ZodSchema<TpmKey> = z.object({
   source: z.literal('tpm'),
 });
+
+/**
+ * The ID of the TPM signing key, distinct from the TPM primary key
+ */
+const TPM_KEY_ID = '0x81000001';
+
+/**
+ * The name of the OpenSSL provider that we're using to interface with the TPM
+ */
+const TPM_OPENSSL_PROVIDER = 'tpm2';
+
+/**
+ * Prepares the OpenSSL params necessary to use a key
+ */
+export function opensslKeyParams(
+  opensslParam: '-CAkey' | '-inkey' | '-key',
+  key: FileKey | InlineKey | TpmKey
+): Array<string | Buffer> {
+  switch (key.source) {
+    case 'file': {
+      return [
+        opensslParam,
+        key.path,
+        ...(key.passphraseFilePath
+          ? ['-passin', `file:${key.passphraseFilePath}`]
+          : []),
+      ];
+    }
+    case 'inline': {
+      return [opensslParam, Buffer.from(key.content, 'utf-8')];
+    }
+    case 'tpm': {
+      return [
+        opensslParam,
+        `handle:${TPM_KEY_ID}`,
+        // This propquery tells OpenSSL to prefer the TPM provider but fall back to the default
+        // provider for operations outside the scope of the TPM provider, like reading files.
+        '-propquery',
+        `?provider=${TPM_OPENSSL_PROVIDER}`,
+      ];
+    }
+    /* istanbul ignore next: Compile-time check for completeness */
+    default: {
+      throwIllegalValue(key, 'source');
+    }
+  }
+}
