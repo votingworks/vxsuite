5438ed948bf1bd7287fcbda370b165c9a35196b5
Brian Donovan <1938+eventualbuddha@users.noreply.github.com>
2024-12-17 16:39:23 -0800

    fix(fixtures): regenerate outputs to sync with inputs

--- libs/res-to-ts/src/convert.ts
+++ libs/res-to-ts/src/convert.ts
@@ -81,8 +81,7 @@ export async function convert({
 
   if (isImageResource) {
     lines.push(
-      `import { createCanvas, Image, ImageData, loadImage } from 'canvas';`,
-      ``
+      `import { createCanvas, Image, ImageData, loadImage } from 'canvas';`
     );
   }
 
