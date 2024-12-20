7e7957b09e37e9fc150b7f2a930c6df4ae8561bb
Brian Donovan <1938+eventualbuddha@users.noreply.github.com>
2024-12-19 17:11:09 -0800

    test(types): remove jest config

--- libs/types/jest.config.js
+++
@@ -1,9 +0,0 @@
-const shared = require('../../jest.config.shared');
-
-/**
- * @type {import('@jest/types').Config.InitialOptions}
- */
-module.exports = {
-  ...shared,
-  coveragePathIgnorePatterns: ['./test/helpers/*', './src/cdf/election-results-reporting/types.ts'],
-};
