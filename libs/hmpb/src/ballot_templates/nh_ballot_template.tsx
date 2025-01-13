adb152f7bf7641c3f12faf8d6a65db52f5365a58
Kevin Shen <kevin@voting.works>
2025-01-13 15:21:46 -0800

    right align bubbles for NH

--- libs/hmpb/src/ballot_templates/nh_ballot_template.tsx
+++ libs/hmpb/src/ballot_templates/nh_ballot_template.tsx
@@ -1,35 +1,60 @@
-import { assertDefined } from '@votingworks/basics';
+import {
+  assertDefined,
+  iter,
+  range,
+  throwIllegalValue,
+} from '@votingworks/basics';
 import { Buffer } from 'node:buffer';
 import {
+  AnyContest,
   BallotStyleId,
   BallotType,
+  CandidateContest as CandidateContestStruct,
   Election,
   ElectionDefinition,
   ElectionSerializationFormat,
+  YesNoContest,
   ballotPaperDimensions,
   getBallotStyle,
+  getContests,
   getPartyForBallotStyle,
 } from '@votingworks/types';
 import {
   BackendLanguageContextProvider,
+  CandidatePartyList,
   electionStrings,
+  RichText,
 } from '@votingworks/ui';
 import {
   BallotPageTemplate,
   BaseBallotProps,
+  PagedElementResult,
   renderAllBallotsAndCreateElectionDefinition,
 } from '../render_ballot';
-import { Renderer } from '../renderer';
-import { Page, TimingMarkGrid, pageMarginsInches } from '../ballot_components';
-import { BallotMode } from '../types';
+import { Renderer, RenderScratchpad } from '../renderer';
+import {
+  Bubble,
+  OptionInfo,
+  Page,
+  TimingMarkGrid,
+  WRITE_IN_OPTION_CLASS,
+  pageMarginsInches,
+} from '../ballot_components';
+import { BallotMode, PixelDimensions } from '../types';
 import { hmpbStrings } from '../hmpb_strings';
 import {
-  BallotPageContent,
+  BlankPageMessage,
+  Box,
+  BubbleWrapper,
+  Colors,
+  ContestHeader,
   DualLanguageText,
   Footer,
   Instructions,
   primaryLanguageCode,
+  WriteInLabel,
 } from './vx_default_ballot_template';
+import { layOutInColumns } from '../layout_in_columns';
 
 export interface NhPrecinctSplitOptions {
   electionTitleOverride?: string;
@@ -216,6 +241,374 @@ function BallotPageFrame({
       </Page>
     </BackendLanguageContextProvider>
   );
+}
+
+function CandidateContest({
+  election,
+  contest,
+}: {
+  election: Election;
+  contest: CandidateContestStruct;
+}) {
+  const voteForText = {
+    1: hmpbStrings.hmpbVoteFor1,
+    2: hmpbStrings.hmpbVoteFor2,
+    3: hmpbStrings.hmpbVoteFor3,
+    4: hmpbStrings.hmpbVoteFor4,
+    5: hmpbStrings.hmpbVoteFor5,
+    6: hmpbStrings.hmpbVoteFor6,
+    7: hmpbStrings.hmpbVoteFor7,
+    8: hmpbStrings.hmpbVoteFor8,
+    9: hmpbStrings.hmpbVoteFor9,
+    10: hmpbStrings.hmpbVoteFor10,
+  }[contest.seats];
+  if (!voteForText) {
+    throw new Error(
+      `Unsupported number of seats for contest: ${contest.seats}`
+    );
+  }
+
+  return (
+    <Box
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        padding: 0,
+      }}
+    >
+      <ContestHeader>
+        <DualLanguageText delimiter="/">
+          <h3>{electionStrings.contestTitle(contest)}</h3>
+        </DualLanguageText>
+        <DualLanguageText delimiter="/">
+          <div>{voteForText}</div>
+        </DualLanguageText>
+        {contest.termDescription && (
+          <DualLanguageText delimiter="/">
+            <div>{electionStrings.contestTerm(contest)}</div>
+          </DualLanguageText>
+        )}
+      </ContestHeader>
+      <ul>
+        {contest.candidates.map((candidate, i) => {
+          const partyText =
+            election.type === 'primary' ? undefined : (
+              <CandidatePartyList
+                candidate={candidate}
+                electionParties={election.parties}
+              />
+            );
+          const optionInfo: OptionInfo = {
+            type: 'option',
+            contestId: contest.id,
+            optionId: candidate.id,
+          };
+          return (
+            <li
+              key={candidate.id}
+              style={{
+                padding: '0.375rem 0.5rem',
+                borderTop:
+                  i !== 0 ? `1px solid ${Colors.DARK_GRAY}` : undefined,
+              }}
+            >
+              <div
+                style={{
+                  display: 'flex',
+                  gap: '0.5rem',
+                }}
+              >
+                <div style={{ flex: 1, textAlign: 'right' }}>
+                  <strong>{candidate.name}</strong>
+                  {partyText && (
+                    <DualLanguageText delimiter="/">
+                      <div>{partyText}</div>
+                    </DualLanguageText>
+                  )}
+                </div>
+                <BubbleWrapper optionInfo={optionInfo} />
+              </div>
+            </li>
+          );
+        })}
+        {contest.allowWriteIns &&
+          range(0, contest.seats).map((writeInIndex) => {
+            const optionInfo: OptionInfo = {
+              type: 'write-in',
+              contestId: contest.id,
+              writeInIndex,
+              writeInArea: {
+                top: 0.8,
+                right: -0.8,
+                bottom: 0.3,
+                left: 8.8,
+              },
+            };
+            return (
+              <li
+                key={writeInIndex}
+                className={WRITE_IN_OPTION_CLASS}
+                style={{
+                  display: 'flex',
+                  gap: '0.5rem',
+                  padding: '0.25rem 0.5rem',
+                  paddingTop: '0.9rem',
+                  borderTop: `1px solid ${Colors.DARK_GRAY}`,
+                }}
+              >
+                <div style={{ flex: 1 }}>
+                  <div
+                    style={{
+                      borderBottom: `1px solid ${Colors.BLACK}`,
+                      height: '1.25rem',
+                    }}
+                  />
+                  <div style={{ fontSize: '0.8rem' }}>
+                    <WriteInLabel />
+                  </div>
+                </div>
+                <BubbleWrapper isWriteIn optionInfo={optionInfo} />
+              </li>
+            );
+          })}
+      </ul>
+    </Box>
+  );
+}
+
+function BallotMeasureContest({ contest }: { contest: YesNoContest }) {
+  return (
+    <Box style={{ padding: 0 }}>
+      <ContestHeader>
+        <DualLanguageText delimiter="/">
+          <h2>{electionStrings.contestTitle(contest)}</h2>
+        </DualLanguageText>
+      </ContestHeader>
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'space-between',
+          gap: '0.25rem',
+        }}
+      >
+        <div
+          style={{
+            padding: '0.5rem 0.5rem',
+            display: 'flex',
+            flexDirection: 'column',
+            gap: '0.5rem',
+          }}
+        >
+          <DualLanguageText>
+            <RichText
+              tableBorderWidth={'1px'}
+              tableBorderColor={Colors.DARKER_GRAY}
+              tableHeaderBackgroundColor={Colors.LIGHT_GRAY}
+            >
+              {electionStrings.contestDescription(contest)}
+            </RichText>
+          </DualLanguageText>
+        </div>
+        <ul
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'end',
+          }}
+        >
+          {[contest.yesOption, contest.noOption].map((option) => (
+            <li
+              key={option.id}
+              style={{
+                padding: '0.375rem 0.5rem',
+                borderTop: `1px solid ${Colors.LIGHT_GRAY}`,
+              }}
+            >
+              <div
+                style={{
+                  display: 'flex',
+                  gap: '0.5rem',
+                  paddingTop: '0.25rem',
+                }}
+              >
+                <strong
+                  style={{ flex: 1, textAlign: 'right', marginTop: '-0.25rem' }}
+                >
+                  <DualLanguageText delimiter="/">
+                    {electionStrings.contestOptionLabel(option)}
+                  </DualLanguageText>
+                </strong>
+                <Bubble
+                  optionInfo={{
+                    type: 'option',
+                    contestId: contest.id,
+                    optionId: option.id,
+                  }}
+                />
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </Box>
+  );
+}
+
+function Contest({
+  contest,
+  election,
+}: {
+  contest: AnyContest;
+  election: Election;
+}) {
+  switch (contest.type) {
+    case 'candidate':
+      return <CandidateContest election={election} contest={contest} />;
+    case 'yesno':
+      return <BallotMeasureContest contest={contest} />;
+    default:
+      return throwIllegalValue(contest);
+  }
+}
+
+async function BallotPageContent(
+  props: (BaseBallotProps & { dimensions: PixelDimensions }) | undefined,
+  scratchpad: RenderScratchpad
+): Promise<PagedElementResult<BaseBallotProps>> {
+  if (!props) {
+    return {
+      currentPageElement: <BlankPageMessage />,
+      nextPageProps: undefined,
+    };
+  }
+
+  const { election, ballotStyleId, dimensions, ...restProps } = props;
+  const ballotStyle = assertDefined(
+    getBallotStyle({ election, ballotStyleId })
+  );
+  // For now, just one section for candidate contests, one for ballot measures.
+  // TODO support arbitrarily defined sections
+  const contests = getContests({ election, ballotStyle });
+  if (contests.length === 0) {
+    throw new Error('No contests assigned to this precinct.');
+  }
+  const contestSections = iter(contests)
+    .partition((contest) => contest.type === 'candidate')
+    .filter((section) => section.length > 0);
+
+  // Add as many contests on this page as will fit.
+  const contestSectionsLeftToLayout = contestSections;
+  const pageSections: JSX.Element[] = [];
+  let heightUsed = 0;
+
+  // TODO is there some way we can use rem here instead of having to know the
+  // font size and map to px?
+  const horizontalGapPx = 0.75 * 16; // Assuming 16px per 1rem
+  const verticalGapPx = horizontalGapPx;
+  while (contestSections.length > 0 && heightUsed < dimensions.height) {
+    const section = assertDefined(contestSectionsLeftToLayout.shift());
+    const contestElements = section.map((contest) => (
+      <Contest key={contest.id} contest={contest} election={election} />
+    ));
+    const numColumns = section[0].type === 'candidate' ? 3 : 2;
+    const columnWidthPx =
+      (dimensions.width - horizontalGapPx * (numColumns - 1)) / numColumns;
+    const contestMeasurements = await scratchpad.measureElements(
+      <BackendLanguageContextProvider
+        currentLanguageCode={primaryLanguageCode(ballotStyle)}
+        uiStringsPackage={election.ballotStrings}
+      >
+        {contestElements.map((contest, i) => (
+          <div
+            className="contestWrapper"
+            key={i}
+            style={{ width: `${columnWidthPx}px` }}
+          >
+            {contest}
+          </div>
+        ))}
+      </BackendLanguageContextProvider>,
+      '.contestWrapper'
+    );
+    const measuredContests = iter(contestElements)
+      .zip(contestMeasurements)
+      .map(([element, measurements]) => ({ element, ...measurements }))
+      .toArray();
+
+    const { columns, height, leftoverElements } = layOutInColumns({
+      elements: measuredContests,
+      numColumns,
+      maxColumnHeight: dimensions.height - heightUsed,
+      elementGap: verticalGapPx,
+    });
+
+    // Put leftover elements back on the front of the queue
+    if (leftoverElements.length > 0) {
+      contestSectionsLeftToLayout.unshift(
+        leftoverElements.map(({ element }) => element.props.contest)
+      );
+    }
+
+    // If there wasn't enough room left for any contests, go to the next page
+    if (height === 0) {
+      break;
+    }
+
+    // Add vertical gap to account for space between sections
+    heightUsed += height + verticalGapPx;
+    pageSections.push(
+      <div
+        key={`section-${pageSections.length + 1}`}
+        style={{ display: 'flex', gap: `${horizontalGapPx}px` }}
+      >
+        {columns.map((column, i) => (
+          <div
+            key={`column-${i}`}
+            style={{
+              flex: 1,
+              display: 'flex',
+              flexDirection: 'column',
+              gap: `${verticalGapPx}px`,
+            }}
+          >
+            {column.map(({ element }) => element)}
+          </div>
+        ))}
+      </div>
+    );
+  }
+
+  const currentPageElement =
+    pageSections.length > 0 ? (
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          gap: `${verticalGapPx}px`,
+        }}
+      >
+        {pageSections}
+      </div>
+    ) : (
+      <BlankPageMessage />
+    );
+  const nextPageProps =
+    contestSectionsLeftToLayout.length > 0
+      ? {
+          ...restProps,
+          ballotStyleId,
+          election: {
+            ...election,
+            contests: contestSectionsLeftToLayout.flat(),
+          },
+        }
+      : undefined;
+
+  return {
+    currentPageElement,
+    nextPageProps,
+  };
 }
 
 export const nhBallotTemplate: BallotPageTemplate<BaseBallotProps> = {
