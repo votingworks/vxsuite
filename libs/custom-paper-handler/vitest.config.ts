7e153e4f977cb34eb5d764001a79647160dad127
Brian Donovan <1938+eventualbuddha@users.noreply.github.com>
2025-01-22 18:50:54 -0800

    test(custom-paper-handler): update coverage thresholds
    This is what they were in the jest config.

--- libs/custom-paper-handler/vitest.config.ts
+++ libs/custom-paper-handler/vitest.config.ts
@@ -3,5 +3,11 @@ import { defineConfig } from '../../vitest.config.shared.mjs';
 export default defineConfig({
   test: {
     clearMocks: true,
+    coverage: {
+      thresholds: {
+        lines: 0,
+        branches: 0,
+      },
+    },
   },
 });
