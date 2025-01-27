2a5d588b665badbac8d03b3ed1ef073a2741add0
Jonah Kagan <jonah@voting.works>
2025-01-27 15:36:28 -0800

    apps/design/frontend: Sentry instrumentation
    Including DSN in the source code for now since it's safe to have public
    and it's the simplest way to configure.

--- apps/design/frontend/src/index.tsx
+++ apps/design/frontend/src/index.tsx
@@ -1,7 +1,19 @@
+import * as Sentry from '@sentry/browser';
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { assert } from '@votingworks/basics';
 import { App } from './app';
+
+/* istanbul ignore next - @preserve */
+if (process.env.NODE_ENV === 'production') {
+  Sentry.init({
+    // Note: This isn't a secret
+    dsn: 'https://941de51b2e7e464aaed34ef659f0036a@o471921.ingest.us.sentry.io/4508717994016768',
+    environment: process.env.NODE_ENV,
+    integrations: [Sentry.browserTracingIntegration()],
+    tracesSampleRate: 0.2,
+  });
+}
 
 const rootElement = document.getElementById('root');
 assert(rootElement);
