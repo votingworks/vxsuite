002493237217bfd698f9f6f1b3568ae1cfaa21be
Jonah Kagan <jonah@voting.works>
2025-02-05 09:51:08 -0800

    V3 NH ballot: Adjust spacing for letter vs legal paper length (#5964)
    Since the timing marks are spaced slightly differently on letter vs
    legal paper, we have to adjust the height of the header slightly
    differently for different paper sizes so that the bubbles will line up
    (since the header isn't snapped to the grid).

--- libs/hmpb/src/render_ballot.test.tsx
+++ libs/hmpb/src/render_ballot.test.tsx
@@ -3,7 +3,7 @@ import {
   electionGeneralFixtures,
 } from '@votingworks/fixtures';
 import { test, expect } from 'vitest';
-import { BallotType } from '@votingworks/types';
+import { BallotType, HmpbBallotPaperSize } from '@votingworks/types';
 import {
   allBaseBallotProps,
   BaseBallotProps,
@@ -81,10 +81,51 @@ test('renderMinimalBallotsToCreateElectionDefinition', async () => {
 });
 
 test(
-  'v3-compatible NH ballot',
+  'v3-compatible NH ballot - letter',
   async () => {
     const fixtureElection = electionGeneralFixtures.readElection();
     const allBallotProps = allBaseBallotProps(fixtureElection);
+    const renderer = await createPlaywrightRenderer();
+    const electionDefinition =
+      await renderMinimalBallotsToCreateElectionDefinition(
+        renderer,
+        ballotTemplates.NhBallotV3,
+        allBallotProps,
+        'vxf'
+      );
+
+    // Bubbles and WIA crop should be snapped to grid
+    for (const gridLayout of electionDefinition.election.gridLayouts!) {
+      for (const gridPosition of gridLayout.gridPositions) {
+        expect(gridPosition.column % 1).toEqual(0);
+        expect(gridPosition.row % 1).toEqual(0);
+      }
+      expect(gridLayout.optionBoundsFromTargetMark.bottom % 1).toEqual(0);
+      expect(gridLayout.optionBoundsFromTargetMark.left % 1).toEqual(0);
+      expect(gridLayout.optionBoundsFromTargetMark.right % 1).toEqual(0);
+      expect(gridLayout.optionBoundsFromTargetMark.top % 1).toEqual(0);
+    }
+
+    // Election date should be off by one day to account for timezone bug in v3
+    expect(fixtureElection.date.toISOString()).toEqual('2020-11-03');
+    expect(electionDefinition.election.date.toISOString()).toEqual(
+      '2020-11-04'
+    );
+  },
+  { timeout: 30_000 }
+);
+
+test(
+  'v3-compatible NH ballot - legal',
+  async () => {
+    const fixtureElection = electionGeneralFixtures.readElection();
+    const allBallotProps = allBaseBallotProps({
+      ...fixtureElection,
+      ballotLayout: {
+        ...fixtureElection.ballotLayout,
+        paperSize: HmpbBallotPaperSize.Legal,
+      },
+    });
     const renderer = await createPlaywrightRenderer();
     const electionDefinition =
       await renderMinimalBallotsToCreateElectionDefinition(
