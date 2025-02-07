58945395e55caf5c35ece0f80f897968968d1f0c
Kevin Shen <kevin@voting.works>
2025-02-06 18:03:10 -0800

    add additionalHashInput to election.json so precinct split images change election hash

--- apps/design/backend/src/ballots.test.ts
+++ apps/design/backend/src/ballots.test.ts
@@ -1,9 +1,20 @@
 import { electionGridLayoutNewHampshireHudsonFixtures } from '@votingworks/fixtures';
-import { Election, LanguageCode } from '@votingworks/types';
+import {
+  DistrictId,
+  Election,
+  ElectionStringKey,
+  LanguageCode,
+  UiStringsPackage,
+} from '@votingworks/types';
+import { TestLanguageCode } from '@votingworks/test-utils';
 import { BallotTemplateId } from '@votingworks/hmpb';
 import { expect, test } from 'vitest';
-import { createBallotPropsForTemplate } from './ballots';
-import { UsState } from './types';
+import { assertDefined } from '@votingworks/basics';
+import {
+  createBallotPropsForTemplate,
+  formatElectionForExport,
+} from './ballots';
+import { Precinct, UsState } from './types';
 
 const election: Election = {
   ...electionGridLayoutNewHampshireHudsonFixtures.readElection(),
@@ -55,4 +66,43 @@ test('compact templates', () => {
       expect(props.compact).toEqual(true);
     }
   }
+});
+
+test('formatElectionForExport', () => {
+  const { CHINESE_SIMPLIFIED, ENGLISH, SPANISH } = TestLanguageCode;
+  const testTranslations: UiStringsPackage = {
+    [CHINESE_SIMPLIFIED]: { [ElectionStringKey.BALLOT_LANGUAGE]: '简体中文' },
+    [ENGLISH]: { [ElectionStringKey.BALLOT_LANGUAGE]: 'English' },
+    [SPANISH]: { [ElectionStringKey.BALLOT_LANGUAGE]: 'Español' },
+  };
+  const testPrecincts: Precinct[] = [
+    {
+      id: 'precinct-1',
+      name: 'Precinct One',
+      splits: [
+        {
+          districtIds: ['district_a' as DistrictId],
+          id: 'split-1',
+          name: 'Split Name',
+          electionTitleOverride: 'election title override',
+          electionSealOverride: '<svg class="somesvg"></svg>',
+          clerkSignatureImage: '<svg class="someothersvg"></svg>',
+        },
+      ],
+    },
+  ];
+
+  const formattedElection = formatElectionForExport(
+    election,
+    testTranslations,
+    testPrecincts
+  );
+  expect(formattedElection).toHaveProperty('additionalHashInput');
+  const hashInput = assertDefined(formattedElection.additionalHashInput);
+  expect(hashInput['precinctSplitSeals']).toMatchObject({
+    'precinct-1-split-1': expect.any(String),
+  });
+  expect(hashInput['precinctSplitSignatureImages']).toMatchObject({
+    'precinct-1-split-1': expect.any(String),
+  });
 });
