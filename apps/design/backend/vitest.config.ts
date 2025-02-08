e3de200b338ca6a80a2ad69ac2d329f4daf92c9d
Kevin Shen <kevin@voting.works>
2025-02-07 20:27:01 -0800

    fix typo

--- apps/design/backend/vitest.config.ts
+++ apps/design/backend/vitest.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
     coverage: {
       thresholds: {
         lines: 71.9,
-        branches: 69.7,
+        branches: 59.7,
       },
       exclude: ['src/configure_sentry.ts'],
     },
