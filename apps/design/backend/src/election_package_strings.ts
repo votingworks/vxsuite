94ca0e15f1d3b136417cd3d4d28d89d56885e596
Kevin Shen <kevin@voting.works>
2025-02-10 14:54:28 -0800

    add precinct split strings to hmpbStrings

---
+++ apps/design/backend/src/election_package_strings.ts
@@ -0,0 +1,23 @@
+import { hasSplits, Precinct } from './types';
+
+export function getUserDefinedHmpbStrings(
+  precincts: Precinct[]
+): Record<string, string> {
+  const catalog: Record<string, string> = {};
+  for (const precinct of precincts) {
+    if (hasSplits(precinct)) {
+      for (const split of precinct.splits) {
+        if (split.clerkSignatureCaption) {
+          catalog[`hmpbClerkSignatureCaption_${split.id}`] =
+            split.clerkSignatureCaption;
+        }
+        if (split.electionTitleOverride) {
+          catalog[`hmpbElectionTitleOverride_${split.id}`] =
+            split.electionTitleOverride;
+        }
+      }
+    }
+  }
+
+  return catalog;
+}
