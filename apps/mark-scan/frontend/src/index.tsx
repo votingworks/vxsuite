8d9752bad5e4143fba209a553d1a4a47ea853211
Arsalan Sufi <arsalan@voting.works>
2025-02-24 14:08:32 -0500

    VxMark: Electrical testing frontend

--- apps/mark-scan/frontend/src/index.tsx
+++ apps/mark-scan/frontend/src/index.tsx
@@ -3,7 +3,12 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { DevDock } from '@votingworks/dev-dock-frontend';
 import { assert } from '@votingworks/basics';
+import {
+  BooleanEnvironmentVariableName,
+  isFeatureFlagEnabled,
+} from '@votingworks/utils';
 import { App } from './app';
+import { ElectricalTestingApp } from './electrical_testing';
 
 const rootElement = document.getElementById('root');
 assert(rootElement);
@@ -11,7 +16,15 @@ const root = createRoot(rootElement);
 
 root.render(
   <React.Fragment>
-    <App enableStringTranslation />
-    <DevDock />
+    {isFeatureFlagEnabled(
+      BooleanEnvironmentVariableName.ENABLE_ELECTRICAL_TESTING_MODE
+    ) ? (
+      <ElectricalTestingApp />
+    ) : (
+      <React.Fragment>
+        <App enableStringTranslation />
+        <DevDock />
+      </React.Fragment>
+    )}
   </React.Fragment>
 );
