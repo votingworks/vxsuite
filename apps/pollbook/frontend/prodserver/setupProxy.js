a1c6eeeafc3138a3db1f024f15c9397a8498cae7
carolinemodic <caroline.modic+github@gmail.com>
2025-02-25 11:43:48 -0800

    get make run directives to actually work (#189)

---
+++ apps/pollbook/frontend/prodserver/setupProxy.js
@@ -0,0 +1,23 @@
+// This file sets up React's proxy in development mode.
+//
+// Currently, non-native Node languages (e.g. typescript) are explicitly not supported:
+// https://facebook.github.io/create-react-app/docs/proxying-api-requests-in-development#configuring-the-proxy-manually
+//
+/* eslint-disable */
+/* istanbul ignore file */
+
+// @ts-check
+
+const { createProxyMiddleware: proxy } = require('http-proxy-middleware');
+
+/**
+ * @param {import('connect').Server} app
+ */
+module.exports = function (app) {
+  app.use(
+    proxy({
+      pathFilter: ['/api', '/dock'],
+      target: 'http://localhost:3004/',
+    })
+  );
+};
