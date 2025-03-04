b1dfd1b14714c2e03921b0d70647f0084dca1715
Kofi <kofi@voting.works>
2025-03-04 14:18:05 -0600

    Update coverage thresholds after rebase

--- apps/design/backend/vitest.config.ts
+++ apps/design/backend/vitest.config.ts
@@ -7,8 +7,8 @@ export default defineConfig({
     clearMocks: true,
     coverage: {
       thresholds: {
-        lines: 86.8,
-        branches: 73.6,
+        lines: 86.5,
+        branches: 73.1,
       },
       exclude: ['src/configure_sentry.ts', '**/*.test.ts'],
     },
