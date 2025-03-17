116c056b8242f8616f472fa5b363c97c9f916bfa
Caroline Mars <caroline@voting.works>
2025-03-17 16:56:04 -0700

    add basic server

---
+++ apps/quickresults/frontend/prodserver/setupProxy.js
@@ -0,0 +1,23 @@
+// This file sets up React's proxy in development mode.
+//
+// Currently, non-native Node languages (e.g. typescript) are explicitly not supported:
+// https://facebook.github.io/create-react-app/docs/proxying-api-requests-in-development#configuring-the-proxy-manually
+//
+/* eslint-disable */
+/* istanbul ignore file - @preserve */
+
+// @ts-check
+
+const { createProxyMiddleware: proxy } = require('http-proxy-middleware');
+
+/**
+ * @param {import('connect').Server} app
+ */
+module.exports = function (app) {
+  app.use(
+    proxy({
+      pathFilter: ['/api'],
+      target: 'https://quickresults-server-staging-39491e83b6be.herokuapp.com',
+    })
+  );
+};
