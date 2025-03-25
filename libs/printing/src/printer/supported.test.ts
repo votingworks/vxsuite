8da799bc0458e2173e02b9e6887c2d6f1d90d6b8
Caroline Mars <caroline@voting.works>
2025-03-25 16:32:43 -0700

    fix supported printer test

--- libs/printing/src/printer/supported.test.ts
+++ libs/printing/src/printer/supported.test.ts
@@ -6,6 +6,6 @@ import { SUPPORTED_PRINTER_CONFIGS, getPpdPath } from './supported';
 test('referenced PPD files exist and are valid', async () => {
   for (const config of SUPPORTED_PRINTER_CONFIGS) {
     const ppdContent = await readFile(getPpdPath(config), 'utf8');
-    expect(ppdContent).toContain('PPD-Adobe: "4.3"');
+    expect(ppdContent).toMatch(/PPD-Adobe:\s*"4\.3"/);
   }
 });
