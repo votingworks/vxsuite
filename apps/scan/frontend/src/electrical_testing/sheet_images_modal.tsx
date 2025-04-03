c13341b60730db4490200276ae17cb04ad33ebbc
Brian Donovan <1938+eventualbuddha@users.noreply.github.com>
2025-04-03 14:17:59 -0700

    feat(scan): add a close button to the latest sheet modal

--- apps/scan/frontend/src/electrical_testing/sheet_images_modal.tsx
+++ apps/scan/frontend/src/electrical_testing/sheet_images_modal.tsx
@@ -1,5 +1,5 @@
 import { SheetOf } from '@votingworks/types';
-import { Modal } from '@votingworks/ui';
+import { Icons, Modal } from '@votingworks/ui';
 import styled from 'styled-components';
 
 export interface Props {
@@ -10,6 +10,28 @@ export interface Props {
 const SheetImage = styled.img`
   max-width: 50%;
 `;
+
+function CloseButton({ onPress }: { onPress: () => void }) {
+  return (
+    // eslint-disable-next-line jsx-a11y/click-events-have-key-events
+    <div
+      role="button"
+      tabIndex={-1}
+      onClick={onPress}
+      style={{
+        position: 'absolute',
+        top: 0,
+        right: 0,
+        background: 'white',
+        borderRadius: '50%',
+        boxShadow: '0 0px 5px 3px rgba(255, 255, 255, 1)',
+        transform: 'translate(25%, -25%)',
+      }}
+    >
+      <Icons.Delete />
+    </div>
+  );
+}
 
 /**
  * Renders a modal showing images for a scanned sheet.
@@ -24,10 +46,12 @@ export function SheetImagesModal({ paths, onClose }: Props): JSX.Element {
             display: 'flex',
             flexDirection: 'row',
             alignItems: 'center',
+            position: 'relative',
           }}
           role="presentation"
           onClick={onClose}
         >
+          <CloseButton onPress={onClose} />
           <SheetImage src={paths[0]} alt="Front Sheet" />
           <SheetImage src={paths[1]} alt="Back Sheet" />
         </div>
