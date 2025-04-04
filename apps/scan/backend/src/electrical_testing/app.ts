24556979d9367dd6bba1c44b9d63aafa7125f5c3
Brian Donovan <1938+eventualbuddha@users.noreply.github.com>
2025-04-03 19:11:28 -0700

    feat(scan): add basic view of latest hardware testing scanned image (#6209)

--- apps/scan/backend/src/electrical_testing/app.ts
+++ apps/scan/backend/src/electrical_testing/app.ts
@@ -1,6 +1,9 @@
 import { createSystemCallApi } from '@votingworks/backend';
+import { iter } from '@votingworks/basics';
 import * as grout from '@votingworks/grout';
+import { SheetOf } from '@votingworks/types';
 import express, { Application } from 'express';
+import { readdir } from 'node:fs/promises';
 import { getMachineConfig } from '../machine_config';
 import { ElectricalTestingComponent } from '../store';
 import { type ServerContext } from './context';
@@ -105,6 +108,37 @@ function buildApi({
       }
     },
 
+    async getLatestScannedSheet(): Promise<SheetOf<string> | null> {
+      const allScannedImageNames = await readdir(workspace.ballotImagesPath);
+      const latestElectricalTestingImageName = iter(allScannedImageNames)
+        .filter((name) => name.startsWith('electrical-testing-'))
+        .max((a, b) => a.localeCompare(b));
+
+      if (!latestElectricalTestingImageName) {
+        return null;
+      }
+
+      const id = latestElectricalTestingImageName.replace(
+        /-(front|back)\.(jpe?g|png)$/,
+        ''
+      );
+      const frontAndBackNames = allScannedImageNames.filter((name) =>
+        name.startsWith(id)
+      );
+      if (frontAndBackNames.length !== 2) {
+        return null;
+      }
+
+      const frontName = frontAndBackNames.find((name) => /-front\./.test(name));
+      const backName = frontAndBackNames.find((name) => /-back\./.test(name));
+
+      if (!frontName || !backName) {
+        return null;
+      }
+
+      return [`/api/images/${frontName}`, `/api/images/${backName}`];
+    },
+
     ...createSystemCallApi({
       usbDrive,
       logger,
@@ -119,6 +153,7 @@ export type ElectricalTestingApi = ReturnType<typeof buildApi>;
 export function buildApp(context: ServerContext): Application {
   const app: Application = express();
   const api = buildApi(context);
+  app.use('/api/images', express.static(context.workspace.ballotImagesPath));
   app.use('/api', grout.buildRouter(api, express));
   return app;
 }
