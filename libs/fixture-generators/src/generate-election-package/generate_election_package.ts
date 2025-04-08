votingworks/vxsuite

commit 8cbc5684617c7c50fdd8ffe78007d79b12e37eeb

add ballot language to Election Info screen (#6069)

libs/fixture-generators/src/generate-election-package/generate_election_package.ts
@@ -16,6 +16,7 @@ import {
   ElectionPackageFileName,
   ElectionPackageMetadata,
   getBallotLanguageConfigs,
+  LanguageCode,
   LATEST_METADATA,
   mergeUiStrings,
 } from '@votingworks/types';
@@ -50,7 +51,9 @@ export async function generateElectionPackage(
     { date: FIXTURES_FILE_DATE }
   );
 
-  const ballotLanguageConfigs = getBallotLanguageConfigs(isMultiLanguage);
+  const ballotLanguageConfigs = getBallotLanguageConfigs(
+    Object.values(LanguageCode)
+  );
   const translator = new GoogleCloudTranslatorWithElectionCache({
     priorElectionPackage,
   });

apps/design/backend/schema.sql
@@ -20,7 +20,8 @@ create table elections (
     constraint fk_background_tasks references background_tasks(id) on delete set null,
   election_package_url text,
   ballot_template_id text not null,
-  ballots_finalized_at timestamptz
+  ballots_finalized_at timestamptz,
+  ballot_language_codes text[]
 );
 
 create table translation_cache (

apps/design/backend/src/app.test.ts
@@ -35,6 +35,7 @@ import {
   ElectionIdSchema,
   DistrictIdSchema,
   ElectionId,
+  getBallotLanguageConfigs,
 } from '@votingworks/types';
 import {
   BooleanEnvironmentVariableName,
@@ -84,7 +85,7 @@ import {
 } from './types';
 import { generateBallotStyles } from './ballot_styles';
 import { ElectionRecord } from '.';
-import { ElectionPackage, getTempBallotLanguageConfigsForCert } from './store';
+import { ElectionPackage } from './store';
 import { renderBallotStyleReadinessReport } from './ballot_style_reports';
 import {
   BALLOT_STYLE_READINESS_REPORT_FILE_NAME,
@@ -195,7 +196,7 @@ test('CRUD elections', async () => {
     precincts: [],
     createdAt: expect.any(String),
     ballotsFinalizedAt: null,
-    ballotLanguageConfigs: getTempBallotLanguageConfigsForCert(nonVxUser.orgId),
+    ballotLanguageConfigs: getBallotLanguageConfigs([LanguageCode.ENGLISH]),
     ballotTemplateId: 'NhBallot',
   });
 
@@ -249,7 +250,7 @@ test('CRUD elections', async () => {
     precincts: expectedPrecincts,
     createdAt: expect.any(String),
     ballotsFinalizedAt: null,
-    ballotLanguageConfigs: getTempBallotLanguageConfigsForCert(nonVxUser.orgId),
+    ballotLanguageConfigs: getBallotLanguageConfigs([LanguageCode.ENGLISH]),
     ballotTemplateId: 'VxDefaultBallot',
   });
 
@@ -299,16 +300,21 @@ test('update election info', async () => {
     seal: '\r\n<svg>updated seal</svg>\r\n',
     type: 'primary',
     date: new DateWithoutTime('2022-01-01'),
+    languageCodes: [LanguageCode.ENGLISH, LanguageCode.SPANISH],
   });
 
   const record = await apiClient.getElection({ user: vxUser, electionId });
-  const { election } = record;
+  const { election, ballotLanguageConfigs } = record;
   expect(election.title).toEqual('Updated Election');
   expect(election.type).toEqual('primary');
   expect(election.state).toEqual('NH');
   expect(election.county.name).toEqual('New Hampshire');
   expect(election.seal).toEqual('\r\n<svg>updated seal</svg>\r\n');
   expect(election.date).toEqual(new DateWithoutTime('2022-01-01'));
+  expect(ballotLanguageConfigs).toEqual([
+    { languages: [LanguageCode.ENGLISH] },
+    { languages: [LanguageCode.SPANISH] },
+  ]);
 
   // empty string values are rejected
   await suppressingConsoleOutput(() =>
@@ -321,6 +327,7 @@ test('update election info', async () => {
         state: '',
         seal: '',
         date: new DateWithoutTime('2022-01-01'),
+        languageCodes: [LanguageCode.ENGLISH],
       })
     ).rejects.toThrow()
   );

apps/design/backend/src/app.ts
@@ -16,6 +16,9 @@ import {
   ElectionIdSchema,
   DateWithoutTimeSchema,
   unsafeParse,
+  LanguageCode,
+  LanguageCodeSchema,
+  getAllBallotLanguages,
 } from '@votingworks/types';
 import express, { Application } from 'express';
 import {
@@ -146,6 +149,7 @@ export interface ElectionInfo {
   state: string;
   jurisdiction: string;
   seal: string;
+  languageCodes: LanguageCode[];
 }
 
 const UpdateElectionInfoInputSchema = z.object({
@@ -156,6 +160,7 @@ const UpdateElectionInfoInputSchema = z.object({
   state: TextInput,
   jurisdiction: TextInput,
   seal: z.string(),
+  languageCodes: z.array(LanguageCodeSchema),
 });
 
 function buildApi({ auth, workspace, translator }: AppContext) {
@@ -282,7 +287,9 @@ function buildApi({ auth, workspace, translator }: AppContext) {
     async getElectionInfo(input: {
       electionId: ElectionId;
     }): Promise<ElectionInfo> {
-      const { election } = await store.getElection(input.electionId);
+      const { election, ballotLanguageConfigs } = await store.getElection(
+        input.electionId
+      );
       return {
         electionId: election.id,
         title: election.title,
@@ -291,12 +298,23 @@ function buildApi({ auth, workspace, translator }: AppContext) {
         state: election.state,
         jurisdiction: election.county.name,
         seal: election.seal,
+        // Not optimal: store.getElection converts from LanguageCode[] to BallotLanguageConfig.
+        // This line converts from BallotLanguageConfig to LanguageCode[]
+        languageCodes: getAllBallotLanguages(ballotLanguageConfigs),
       };
     },
 
     async updateElectionInfo(input: ElectionInfo) {
-      const { electionId, title, date, type, state, jurisdiction, seal } =
-        unsafeParse(UpdateElectionInfoInputSchema, input);
+      const {
+        electionId,
+        title,
+        date,
+        type,
+        state,
+        jurisdiction,
+        seal,
+        languageCodes,
+      } = unsafeParse(UpdateElectionInfoInputSchema, input);
       const { election } = await store.getElection(electionId);
       await store.updateElection(electionId, {
         ...election,
@@ -310,6 +328,7 @@ function buildApi({ auth, workspace, translator }: AppContext) {
         },
         seal,
       });
+      await store.updateBallotLanguageCodes(electionId, languageCodes);
     },
 
     async updateElection(input: {

apps/design/backend/src/store.ts
@@ -9,15 +9,11 @@ import {
   ElectionSerializationFormat,
   BallotLanguageConfigs,
   LanguageCode,
-  getBallotLanguageConfigs,
   safeParseJson,
   ElectionId,
+  BallotLanguageConfig,
 } from '@votingworks/types';
 import { v4 as uuid } from 'uuid';
-import {
-  BooleanEnvironmentVariableName,
-  isFeatureFlagEnabled,
-} from '@votingworks/utils';
 import { BaseLogger } from '@votingworks/logging';
 import { BallotTemplateId } from '@votingworks/hmpb';
 import {
@@ -30,18 +26,6 @@ import {
 import { generateBallotStyles } from './ballot_styles';
 import { Db } from './db/db';
 import { Bindable } from './db/client';
-import { isVxOrSliOrg } from './features';
-
-export function getTempBallotLanguageConfigsForCert(
-  orgId: string
-): BallotLanguageConfigs {
-  const translationsEnabled =
-    isVxOrSliOrg(orgId) &&
-    isFeatureFlagEnabled(
-      BooleanEnvironmentVariableName.ENABLE_CLOUD_TRANSLATION_AND_SPEECH_SYNTHESIS
-    );
-  return getBallotLanguageConfigs(translationsEnabled);
-}
 
 export interface ElectionRecord {
   orgId: string;
@@ -80,6 +64,8 @@ const getBackgroundTasksBaseQuery = `
   from background_tasks
 `;
 
+const DEFAULT_LANGUAGE_CODES = [LanguageCode.ENGLISH];
+
 interface BackgroundTaskRow {
   id: Id;
   taskName: string;
@@ -116,12 +102,14 @@ function hydrateElection(row: {
   systemSettingsData: string;
   ballotOrderInfoData: string;
   createdAt: Date;
-  ballotLanguageConfigs: BallotLanguageConfigs;
   ballotTemplateId: BallotTemplateId;
   ballotsFinalizedAt: Date | null;
   orgId: string;
+  ballotLanguageCodes: LanguageCode[];
 }): ElectionRecord {
-  const { ballotLanguageConfigs } = row;
+  const ballotLanguageConfigs = row.ballotLanguageCodes.map(
+    (l): BallotLanguageConfig => ({ languages: [l] })
+  );
   const rawElection = JSON.parse(row.electionData);
   const precincts: Precinct[] = JSON.parse(row.precinctData);
   const ballotStyles = generateBallotStyles({
@@ -198,7 +186,8 @@ export class Store {
                 precinct_data as "precinctData",
                 ballot_template_id as "ballotTemplateId",
                 ballots_finalized_at as "ballotsFinalizedAt",
-                created_at as "createdAt"
+                created_at as "createdAt",
+                ballot_language_codes as "ballotLanguageCodes"
               from elections
               ${whereClause}
             `,
@@ -214,14 +203,12 @@ export class Store {
             ballotTemplateId: BallotTemplateId;
             ballotsFinalizedAt: Date | null;
             createdAt: Date;
+            ballotLanguageCodes: LanguageCode[];
           }>
       )
     ).map((row) =>
       hydrateElection({
         ...row,
-
-        // TODO: Write/read these to/from the DB based on user selections:
-        ballotLanguageConfigs: getTempBallotLanguageConfigsForCert(row.orgId),
       })
     );
   }
@@ -239,7 +226,8 @@ export class Store {
               precinct_data as "precinctData",
               ballot_template_id as "ballotTemplateId",
               ballots_finalized_at as "ballotsFinalizedAt",
-              created_at as "createdAt"
+              created_at as "createdAt",
+              ballot_language_codes as "ballotLanguageCodes"
             from elections
             where id = $1
           `,
@@ -255,16 +243,12 @@ export class Store {
       ballotTemplateId: BallotTemplateId;
       ballotsFinalizedAt: Date | null;
       createdAt: Date;
+      ballotLanguageCodes: LanguageCode[];
     };
     assert(electionRow !== undefined);
     return hydrateElection({
       id: electionId,
       ...electionRow,
-
-      // TODO: Write/read these to/from the DB based on user selections:
-      ballotLanguageConfigs: getTempBallotLanguageConfigsForCert(
-        electionRow.orgId
-      ),
     });
   }
 
@@ -285,9 +269,10 @@ export class Store {
             system_settings_data,
             ballot_order_info_data,
             precinct_data,
-            ballot_template_id
+            ballot_template_id,
+            ballot_language_codes
           )
-          values ($1, $2, $3, $4, $5, $6, $7)
+          values ($1, $2, $3, $4, $5, $6, $7, string_to_array($8, ','))
         `,
         election.id,
         orgId,
@@ -295,7 +280,8 @@ export class Store {
         JSON.stringify(systemSettings),
         JSON.stringify({}),
         JSON.stringify(precincts),
-        ballotTemplateId
+        ballotTemplateId,
+        DEFAULT_LANGUAGE_CODES.join(',')
       )
     );
   }
@@ -363,6 +349,23 @@ export class Store {
           where id = $2
         `,
         JSON.stringify(precincts),
+        electionId
+      )
+    );
+  }
+
+  async updateBallotLanguageCodes(
+    electionId: ElectionId,
+    ballotLanguageCodes: LanguageCode[]
+  ): Promise<void> {
+    await this.db.withClient((client) =>
+      client.query(
+        `
+          update elections
+          set ballot_language_codes = string_to_array($1, ',')
+          where id = $2
+        `,
+        ballotLanguageCodes.join(','),
         electionId
       )
     );

apps/design/frontend/src/election_info_screen.test.tsx
@@ -1,6 +1,6 @@
 import { afterEach, beforeEach, expect, test } from 'vitest';
 import userEvent from '@testing-library/user-event';
-import { ElectionId } from '@votingworks/types';
+import { ElectionId, LanguageCode } from '@votingworks/types';
 import { Buffer } from 'node:buffer';
 import { createMemoryHistory } from 'history';
 import { DateWithoutTime } from '@votingworks/basics';
@@ -105,9 +105,9 @@ test('edit and save election', async () => {
   const electionRecord = generalElectionRecord(nonVxUser.orgId);
   const { election } = electionRecord;
   const electionId = election.id;
-  apiMock.getUser.expectRepeatedCallsWith().resolves(nonVxUser);
+  apiMock.getUser.expectRepeatedCallsWith().resolves(vxUser);
   apiMock.getElection
-    .expectCallWith({ user: nonVxUser, electionId })
+    .expectCallWith({ user: vxUser, electionId })
     .resolves(electionRecord);
   apiMock.getElectionInfo
     .expectCallWith({ electionId })
@@ -181,6 +181,13 @@ test('edit and save election', async () => {
     )
   );
 
+  const languageSection = screen.getByLabelText('Ballot Languages');
+  const spanishBallotLanguageCheckbox = within(languageSection).getByRole(
+    'checkbox',
+    { name: 'Spanish' }
+  );
+  userEvent.click(spanishBallotLanguageCheckbox);
+
   const updatedElectionInfo: ElectionInfo = {
     electionId,
     title: 'New Title',
@@ -189,9 +196,10 @@ test('edit and save election', async () => {
     state: 'New State',
     jurisdiction: 'New County',
     seal: '<svg>updated seal</svg>',
+    languageCodes: [LanguageCode.ENGLISH, LanguageCode.SPANISH],
   };
   apiMock.updateElectionInfo.expectCallWith(updatedElectionInfo).resolves();
-  apiMock.getElection.expectCallWith({ user: nonVxUser, electionId }).resolves({
+  apiMock.getElection.expectCallWith({ user: vxUser, electionId }).resolves({
     ...electionRecord,
     election: {
       ...electionRecord.election,

apps/design/frontend/src/election_info_screen.tsx
@@ -1,7 +1,12 @@
 import React, { useState } from 'react';
-import { ElectionIdSchema, unsafeParse } from '@votingworks/types';
+import {
+  ElectionIdSchema,
+  LanguageCode,
+  unsafeParse,
+} from '@votingworks/types';
 import {
   Button,
+  CheckboxGroup,
   H1,
   MainContent,
   MainHeader,
@@ -82,7 +87,8 @@ function ElectionInfoForm({
     return () => {
       setElectionInfo((prev = savedElectionInfo) => ({
         ...prev,
-        [field]: prev[field]?.trim(),
+        [field]:
+          typeof prev[field] === 'string' ? prev[field]?.trim() : prev[field],
       }));
     };
   }
@@ -191,6 +197,31 @@ function ElectionInfoForm({
           />
         </div>
       </div>
+      {features.BALLOT_LANGUAGE_CONFIG && (
+        <div style={{ width: '18rem' }}>
+          <CheckboxGroup
+            disabled={!isEditing}
+            label="Ballot Languages"
+            value={electionInfo.languageCodes}
+            onChange={(value) => {
+              const languageCodes = value.map((v) => v as LanguageCode);
+              setElectionInfo({ ...electionInfo, languageCodes });
+            }}
+            options={[
+              { label: 'English', value: LanguageCode.ENGLISH },
+              { label: 'Spanish', value: LanguageCode.SPANISH },
+              {
+                label: 'Chinese (Simplified)',
+                value: LanguageCode.CHINESE_SIMPLIFIED,
+              },
+              {
+                label: 'Chinese (Traditional)',
+                value: LanguageCode.CHINESE_TRADITIONAL,
+              },
+            ]}
+          />
+        </div>
+      )}
 
       {isEditing ? (
         <FormActionsRow>

apps/design/frontend/src/features_context.tsx
@@ -65,6 +65,10 @@ enum UserFeature {
    * precinct splits.
    */
   CREATE_DELETE_PRECINCT_SPLITS = 'CREATE_DELETE_PRECINCT_SPLITS',
+  /**
+   * Allow the user to select ballot languages.
+   */
+  BALLOT_LANGUAGE_CONFIG = 'BALLOT_LANGUAGE_CONFIG',
 }
 
 /**
@@ -114,6 +118,7 @@ export const userFeatureConfigs = {
     CREATE_DELETE_DISTRICTS: true,
     CREATE_DELETE_PRECINCTS: true,
     CREATE_DELETE_PRECINCT_SPLITS: true,
+    BALLOT_LANGUAGE_CONFIG: true,
   },
   sli: {
     ACCESS_ALL_ORGS: false,
@@ -128,6 +133,7 @@ export const userFeatureConfigs = {
     CREATE_DELETE_DISTRICTS: true,
     CREATE_DELETE_PRECINCTS: true,
     CREATE_DELETE_PRECINCT_SPLITS: true,
+    BALLOT_LANGUAGE_CONFIG: true,
   },
   nh: {
     ACCESS_ALL_ORGS: false,
@@ -144,6 +150,7 @@ export const userFeatureConfigs = {
     CREATE_DELETE_DISTRICTS: false,
     CREATE_DELETE_PRECINCTS: false,
     CREATE_DELETE_PRECINCT_SPLITS: false,
+    BALLOT_LANGUAGE_CONFIG: false,
   },
 } satisfies Record<string, UserFeaturesConfig>;
 

commit d7eca7f186926f4e109e4e1fc5abc6b9b7fac975

libs/fixture-generators: Fix bug when picking languages to translate
Regression from https://github.com/votingworks/vxsuite/pull/6069 that
caused all election fixtures to be generated with multiple languages
instead of respecting the `isMultiLanguage` flag.

libs/fixture-generators/src/generate-election-package/generate_election_package.ts
@@ -53,7 +53,7 @@ export async function generateElectionPackage(
   );
 
   const ballotLanguageConfigs = getBallotLanguageConfigs(
-    Object.values(LanguageCode)
+    isMultiLanguage ? Object.values(LanguageCode) : [LanguageCode.ENGLISH]
   );
   const translator = new GoogleCloudTranslatorWithElectionCache({
     priorElectionPackage,