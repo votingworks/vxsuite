ad80abb696aba0269824f8add2659295f46d5c3a
Kofi <kofi@voting.works>
2025-04-10 15:12:10 -0500

    [VxDesign] Resume grayscale PDF conversion for non-tinted NH ballots (#6271)

---
+++ apps/design/backend/src/worker/ballot_pdfs.test.ts
@@ -0,0 +1,66 @@
+import { expect, Mocked, test, vi } from 'vitest';
+import { Buffer } from 'node:buffer';
+import { createReadStream, ReadStream } from 'node:fs';
+
+import {
+  BaseBallotProps,
+  ColorTints,
+  NhBallotProps,
+  RenderDocument,
+} from '@votingworks/hmpb';
+
+import { renderBallotPdf } from './ballot_pdfs';
+import { convertPdfToGrayscale } from './grayscale';
+
+vi.mock('./grayscale.js');
+
+test('renderBallotPdf - converts non-tinted ballots to grayscale', async () => {
+  const nhProps: NhBallotProps = {
+    colorTint: undefined,
+    precinctId: 'nh-precinct',
+  } as unknown as NhBallotProps;
+
+  const mockColorPdf = Buffer.of(0xca, 0xfe);
+  const mockDocument: Mocked<RenderDocument> = {
+    renderToPdf: vi.fn(() => mockColorPdf),
+  } as unknown as Mocked<RenderDocument>;
+
+  const mockGrayscalePdfNh: ReadStream = createReadStream('/dev/null');
+  vi.mocked(convertPdfToGrayscale).mockResolvedValueOnce(mockGrayscalePdfNh);
+
+  expect(await renderBallotPdf(nhProps, mockDocument)).toStrictEqual(
+    mockGrayscalePdfNh
+  );
+  expect(convertPdfToGrayscale).toHaveBeenCalledWith(mockColorPdf);
+
+  const nonNhProps: BaseBallotProps = {
+    precinctId: 'non-nh-precinct',
+  } as unknown as BaseBallotProps;
+
+  const mockGrayscalePdfNonNh: ReadStream = createReadStream('/dev/null');
+  vi.mocked(convertPdfToGrayscale).mockResolvedValueOnce(mockGrayscalePdfNonNh);
+
+  expect(await renderBallotPdf(nonNhProps, mockDocument)).toStrictEqual(
+    mockGrayscalePdfNonNh
+  );
+});
+
+test('renderBallotPdf - renders tinted NH ballots in color', async () => {
+  const nhProps: NhBallotProps = {
+    colorTint: ColorTints.YELLOW,
+    precinctId: 'nh-precinct',
+  } as unknown as NhBallotProps;
+
+  const mockColorPdf = Buffer.of(0xca, 0xfe);
+  const mockDocument: Mocked<RenderDocument> = {
+    renderToPdf: vi.fn(() => mockColorPdf),
+  } as unknown as Mocked<RenderDocument>;
+
+  vi.mocked(convertPdfToGrayscale).mockRejectedValue(
+    new Error('unexpected grayscale conversion')
+  );
+
+  expect(await renderBallotPdf(nhProps, mockDocument)).toStrictEqual(
+    mockColorPdf
+  );
+});
