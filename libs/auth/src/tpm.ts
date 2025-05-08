f46db70bcea22d12b573cd201418884b58af71c8
Arsalan Sufi <arsalan@voting.works>
2025-05-08 17:47:42 -0400

    Remove no longer necessary keys.ts types

---
+++ libs/auth/src/tpm.ts
@@ -0,0 +1,9 @@
+/**
+ * The ID of the TPM signing key, distinct from the TPM primary key
+ */
+export const TPM_KEY_ID = '0x81000001';
+
+/**
+ * The name of the OpenSSL provider that we're using to interface with the TPM
+ */
+export const TPM_OPENSSL_PROVIDER = 'tpm2';
