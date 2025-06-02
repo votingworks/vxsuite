417fb89a49c19a00073ce5d8d93a5abd585c8d1c
Kevin Shen <kevin@voting.works>
2025-06-02 12:31:49 -0700

    invoke socket server and connect from frontend

--- apps/pollbook/backend/src/types.ts
+++ apps/pollbook/backend/src/types.ts
@@ -418,3 +418,22 @@ export type ConfigurationStatus =
   | 'network-configuration-error'
   | 'recently-unconfigured'
   | 'network-conflicting-pollbook-packages-match-card';
+
+export interface AamvaDocument {
+  issuingJurisdiction: string;
+  firstName: string;
+  middleName: string;
+  lastName: string;
+  nameSuffix: string;
+}
+
+export const AamvaDocumentSchema: z.ZodSchema<AamvaDocument> = z
+  .object({
+    issuingJurisdiction: z.string(),
+
+    firstName: z.string(),
+    middleName: z.string(),
+    lastName: z.string(),
+    nameSuffix: z.string(),
+  })
+  .strict() as z.ZodSchema<AamvaDocument>;
