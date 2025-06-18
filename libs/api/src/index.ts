f151a0ce26dd65f916d65a1f417cc11436b29f29
Brian Donovan <1938+eventualbuddha@users.noreply.github.com>
2025-06-18 12:33:30 -0700

    chore: remove `api` package

--- libs/api/src/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './base';
-export * as PrintScan from './services/print-scan';
--- libs/api/vitest.config.ts
+++
@@ -1,3 +0,0 @@
-import { defineConfig } from '../../vitest.config.shared.mjs';
-
-export default defineConfig();
