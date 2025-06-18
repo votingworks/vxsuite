242cfec9cd297dd6140b4478c3415d1fec625f52
Brian Donovan <1938+eventualbuddha@users.noreply.github.com>
2025-06-18 15:26:13 -0700

    test(backend): update string counts

--- libs/backend/src/language_and_audio/election_package_string.test.ts
+++ libs/backend/src/language_and_audio/election_package_string.test.ts
@@ -41,7 +41,7 @@ describe('getAllStringsForElectionPackage', () => {
       LanguageCode.SPANISH,
     ]);
     assert(appStrings[LanguageCode.ENGLISH]);
-    expect(Object.keys(appStrings[LanguageCode.ENGLISH])).toHaveLength(438);
+    expect(Object.keys(appStrings[LanguageCode.ENGLISH])).toHaveLength(440);
 
     expect(hmpbStrings).toBeDefined();
     expect(Object.keys(hmpbStrings)).toEqual([
