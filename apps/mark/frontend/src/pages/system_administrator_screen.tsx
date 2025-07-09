91d77a705a4af3e84250e4d039031224d361dd79
Kofi <264902+kofi-q@users.noreply.github.com>
2025-07-09 09:43:16 -0500

    [Mark] Update Sys Admin screen to match MarkScan (#6734)

--- apps/mark/frontend/src/pages/system_administrator_screen.tsx
+++ apps/mark/frontend/src/pages/system_administrator_screen.tsx
@@ -1,13 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import {
+  Button,
+  ElectionInfoBar,
   H3,
   Main,
   Screen,
+  SignedHashValidationButton,
   SystemAdministratorScreenContents,
 } from '@votingworks/ui';
 import { UsbDriveStatus } from '@votingworks/usb-drive';
-import { logOut } from '../api';
+import type { MachineConfig } from '@votingworks/mark-backend';
+import { ElectionDefinition, PrecinctSelection } from '@votingworks/types';
+import { logOut, useApiClient } from '../api';
+import { DiagnosticsScreen } from './diagnostics_screen';
 
 const resetPollsToPausedText =
   'The polls are closed and voting is complete. After resetting the polls to paused, it will be possible to re-open the polls and resume voting. The printed ballots count will be preserved.';
@@ -17,6 +23,10 @@ interface Props {
   isMachineConfigured: boolean;
   resetPollsToPaused?: () => Promise<void>;
   usbDriveStatus: UsbDriveStatus;
+  electionDefinition?: ElectionDefinition;
+  electionPackageHash?: string;
+  machineConfig: MachineConfig;
+  precinctSelection?: PrecinctSelection;
 }
 
 /**
@@ -27,19 +37,42 @@ export function SystemAdministratorScreen({
   isMachineConfigured,
   resetPollsToPaused,
   usbDriveStatus,
+  electionDefinition,
+  electionPackageHash,
+  machineConfig,
+  precinctSelection,
 }: Props): JSX.Element {
+  const apiClient = useApiClient();
   const logOutMutation = logOut.useMutation();
+  const [isDiagnosticsScreenOpen, setIsDiagnosticsScreenOpen] = useState(false);
+
+  if (isDiagnosticsScreenOpen) {
+    return (
+      <DiagnosticsScreen
+        onBackButtonPress={() => setIsDiagnosticsScreenOpen(false)}
+      />
+    );
+  }
+
   return (
     <Screen>
       <Main padded>
         <H3 as="h1">System Administrator Menu</H3>
         <SystemAdministratorScreenContents
+          additionalButtons={
+            <React.Fragment>
+              <Button onPress={() => setIsDiagnosticsScreenOpen(true)}>
+                Diagnostics
+              </Button>
+              <SignedHashValidationButton apiClient={apiClient} />
+            </React.Fragment>
+          }
           displayRemoveCardToLeavePrompt
           resetPollsToPausedText={resetPollsToPausedText}
           resetPollsToPaused={resetPollsToPaused}
           primaryText={
             <React.Fragment>
-              To adjust settings for the current election,› please insert an
+              To adjust settings for the current election, please insert an
               election manager or poll worker card.
             </React.Fragment>
           }
@@ -49,6 +82,14 @@ export function SystemAdministratorScreen({
           logOut={() => logOutMutation.mutate()}
         />
       </Main>
+      <ElectionInfoBar
+        mode="admin"
+        electionDefinition={electionDefinition}
+        electionPackageHash={electionPackageHash}
+        codeVersion={machineConfig.codeVersion}
+        machineId={machineConfig.machineId}
+        precinctSelection={precinctSelection}
+      />
     </Screen>
   );
 }
