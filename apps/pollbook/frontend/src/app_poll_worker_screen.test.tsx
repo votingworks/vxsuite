caaf22382dcb84b6c62f60c8a180ae10328c0b5b
Kevin Shen <kevin@voting.works>
2025-07-16 13:59:29 -0700

    simplify primary election check in happy path test

--- apps/pollbook/frontend/src/app_poll_worker_screen.test.tsx
+++ apps/pollbook/frontend/src/app_poll_worker_screen.test.tsx
@@ -142,100 +142,78 @@ describe('PollWorkerScreen', () => {
     unmount();
   });
 
-  test.skip('primary election e2e flow with party selection', async () => {
+  test('primary election happy path e2e flow with party selection', async () => {
     apiMock.expectGetDeviceStatuses();
-    apiMock.authenticateAsPollWorker(multiPartyPrimaryElection);
-    apiMock.setElection(multiPartyPrimaryElectionDefinition);
-    const { unmount } = render(<App apiClient={apiMock.mockApiClient} />);
-    await screen.findByText('Connect printer to continue.');
-
     apiMock.setPrinterStatus(true);
     apiMock.setIsAbsenteeMode(false);
-    await screen.findByText('No Precinct Selected');
-
+    apiMock.authenticateAsPollWorker(multiPartyPrimaryElection);
     apiMock.setElection(
       multiPartyPrimaryElectionDefinition,
       multiPartyPrimaryElection.precincts[0].id
     );
-
-    apiMock.expectGetCheckInCounts({ allMachines: 25, thisMachine: 5 });
-    await vi.waitFor(() => {
-      screen.getByText('Check-In');
-    });
     apiMock.expectGetScannedIdDocument();
     apiMock.expectSearchVotersNull({});
+    apiMock.expectGetCheckInCounts({ allMachines: 25, thisMachine: 5 });
 
-    await screen.findByText('Total Check-ins');
-    const total = screen.getByTestId('total-check-ins');
-    within(total).getByText('25');
-    const machine = screen.getByTestId('machine-check-ins');
-    within(machine).getByText('5');
-
-    apiMock.expectSearchVotersTooMany({ firstName: '', lastName: 'SM' }, 153);
-    const lastNameInput = screen.getByLabelText('Last Name');
-    userEvent.type(lastNameInput, 'SM');
-    act(() => {
-      vi.advanceTimersByTime(DEFAULT_QUERY_REFETCH_INTERVAL);
-    });
-    await screen.findByText(
-      'Voters matched: 153. Refine your search further to view results.'
-    );
-
+    const { unmount } = render(<App apiClient={apiMock.mockApiClient} />);
     const voter = createMockVoter(
       '123',
       'Abigail',
       'Adams',
       primaryPrecinct.id
     );
-    const voterWrongPrecinct = createMockVoter(
-      '124',
-      'Abigail',
-      'Addams',
-      multiPartyPrimaryElection.precincts[1].id
-    );
+
+    await vi.waitFor(() => {
+      screen.getByRole('heading', { name: 'Voter Check-In' });
+    });
 
     apiMock.expectSearchVotersWithResults(
       { firstName: 'ABI', lastName: 'AD' },
-      [voter, voterWrongPrecinct]
+      [voter]
     );
-    userEvent.clear(lastNameInput);
+
+    const lastNameInput = screen.getByLabelText('Last Name');
     userEvent.type(lastNameInput, 'AD');
     const firstNameInput = screen.getByLabelText('First Name');
     userEvent.type(firstNameInput, 'ABI');
+
     act(() => {
       vi.advanceTimersByTime(DEFAULT_QUERY_REFETCH_INTERVAL);
     });
+
     const firstRow = await screen.findByTestId('voter-row#123');
     within(firstRow).getByText(/Adams, Abigail/i);
     within(firstRow).getByText(
       new RegExp(multiPartyPrimaryElection.precincts[0].name, 'i')
     );
     const checkInButton = screen.getByTestId('check-in-button#123');
-    const checkInButtonWrongPrecinct = screen.getByTestId(
-      'check-in-button#124'
-    );
     within(checkInButton).getByText('Start Check-In');
-    within(checkInButtonWrongPrecinct).getByText('View Details');
 
+    // Start check-in flow for voter
     apiMock.expectGetVoter(voter);
     userEvent.click(checkInButton);
     await screen.findByRole('heading', { name: 'Confirm Voter Identity' });
 
+    // Confirm Identity button should advance to Select Party screen
     const confirmIdentityButton = screen.getButton('Confirm Identity');
     userEvent.click(confirmIdentityButton);
 
     await screen.findByRole('heading', { name: 'Select Party' });
 
-    // Test back button
+    // Back button should return to Confirm Identity screen
     userEvent.click(screen.getButton('Back'));
     await screen.findByRole('heading', { name: 'Confirm Voter Identity' });
-    userEvent.click(screen.getButton('Confirm Identity'));
 
+    // Go forward again to Select Party screen
+    userEvent.click(screen.getButton('Confirm Identity'));
     const confirmCheckInButton = screen.getButton('Confirm Check-In');
+
+    // Can't confirm check-in without party selection
     expect(confirmCheckInButton).toBeDisabled();
     userEvent.click(screen.getButton('Democratic'));
     expect(confirmCheckInButton).not.toBeDisabled();
 
+    // Can check in once a party is selected
     apiMock.expectCheckInVoter(voter, 'DEM');
     apiMock.expectGetVoter(voter);
     userEvent.click(confirmCheckInButton);
@@ -281,7 +259,9 @@ describe('PollWorkerScreen', () => {
     act(() => {
       vi.advanceTimersByTime(DEFAULT_QUERY_REFETCH_INTERVAL);
     });
-    const firstRow = await screen.findByTestId('voter-row#123');
+    const firstRow = await vi.waitFor(() =>
+      screen.getByTestId('voter-row#123')
+    );
     within(firstRow).getByText(/Adams, Abigail/i);
 
     expect(
