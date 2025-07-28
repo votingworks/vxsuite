9a4fd4133aa0e3cbf92efdb9b046c80156a43c3a
Jonah Kagan <jonah@voting.works>
2025-07-28 10:36:45 -0700

    VxDesign: Migration to remove compact template IDs

---
+++ apps/design/backend/migrations/1753724000020_remove-compact-template-ids.js
@@ -0,0 +1,21 @@
+/**
+ * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
+ */
+exports.shorthands = undefined;
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+exports.up = (pgm) => {
+  // Replace the compact ballot template IDs with the standard template IDs for
+  // those templates. This should have been done in the add-ballot-compact
+  // migration, but was forgotten.
+  pgm.sql(
+    `UPDATE elections SET ballot_template_id = 'NhBallot' WHERE ballot_template_id = 'NhBallotCompact'`
+  );
+  pgm.sql(
+    `UPDATE elections SET ballot_template_id = 'NhBallotV3' WHERE ballot_template_id = 'NhBallotV3Compact'`
+  );
+};
