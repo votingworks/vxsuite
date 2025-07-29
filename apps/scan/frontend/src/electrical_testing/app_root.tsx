29ae0b2769282d3a26ffda0a90a48ce01f78dcb5
Brian Donovan <1938+eventualbuddha@users.noreply.github.com>
2025-07-29 14:18:27 -0700

    feat(hwta-scan): switch to `desktop` size mode (#6871)
    * refactor(hwta-scan): inline `ElectricalTestingScreen` here
    
    I'm going to heavily modify it here, but not in VxMarkScan, so I'm
    leaving the shared one alone.
    
    * fix(hwta-scan): switch to `desktop` size mode
    
    This lets us get rid of the janky `scale` styles and small font sizes.

--- apps/scan/frontend/src/electrical_testing/app_root.tsx
+++ apps/scan/frontend/src/electrical_testing/app_root.tsx
@@ -1,9 +1,16 @@
 import {
   Button,
   Caption,
-  ElectricalTestingScreen,
+  Card,
+  ExportLogsModal,
+  H6,
   Icons,
+  Main,
+  Screen,
+  Task,
 } from '@votingworks/ui';
+import { iter } from '@votingworks/basics';
+import type { UsbDriveStatus } from '@votingworks/usb-drive';
 import { DateTime } from 'luxon';
 import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
@@ -18,14 +25,18 @@ function CounterButton() {
   const [count, setCount] = useState(0);
 
   return (
-    <Button
-      onPress={() => setCount((prev) => prev + 1)}
-      style={{ transform: 'scale(0.5)' }}
-    >
+    <Button onPress={() => setCount((prev) => prev + 1)}>
       Tap Count: {count}
     </Button>
   );
 }
+
+const Row = styled.div<{ gap?: string }>`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  gap: ${({ gap = 0 }) => gap};
+`;
 
 const Column = styled.div<{ gap?: string }>`
   display: flex;
@@ -35,11 +46,170 @@ const Column = styled.div<{ gap?: string }>`
 `;
 
 const Small = styled.span`
-  font-size: 0.45rem;
+  font-size: 0.9rem;
 `;
+
+const ExtraSmall = styled.span`
+  font-size: 0.6rem;
+`;
+
+const PlayPauseButtonBase = styled.button`
+  flex-shrink: 0;
+  background-color: #ddd;
+  width: 3rem;
+  height: 3rem;
+  border-radius: 50%;
+  border: none;
+  cursor: pointer;
+  font-size: 2rem;
+  padding: 0;
+`;
+
+function PlayPauseButton({
+  isRunning,
+  onPress,
+}: {
+  isRunning: boolean;
+  onPress: () => void;
+}) {
+  return (
+    <PlayPauseButtonBase onClick={onPress}>
+      {isRunning ? <Icons.Pause /> : <Icons.Play />}
+    </PlayPauseButtonBase>
+  );
+}
 
 function formatTimestamp(timestamp: DateTime): string {
   return timestamp.toLocal().toFormat('h:mm:ss a MM/dd/yyyy');
+}
+
+function StatusCard({
+  title,
+  statusMessage,
+  body,
+  updatedAt,
+  isRunning,
+  onToggleRunning,
+}: {
+  title: React.ReactNode;
+  statusMessage?: React.ReactNode;
+  body?: React.ReactNode;
+  updatedAt?: DateTime;
+  isRunning?: boolean;
+  onToggleRunning?: () => void;
+}) {
+  return (
+    <Card
+      style={{
+        width: '600px',
+        height: '235px',
+        overflow: 'hidden',
+        position: 'relative',
+      }}
+    >
+      <Row style={{ height: '100%', alignItems: 'stretch' }}>
+        <Column style={{ flexGrow: 1 }}>
+          <H6 style={{ flexGrow: 0 }}>{title}</H6>
+          {statusMessage && (
+            <Caption
+              style={{
+                flexGrow: 1,
+                overflowWrap: 'anywhere',
+                maxHeight: '2rem',
+                overflow: 'hidden',
+              }}
+            >
+              <Small>{statusMessage}</Small>
+            </Caption>
+          )}
+          {body && <Caption style={{ flexGrow: 1 }}>{body}</Caption>}
+          {updatedAt && (
+            <Caption style={{ flexGrow: 0 }}>
+              <ExtraSmall>{formatTimestamp(updatedAt)}</ExtraSmall>
+            </Caption>
+          )}
+        </Column>
+        {typeof isRunning === 'boolean' && onToggleRunning && (
+          <Column style={{ flexGrow: 0, alignContent: 'center' }}>
+            <PlayPauseButton isRunning={isRunning} onPress={onToggleRunning} />
+          </Column>
+        )}
+      </Row>
+    </Card>
+  );
+}
+
+function ElectricalTestingScreen<Id extends React.Key>({
+  tasks,
+  perRow,
+  modals,
+  powerDown,
+  usbDriveStatus,
+}: {
+  tasks: ReadonlyArray<Task<Id>>;
+  perRow: number;
+  modals?: React.ReactNode;
+  powerDown: () => void;
+  usbDriveStatus?: UsbDriveStatus;
+}): JSX.Element {
+  const [isSaveLogsModalOpen, setIsSaveLogsModalOpen] = React.useState(false);
+
+  return (
+    <Screen>
+      <Main centerChild>
+        <Column style={{ height: '100%' }}>
+          <Column
+            style={{
+              alignItems: 'center',
+              gap: '1rem',
+              justifyContent: 'center',
+            }}
+          >
+            {iter(tasks)
+              .chunks(perRow)
+              .map((tt) => (
+                <Row gap="1rem" key={tt.map((t) => t.id).join('-')}>
+                  {tt.map((t) => (
+                    <StatusCard
+                      key={t.id}
+                      title={
+                        <React.Fragment>
+                          {t.icon} {t.title}
+                        </React.Fragment>
+                      }
+                      body={t.body}
+                      statusMessage={t.statusMessage}
+                      updatedAt={t.updatedAt}
+                      isRunning={t.isRunning}
+                      onToggleRunning={t.toggleIsRunning}
+                    />
+                  ))}
+                </Row>
+              ))
+              .toArray()}
+          </Column>
+          <Row style={{ justifyContent: 'center' }}>
+            <Button
+              icon={<Icons.Save />}
+              onPress={() => setIsSaveLogsModalOpen(true)}
+            >
+              Save Logs
+            </Button>
+            <Button icon={<Icons.PowerOff />} onPress={powerDown}>
+              Power Down
+            </Button>
+          </Row>
+        </Column>
+        {isSaveLogsModalOpen && usbDriveStatus && (
+          <ExportLogsModal
+            onClose={() => setIsSaveLogsModalOpen(false)}
+            usbDriveStatus={usbDriveStatus}
+          />
+        )}
+        {modals}
+      </Main>
+    </Screen>
+  );
 }
 
 export function AppRoot(): JSX.Element {
@@ -162,7 +332,7 @@ export function AppRoot(): JSX.Element {
                     position: 'absolute',
                     right: '0',
                     bottom: '0',
-                    transform: 'scale(0.3) translate(100%, 100%)',
+                    transform: 'translate(100%, 100%)',
                   }}
                 >
                   View Latest Sheet
