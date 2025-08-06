80e5b7daab6a009449c166548146e4351851753a
Jonah Kagan <jonah@voting.works>
2025-08-06 11:46:38 -0700

    Candidates and votes allowed

--- apps/design/backend/scripts/convert_la_election.ts.ts
+++ apps/design/backend/scripts/convert_la_election.ts.ts
@@ -1,15 +1,21 @@
 import {
-  AnyContest,
+  CandidateContest,
   District,
   Election,
   ElectionId,
   hasSplits,
   HmpbBallotPaperSize,
   Precinct,
+  safeParseInt,
 } from '@votingworks/types';
 import { parse } from 'csv-parse/sync';
 import { readdirSync, readFileSync } from 'node:fs';
-import { DateWithoutTime, find } from '@votingworks/basics';
+import {
+  assert,
+  assertDefined,
+  DateWithoutTime,
+  find,
+} from '@votingworks/basics';
 import { join } from 'node:path';
 import { generateId } from '../src/utils';
 
@@ -38,39 +44,65 @@ export function createElectionSkeleton(id: ElectionId): Election {
   };
 }
 
-const knownAbbreviations = ['ISD', 'ESA', 'ESD'];
-
-function toTitleCase(str: string): string {
-  return str
-    .split(' ')
-    .map((word) => {
-      if (knownAbbreviations.includes(word)) {
-        return word;
-      }
-      return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
-    })
-    .join(' ');
-}
-
 function trimLeadingZeros(str: string): string {
   return str.replace(/^0+/, '');
 }
 
+function getContestTitle(row: {
+  contestTitle1: string;
+  contestTitle2: string;
+}): string {
+  return `${row.contestTitle1} ${row.contestTitle2}`;
+}
+
+function districtNameFromContestTitle(contestTitle: string): string {
+  return `District for Contest: ${contestTitle}`;
+}
+
+const OFFICE_FILE_COLUMNS = [
+  'officeName',
+  'votesAllowed',
+  'unknown',
+  'isSpecialElection',
+  'blank',
+  'contestTitle1',
+  'contestTitle2',
+  'officeNumber',
+] as const;
+type OfficeFileRow = Record<(typeof OFFICE_FILE_COLUMNS)[number], string>;
+
+const CANDIDATE_FILE_COLUMNS = [
+  'lastName',
+  'firstName',
+  'candidateNumber',
+  'party',
+  'race',
+  'ballotOrderNumber',
+  'contestTitle1',
+  'contestTitle2',
+  'candidateNumberWithLeadingZeros',
+] as const;
+type CandidateFileRow = Record<(typeof CANDIDATE_FILE_COLUMNS)[number], string>;
+
+const PRECINCT_FILE_COLUMNS = [
+  'wardNumber',
+  'precinctNumber',
+  'precinctSplitLetter',
+  'wardAndPrecinctNumber',
+  'ballotStyleNumber',
+  'contestTitle1',
+  'contestTitle2',
+  'parishWardAndPrecinctNumber',
+] as const;
+type PrecinctFileRow = Record<(typeof PRECINCT_FILE_COLUMNS)[number], string>;
+
 function convertToElection(
-  precinctFileContents: string,
-  candidateFileContents: string
+  officeFileContents: string,
+  candidateFileContents: string,
+  precinctFileContents: string
 ): Election {
-  const precinctFileRows = parse(precinctFileContents, {
-    columns: [
-      'wardNumber',
-      'precinctNumber',
-      'precinctSplitLetter',
-      'wardAndPrecinctNumber',
-      'ballotStyleNumber',
-      'contestTitle1',
-      'contestTitle2',
-      'parishWardAndPrecinctNumber',
-    ],
+  const officeFileRows: OfficeFileRow[] = parse(officeFileContents, {
+    columns: [...OFFICE_FILE_COLUMNS],
     skipEmptyLines: true,
     delimiter: ';',
     ignore_last_delimiters: true,
@@ -78,9 +110,72 @@ function convertToElection(
     trim: true,
   });
 
+  const votesAllowedByContestTitle = new Map<string, number>();
+  for (const row of officeFileRows) {
+    const contestTitle = getContestTitle(row);
+    const votesAllowed = safeParseInt(row.votesAllowed).unsafeUnwrap();
+    votesAllowedByContestTitle.set(contestTitle, votesAllowed);
+  }
+
+  const candidateFileRows: CandidateFileRow[] = parse(candidateFileContents, {
+    columns: [...CANDIDATE_FILE_COLUMNS],
+    skipEmptyLines: true,
+    delimiter: ';',
+    ignore_last_delimiters: true,
+    relaxQuotes: true,
+    trim: true,
+  });
+
+  const candidateContests = new Map<string, CandidateContest>();
   const districts = new Map<string, District>();
+
+  for (const row of candidateFileRows) {
+    // Create a district for each contest
+    // TODO can we do better and merge these later?
+    const contestTitle = getContestTitle(row);
+    const districtName = districtNameFromContestTitle(contestTitle);
+    const district = districts.get(districtName) ?? {
+      id: generateId(),
+      name: districtName,
+    };
+    districts.set(districtName, district);
+    const contest = candidateContests.get(contestTitle) ?? {
+      id: generateId(),
+      title: contestTitle,
+      type: 'candidate',
+      candidates: [],
+      districtId: district.id,
+      seats: assertDefined(
+        votesAllowedByContestTitle.get(contestTitle),
+        `Votes allowed not found for contest: ${contestTitle}`
+      ),
+      allowWriteIns: false,
+    };
+    candidateContests.set(contestTitle, {
+      ...contest,
+      candidates: [
+        ...contest.candidates,
+        {
+          id: generateId(),
+          lastName: row.lastName,
+          firstName: row.firstName,
+          name: `${row.firstName} ${row.lastName}`,
+          // TODO partyId
+        },
+      ],
+    });
+  }
+
+  const precinctFileRows: PrecinctFileRow[] = parse(precinctFileContents, {
+    columns: [...PRECINCT_FILE_COLUMNS],
+    skipEmptyLines: true,
+    delimiter: ';',
+    ignore_last_delimiters: true,
+    relaxQuotes: true,
+    trim: true,
+  });
+
   const precincts = new Map<string, Precinct>();
-  const contests = new Map<string, AnyContest>();
 
   // Each row represents the assignment of a single contest to a precinct or split.
   for (const row of precinctFileRows) {
@@ -88,26 +183,13 @@ function convertToElection(
       row.wardNumber
     )}, Precinct ${trimLeadingZeros(row.precinctNumber)}`;
 
-    // Create a district for each contest
-    // TODO can we do better and merge these later?
-    const contestTitle = `${row.contestTitle1} ${row.contestTitle2}`;
-    const districtName = `District for Contest: ${contestTitle}`;
-    const district = districts.get(districtName) ?? {
-      id: generateId(),
-      name: districtName,
-    };
-    districts.set(districtName, district);
-
-    const contest: AnyContest = contests.get(contestTitle) ?? {
-      id: generateId(),
-      title: contestTitle,
-      type: 'candidate',
-      candidates: [],
-      districtId: district.id,
-      seats: 1,
-      allowWriteIns: false,
-    };
-    contests.set(contestTitle, contest);
+    const district = districts.get(
+      districtNameFromContestTitle(getContestTitle(row))
+    );
+    assert(
+      district,
+      `District not found for contest in precinct file: ${getContestTitle(row)}`
+    );
 
     const existingPrecinct = precincts.get(precinctName);
     if (existingPrecinct) {
@@ -163,7 +245,7 @@ function convertToElection(
 
   const electionPrecincts = Array.from(precincts.values());
   const electionDistricts = Array.from(districts.values());
-  const electionContests = Array.from(contests.values());
+  const electionContests = Array.from(candidateContests.values());
   return {
     ...createElectionSkeleton(generateId()),
     precincts: electionPrecincts,
@@ -193,11 +275,11 @@ function main(args: readonly string[]): void {
     (fileName) => fileName.endsWith('.txt')
   );
 
-  const precinctFileName = find(electionFileNames, (fileName) =>
-    fileName.includes('PCT-DR')
+  const officeFileName = find(electionFileNames, (fileName) =>
+    fileName.includes('MO-OR')
   );
-  const precinctFileContents = readFileSync(
-    join(electionDataDirectory, precinctFileName),
+  const officeFileContents = readFileSync(
+    join(electionDataDirectory, officeFileName),
     'utf-8'
   );
 
@@ -209,9 +291,18 @@ function main(args: readonly string[]): void {
     'utf-8'
   );
 
+  const precinctFileName = find(electionFileNames, (fileName) =>
+    fileName.includes('PCT-DR')
+  );
+  const precinctFileContents = readFileSync(
+    join(electionDataDirectory, precinctFileName),
+    'utf-8'
+  );
+
   const election = convertToElection(
-    precinctFileContents,
-    candidateFileContents
+    officeFileContents,
+    candidateFileContents,
+    precinctFileContents
   );
   process.stdout.write(JSON.stringify(election, null, 2));
   process.stdout.write('\n');
