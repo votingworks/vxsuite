42ebe96b2bb2a53358bb742ff903b2b383abed1f
Jonah Kagan <jonah@voting.works>
2025-08-13 13:27:21 -0700

    Migration to update existing elections with v3 template

---
+++ apps/design/backend/migrations/1755116181957_remove-v3-nh-ballot-template.js
@@ -0,0 +1,17 @@
+/**
+ * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
+ */
+exports.shorthands = undefined;
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+exports.up = (pgm) => {
+  pgm.sql(`
+    UPDATE elections
+    SET ballot_template_id = 'NhBallot'
+    WHERE ballot_template_id = 'NhBallotV3';
+  `);
+};
