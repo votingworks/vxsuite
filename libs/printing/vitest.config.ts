e4afabbaed46dbcdbdd9d2103f4d901a3150b489
Jonah Kagan <jonah@voting.works>
2025-08-14 15:26:35 -0700

    libs/printing: Mock hashes in snapshots

--- libs/printing/vitest.config.ts
+++ libs/printing/vitest.config.ts
@@ -1,7 +1,18 @@
+import { join } from 'node:path';
 import { defineConfig } from '../../vitest.config.shared.mjs';
 
 export default defineConfig({
   test: {
     setupFiles: ['test/setupTests.ts'],
+    alias: [
+      {
+        find: '@votingworks/types',
+        replacement: join(__dirname, '../types/src/index.ts'),
+      },
+      {
+        find: '@votingworks/ui',
+        replacement: join(__dirname, '../ui/src/index.ts'),
+      },
+    ],
   },
 });
