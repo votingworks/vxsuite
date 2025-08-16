336022a070e6efedece233cfaf9eb01f1f231edb
Kevin Shen <kevin@voting.works>
2025-08-16 08:38:26 -0400

    update TTL duration

--- apps/pollbook/backend/src/barcode_scanner/client.ts
+++ apps/pollbook/backend/src/barcode_scanner/client.ts
@@ -24,8 +24,8 @@ const DEVICE_PATH = '/dev/barcode_scanner';
 // Because only certain pages in the frontend handle ID scans,
 // we don't want stale scans to sit around in memory to be read
 // later when the user has no context on the scan.
-// The duration should be at least 2x the client polling interval
-export const SCAN_DATA_TTL_MS = 2000;
+// The duration should be >= client polling interval for scan data
+export const SCAN_DATA_TTL_MS = 1000;
 
 /**
  * Attempts to connect to the barcode scanner Unix socket within a retry loop.
