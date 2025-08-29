dd26cd5acf1b27b03ffdc253ffd71142d66a7138
Nikhil Bhatia <nikhil@voting.works>
2025-08-29 15:39:40 -0700

    Now has all main migrations but adds one that is too old

---
+++ apps/design/backend/migrations/1756339897331_add-election-signature.js
@@ -0,0 +1,16 @@
+/**
+ * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
+ */
+exports.shorthands = undefined;
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+exports.up = (pgm) => {
+  // Add optional signature field to elections table
+  pgm.addColumns('elections', {
+    signature: { type: 'jsonb', notNull: false },
+  });
+};
