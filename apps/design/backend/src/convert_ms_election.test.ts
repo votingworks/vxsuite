be7e1c1bc17d5f11a52146009d5c738375fdb7a9
Jonah Kagan <jonah@voting.works>
2025-10-16 16:42:04 -0700

    Fixtures and tests for conversion
    The fixtures are a selection of files from previous elections. I
    replaced all of the town and candidate names with mock data.

---
+++ apps/design/backend/src/convert_ms_election.test.ts
@@ -0,0 +1,75 @@
+import { test, expect, vi, beforeAll, afterAll } from 'vitest';
+import { mockBaseLogger } from '@votingworks/logging';
+import { Election, safeParseElection } from '@votingworks/types';
+import { convertMsElection } from './convert_ms_election';
+import { TestStore } from '../test/test_store';
+import { Org } from './types';
+import { readFixture } from '../test/helpers';
+
+const logger = mockBaseLogger({ fn: vi.fn });
+const testStore = new TestStore(logger);
+const store = testStore.getStore();
+const org: Org = {
+  id: 'test-org-id',
+  name: 'Test Org',
+};
+
+beforeAll(async () => {
+  await testStore.init();
+  await store.syncOrganizationsCache([org]);
+});
+
+afterAll(async () => {
+  await testStore.cleanUp();
+});
+
+async function expectValidElection(election: Election) {
+  await store.createElection(org.id, election, 'VxDefaultBallot');
+  expect(
+    safeParseElection((await store.getElection(election.id)).election).err()
+  ).toBeUndefined();
+}
+
+test('convert general election', async () => {
+  const election = convertMsElection(
+    'election-id-1',
+    await readFixture('ms-sems-election-general-10.csv'),
+    await readFixture('ms-sems-election-candidates-general-10.csv')
+  );
+  await expectValidElection(election);
+  expect(election).toMatchSnapshot();
+});
+
+test('convert primary election', async () => {
+  const election = convertMsElection(
+    'election-id-2',
+    await readFixture('ms-sems-election-primary-60.csv'),
+    await readFixture('ms-sems-election-candidates-primary-60.csv')
+  );
+  await expectValidElection(election);
+  expect(election).toMatchSnapshot();
+});
+
+test('convert election with ballot measures', async () => {
+  const election = convertMsElection(
+    'election-id-3',
+    await readFixture('ms-sems-election-general-ballot-measures-10.csv'),
+    await readFixture(
+      'ms-sems-election-candidates-general-ballot-measures-10.csv'
+    )
+  );
+  await expectValidElection(election);
+  expect(election).toMatchSnapshot();
+});
+
+test('convert election with precinct splits', async () => {
+  const election = convertMsElection(
+    'election-id-4',
+    await readFixture('ms-sems-election-primary-precinct-splits-75.csv'),
+    await readFixture(
+      'ms-sems-election-candidates-primary-precinct-splits-75.csv'
+    )
+  );
+  await expectValidElection(election);
+  expect(election).toMatchSnapshot();
+});
