d62b05a7eba6341c72d5826de6ff8efd35c5fe4d
Jonah Kagan <jonah@voting.works>
2025-10-21 11:47:01 -0700

    Add missing args to InvalidCardScreen story

--- libs/ui/src/invalid_card_screen.stories.tsx
+++ libs/ui/src/invalid_card_screen.stories.tsx
@@ -1,10 +1,16 @@
 import { Meta } from '@storybook/react';
+import { InvalidCardScreen, Props } from './invalid_card_screen';
 
-import { InvalidCardScreen } from './invalid_card_screen';
+const args: Props = {
+  reasonAndContext: {
+    reason: 'wrong_election',
+  },
+};
 
 const meta: Meta<typeof InvalidCardScreen> = {
   title: 'libs-ui/InvalidCardScreen',
   component: InvalidCardScreen,
+  args,
 };
 
 export default meta;
