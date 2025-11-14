3a6168466ebe39f109e8ac4f1ef39694b050b99b
Nikhil Bhatia <nikhil@voting.works>
2025-11-14 14:12:47 -0800

    print/backend: Update process env var usage (#7528)

--- apps/print/backend/src/env.d.ts
+++ apps/print/backend/src/env.d.ts
@@ -3,5 +3,7 @@ declare namespace NodeJS {
   export interface ProcessEnv {
     readonly PORT?: string;
     readonly PRINT_WORKSPACE?: string;
+    readonly VX_MACHINE_ID?: string;
+    readonly VX_CODE_VERSION?: string;
   }
 }
