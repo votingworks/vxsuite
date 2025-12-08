63d867aea97f40e29e8ebf729bf33b9164b54b37
Jonah Kagan <jonah@voting.works>
2025-12-08 11:13:26 -0800

    Update ballot-interpreter benchmarks (#7632)
    * Clean up benchmark test naming
    
    * Update benchmark snapshots
    
    * Increase timeout

--- libs/ballot-interpreter/benchmarks/interpret_hmpb.bench.ts
+++ libs/ballot-interpreter/benchmarks/interpret_hmpb.bench.ts
@@ -1,4 +1,4 @@
-import { describe, test } from 'vitest';
+import { test } from 'vitest';
 import { assertDefined } from '@votingworks/basics';
 import { vxFamousNamesFixtures } from '@votingworks/hmpb';
 import { asSheet, DEFAULT_MARK_THRESHOLDS } from '@votingworks/types';
@@ -7,57 +7,55 @@ import { interpretSheet } from '../src';
 import { pdfToPageImages } from '../test/helpers/interpretation';
 import { benchmarkRegressionTest } from './benchmarking';
 
-describe('Interpretation benchmark', () => {
-  const { electionDefinition, precinctId, blankBallotPath, markedBallotPath } =
-    vxFamousNamesFixtures;
+const { electionDefinition, precinctId, blankBallotPath, markedBallotPath } =
+  vxFamousNamesFixtures;
 
-  test('Blank HMPB', async () => {
-    const ballotImages = asSheet(
-      await pdfToPageImages(blankBallotPath).toArray()
-    );
+test('Blank HMPB', async () => {
+  const ballotImages = asSheet(
+    await pdfToPageImages(blankBallotPath).toArray()
+  );
 
-    await benchmarkRegressionTest({
-      label: 'Blank HMPB interpretation',
-      func: async () => {
-        await interpretSheet(
-          {
-            electionDefinition,
-            precinctSelection: singlePrecinctSelectionFor(
-              assertDefined(precinctId)
-            ),
-            testMode: true,
-            markThresholds: DEFAULT_MARK_THRESHOLDS,
-            adjudicationReasons: [],
-          },
-          ballotImages
-        );
-      },
-      runs: 50,
-    });
+  await benchmarkRegressionTest({
+    label: 'Blank HMPB interpretation',
+    func: async () => {
+      await interpretSheet(
+        {
+          electionDefinition,
+          precinctSelection: singlePrecinctSelectionFor(
+            assertDefined(precinctId)
+          ),
+          testMode: true,
+          markThresholds: DEFAULT_MARK_THRESHOLDS,
+          adjudicationReasons: [],
+        },
+        ballotImages
+      );
+    },
+    runs: 50,
   });
+});
 
-  test('Marked HMPB', async () => {
-    const ballotImages = asSheet(
-      await pdfToPageImages(markedBallotPath).toArray()
-    );
+test('Marked HMPB', async () => {
+  const ballotImages = asSheet(
+    await pdfToPageImages(markedBallotPath).toArray()
+  );
 
-    await benchmarkRegressionTest({
-      label: 'Marked HMPB interpretation',
-      func: async () => {
-        await interpretSheet(
-          {
-            electionDefinition,
-            precinctSelection: singlePrecinctSelectionFor(
-              assertDefined(precinctId)
-            ),
-            testMode: true,
-            markThresholds: DEFAULT_MARK_THRESHOLDS,
-            adjudicationReasons: [],
-          },
-          ballotImages
-        );
-      },
-      runs: 50,
-    });
+  await benchmarkRegressionTest({
+    label: 'Marked HMPB interpretation',
+    func: async () => {
+      await interpretSheet(
+        {
+          electionDefinition,
+          precinctSelection: singlePrecinctSelectionFor(
+            assertDefined(precinctId)
+          ),
+          testMode: true,
+          markThresholds: DEFAULT_MARK_THRESHOLDS,
+          adjudicationReasons: [],
+        },
+        ballotImages
+      );
+    },
+    runs: 50,
   });
 });
