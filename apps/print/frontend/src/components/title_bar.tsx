bd31e824c336312df85a9a7ebae9840da253af94
Nikhil Bhatia <nikhil@voting.works>
2025-12-15 14:21:29 -0800

    libs/ui: Extract desktop style TestModeCallout to shared location

--- apps/print/frontend/src/components/title_bar.tsx
+++ apps/print/frontend/src/components/title_bar.tsx
@@ -1,4 +1,4 @@
-import { Card, H1, Icons, MainHeader } from '@votingworks/ui';
+import { H1, MainHeader, TestModeCallout } from '@votingworks/ui';
 import styled from 'styled-components';
 import { getElectionRecord, getTestMode } from '../api';
 
@@ -31,18 +31,6 @@ const BannerSection = styled.div`
   justify-content: center;
 `;
 
-// TODO: Factor out shared desktop-friendly test mode banner component with central-scan
-const TestModeCallout = styled(Card).attrs({ color: 'warning' })`
-  font-size: ${(p) => p.theme.sizes.headingsRem.h3}rem;
-  font-weight: ${(p) => p.theme.sizes.fontWeight.semiBold};
-
-  > div {
-    padding: 0.5rem 1rem;
-  }
-
-  flex-shrink: 0;
-`;
-
 export function TitleBar({
   title,
   actions,
@@ -60,11 +48,7 @@ export function TitleBar({
     <Header>
       <H1>{title}</H1>
       <BannerSection>
-        {isTestMode && isConfigured && (
-          <TestModeCallout>
-            <Icons.Warning color="warning" /> Test Ballot Mode
-          </TestModeCallout>
-        )}
+        {isTestMode && isConfigured && <TestModeCallout forDesktop />}
       </BannerSection>
       <RightSection>
         {actions && <ButtonRow as="div">{actions}</ButtonRow>}
