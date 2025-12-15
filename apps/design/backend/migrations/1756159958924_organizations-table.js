4ae7890215367348a39dbb77f51cc08aec0d2fa5
Jonah Kagan <jonah@voting.works>
2025-12-15 12:09:05 -0800

    Remove jurisdiction ID deps from migrations
    Since we won't have the special jurisdiction IDs in the env anymore, the
    previous migrations that relied on them need to change. Since those
    migrations have already run, we can actually remove all of parts that
    specifically existed to migrate existing data in Auth0/the database and
    assume we're starting from an empty database from now on (basically,
    only to be used in development/test). I also moved the insertion of dev
    data to a separate migration for clarity.

--- apps/design/backend/migrations/1756159958924_organizations-table.js
+++ apps/design/backend/migrations/1756159958924_organizations-table.js
@@ -1,8 +1,5 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-const { ManagementClient } = require('auth0');
-const { assertDefined } = require('@votingworks/basics');
 const { loadEnvVarsFromDotenvFiles } = require('@votingworks/backend');
-const { votingWorksJurisdictionId } = require('../build/globals');
 
 loadEnvVarsFromDotenvFiles();
 
@@ -14,38 +11,13 @@ exports.shorthands = undefined;
 /**
  * @param pgm {import('node-pg-migrate').MigrationBuilder}
  * @param run {() => void | undefined}
- * @returns {Promise<void>}
+ * @returns {Promise<void> | void}
  */
-exports.up = async (pgm) => {
+exports.up = (pgm) => {
   pgm.createTable('organizations', {
     id: { type: 'text', primaryKey: true },
     name: { type: 'text', notNull: true, unique: true },
   });
-
-  if (process.env.NODE_ENV === 'production' || process.env.AUTH_ENABLED) {
-    const auth0Client = new ManagementClient({
-      clientId: assertDefined(process.env.AUTH0_CLIENT_ID),
-      clientSecret: assertDefined(process.env.AUTH0_SECRET),
-      domain: assertDefined(process.env.AUTH0_CLIENT_DOMAIN),
-    });
-    const organizations = (await auth0Client.organizations.getAll()).data;
-    for (const org of organizations) {
-      pgm.sql(
-        `INSERT INTO organizations (id, name) VALUES (
-        '${org.id}',
-        '${org.display_name}'
-      )`
-      );
-    }
-  } else if (process.env.NODE_ENV !== 'test') {
-    // In dev mode, create the default dev organization (use VotingWorks org ID to get full features)
-    pgm.sql(`
-      INSERT INTO organizations (id, name) VALUES (
-        '${votingWorksJurisdictionId()}',
-        'VotingWorks'
-      );
-    `);
-  }
 
   pgm.addConstraint('elections', null, {
     foreignKeys: {
