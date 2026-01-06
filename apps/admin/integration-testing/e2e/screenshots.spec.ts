2367ddccf1aa4bb4ca6eacb76d4867587d20086c
Nikhil Bhatia <nikhil@voting.works>
2026-01-06 10:40:36 -0800

    Increase test timeout for admin integration test (#7743)

--- apps/admin/integration-testing/e2e/screenshots.spec.ts
+++ apps/admin/integration-testing/e2e/screenshots.spec.ts
@@ -422,7 +422,7 @@ async function insertUsbDriveWithCvrs({
 }
 
 test('results', async ({ page }) => {
-  test.setTimeout(70_000);
+  test.setTimeout(120_000);
   const usbHandler = getMockFileUsbDriveHandler();
   const printerHandler = getMockFilePrinterHandler();
   printerHandler.connectPrinter(HP_LASER_PRINTER_CONFIG);
