81a254f5f7330137ff7aebf83036323aac85398f
Nikhil Bhatia <nikhil@voting.works>
2026-01-08 17:17:46 -0800

    design/frontend: Disallow adding/removing parties for MS SEMS elections

--- apps/design/frontend/src/parties_screen.tsx
+++ apps/design/frontend/src/parties_screen.tsx
@@ -13,7 +13,7 @@ import { DuplicatePartyError } from '@votingworks/design-backend';
 import styled from 'styled-components';
 import { ElectionNavScreen, Header } from './nav_screen';
 import { ElectionIdParams, electionParamRoutes, routes } from './routes';
-import { getBallotsFinalizedAt, listParties } from './api';
+import { getBallotsFinalizedAt, getElectionInfo, listParties } from './api';
 import { useTitle } from './hooks/use_title';
 import { generateId } from './utils';
 import * as api from './api';
@@ -47,9 +47,9 @@ export function PartiesScreen(): JSX.Element {
   );
 }
 
-const Viewport = styled(FixedViewport)`
+const Viewport = styled(FixedViewport)<{ hasActionsRow: boolean }>`
   display: grid;
-  grid-template-rows: min-content 1fr;
+  grid-template-rows: ${(p) => (p.hasActionsRow ? 'min-content 1fr' : '1fr')};
 `;
 
 const Body = styled.div`
@@ -107,6 +107,7 @@ function Contents(props: { editing: boolean }): React.ReactNode {
 
   const savedPartiesQuery = listParties.useQuery(electionId);
   const ballotsFinalizedAtQuery = getBallotsFinalizedAt.useQuery(electionId);
+  const getElectionInfoQuery = getElectionInfo.useQuery(electionId);
 
   // Reset form on initial and post-save fetches:
   React.useEffect(() => {
@@ -120,7 +121,13 @@ function Contents(props: { editing: boolean }): React.ReactNode {
   const updatePartiesMutation = api.updateParties.useMutation();
   const error = updatePartiesMutation.data?.err();
 
-  if (!(savedPartiesQuery.isSuccess && ballotsFinalizedAtQuery.isSuccess)) {
+  if (
+    !(
+      savedPartiesQuery.isSuccess &&
+      ballotsFinalizedAtQuery.isSuccess &&
+      getElectionInfoQuery.isSuccess
+    )
+  ) {
     return null;
   }
 
@@ -162,6 +169,7 @@ function Contents(props: { editing: boolean }): React.ReactNode {
   }
 
   const ballotsFinalized = !!ballotsFinalizedAtQuery.data;
+  const hasExternalSource = Boolean(getElectionInfoQuery.data.externalSource);
   const savedParties = savedPartiesQuery.data;
   const updating = updatePartiesMutation.isLoading;
   const disabled = ballotsFinalized || !editing || updating;
@@ -174,21 +182,22 @@ function Contents(props: { editing: boolean }): React.ReactNode {
   }
 
   return (
-    <Viewport>
-      <ListActionsRow>
-        <Button
-          disabled={updating || ballotsFinalized}
-          icon="Add"
-          onPress={() => {
-            setNewParties([...newParties, createBlankParty()]);
-            setEditing(true);
-          }}
-          variant="primary"
-        >
-          Add Party
-        </Button>
-      </ListActionsRow>
-
+    <Viewport hasActionsRow={!hasExternalSource}>
+      {!hasExternalSource && (
+        <ListActionsRow>
+          <Button
+            disabled={updating || ballotsFinalized}
+            icon="Add"
+            onPress={() => {
+              setNewParties([...newParties, createBlankParty()]);
+              setEditing(true);
+            }}
+            variant="primary"
+          >
+            Add Party
+          </Button>
+        </ListActionsRow>
+      )}
       <Body>
         <FormFixed
           editing={editing}
@@ -218,6 +227,7 @@ function Contents(props: { editing: boolean }): React.ReactNode {
                 {updatedParties.map((party, i) => (
                   <PartyRow
                     disabled={disabled}
+                    canDelete={!hasExternalSource}
                     editing={editing}
                     key={party.id}
                     onChange={(p) =>
@@ -236,6 +246,7 @@ function Contents(props: { editing: boolean }): React.ReactNode {
                 {newParties.map((party, i) => (
                   <PartyRow
                     disabled={disabled}
+                    canDelete={!hasExternalSource}
                     editing={editing}
                     key={party.id}
                     onChange={(p) =>
@@ -307,6 +318,7 @@ const ERROR_MESSAGE: Record<DuplicatePartyError['code'], string> = {
 
 function PartyRow(props: {
   disabled?: boolean;
+  canDelete: boolean;
   editing: boolean;
   onChange: (party: Party) => void;
   onDelete: (partyId: string) => void;
@@ -314,7 +326,15 @@ function PartyRow(props: {
   // eslint-disable-next-line vx/gts-use-optionals -- require explicit prop
   updateError: DuplicatePartyError | undefined;
 }) {
-  const { disabled, editing, updateError, onChange, onDelete, party } = props;
+  const {
+    disabled,
+    canDelete,
+    editing,
+    updateError,
+    onChange,
+    onDelete,
+    party,
+  } = props;
   const { electionId } = useParams<ElectionIdParams>();
   const partiesRoutes = routes.election(electionId).parties;
 
@@ -385,7 +405,7 @@ function PartyRow(props: {
         }}
       />
 
-      {editing ? (
+      {editing && canDelete ? (
         <TooltipContainer as="div" style={{ width: 'min-content' }}>
           <Button
             aria-label={`Delete Party ${party.fullName}`}
