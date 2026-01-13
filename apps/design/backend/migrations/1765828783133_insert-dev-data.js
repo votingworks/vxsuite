9a8da935fb17af353fb30e377956d3a10b7b5786
arsalansufi <arsalan@voting.works>
2026-01-13 10:56:55 -0500

    VxDesign: Fix deploys by restoring migration file (#7771)
    * Fix VxDesign deploys by restoring migration file
    
    * Tweak CI check to ignore intentionally old timestamp

---
+++ apps/design/backend/migrations/1765828783133_insert-dev-data.js
@@ -0,0 +1,7 @@
+/**
+ * This migration is intentionally left blank. It used to seed data for local development when
+ * NODE_ENV=development and AUTH_ENABLED=false. We've since moved that functionality to a script
+ * outside of the migrations framework, but we still need this file intact for the migration chain
+ * of record. Without this file, Heroku deploys will error.
+ */
+exports.up = () => {};
