1916b4b0be287ac6ae8b7d24ea41b3175e933ff4
Jonah Kagan <jonah@voting.works>
2026-01-21 14:32:34 -0800

    Add contest section header type to ballot props

--- libs/types/src/hmpb.ts
+++ libs/types/src/hmpb.ts
@@ -6,6 +6,7 @@ import {
   ContestId,
   ContestOption,
   ContestOptionSchema,
+  ContestTypes,
   Election,
   HmpbBallotPageMetadata,
   HmpbBallotPageMetadataSchema,
@@ -42,6 +43,22 @@ export interface BaseBallotProps {
   compact?: boolean;
   ballotAuditId?: string;
 }
+
+export interface ContestSectionHeader {
+  /**
+   * Title for the section header.
+   */
+  title: string;
+  /**
+   * HTML text to display below the title in the section header.
+   */
+  description?: string;
+}
+
+export type ContestSectionHeaders = Record<
+  ContestTypes,
+  ContestSectionHeader | undefined
+>;
 
 export type BallotPageMetadata = HmpbBallotPageMetadata;
 export const BallotPageMetadataSchema = HmpbBallotPageMetadataSchema;
