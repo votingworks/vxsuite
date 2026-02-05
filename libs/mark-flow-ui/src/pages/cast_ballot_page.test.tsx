cbb22aeca7aa4194c4724decc8f732cf60b34063
Caroline Mars <caroline@voting.works>
2026-02-05 14:47:52 -0800

    fix tests

--- libs/mark-flow-ui/src/pages/cast_ballot_page.test.tsx
+++ libs/mark-flow-ui/src/pages/cast_ballot_page.test.tsx
@@ -6,6 +6,6 @@ it('renders CastBallotPage', () => {
   render(<CastBallotPage hidePostVotingInstructions={vi.fn()} />);
 
   screen.getByRole('heading', { name: /almost done/i });
-  screen.getByText(/verify your official ballot/i);
-  screen.getByText(/scan your official ballot/i);
+  screen.getByText(/verify your ballot/i);
+  screen.getByText(/scan your ballot/i);
 });
