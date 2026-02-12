7296469e2626a44fe172203bb42233eaecc882ac
Brian Donovan <brian@voting.works>
2026-02-12 10:00:48 -0800

    fix(ballot-interpreter): use `x_coordinates` to suppress `unused-assignments` error
    I don't really want to show the whole debug value, but showing how many
    seems sensible.

--- libs/ballot-interpreter/src/bubble-ballot-rust/interpret.rs
+++ libs/ballot-interpreter/src/bubble-ballot-rust/interpret.rs
@@ -256,7 +256,7 @@ pub enum Error {
     #[error("could not compute layout for {side:?}")]
     CouldNotComputeLayout { side: BallotSide },
 
-    #[error("vertical streaks detected on {label}")]
+    #[error("vertical streaks detected on {label} (found {})", x_coordinates.len())]
     #[serde(rename_all = "camelCase")]
     VerticalStreaksDetected {
         label: String,
