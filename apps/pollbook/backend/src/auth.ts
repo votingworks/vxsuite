ef0a9722048358402085f34d49213e1355f94149
arsalansufi <arsalan@voting.works>
2026-02-13 15:22:54 -0500

    VxPollBook: Use single copy of constructAuthMachineState with all params passed (#7993)
    * Use single copy of constructAuthMachineState with all params passed
    
    * Adjust coverage threshold

--- apps/pollbook/backend/src/auth.ts
+++ apps/pollbook/backend/src/auth.ts
@@ -1,21 +1,32 @@
 import {
+  DEV_JURISDICTION,
   DippedSmartCardAuthApi,
   DippedSmartCardAuthMachineState,
 } from '@votingworks/auth';
-import { DEFAULT_SYSTEM_SETTINGS } from '@votingworks/types';
+import { DEFAULT_SYSTEM_SETTINGS, TEST_JURISDICTION } from '@votingworks/types';
 import { LoggingUserRole } from '@votingworks/logging';
+import { isIntegrationTest } from '@votingworks/utils';
 import { LocalWorkspace, PeerWorkspace } from './types';
 
 export function constructAuthMachineState(
   workspace: LocalWorkspace | PeerWorkspace
 ): DippedSmartCardAuthMachineState {
   const election = workspace.store.getElection();
+  const { configuredPrecinctId } =
+    workspace.store.getPollbookConfigurationInformation();
+
+  const jurisdiction = isIntegrationTest()
+    ? TEST_JURISDICTION
+    : process.env.VX_MACHINE_JURISDICTION ?? DEV_JURISDICTION;
+
   return {
     ...DEFAULT_SYSTEM_SETTINGS['auth'],
     electionKey: election && {
       id: election.id,
       date: election.date,
     },
+    isConfigured: Boolean(election && configuredPrecinctId),
+    jurisdiction,
     machineType: 'poll-book',
   };
 }
