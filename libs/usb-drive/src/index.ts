65db4db74a1734eedc4ae87a052f36879438eed4
Brian Donovan <brian@voting.works>
2026-03-18 14:55:09 -0700

    feat(usb-drive): add a multi-USB drive aware API

--- libs/usb-drive/src/index.ts
+++ libs/usb-drive/src/index.ts
@@ -2,6 +2,7 @@
 export * from './list_directory_on_usb_drive';
 export * from './mocks/memory_usb_drive';
 export * from './mocks/file_usb_drive';
+export * from './multi_usb_drive';
 export * from './types';
 export * from './usb_drive';
 export type { MockFileTree } from './mocks/helpers';
