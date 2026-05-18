9519d9e856b0408003489ee2eedb3a1a67d9aa79
Jonah Kagan <jonah@voting.works>
2026-05-18 11:57:35 -0700

    types: Add NO_PARTY_ID sentinel value
    In tabulation, instead of representing open primary ballots with no inferred party by `undefined`, use a sentinel object value. We use an object instead of a string because party IDs are already strings, so this object allows the type system to ensure we handle it exhaustively.
    
    The motivation for this change is twofold:
    - It was getting confusing tracking when partyId was undefined
      intentionally (meaning "no party") or for other reasons
    - While adding the ability to build custom reports that filter to only
      the "no party" ballots, I realized that we needed some way to
    distinguish the absence of a party filter (which is currently
    represented by partyId `undefined`) from filtering to "no party"
    ballots. While there were other ways to hack around this, being more
    explicit seemed best.

--- libs/types/src/tabulation.test.ts
+++ libs/types/src/tabulation.test.ts
@@ -14,3 +14,8 @@ test('formatBatchId returns the ID unchanged when there are no hyphens', () => {
 test('formatBatchId returns first and last segments for a two-part ID', () => {
   expect(Tabulation.formatBatchId('batch-1')).toEqual('batch-1');
 });
+
+test('isNoPartyId', () => {
+  expect(Tabulation.isNoPartyId(Tabulation.NO_PARTY_ID)).toEqual(true);
+  expect(Tabulation.isNoPartyId('some-party-id')).toEqual(false);
+});
