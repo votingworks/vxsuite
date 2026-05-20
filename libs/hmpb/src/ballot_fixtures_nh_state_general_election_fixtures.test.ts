887a999db22c58e757ad5f84938d7c8c7cc3b019
Jonah Kagan <jonah@voting.works>
2026-05-20 16:46:51 -0700

    Add fixtures

---
+++ libs/hmpb/src/ballot_fixtures_nh_state_general_election_fixtures.test.ts
@@ -0,0 +1,46 @@
+import { readElection } from '@votingworks/fs';
+import { afterAll, beforeAll, expect, test, vi } from 'vitest';
+import { nhStateGeneralElectionFixtures } from './ballot_fixtures';
+import { createPlaywrightRendererPool } from './playwright_renderer';
+import { expectToMatchSavedPdf } from '../test/helpers';
+import { RendererPool } from './renderer';
+
+vi.setConfig({
+  testTimeout: 120_000,
+});
+
+let rendererPool: RendererPool;
+beforeAll(async () => {
+  rendererPool = await createPlaywrightRendererPool();
+});
+
+afterAll(async () => {
+  await rendererPool.close();
+});
+
+// run `pnpm generate-fixtures` if this test fails
+test('NH state general election fixtures', async () => {
+  const fixtures = nhStateGeneralElectionFixtures;
+  const generated = await fixtures.generate(rendererPool);
+
+  expect(generated.electionDefinition.election).toEqual(
+    (await readElection(fixtures.electionPath)).ok()?.election
+  );
+
+  await expectToMatchSavedPdf(
+    generated.blankBallotPdf,
+    fixtures.blankBallotPath
+  );
+  await expectToMatchSavedPdf(
+    generated.markedBallotPdf,
+    fixtures.markedBallotPath
+  );
+  await expectToMatchSavedPdf(
+    generated.handCountBlankBallotPdf,
+    fixtures.handCountBlankBallotPath
+  );
+  await expectToMatchSavedPdf(
+    generated.federalOnlyBlankBallotPdf,
+    fixtures.federalOnlyBlankBallotPath
+  );
+});
