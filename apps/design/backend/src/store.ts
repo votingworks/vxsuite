1fa1f9c7a008831eb1493324a69e297eaffe3a00
Jonah Kagan <jonah@voting.works>
2026-05-21 15:02:25 -0700

    Enable signature input for NhStateBallot template
    Unrelated to FOO ballots, but needed for NH state ballots in general

--- apps/design/backend/src/store.ts
+++ apps/design/backend/src/store.ts
@@ -1174,7 +1174,8 @@ export class Store {
         seal: electionRow.seal,
         // Only include signature for the NhBallot
         signature:
-          electionRow.ballotTemplateId === 'NhBallot'
+          electionRow.ballotTemplateId === 'NhBallot' ||
+          electionRow.ballotTemplateId === 'NhStateBallot'
             ? electionRow.signature || undefined
             : undefined,
         districts,
