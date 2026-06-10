efcaa433b46f05349643147a98634360ebc5cc19
Kofi <264902+kofi-q@users.noreply.github.com>
2026-06-10 10:50:47 -0500

    Remove precinct selection from shared report components

--- libs/ui/src/diagnostics/scan_readiness_report.test.tsx
+++ libs/ui/src/diagnostics/scan_readiness_report.test.tsx
@@ -5,7 +5,6 @@ import { assertDefined } from '@votingworks/basics';
 import {
   getFeatureFlagMock,
   BooleanEnvironmentVariableName,
-  ALL_PRECINCTS_SELECTION,
 } from '@votingworks/utils';
 import { ScanReadinessReport } from '.';
 import { render, screen } from '../../test/react_testing_library';
@@ -21,21 +20,9 @@ mockFeatureFlagger.enableFeatureFlag(ENABLE_POLLING_PLACES);
 
 const electionDef = readElectionTwoPartyPrimaryDefinition();
 const { election } = electionDef;
-const precinctSelection = ALL_PRECINCTS_SELECTION;
 const selectedPollingPlace = assertDefined(election.pollingPlaces)[0];
 
 test('ScanReadinessReport', () => {
-  mockFeatureFlagger.disableFeatureFlag(ENABLE_POLLING_PLACES);
-  testReport('Precinct: All Precincts');
-});
-
-test('ScanReadinessReport - polling places enabled', () => {
-  mockFeatureFlagger.enableFeatureFlag(ENABLE_POLLING_PLACES);
-  testReport(`Polling Place: ${selectedPollingPlace.name}`);
-});
-
-// [TODO] Merge into test after migration to polling places.
-function testReport(expectedPrecinctOrPollingPlaceString: string) {
   const generatedAtTime = new Date('2022-01-01T00:00:00');
   const machineId = 'MOCK';
   render(
@@ -59,7 +46,6 @@ function testReport(expectedPrecinctOrPollingPlaceString: string) {
       electionDefinition={electionDef}
       electionPackageHash="mock-election-package-hash"
       pollingPlaceId={selectedPollingPlace.id}
-      precinctSelection={precinctSelection}
     />
   );
 
@@ -69,7 +55,7 @@ function testReport(expectedPrecinctOrPollingPlaceString: string) {
 
   screen.getByText(/Example Primary Election/);
 
-  screen.getByText(expectedPrecinctOrPollingPlaceString);
+  screen.getByText(`Polling Place: ${selectedPollingPlace.name}`);
 
   screen.getByText('Free Disk Space: 50% (500 GB / 1000 GB)');
 
@@ -78,4 +64,4 @@ function testReport(expectedPrecinctOrPollingPlaceString: string) {
   screen.getByText('The printer is loaded with paper and ready to print.');
 
   screen.getByText('Test print successful, 1/1/2022, 12:00:00 AM');
-}
+});
