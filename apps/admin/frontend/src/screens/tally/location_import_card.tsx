f770992218d58aeaa85d7d788add22ff2d8c463b
Kofi <264902+kofi-q@users.noreply.github.com>
2026-07-08 10:02:59 -0500

    [VxAdmin] Add CVR import button card component (#8848)
    Adding the import card for use in the upcoming import panel on the CVR management screen. Supports multi-scanner exports, but we only have single-scanner CVR exports in practice, at the moment.
    
    Also making some tweaks to the base location card component:
    - Remove the padding special case for when the icon container includes a label - moving that responsibility to the import card implementation.
    - Adding a tiny bit of spacing between lines - import card starts to look crowded with 3 lines.

---
+++ apps/admin/frontend/src/screens/tally/location_import_card.tsx
@@ -0,0 +1,110 @@
+import React from 'react';
+import styled, { css } from 'styled-components';
+import { DateTime } from 'luxon';
+
+import { Font, Icons } from '@votingworks/ui';
+import { PollingPlaceType, pollingPlaceTypeName } from '@votingworks/types';
+
+import { TIME_FORMAT } from '../../config/globals';
+import { LocationCvrCard } from './location_cvr_card';
+
+export type LocationImportCardProps = Props;
+interface Props {
+  disabled: boolean;
+  exportTimestamp: Date;
+  name: string;
+  path: string;
+  nCvrs: number;
+  onPress: (path: string) => void;
+  scannerIds: string[];
+  status: Status;
+  testExport: boolean;
+  type: PollingPlaceType;
+}
+
+export type Status = 'imported' | 'importing' | 'ready';
+
+const ColorText = styled.span<{ color: 'primary' | 'warningAccent' }>`
+  color: ${(p) => p.theme.colors[p.color]};
+  font-weight: ${(p) => p.theme.sizes.fontWeight.bold};
+`;
+
+const HEADER_MUTED_CSS = css`
+  color: ${(p) => p.theme.colors.onBackgroundMuted};
+
+  ${ColorText} {
+    color: ${(p) => p.theme.colors.onBackgroundMuted};
+  }
+`;
+
+const Header = styled(Font)<{ muted: boolean }>`
+  color: ${(p) => p.theme.colors.onBackground};
+  ${(p) => p.muted && HEADER_MUTED_CSS}
+`;
+
+const Label = styled.div`
+  text-align: center;
+
+  /* Keep width consistent across states, to avoid container width changes. */
+  width: 7ch;
+`;
+
+const ICONS: Record<Status, JSX.Element> = {
+  importing: <Icons.Loading />,
+  imported: <Icons.Done />,
+  ready: <Icons.Import color="primary" />,
+};
+
+const ICON_LABELS: Record<Status, string> = {
+  imported: 'Loaded',
+  importing: 'Loading',
+  ready: 'Load',
+};
+
+export function LocationImportCard(props: Props): React.ReactNode {
+  const {
+    disabled,
+    exportTimestamp,
+    name,
+    nCvrs,
+    onPress,
+    path,
+    scannerIds,
+    status,
+    testExport,
+    type,
+  } = props;
+
+  const muted = disabled || status !== 'ready';
+  const separator = <span> &bull; </span>;
+
+  return (
+    <LocationCvrCard
+      disabled={muted}
+      id={path}
+      onClick={onPress}
+      icon={ICONS[status]}
+      iconLabel={<Label>{ICON_LABELS[status]}</Label>}
+      header={
+        <Header muted={muted}>
+          <ColorText color={testExport ? 'warningAccent' : 'primary'}>
+            {testExport ? 'TEST' : 'OFFICIAL'}
+          </ColorText>
+          {separator}
+          {DateTime.fromJSDate(exportTimestamp).toFormat(TIME_FORMAT)}
+        </Header>
+      }
+      name={name}
+      caption={
+        <React.Fragment>
+          {pollingPlaceTypeName(type)}
+          {separator}
+          {scannerIds.length === 1
+            ? `Scanner ${scannerIds[0]}`
+            : `Scanners: ${scannerIds.join(', ')}`}
+        </React.Fragment>
+      }
+      count={nCvrs}
+    />
+  );
+}
