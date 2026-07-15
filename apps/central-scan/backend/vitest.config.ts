1e9d5462597eb533614b6583ea74483e42a81357
Arsalan Sufi <arsalan@voting.works>
2026-07-15 08:20:40 -0400

    Drop coverage thresholds

--- apps/central-scan/backend/vitest.config.ts
+++ apps/central-scan/backend/vitest.config.ts
@@ -18,8 +18,8 @@ export default defineConfig({
         'test/**/*',
       ],
       thresholds: {
-        lines: -50,
-        branches: -42,
+        lines: 70,
+        branches: 70,
       },
     },
     // Ensure only one instance of each library is loaded by loading the TS
--- apps/central-scan/frontend/vitest.config.ts
+++ apps/central-scan/frontend/vitest.config.ts
@@ -8,8 +8,8 @@ export default defineConfig({
     setupFiles: ['react-app-polyfill/jsdom', 'src/setupTests.ts'],
     coverage: {
       thresholds: {
-        lines: 91,
-        branches: 86,
+        lines: 70,
+        branches: 70,
       },
       exclude: [
         'src/config',
