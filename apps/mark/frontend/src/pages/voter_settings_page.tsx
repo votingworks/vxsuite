c359ce26d669a68256452d3bc066dc3b4acfaa2d
Brian Donovan <brian@voting.works>
2026-09-21 15:22:21 -0700

    chore(mark): remove unused code (#9405)
    * chore(mark): remove unused `screenOrientation`
    
    Unused since #3683.
    
    * chore(mark): remove unused `VoterSettingsPage`
    
    Unused since #4769.
    
    * chore(mark): remove unused `memoize`
    
    Unused since #4555.

--- apps/mark/frontend/src/pages/voter_settings_page.tsx
+++
@@ -1,9 +0,0 @@
-import { useHistory } from 'react-router-dom';
-
-import { VoterSettings } from '@votingworks/ui';
-
-export function VoterSettingsPage(): JSX.Element {
-  const history = useHistory();
-
-  return <VoterSettings onClose={history.goBack} allowAudioVideoOnlyToggles />;
-}
